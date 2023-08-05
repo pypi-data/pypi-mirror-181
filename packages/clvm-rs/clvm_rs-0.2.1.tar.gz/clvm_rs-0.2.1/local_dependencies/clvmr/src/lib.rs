11500ca20bf68c3c49c38667004da3a086c59206
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:21 +0000

    Add package clvm-rs/0.2.1/clvm_rs-0.2.1.tar.gz

---
+++ packages/clvm-rs/clvm_rs-0.2.1.tar.gz/clvm_rs-0.2.1/local_dependencies/clvmr/src/lib.rs
@@ -0,0 +1,23 @@
+pub mod allocator;
+pub mod chia_dialect;
+pub mod core_ops;
+pub mod cost;
+pub mod dialect;
+pub mod err_utils;
+pub mod f_table;
+pub mod more_ops;
+pub mod node;
+pub mod number;
+pub mod op_utils;
+pub mod reduction;
+pub mod run_program;
+pub mod runtime_dialect;
+pub mod serde;
+pub mod sha2;
+pub mod traverse_path;
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(test)]
+mod test_ops;
