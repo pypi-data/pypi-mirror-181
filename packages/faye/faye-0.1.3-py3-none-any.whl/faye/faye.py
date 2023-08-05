f19be23e72d02403b92234a7436055d87e315545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:49 +0000

    Add package faye/0.1.3/faye-0.1.3-py3-none-any.whl

---
+++ packages/faye/faye-0.1.3-py3-none-any.whl/faye/faye.py
@@ -0,0 +1,37 @@
+"""
+    Owner: CyberSuki (https://github.com/cybersuki)
+    Project: Faye
+    License: BSD 2-Clause
+"""
+
+class Faye:
+
+    def log(self, msg: str = "Logger init", level: str = "INFO") -> str:
+        """
+        Generic logging method
+
+        msg: Logging message
+        level: Level message
+
+        rtype: str
+        """
+
+        # Checks for empty string parameters
+        if level != "" and msg != "":
+            return f"[{level}] => {msg}"
+        else:
+            raise NameError
+
+    def progress(self, total: int = 0, description: str = ""):
+        """
+        Progress bar built with tqdm
+
+        total: The length of the progress bar
+        description: Description of the progress bar
+        """
+
+        from tqdm import tqdm
+
+        # Progress bar configuration
+        for p in tqdm(range(total), desc=description):
+            pass
