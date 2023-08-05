a49aff603228bc0d6121e02cc0bf80933feb4fda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:46 +0000

    Add package nonholonomics/0.0.7/nonholonomics-0.0.7.tar.gz

---
+++ packages/nonholonomics/nonholonomics-0.0.7.tar.gz/nonholonomics-0.0.7/nonholonomics/__init__.py
@@ -0,0 +1,9 @@
+"Welcome to nonholonomics!"
+
+
+import sympy as sym
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+from NHL import *
+from Simulation import *
