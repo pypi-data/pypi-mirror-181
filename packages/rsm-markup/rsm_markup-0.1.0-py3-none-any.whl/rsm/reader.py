c609fc12b9bf5d66eb788b3de047c8f0204b008f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:13 +0000

    Add package rsm-markup/0.1.0/rsm_markup-0.1.0-py3-none-any.whl

---
+++ packages/rsm-markup/rsm_markup-0.1.0-py3-none-any.whl/rsm/reader.py
@@ -0,0 +1,30 @@
+"""
+reader.py
+---------
+
+RSM Reader: take a file path and return a string.
+
+"""
+
+from pathlib import Path
+
+import logging
+
+logger = logging.getLogger("RSM").getChild("read ")
+
+
+class Reader:
+    def __init__(self) -> None:
+        self.path: Path = Path()
+        self.src: str = ""
+
+    def read(self, path: Path | None) -> str:
+        print("READING READING READING READING READING READING ")
+        logger.info("Reading...")
+        if not path:
+            raise TypeError("Expected path, got None")
+        self.path = Path(path)
+        with open(path) as file:
+            self.src = file.read()
+        # return PlainTextManuscript(self.src)
+        return self.src
