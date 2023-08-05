6b19b4aac34a26beaae0480e87fda6b35fddc62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:17 +0000

    Add package vmas/1.2.1/vmas-1.2.1.tar.gz

---
+++ packages/vmas/vmas-1.2.1.tar.gz/vmas-1.2.1/vmas/make_env.py
@@ -0,0 +1,41 @@
+"""
+Code for creating a multiagent environment with one of the scenarios listed
+in ./scenarios/.
+Can be called by using, for example:
+    env = make_env('waterfall')
+After producing the env object, can be used similarly to an OpenAI gym
+environment.
+"""
+
+#  Copyright (c) 2022.
+#  ProrokLab (https://www.proroklab.org/)
+#  All rights reserved.
+
+from vmas import scenarios
+from vmas.simulator.environment import Environment
+from vmas.simulator.environment import Wrapper
+
+
+def make_env(
+    scenario_name,
+    num_envs: int = 32,
+    device: str = "cpu",
+    continuous_actions: bool = True,
+    wrapper: Wrapper = None,  # One of: None, vmas.Wrapper.RLLIB, and vmas.Wrapper.GYM
+    max_steps: int = None,
+    **kwargs,
+):
+    # load scenario from script
+    if not scenario_name.endswith(".py"):
+        scenario_name += ".py"
+    scenario = scenarios.load(scenario_name).Scenario()
+    env = Environment(
+        scenario,
+        num_envs=num_envs,
+        device=device,
+        continuous_actions=continuous_actions,
+        max_steps=max_steps,
+        **kwargs,
+    )
+
+    return wrapper.get_env(env) if wrapper is not None else env
