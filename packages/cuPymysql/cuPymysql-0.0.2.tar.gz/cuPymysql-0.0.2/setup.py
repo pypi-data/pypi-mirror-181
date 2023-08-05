446d71f0a9eb40c0a3908e543d3bab35e02b5807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:21 +0000

    Add package cuPymysql/0.0.2/cuPymysql-0.0.2.tar.gz

---
+++ packages/cuPymysql/cuPymysql-0.0.2.tar.gz/cuPymysql-0.0.2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="cuPymysql",
+    version="0.0.2",
+    author="davek",
+    author_email="hwangdoyeouyu@gmail.com",
+    description="Instead of PyMySQL",
+    python_requires=">=3.6",
+)
