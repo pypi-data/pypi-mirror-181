d5fbd459c637b7ff68046617031139613b44f77c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:46 +0000

    Add package nonholonomics/0.0.6/nonholonomics-0.0.6.tar.gz

---
+++ packages/nonholonomics/nonholonomics-0.0.6.tar.gz/nonholonomics-0.0.6/setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name='nonholonomics',
+    version='0.0.6',
+    author='Jamal Ardister',
+    author_email='ardiste3@msu.edu',
+    description='A collection of tools to used to evaluate dynamical systems with nonholonomic constraints',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ardiste3/nonholonomics',
+    project_urls={
+        "Bug Tracker": "https://github.com/ardiste3/nonholonomics/issues"
+    },
+    license='GNU AGPLv3',
+    packages=['nonholonomics'],
+    install_requires=['numpy', 'sympy', 'scipy', 'matplotlib'],
+)
