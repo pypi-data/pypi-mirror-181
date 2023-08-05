2fd4af13173296931412dfaa06ddcbe4ee64de41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:06 +0000

    Add package developers-chamber/0.0.65/developers-chamber-0.0.65.tar.gz

---
+++ packages/developers-chamber/developers-chamber-0.0.65.tar.gz/developers-chamber-0.0.65/developers_chamber/scripts/qa.py
@@ -0,0 +1,88 @@
+from developers_chamber.qa.base import QACheck, QACheckRunner
+from developers_chamber.qa.checks import (ImportOrderQACheck,
+                                          MigrationFilenamesQACheck,
+                                          MissingMigrationsQACheck,
+                                          MissingTranslationsQACheck,
+                                          TestMethodNamesQACheck,
+                                          PrintStatementsQACheck,
+                                          UnusedImportsQACheck)
+from developers_chamber.scripts import cli
+
+
+@cli.group()
+def qa():
+    """
+    Quality assurance commands.
+    """
+
+
+@qa.command()
+def all():
+    """
+    Runs all defined QA checks.
+    """
+    QACheckRunner(
+        MissingMigrationsQACheck(),
+        MigrationFilenamesQACheck(),
+        MissingTranslationsQACheck(),
+        ImportOrderQACheck(),
+        UnusedImportsQACheck(),
+        TestMethodNamesQACheck(),
+        PrintStatementsQACheck(),
+    ).run()
+
+
+@qa.command()
+def missing_migrations():
+    """
+    Runs missing migrations QA check.
+    """
+    QACheckRunner(MissingMigrationsQACheck()).run()
+
+
+@qa.command()
+def migration_filenames():
+    """
+    Runs migration filenames QA check.
+    """
+    QACheckRunner(MigrationFilenamesQACheck()).run()
+
+
+@qa.command()
+def missing_translations():
+    """
+    Runs missing translations QA check.
+    """
+    QACheckRunner(MissingTranslationsQACheck()).run()
+
+
+@qa.command()
+def import_order():
+    """
+    Runs import order QA check.
+    """
+    QACheckRunner(ImportOrderQACheck()).run()
+
+
+@qa.command()
+def unused_imports():
+    """
+    Runs unused imports QA check.
+    """
+    QACheckRunner(UnusedImportsQACheck()).run()
+
+
+@qa.command()
+def test_method_names():
+    """
+    Runs test method names QA check.
+    """
+    QACheckRunner(TestMethodNamesQACheck()).run()
+
+
+@qa.command()
+def print_statements():
+    """
+    Runs print statements names QA check.
+    """
+    QACheckRunner(PrintStatementsQACheck()).run()
