99a855f2369097aa01afa8575e09be463cc047cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:50 +0000

    Add package wenxin-api/0.1.3/wenxin-api-0.1.3.tar.gz

---
+++ packages/wenxin-api/wenxin-api-0.1.3.tar.gz/wenxin-api-0.1.3/wenxin_api/tasks/cloze.py
@@ -0,0 +1,23 @@
+""" cloze """
+from wenxin_api.api import Task
+from wenxin_api.error import IllegalRequestArgumentError
+from wenxin_api import log
+logger = log.get_logger()
+class Cloze(Task):
+    """ cloze task """
+    @staticmethod
+    def _resolve_result(resp):
+        rst = {}
+        rst["result"] = resp["result"]
+        return rst
+
+    @classmethod
+    def create(cls, text=None, model=None, **params):
+        """ create """
+        if text is None:
+            raise IllegalRequestArgumentError("text shouldn't be none")
+        model_id = 1 if model is None else model.id
+        params["api_type"] = params.get("api_type", 7)
+        logger.info("model {}: starts writing".format(model_id))
+        resp = cls.default_request(text=text, **params)
+        return cls._resolve_result(resp)
