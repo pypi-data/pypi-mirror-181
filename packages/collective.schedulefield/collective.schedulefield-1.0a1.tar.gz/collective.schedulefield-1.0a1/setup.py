9a0c4b4cc3059fafbee7aee57f737e96c5bd48b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:25 +0000

    Add package collective.schedulefield/1.0a1/collective.schedulefield-1.0a1.tar.gz

---
+++ packages/collective.schedulefield/collective.schedulefield-1.0a1.tar.gz/collective.schedulefield-1.0a1/setup.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+version = '1.0a1'
+
+from setuptools import setup, find_packages
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+setup(name='collective.schedulefield',
+      version=version,
+      description='Schedule field for zope forms',
+      long_description=long_description,
+      classifiers=[
+          "Environment :: Web Environment",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.8",
+          "Framework :: Plone",
+          "Framework :: Plone :: 4.2",
+          "Framework :: Plone :: 4.3",
+          "Framework :: Plone :: 5.2",
+      ],
+      keywords='',
+      author='IMIO',
+      author_email='support@imio.be',
+      url='https://github.com/imio/',
+      license='gpl',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+          'test': [
+          ]
+      },
+      entry_points={},
+      )
