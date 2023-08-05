4981cb3e603701c2f57f91e605f66584c4949924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:47 +0000

    Add package deepspeed/0.7.7/deepspeed-0.7.7.tar.gz

---
+++ packages/deepspeed/deepspeed-0.7.7.tar.gz/deepspeed-0.7.7/deepspeed/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.7.7'
+git_hash='unknown'
+git_branch='unknown'
+installed_ops={'cpu_adam': False, 'cpu_adagrad': False, 'fused_adam': False, 'fused_lamb': False, 'sparse_attn': False, 'transformer': False, 'stochastic_transformer': False, 'async_io': False, 'utils': False, 'quantizer': False, 'transformer_inference': False, 'spatial_inference': False}
+compatible_ops={'cpu_adam': True, 'cpu_adagrad': True, 'fused_adam': True, 'fused_lamb': True, 'sparse_attn': True, 'transformer': True, 'stochastic_transformer': True, 'async_io': False, 'utils': True, 'quantizer': True, 'transformer_inference': True, 'spatial_inference': True}
+torch_info={'version': '1.13', 'bf16_support': True, 'cuda_version': '11.6', 'nccl_version': '2.14', 'hip_version': '0.0'}
