d1fbd447f36c0079b2a785a4a49dc8fbca1f0e40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:18 +0000

    Add package DrissionPage/3.0.21/DrissionPage-3.0.21.tar.gz

---
+++ packages/DrissionPage/DrissionPage-3.0.21.tar.gz/DrissionPage-3.0.21/DrissionPage/chromium_tab.pyi
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+from .chromium_base import ChromiumBase
+from .chromium_page import ChromiumPage
+
+
+class ChromiumTab(ChromiumBase):
+    """实现浏览器标签页的类"""
+
+    def __init__(self, page:ChromiumPage, tab_id: str = ...):
+        self.page: ChromiumPage = ...
+
+    def _set_options(self) -> None: ...
---
+++ packages/DrissionPage/DrissionPage-3.0.21.tar.gz/DrissionPage-3.0.21/DrissionPage/tools.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+"""
+实用工具
+"""
+from FlowViewer import Listener, RequestMan
+
+from .session_element import make_session_ele
+from .easy_set import get_match_driver
