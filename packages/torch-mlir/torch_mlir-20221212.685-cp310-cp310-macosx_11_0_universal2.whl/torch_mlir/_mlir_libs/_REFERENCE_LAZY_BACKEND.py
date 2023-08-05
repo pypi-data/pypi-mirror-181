95030eaa8c385270f83f7cde7d6acc9496f79c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:10 +0000

    Add package torch-mlir/20221212.685/torch_mlir-20221212.685-cp310-cp310-macosx_11_0_universal2.whl

---
+++ packages/torch-mlir/torch_mlir-20221212.685-cp310-cp310-macosx_11_0_universal2.whl/torch_mlir/_mlir_libs/_REFERENCE_LAZY_BACKEND.py
@@ -0,0 +1,7 @@
+
+# This file was automatically generated due to LTC being disabled in build. 
+    
+class LazyTensorCoreTestConfig:
+    def __init__(self):
+        assert False, "LTC is not enabled. Check the value of `TORCH_MLIR_ENABLE_LTC`"
+    
