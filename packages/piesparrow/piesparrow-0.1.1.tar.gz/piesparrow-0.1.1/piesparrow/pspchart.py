e5c76c32d4b0f3191a8cdd2853cc93f3ace75ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:19 +0000

    Add package piesparrow/0.1.1/piesparrow-0.1.1.tar.gz

---
+++ packages/piesparrow/piesparrow-0.1.1.tar.gz/piesparrow-0.1.1/piesparrow/pspchart.py
@@ -0,0 +1,53 @@
+import pandas as pd
+
+def chart(title, df, columns, xcolumn,xaxistype='category', type='bar',datalabels='true', zoom='true', legend='true', legendposition='bottom', grid='true', xlabel='', ylabel='', height=500):
+    z=''
+    for column in columns:
+        lst = df[column].to_list()
+        z = z + f'''"{column}":{lst},'''
+    a = '{'+z+'}'
+    content = f'''
+    <div id="{title}"></div>
+    <script>
+    var chart = bb.generate({{
+        size: {{height:{height}}},
+        data: {{
+            x: "{xcolumn}",
+            json: {a} ,
+            type: "{type}",
+            labels: {datalabels}
+        }},
+        zoom: {{
+            enabled: {zoom},
+            type: "drag"
+        }},
+        legend: {{
+            show: {legend},
+            position: "{legendposition}"
+        }},
+        grid: {{
+            x: {{show: {grid}}},
+            y: {{show: {grid}}}
+        }},
+        axis: {{
+            x:{{
+                label: {{
+                    text: "{xlabel}",
+                    positon: "outer-center"
+                }},
+                show: true,
+                type: "{xaxistype}"
+            }},
+            y:{{
+                label: {{
+                    text: "{ylabel}",
+                    positon: "outer-center"
+                }},
+                show: true,
+            }}
+        }},
+        bindto:'#{title}'
+        }});
+    </script>
+    '''
+    return(content)
