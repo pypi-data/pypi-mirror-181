494783e2bbf307b1a4706b2b3ed1f99e48b82398
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:55 +0000

    Add package piesparrow/0.1.0/piesparrow-0.1.0-py3-none-any.whl

---
+++ packages/piesparrow/piesparrow-0.1.0-py3-none-any.whl/psptable.py
@@ -0,0 +1,11 @@
+# pieSparrow table include
+
+import pandas as pd
+import re
+
+def table(df):
+    tab = df.to_html(index=False)
+    chart = re.sub("border=\"1\" class=\"dataframe\"",'',tab)
+    chart2 = re.sub("style=\"text-align: right;\"",'',chart)
+    chart3 = '''<div style="overflow-x:auto;">'''+chart2+'''</div>'''
+    return chart3
