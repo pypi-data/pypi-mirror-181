f354716c12e629b059954bf4dce73ce817d7295b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:00 +0000

    Add package trStr/1.0.0/trStr-1.0.0.tar.gz

---
+++ packages/trStr/trStr-1.0.0.tar.gz/trStr-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+setup(
+    name="trStr",
+    packages=['trStr'],
+    version="1.0.0",
+    license='MIT',
+    description="String methods for Turkish characters",
+    author="Muhammet Fatih DİNÇ",
+    author_email="mfatihdinc61@gmail.com",
+    url="https://seridilekce.com",
+    keywords=['turkish', 'string', 'upper'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
