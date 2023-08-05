768d8b5552842979a024cd4ac08347f0ca54d84e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:42 +0000

    Add package KnitScript/0.1/KnitScript-0.1.tar.gz

---
+++ packages/KnitScript/KnitScript-0.1.tar.gz/KnitScript-0.1/setup.py
@@ -0,0 +1,25 @@
+"""Set Up file for project"""
+from setuptools import setup, find_packages
+
+setup(
+    name='KnitScript',
+    version='0.1',
+    packages=find_packages(),
+    package_dir={"KnitScript": "KnitScript"},
+    package_data={"KnitScript": ["*.pg", "*.js"], "knit_script_interpreter": ["*.pg", "*.js"], "": ["*.pg", "*.js"]},
+    include_package_data=True,
+    url='https://github.khoury.northeastern.edu/mhofmann/KnitScript',
+    license='MIT',
+    author='Megan Hofmann',
+    author_email='m.hofmann@northeastern.edu',
+    description='Interpreter from KnitScript to Knitout Instructions',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'networkx==2.8.8',
+        'parglare==0.16.0',
+        'Naked==0.1.32',
+    ],
+    entry_points={
+        'console_scripts': ['knitscript=KnitScript.interpret:main']
+    }
+)
---
+++ packages/KnitScript/KnitScript-0.1.tar.gz/KnitScript-0.1/tests/test_calibration.py
@@ -0,0 +1,21 @@
+"""Sample knitscript from calibration directory"""
+import os
+from unittest import TestCase
+
+from KnitScript.knit_script_interpreter.Knit_Script_Interpreter import Knit_Script_Interpreter
+from KnitScript.knit_script_interpreter.run_dat_compiler import knitout_to_dat
+
+
+class Test_Calibration(TestCase):
+    sample_directory = f"{os.getcwd()}{os.path.sep}calibration_knits{os.path.sep}"
+    parser = Knit_Script_Interpreter(debug_grammar=False, debug_parser=False, debug_parser_layout=False)
+
+    def test_gauge_stst(self):
+        name = f"{self.sample_directory}calibration_stst"
+        knitout, knit_graph = self.parser.write_knitout(f"{name}.ks", f"{name}.k", pattern_is_file=True)
+        knitout_to_dat(f"{name}.k", f"{name}.dat")
+
+    def test_stst(self):
+        name = f"{self.sample_directory}stst"
+        knitout, knit_graph = self.parser.write_knitout(f"{name}.ks", f"{name}.k", pattern_is_file=True)
+        knitout_to_dat(f"{name}.k", f"{name}.dat")
