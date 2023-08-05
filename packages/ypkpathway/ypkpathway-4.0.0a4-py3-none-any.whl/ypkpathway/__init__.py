03635f0dd786c4d6edc1fe227b2287f87ca532bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:15 +0000

    Add package ypkpathway/4.0.0a4/ypkpathway-4.0.0a4-py3-none-any.whl

---
+++ packages/ypkpathway/ypkpathway-4.0.0a4-py3-none-any.whl/ypkpathway/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""docstring."""
+
+__author__ = "Björn Johansson"
+__copyright__ = "Copyright 2013 - 2022 Björn Johansson"
+__credits__ = ["Björn Johansson"]
+__license__ = "BSD"
+__maintainer__ = "Björn Johansson"
+__email__ = "bjornjobb@gmail.com"
+
+from ._version import version as __version__
