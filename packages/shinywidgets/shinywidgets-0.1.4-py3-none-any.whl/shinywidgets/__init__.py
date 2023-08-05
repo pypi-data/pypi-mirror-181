43b1ff8e6755f6a006831f34fef8a1c3aaf3044a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:10 +0000

    Add package shinywidgets/0.1.4/shinywidgets-0.1.4-py3-none-any.whl

---
+++ packages/shinywidgets/shinywidgets-0.1.4-py3-none-any.whl/shinywidgets/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for shinywidgets."""
+
+__author__ = """Carson Sievert"""
+__email__ = "carson@rstudio.com"
+__version__ = "0.1.4"
+
+
+from ._shinywidgets import output_widget, register_widget, render_widget, reactive_read
+
+__all__ = ("output_widget", "register_widget", "render_widget", "reactive_read")
