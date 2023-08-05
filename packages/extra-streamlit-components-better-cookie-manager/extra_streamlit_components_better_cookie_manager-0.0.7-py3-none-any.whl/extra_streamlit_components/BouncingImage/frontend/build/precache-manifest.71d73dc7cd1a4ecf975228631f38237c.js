a97253960c7b0811c9e41859466f82525cd77177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:15 +0000

    Add package extra-streamlit-components-better-cookie-manager/0.0.7/extra_streamlit_components_better_cookie_manager-0.0.7-py3-none-any.whl

---
+++ packages/extra-streamlit-components-better-cookie-manager/extra_streamlit_components_better_cookie_manager-0.0.7-py3-none-any.whl/extra_streamlit_components/BouncingImage/frontend/build/precache-manifest.71d73dc7cd1a4ecf975228631f38237c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc5dfd6fc236494666ae13e28fa0285",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5549ace150548cbf075",
+    "url": "./static/js/2.44d5bbe2.chunk.js"
+  },
+  {
+    "revision": "fbabe484400fd249a0740ba2c0d294dd",
+    "url": "./static/js/2.44d5bbe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d201b6efb047e1d127b8",
+    "url": "./static/js/main.b57f7b4b.chunk.js"
+  },
+  {
+    "revision": "ea064b445fc55f3a8e56",
+    "url": "./static/js/runtime-main.11d11adb.js"
+  }
+]);
---
+++ packages/extra-streamlit-components-better-cookie-manager/extra_streamlit_components_better_cookie_manager-0.0.7-py3-none-any.whl/extra_streamlit_components/TabBar/frontend/build/precache-manifest.5463481933a5488e9920681da3628aca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa856ec0ce5115de388deee55701a5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e171f4febb8592d39bc",
+    "url": "./static/js/2.91f8cc2d.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.91f8cc2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32ea7810b8c4dd1c96d",
+    "url": "./static/js/main.7758c12d.chunk.js"
+  },
+  {
+    "revision": "0970136a891add58a05a",
+    "url": "./static/js/runtime-main.9cdf7577.js"
+  }
+]);
