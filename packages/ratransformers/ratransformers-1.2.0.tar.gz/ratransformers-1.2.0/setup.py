e81ae7b1abe2cc71bdb4fb06434cf9e6632915cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:59 +0000

    Add package ratransformers/1.2.0/ratransformers-1.2.0.tar.gz

---
+++ packages/ratransformers/ratransformers-1.2.0.tar.gz/ratransformers-1.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+with open('requirements.txt', encoding='utf-8') as f:
+    required = f.read().splitlines()
+
+
+setup(
+    name='ratransformers',
+    version='1.2.0',
+    description='RATransformer - make a transformer model learn implicit relations passed in the input',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/JoaoLages/RATransformers',
+    author='Joao Lages',
+    author_email='joaop.glages@gmail.com',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=required
+)
