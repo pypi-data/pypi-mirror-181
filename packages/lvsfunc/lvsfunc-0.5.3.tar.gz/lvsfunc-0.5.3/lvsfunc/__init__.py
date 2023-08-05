06b6c923c1667814ee362e5c2bd4d07ebabda95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:17:33 +0000

    Add package lvsfunc/0.5.3/lvsfunc-0.5.3.tar.gz

---
+++ packages/lvsfunc/lvsfunc-0.5.3.tar.gz/lvsfunc-0.5.3/lvsfunc/__init__.py
@@ -0,0 +1,21 @@
+"""
+    lvsfunc, a collection of VapourSynth functions and wrappers written and/or modified by LightArrowsEXE.
+
+    If you spot any issues, please do not hesitate to send in a Pull Request
+    or reach out to me on Discord (LightArrowsEXE#0476)!
+
+    For further support, drop by `#lvsfunc` in the `IEW Discord server <https://discord.gg/qxTxVJGtst>`_.
+"""
+
+# flake8: noqa
+
+from . import comparison, deblock, dehardsub, exceptions, mask, misc, render, types, util
+from .comparison import *
+from .deblock import *
+from .dehardsub import *
+from .exceptions import *
+from .mask import *
+from .misc import *
+from .render import *
+from .types import *
+from .util import *
