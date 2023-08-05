dc2df08681a2bf31ad08d01a7d1b10b667c1f1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:15:48 +0000

    Add package kiki-utils/1.2.1/kiki_utils-1.2.1-py3-none-any.whl

---
+++ packages/kiki-utils/kiki_utils-1.2.1-py3-none-any.whl/kikiutils/network.py
@@ -0,0 +1,18 @@
+from socket import gethostbyname
+from urllib.parse import urlparse
+
+
+# Network utils
+
+def get_domain_ip(domain: str):
+    return gethostbyname(domain)
+
+
+def get_host(url: str):
+    """Get the host of the input url."""
+
+    return urlparse(url).hostname
+
+
+def domains_is_ip(domains: list[str], ip: str):
+    return all([get_domain_ip(domain) == ip for domain in domains])
