d6e3e5374c61de40037c3ea7a75e20e9fd4db472
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:16 +0000

    Add package PyMieSim/0.8.4/PyMieSim-0.8.4-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/PyMieSim/PyMieSim-0.8.4-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/PyMieSim/Examples/__init__.py
@@ -0,0 +1,30 @@
+from .Detector import plot_LP01 as Example01
+from .Detector import plot_LP11 as Example02
+from .Detector import plot_LP02 as Example03
+from .Detector import plot_photodiode as Example04
+
+from .Scatterer import plot_FarField as Example05
+from .Scatterer import plot_SPF as Example06
+from .Scatterer import plot_Stokes as Example07
+from .Scatterer import plot_S1S2 as Example08
+from .Scatterer import plot_Footprint as Example09
+
+from .Experiment.sphere import plot_A1_vs_diameter as Example10
+from .Experiment.sphere import plot_B1_vs_diameter as Example11
+from .Experiment.sphere import plot_coupling_vs_diameter as Example12
+from .Experiment.sphere import plot_coupling_vs_phioffset as Example13
+from .Experiment.sphere import plot_coupling_vs_wavelength as Example14
+from .Experiment.sphere import plot_Qabs_vs_diameter as Example15
+from .Experiment.sphere import plot_Qsca_vs_diameter as Example16
+from .Experiment.sphere import plot_Qback_vs_corediameter as Example17
+from .Experiment.sphere import plot_Qsca_vs_index as Example18
+from .Experiment.sphere import plot_Qsca_vs_wavelength_std as Example19
+from .Experiment.coreshell import plot_Qback_vs_corediameter as Example20
+
+from .Validation import plot_BH87 as Example21
+from .Validation import plot_BH88 as Example22
+from .Validation import plot_BH810 as Example23
+from .Validation import plot_pymiescatt_0 as Example24
+from .Validation import plot_pymiescatt_1 as Example25
+from .Validation import plot_pymiescatt_2 as Example26
+from .Validation import plot_pymiescatt_3 as Example27
