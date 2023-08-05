de2da121ba6465909167ab665511b017257249b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:00 +0000

    Add package seeq-sysid/0.6.0/seeq_sysid-0.6.0.tar.gz

---
+++ packages/seeq-sysid/seeq_sysid-0.6.0.tar.gz/seeq_sysid-0.6.0/seeq_sysid/__init__.py
@@ -0,0 +1,4 @@
+from . import gui, model
+from ._version import __version__
+
+__all__ = ['__version__']
---
+++ packages/seeq-sysid/seeq_sysid-0.6.0.tar.gz/seeq_sysid-0.6.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+with open(file='README.md', mode='r') as readme_handle:
+    long_description = readme_handle.read()
+
+setup(
+    name='seeq_sysid',
+    version='0.6.0',
+    packages=find_packages(),
+    url='https://github.com/BYU-PRISM/Seeq',
+    license='MIT',
+    author='Junho Park, Mohammad Reza Babaei',
+    author_email='jnho.park@gmail.com, babaei_mr@outlook.com',
+    description='Seeq System Identification Addon',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'ipyvuetify>=1.2.2',
+        'ipywidgets',
+        'gekko',
+        'numpy',
+        'pandas',
+        'plotly',
+        'seeq',
+        'tensorflow>=2.1',
+        'keras-tuner==1.0.4',
+	   'scipy'
+    ],
+    keywords='Seeq Prism System Identification ARX FIR ARIMAX Subspace State-Space Neural-Network Addon Time-Series Transfer Function',
+    include_package_data=True,
+    package_data={'seeq_sysid': ['data/seeq_logo.png']},
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ]
+)
