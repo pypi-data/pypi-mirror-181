a2f413ebed0efabf66e51721fb1073be2ef51420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:50 +0000

    Add package detimer/0.2.0/detimer-0.2.0-py3-none-any.whl

---
+++ packages/detimer/detimer-0.2.0-py3-none-any.whl/detimer/matcher.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+import os
+
+
+class Matcher:
+    """Inclusion/exclusion rule"""
+
+    def __init__(self, pattern: str, exclude=False) -> None:
+        self.pattern = pattern
+        self.exclude = exclude
+
+    @staticmethod
+    def parse(line: str, source: str) -> Matcher:
+        # Each line (pattern) takes one of the following forms:
+        # + path/to/include
+        # - path/to-exclude
+
+        line = line.strip()
+
+        if not line.startswith("+") and not line.startswith("-"):
+            raise ValueError("Each pattern must begin with '+' or '-'")
+
+        exclude = line.startswith("-")
+        pattern = line[1:].strip()
+
+        # If it's a relative path, prepend the base source path, because
+        # rdiff-backup doesn't support relative paths
+        if not pattern.startswith(os.path.sep):
+            pattern = os.path.join(source, pattern)
+
+        return Matcher(pattern, exclude)
