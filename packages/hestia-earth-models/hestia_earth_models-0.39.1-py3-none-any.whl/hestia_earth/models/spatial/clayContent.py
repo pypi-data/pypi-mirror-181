9b0d21198e29972e8b562a0f6ad562c7620757b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:51 +0000

    Add package hestia-earth-models/0.39.1/hestia_earth_models-0.39.1-py3-none-any.whl

---
+++ packages/hestia-earth-models/hestia_earth_models-0.39.1-py3-none-any.whl/hestia_earth/models/spatial/clayContent.py
@@ -0,0 +1,68 @@
+from hestia_earth.schema import MeasurementStatsDefinition
+
+from hestia_earth.models.log import logRequirements, logShouldRun
+from hestia_earth.models.utils.measurement import _new_measurement
+from .utils import MAX_AREA_SIZE, download, find_existing_measurement, has_geospatial_data, should_download
+from . import MODEL
+
+REQUIREMENTS = {
+    "Site": {
+        "or": [
+            {"latitude": "", "longitude": ""},
+            {"boundary": {}},
+            {"region": {"@type": "Term", "termType": "region"}}
+        ]
+    }
+}
+RETURNS = {
+    "Measurement": [{
+        "value": "",
+        "depthUpper": "0",
+        "depthLower": "30",
+        "statsDefinition": "spatial"
+    }]
+}
+TERM_ID = 'clayContent'
+EE_PARAMS = {
+    'collection': 'T_CLAY',
+    'ee_type': 'raster',
+    'reducer': 'mean',
+    'fields': 'mean'
+}
+BIBLIO_TITLE = 'The harmonized world soil database. verson 1.0'
+
+
+def _measurement(value: int):
+    measurement = _new_measurement(TERM_ID, MODEL, BIBLIO_TITLE)
+    measurement['value'] = [value]
+    measurement['depthUpper'] = 0
+    measurement['depthLower'] = 30
+    measurement['statsDefinition'] = MeasurementStatsDefinition.SPATIAL.value
+    return measurement
+
+
+def _download(site: dict):
+    value = download(TERM_ID, site, EE_PARAMS).get(EE_PARAMS['reducer'])
+    return None if value is None else round(value, 2)
+
+
+def _run(site: dict):
+    value = find_existing_measurement(TERM_ID, site) or _download(site)
+    return [_measurement(value)] if value is not None else []
+
+
+def _should_run(site: dict):
+    geospatial_data = has_geospatial_data(site)
+    below_max_area_size = should_download(site)
+
+    logRequirements(site, model=MODEL, term=TERM_ID,
+                    geospatial_data=geospatial_data,
+                    max_area_size=MAX_AREA_SIZE,
+                    below_max_area_size=below_max_area_size)
+
+    should_run = all([geospatial_data, below_max_area_size])
+    logShouldRun(site, MODEL, TERM_ID, should_run)
+    return should_run
+
+
+def run(site: dict): return _run(site) if _should_run(site) else []
---
+++ packages/hestia-earth-models/hestia_earth_models-0.39.1-py3-none-any.whl/hestia_earth/models/spatial/slope.py
@@ -0,0 +1,63 @@
+from hestia_earth.schema import MeasurementStatsDefinition
+
+from hestia_earth.models.log import logRequirements, logShouldRun
+from hestia_earth.models.utils.measurement import _new_measurement
+from .utils import MAX_AREA_SIZE, download, find_existing_measurement, has_geospatial_data, should_download
+from . import MODEL
+
+REQUIREMENTS = {
+    "Site": {
+        "or": [
+            {"latitude": "", "longitude": ""},
+            {"boundary": {}},
+            {"region": {"@type": "Term", "termType": "region"}}
+        ]
+    }
+}
+RETURNS = {
+    "Measurement": [{
+        "value": "",
+        "statsDefinition": "spatial"
+    }]
+}
+TERM_ID = 'slope'
+EE_PARAMS = {
+    'collection': 'GMTEDSLOPE',
+    'ee_type': 'raster',
+    'reducer': 'mean',
+    'fields': 'mean'
+}
+BIBLIO_TITLE = 'An Enhanced Global Elevation Model Generalized From Multiple Higher Resolution Source Datasets'
+
+
+def _measurement(value: float):
+    measurement = _new_measurement(TERM_ID, MODEL, BIBLIO_TITLE)
+    measurement['value'] = [round(value, 7)]
+    measurement['statsDefinition'] = MeasurementStatsDefinition.SPATIAL.value
+    return measurement
+
+
+def _download(site: dict):
+    return download(TERM_ID, site, EE_PARAMS).get(EE_PARAMS['reducer'])
+
+
+def _run(site: dict):
+    value = find_existing_measurement(TERM_ID, site) or _download(site)
+    return [] if value is None else [_measurement(value)]
+
+
+def _should_run(site: dict):
+    geospatial_data = has_geospatial_data(site)
+    below_max_area_size = should_download(site)
+
+    logRequirements(site, model=MODEL, term=TERM_ID,
+                    geospatial_data=geospatial_data,
+                    max_area_size=MAX_AREA_SIZE,
+                    below_max_area_size=below_max_area_size)
+
+    should_run = all([geospatial_data, below_max_area_size])
+    logShouldRun(site, MODEL, TERM_ID, should_run)
+    return should_run
+
+
+def run(site: dict): return _run(site) if _should_run(site) else []
