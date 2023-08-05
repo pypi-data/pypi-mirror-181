9e0e647bf12fa7fc09af4c3286ab0e1edf8142ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:02 +0000

    Add package pythonPackaging-Rushik2900/0.0.7/pythonPackaging-Rushik2900-0.0.7.tar.gz

---
+++ packages/pythonPackaging-Rushik2900/pythonPackaging-Rushik2900-0.0.7.tar.gz/pythonPackaging-Rushik2900-0.0.7/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding ="utf-8") as f:
+    long_description = f.read()
+
+PROJECT_NAME = "pythonPackaging"
+USER_NAME = "Rushik2900"
+
+setuptools.setup(
+    name = f"{PROJECT_NAME}-{USER_NAME}",
+    version = "0.0.7",
+    author = USER_NAME,
+    author_email = "rush2900@gmail.com",
+    description = "Its an implementation of perceptron",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = f"https://github.com/{USER_NAME}/{PROJECT_NAME}",
+    project_url = {
+        "Bug Tracker" : f"https://github.com/{USER_NAME}/{PROJECT_NAME}/issues",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"" : "src"},
+    packages = setuptools.find_packages(where = "src"),
+    python_requires = ">=3.7",
+    install_requires = [
+        "numpy",
+        "tqdm"
+    ]
+)
