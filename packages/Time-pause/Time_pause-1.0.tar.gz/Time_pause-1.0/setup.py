d1c0f9dfcdf4bb7a0c426167645e4be1e6d318a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:56 +0000

    Add package Time-pause/1.0/Time_pause-1.0.tar.gz

---
+++ packages/Time-pause/Time_pause-1.0.tar.gz/Time_pause-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Time_pause',
+    version='1.0',
+    license='MIT',
+    author="PokechaNyaa",
+    author_email='pokechan6446@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Pokecraft-exe/time_pause',
+    keywords='Project to pause time'
+)
