ecadb14ea2cf77d04d31b589799283b2f2899027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:57 +0000

    Add package KSDTapi/0.1.4/KSDTapi-0.1.4-py3-none-any.whl

---
+++ packages/KSDTapi/KSDTapi-0.1.4-py3-none-any.whl/KSDTapi/jobs.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+def create(url, uploadType, shot, resourceId, filePath, volumeName):
+    url_path = f"{url}/qcc/job"
+
+    headers = {
+        #"Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/json"
+    }
+
+    data = json.dumps({
+        "uploadType": uploadType,
+        "shot": shot,
+        "resourceId": resourceId,
+        "filePath": filePath,
+        "volumeName": volumeName
+    })
+
+    # params = {
+    #     "namespace": f"{nameSpace}"
+    # }
+
+    response = requests.post(f"{url_path}", headers=headers, data=data) 
+
+    print("[Job CREATE] ",response.text, flush=True)
+
+    return response.text
+
+def getList(url):
+    url_path = f"{url}/qcc/job"
+
+    # params = {
+    #     "volumeName": volumeName,
+    #     "hostPath": hostPath
+    # }
+
+    response = requests.get(f"{url_path}") # , params=params)
+
+    print("[Job LIST] ", response.text, flush=True)
+
+    return response.text
