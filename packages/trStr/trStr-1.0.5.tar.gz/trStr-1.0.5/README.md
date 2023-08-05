b9802e66388dd301d0c439996e12d243347b0321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:54 +0000

    Add package trStr/1.0.5/trStr-1.0.5.tar.gz

---
+++ packages/trStr/trStr-1.0.5.tar.gz/trStr-1.0.5/README.md
@@ -0,0 +1,9 @@
+Solves the problem of string methods to uppercase/lowercase of 'i, I' characters in Turkish; for upper, lower, capitalize, title methods.
+
+Normal string methods is problematic for Turkish strings as follows:
+
+"aliler bize geldi".upper() -> "ALILER BIZE GELDI" // this module adresses to this problem
+
+Usage: 
+
+trStr(your_str).lower() // .upper(), .title(), .capitalize() methods also can be used.
---
+++ packages/trStr/trStr-1.0.5.tar.gz/trStr-1.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+    name="trStr",
+    packages=['trStr'],
+    version="1.0.5",
+    license='MIT',
+    description="String methods for Turkish characters",
+    long_description=open('README.md').read(),
+    author="Muhammet Fatih DİNÇ",
+    author_email="mfatihdinc61@gmail.com",
+    url="https://seridilekce.com",
+    keywords=['turkish', 'string', 'upper'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
