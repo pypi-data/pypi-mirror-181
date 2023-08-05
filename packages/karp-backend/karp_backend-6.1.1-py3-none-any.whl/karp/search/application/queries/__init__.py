fee0cd0f660d5a932a140f73c212f64404d422ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:07 +0000

    Add package karp-backend/6.1.1/karp_backend-6.1.1-py3-none-any.whl

---
+++ packages/karp-backend/karp_backend-6.1.1-py3-none-any.whl/karp/search/application/queries/__init__.py
@@ -0,0 +1,11 @@
+from .search_service import (
+    EntryDto,
+    SearchService,
+    QueryRequest,
+    QueryResponse,
+    QuerySplitResponse,
+    StatisticsDto,
+)
+from .resources import ResourceViews
+from karp.search.application.queries.entries import (
+    PreviewEntry, PreviewEntryInputDto, EntryPreviewDto)
