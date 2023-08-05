ef9c2e7640e08dbb74a5024753f32a170dd86711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:56 +0000

    Add package py-metarium-decoder/0.0.1.1/py_metarium_decoder-0.0.1.1-py3-none-any.whl

---
+++ packages/py-metarium-decoder/py_metarium_decoder-0.0.1.1-py3-none-any.whl/py_metarium_decoder/base/decoder.py
@@ -0,0 +1,28 @@
+from py_metarium import (
+    Being
+)
+
+
+class Decoder:
+
+    def __init__(self, url=None) -> None:
+        url = url or None
+        assert url is not None
+        being_initialization_parameters = {
+            "timeline": {
+                "type": "substrate",
+                "parameters": {
+                    "url" : url
+                }
+            }
+        }
+
+        b = Being(**being_initialization_parameters)
+
+        self.metarium_node = b.timeline_portal
+    
+    def info(self):
+        return self.metarium_node.info()
+
+    def decode(self, direction, block_hash=None, block_count=None):
+        return self.metarium_node.get_points(direction=direction, block_hash=block_hash, block_count=block_count)
