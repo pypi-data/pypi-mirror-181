15d05ae3fd1538645b06d77844ccf101b88738ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:15 +0000

    Add package abst/1.7.2/abst-1.7.2.tar.gz

---
+++ packages/abst/abst-1.7.2.tar.gz/abst-1.7.2/abst/__version__.py
@@ -0,0 +1,19 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = "abst"
+__description__ = (
+    "CLI Command making OCI Bastion and kubernetes usage simple and fast"
+)
+
+__version__ = "1.7.2"
+__author__ = "Jiri Otoupal"
+__author_email__ = "jiri-otoupal@ips-database.eu"
+__license__ = "MIT"
+__url__ = "https://github.com/jiri-otoupal/abst"
+
+__version_name__ = "Kitten w/ helmet no CLI"
