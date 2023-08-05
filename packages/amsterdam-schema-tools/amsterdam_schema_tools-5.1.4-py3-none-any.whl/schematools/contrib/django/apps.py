1cb5ed18d2e19207cca9569814b3794232bba805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:26 +0000

    Add package amsterdam-schema-tools/5.1.4/amsterdam_schema_tools-5.1.4-py3-none-any.whl

---
+++ packages/amsterdam-schema-tools/amsterdam_schema_tools-5.1.4-py3-none-any.whl/schematools/contrib/django/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class SchematoolsAppConfig(AppConfig):
+    name = "schematools.contrib.django"
+    # Alias as `datasets` app to avoid unnecessary migrations on legacy systems.
+    label = "datasets"
+    default_auto_field = "django.db.models.BigAutoField"
