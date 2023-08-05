f9c545b1e5b3ff35a433f69bb4b761c6a6f14877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:42 +0000

    Add package instead-of-pymysql/0.0.1/instead-of-pymysql-0.0.1.tar.gz

---
+++ packages/instead-of-pymysql/instead-of-pymysql-0.0.1.tar.gz/instead-of-pymysql-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="instead-of-pymysql",
+    version="0.0.1",
+    author="davek",
+    author_email="hwangdoyeouyu@gmail.com",
+    description="Instead of PyMySQL",
+    python_requires=">=3.6",
+)
