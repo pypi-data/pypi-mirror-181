90e9d56ebdc2f0c7d8b3144a009953da8a14a8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:21 +0000

    Add package macchanger1/0.0.4/macchanger1-0.0.4-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.whl

---
+++ packages/macchanger1/macchanger1-0.0.4-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.whl/macchanger.pyi
@@ -0,0 +1,15 @@
+
+def get_current_mac (interface:str) -> None:
+	''' Get the current MAC address '''
+
+def get_permanent_mac (interface:str) -> None:
+	''' Get the permanent MAC address '''
+
+def change_mac (interface:str, mac:str) -> None:
+	''' Set the MAC XX:XX:XX:XX:XX:XX '''
+
+def change_mac_to_random (interface:str) -> None:
+	''' Set to fully random MAC '''
+
+def change_mac_to_permanent (interface:str) -> None:
+ ''' Reset to original, permanent hardware MAC '''
