ad850bc9a6bd3d209b0838665cab9b2a0fed3607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:07 +0000

    Add package audio-feature/0.0.4/audio_feature-0.0.4.tar.gz

---
+++ packages/audio-feature/audio_feature-0.0.4.tar.gz/audio_feature-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='audio_feature',
+      version='0.0.4',
+      description='get features from wav',
+      author='AaronLi',
+      author_email='1470409562@qq.com',
+      requires= ['torchaudio'], # 定义依赖哪些模块
+      packages=find_packages(),  # 系统自动从当前目录开始找包
+      # 如果有的文件不用打包，则只能指定需要打包的文件
+      # packages=['compute_fbank',
+      #           'compute_melspectrogram',
+      #           '__init__'], #指定目录中需要打包的py文件，注意不要.py后缀
+      license="apache 3.0"
+      )
