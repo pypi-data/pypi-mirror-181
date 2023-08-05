27a6ed1241acab78761212d68002a6cdfa4ec93f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:41 +0000

    Add package python-mecab-ko/1.2.9/python-mecab-ko-1.2.9.tar.gz

---
+++ packages/python-mecab-ko/python-mecab-ko-1.2.9.tar.gz/python-mecab-ko-1.2.9/mecab/pybind/_mecab/utils.cpp
@@ -0,0 +1,14 @@
+#include "utils.h"
+
+std::vector<char *> to_argv(const std::vector<std::string> &arguments) {
+  std::vector<char *> argv{const_cast<char *>("")}; // argv[0] is excutable name
+  for (const auto &argument : arguments) {
+    argv.push_back(const_cast<char *>(argument.c_str()));
+  }
+  return argv;
+}
+
+std::string escape(const std::string &text) {
+  std::regex pattern("([\"\\\\])");
+  return std::regex_replace(text, pattern, "\\$1");
+}
