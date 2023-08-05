ce329a5970ff510efa9f993d632fabc53d881367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:43 +0000

    Add package xj-migrate/1.0.4/xj_migrate-1.0.4-py3-none-any.whl

---
+++ packages/xj-migrate/xj_migrate-1.0.4-py3-none-any.whl/xj_migrate/apis/data_excel.py
@@ -0,0 +1,39 @@
+import datetime
+import re
+import json
+import xlrd
+from rest_framework.views import APIView
+from xlrd import xldate_as_datetime
+from main.settings import BASE_DIR
+from ..services.DataConsolidation import DataConsolidation
+# from ..services.data_execl_service2 import DataExeclService
+from ..services.data_execl_service import DataExeclService
+from ..utils.custom_response import util_response
+
+
+class DataExcel(APIView):
+    def data_proving(self):
+        """
+        数据迁移
+        @param configure 连接配置（json）
+        @param table_name 表名
+        """
+        configure = self.POST.get('configure')
+        filename = self.POST.get('filename')  # 文件路径
+        table_name = self.POST.get('table_name')
+        data, err_txt = DataExeclService.excl_import(configure, table_name, filename)
+        if not err_txt:
+            return util_response(data=data)
+        return util_response(err=47767, msg=err_txt)
+
+    def data_match_write(self):
+        filename = self.POST.get('filename')  # 文件路径
+        configure = self.POST.get('configure')
+        export_field = self.POST.get('export_field')
+        old_table_id = self.POST.get('old_table_id')
+        new_table_id = self.POST.get('new_table_id')
+        data, err_txt = DataExeclService.data_migrate(filename, configure, export_field, old_table_id,
+                                                      new_table_id)
+        if not err_txt:
+            return util_response(data=data)
+        return util_response(err=47767, msg=err_txt)
---
+++ packages/xj-migrate/xj_migrate-1.0.4-py3-none-any.whl/xj_migrate/apis/migrate_platfrom.py
@@ -0,0 +1,30 @@
+# _*_coding:utf-8_*_
+
+import os, logging, time, json, copy
+from rest_framework.response import Response
+from rest_framework import generics
+
+from ..utils.model_handle import parse_data, util_response
+from ..services.migrate_platfrom_service import MigratePlatfromService
+
+logger = logging.getLogger(__name__)
+
+
+# 迁移平台
+class MigratePlatfrom(generics.UpdateAPIView):  # 或继承(APIView)
+
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'err': 0,
+            'msg': 'OK',
+            'data': MigratePlatfromService.get()
+        })
+
+    def post(self, request, *args, **kwargs):
+        params = parse_data(request)
+        if not params:
+            return util_response(err=6046, msg='至少需要一个请求参数')
+        data, err_txt = MigratePlatfromService.post(params=params)
+        if err_txt is None:
+            return util_response(data=data)
+        return util_response(err=47767, msg=err_txt)
