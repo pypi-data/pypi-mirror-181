a810f3d6c97688b4b4009390ccc722a4599b35ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:03 +0000

    Add package extra-streamlit-components-better-cookie-manager/0.0.9/extra_streamlit_components_better_cookie_manager-0.0.9.tar.gz

---
+++ packages/extra-streamlit-components-better-cookie-manager/extra_streamlit_components_better_cookie_manager-0.0.9.tar.gz/extra_streamlit_components_better_cookie_manager-0.0.9/extra_streamlit_components/StepperBar/frontend/build/precache-manifest.2551b7ab5705bbc9a38fb8425d16d2f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed8542b344af38282a5c98827d73390",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eeb51ab1718c505c0b07",
+    "url": "./static/js/2.1c4d4c34.chunk.js"
+  },
+  {
+    "revision": "a7be989d5c13ce43f1e9c54cec9c1a3d",
+    "url": "./static/js/2.1c4d4c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0be68382b9c40af9a030",
+    "url": "./static/js/main.0cea5b74.chunk.js"
+  },
+  {
+    "revision": "9116370eeca9ec72495b",
+    "url": "./static/js/runtime-main.4add7634.js"
+  }
+]);
