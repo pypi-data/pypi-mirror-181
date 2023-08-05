8dafa699803cf31057c11d140a33c0e0c6a1cfdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:46 +0000

    Add package prelude-cli/0.9.20/prelude-cli-0.9.20.tar.gz

---
+++ packages/prelude-cli/prelude-cli-0.9.20.tar.gz/prelude-cli-0.9.20/prelude_cli/templates/template.cs
@@ -0,0 +1,26 @@
+/*
+NAME: $NAME
+QUESTION: $QUESTION
+CREATED: $CREATED
+*/
+using System;
+
+class SecurityTest {
+    static void Test() {
+        Console.WriteLine("Run test");
+        Environment.Exit(100);
+    }
+
+    static void Clean() {
+        Console.WriteLine("Clean up");
+        Environment.Exit(100);
+    }
+
+    static void Main(string[] args) {
+        if (args.Length == 0) {
+            Test();
+        } else {
+            Clean();
+        }
+    }
+}
