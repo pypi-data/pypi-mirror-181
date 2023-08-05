9543bbc353da81f5a6386da1f7e6405640b220cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:05 +0000

    Add package core-main-app/2.1.0/core_main_app-2.1.0.tar.gz

---
+++ packages/core-main-app/core_main_app-2.1.0.tar.gz/core_main_app-2.1.0/core_main_app/components/template/access_control.py
@@ -0,0 +1,168 @@
+""" Template access control
+"""
+from django.db.models import Q
+
+from core_main_app.access_control.exceptions import AccessControlError
+from core_main_app.components.template.models import Template
+from django.conf import settings
+from core_main_app.utils.requests_utils.access_control import (
+    get_request_from_args,
+)
+
+
+def can_read(func, document_id, request):
+    """Can read document.
+
+    Args:
+        func:
+        document_id:
+        request:
+
+    Returns:
+
+    """
+    request = get_request_from_args(document_id, request=request)
+
+    # super user
+    if request.user.is_superuser:
+        return func(document_id, request=request)
+
+    # get the document
+    document = func(document_id, request=request)
+
+    # anonymous user
+    if request.user.is_anonymous:
+        if document.user:
+            raise AccessControlError(
+                "Template: The user doesn't have enough rights."
+            )
+        if not settings.CAN_ANONYMOUS_ACCESS_PUBLIC_DOCUMENT:
+            raise AccessControlError(
+                "Template: The user doesn't have enough rights."
+            )
+
+    # user is set
+    if document.user and document.user != str(request.user.id):
+        raise AccessControlError(
+            "Template: The user doesn't have enough rights."
+        )
+
+    return document
+
+
+def can_read_global(func, *args, **kwargs):
+    """Can read global template
+
+    Args:
+        func:
+        *args:
+        **kwargs:
+
+    Returns:
+
+    """
+    request = get_request_from_args(*args, **kwargs)
+    if request.user.is_anonymous:
+        if not settings.CAN_ANONYMOUS_ACCESS_PUBLIC_DOCUMENT:
+            raise AccessControlError(
+                "Template: The user doesn't have enough rights."
+            )
+    return func(*args, **kwargs)
+
+
+def can_write(func, *args, **kwargs):
+    """Can write template.
+
+    Args:
+        func:
+        args:
+        kwargs:
+
+    Returns:
+
+    """
+    request = get_request_from_args(*args, **kwargs)
+
+    template = next((arg for arg in args if isinstance(arg, Template)), None)
+
+    # super user
+    if request.user.is_superuser:
+        return func(*args, **kwargs)
+
+    # anonymous
+    if request.user.is_anonymous:
+        raise AccessControlError(
+            "Template: The user doesn't have enough rights."
+        )
+
+    # user is set
+    if template.user:
+        if template.user == str(request.user.id):
+            return func(*args, **kwargs)
+    # user is not set
+    else:
+        if request.user.is_staff:
+            return func(*args, **kwargs)
+
+    raise AccessControlError("Template: The user doesn't have enough rights.")
+
+
+def get_accessible_owners(request):
+    """Get a list of templates owners, the user can read from
+
+    Args:
+        request:
+
+    Returns:
+
+    """
+    if not request or request.user.is_anonymous:
+        if settings.CAN_ANONYMOUS_ACCESS_PUBLIC_DOCUMENT:
+            # global templates only
+            return Q(user__isnull=True)
+        # nothing
+        return Q(user__in=[])
+    if request.user.is_superuser:
+        # no restrictions
+        return None
+    # global and owned templates
+    in_q_list = Q()
+    in_q_list |= Q(user__isnull=True)
+    in_q_list |= Q(user=str(request.user.id))
+    return in_q_list
+
+
+def can_read_list(func, *args, **kwargs):
+    """Can read list of version managers.
+
+    Args:
+        func:
+
+    Returns:
+
+    """
+    request = get_request_from_args(*args, **kwargs)
+    # super user
+    if request and request.user.is_superuser:
+        return func(*args, **kwargs)
+
+    document_list = func(*args, **kwargs)
+    for template in document_list:
+        # anonymous user
+        if not request or request.user.is_anonymous:
+            if template.user:
+                raise AccessControlError(
+                    "Template: The user doesn't have enough rights."
+                )
+            if not settings.CAN_ANONYMOUS_ACCESS_PUBLIC_DOCUMENT:
+                raise AccessControlError(
+                    "Template: The user doesn't have enough rights."
+                )
+
+        # user is set
+        if template.user and template.user != str(request.user.id):
+            raise AccessControlError(
+                "Template: The user doesn't have enough rights."
+            )
+
+    return document_list
---
+++ packages/core-main-app/core_main_app-2.1.0.tar.gz/core_main_app-2.1.0/core_main_app/rest/blob/serializers.py
@@ -0,0 +1,159 @@
+"""
+    Serializers used throughout the Rest API
+"""
+from os.path import join
+from urllib.parse import urljoin
+
+from django.http import Http404
+from django.urls import reverse
+from rest_framework.fields import CharField
+from rest_framework.fields import SerializerMethodField
+from rest_framework.serializers import ModelSerializer
+
+from core_main_app import settings
+from core_main_app.commons.exceptions import DoesNotExist
+from core_main_app.components.blob import api as blob_api
+from core_main_app.components.blob.models import Blob
+from core_main_app.components.blob.utils import get_blob_download_uri
+
+
+class BlobSerializer(ModelSerializer):
+    """Blob serializer"""
+
+    handle = SerializerMethodField()
+    upload_date = SerializerMethodField()
+    if "core_linked_records_app" in settings.INSTALLED_APPS:
+        pid = SerializerMethodField()
+
+    class Meta:
+        """Meta"""
+
+        model = Blob
+        fields = [
+            "id",
+            "user_id",
+            "filename",
+            "handle",
+            "blob",
+            "checksum",
+            "upload_date",
+        ]
+        read_only_fields = (
+            "id",
+            "user_id",
+            "filename",
+            "handle",
+            "checksum",
+            "upload_date",
+        )
+        if "core_linked_records_app" in settings.INSTALLED_APPS:
+            fields.append("pid")
+            read_only_fields = read_only_fields + ("pid",)
+
+    def get_pid(self, instance):
+        """Return pid
+
+        Args:
+            instance:
+
+        Returns:
+
+        """
+        # return pid  if assigned
+        if "core_linked_records_app" not in settings.INSTALLED_APPS:
+            return None
+        else:
+            from core_linked_records_app.components.blob import (
+                api as linked_blob_api,
+            )
+            from core_linked_records_app.settings import (
+                ID_PROVIDER_SYSTEM_NAME,
+            )
+
+            try:
+                sub_url = reverse(
+                    "core_linked_records_provider_record",
+                    kwargs={"provider": ID_PROVIDER_SYSTEM_NAME, "record": ""},
+                )
+                blob_pid = linked_blob_api.get_pid_for_blob(str(instance.id))
+
+                return urljoin(
+                    settings.SERVER_URI, join(sub_url, blob_pid.record_name)
+                )
+            except Exception:
+                return None
+
+    def get_handle(self, instance):
+        """Return handle
+
+        Args:
+            instance:
+
+        Returns:
+
+        """
+        # get request from context
+        request = self.context.get("request")
+        # return download handle
+        return get_blob_download_uri(instance, request)
+
+    def get_upload_date(self, instance):
+        """Return upload date
+
+        Args:
+            instance:
+
+        Returns:
+
+        """
+        # Return instance generation time
+        return str(instance.creation_date)
+
+    def create(self, validated_data):
+        """Create and return a new `Blob` instance, given the validated data.
+
+        Args:
+            validated_data:
+
+        Returns:
+
+        """
+        # Create blob
+        blob_object = Blob(
+            filename=validated_data["blob"].name,
+            user_id=str(self.context["request"].user.id),
+            blob=validated_data["blob"],
+        )
+
+        # Save the blob
+        blob_api.insert(blob_object, self.context["request"].user)
+
+        return blob_object
+
+
+class DeleteBlobsSerializer(ModelSerializer):
+    """Delete Blob serializer."""
+
+    id = CharField()
+
+    class Meta:
+        """Meta"""
+
+        model = Blob
+        fields = ("id",)
+
+    def validate_id(self, id):
+        """Validate id field
+
+        Args:
+            id:
+
+        Returns:
+
+        """
+        request = self.context.get("request")
+        try:
+            blob_api.get_by_id(id, request.user)
+        except DoesNotExist:
+            raise Http404
+        return id
