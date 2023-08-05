bd501da981973c4b6b23a443a12a2f7a392c7c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:35 +0000

    Add package colemen-utils/2.9.55/colemen_utils-2.9.55.tar.gz

---
+++ packages/colemen-utils/colemen_utils-2.9.55.tar.gz/colemen_utils-2.9.55/colemen_utilities/database_utils/MySQL/Column/__init__.py
@@ -0,0 +1,11 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=line-too-long
+# pylint: disable=unused-import
+
+
+import colemen_utilities.database_utils.MySQL.Column.Column
+import colemen_utilities.database_utils.MySQL.Column.column_utils
+
+
+
