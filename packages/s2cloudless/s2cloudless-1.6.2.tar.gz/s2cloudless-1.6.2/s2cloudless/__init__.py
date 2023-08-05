c9049defc98cff2c8e8c2fb1a6f41053f4e310f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:12 +0000

    Add package s2cloudless/1.6.2/s2cloudless-1.6.2.tar.gz

---
+++ packages/s2cloudless/s2cloudless-1.6.2.tar.gz/s2cloudless-1.6.2/s2cloudless/__init__.py
@@ -0,0 +1,10 @@
+"""
+This module lists all externally useful classes and functions
+"""
+
+from .cloud_detector import S2PixelCloudDetector
+from .pixel_classifier import PixelClassifier
+from .sentinelhub_masking import CloudMaskRequest, NoDataAvailableException
+from .utils import get_s2_evalscript
+
+__version__ = "1.6.2"
