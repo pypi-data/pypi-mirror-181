a66d15907aee8b0866ea5ab13c780ea258abf26a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:06 +0000

    Add package sqlalchemy-risingwave/0.0.8/sqlalchemy-risingwave-0.0.8.tar.gz

---
+++ packages/sqlalchemy-risingwave/sqlalchemy-risingwave-0.0.8.tar.gz/sqlalchemy-risingwave-0.0.8/test/test_schema.py
@@ -0,0 +1,36 @@
+from sqlalchemy import text
+from sqlalchemy import MetaData, Table, testing, inspect
+from sqlalchemy.testing import fixtures
+
+
+class SchemaTest(fixtures.TestBase):
+
+    def teardown_method(self, method):
+        with testing.db.begin() as conn:
+            conn.execute(text("DROP TABLE IF EXISTS users"))
+
+    def setup_method(self):
+        with testing.db.begin() as conn:
+            conn.execute(
+                text(
+                    """
+                    CREATE TABLE users (
+                        name STRING PRIMARY KEY
+                    )
+                    """
+                )
+            )
+        self.meta = MetaData(schema="public")
+
+    def test_get_columns_indexes_across_schema(self):
+        # get_columns and get_indexes use default db uri schema.
+        # across schema table must use schema.table
+        Table("users", self.meta, autoload_with=testing.db, schema="public")
+
+    def test_returning_clause(self):
+        with testing.db.begin() as conn:
+            insp = inspect(testing.db)
+            table_names = insp.get_table_names()
+
+            for t in table_names:
+                assert t == str("users")
