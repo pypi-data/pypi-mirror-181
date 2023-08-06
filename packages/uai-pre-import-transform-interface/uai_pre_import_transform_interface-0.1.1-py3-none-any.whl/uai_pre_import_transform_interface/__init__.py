511300b587e44dff10dabac55f71521e932bf08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:25 +0000

    Add package uai-pre-import-transform-interface/0.1.1/uai_pre_import_transform_interface-0.1.1-py3-none-any.whl

---
+++ packages/uai-pre-import-transform-interface/uai_pre_import_transform_interface-0.1.1-py3-none-any.whl/uai_pre_import_transform_interface/__init__.py
@@ -0,0 +1,6 @@
+from typing import Sequence
+
+from .pre_import_transformer import PreImportTransformerInterface
+from uai_uri_interface.uri import URI
+
+__all__: Sequence[str] = ["PreImportTransformerInterface", "URI"]
