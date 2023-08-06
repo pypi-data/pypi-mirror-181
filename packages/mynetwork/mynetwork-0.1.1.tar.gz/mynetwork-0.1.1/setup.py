8d57be817a173b2c0b1074898967ffacc07db018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:13 +0000

    Add package mynetwork/0.1.1/mynetwork-0.1.1.tar.gz

---
+++ packages/mynetwork/mynetwork-0.1.1.tar.gz/mynetwork-0.1.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['mynetwork', 'mynetwork.myip']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.3,<9.0.0', 'requests>=2.28.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['mynetwork = mynetwork.cli:main']}
+
+setup_kwargs = {
+    'name': 'mynetwork',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': 'install dep: poetry install\nupdate dep: poetry update\nadd dep: poetry add click requests\nshow dep: poetry show\nremove dep: poetry remove pendulum\n\nbuild: poetry build\n\ncache list: poetry cache list\n\nVEnv: poetry env info',
+    'author': 'Yogesh Sharma',
+    'author_email': 'yks0000@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
