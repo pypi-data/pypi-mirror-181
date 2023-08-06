76a3e34b91c63dd3f528ca8d6f819402b1620ca3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:46 +0000

    Add package ckanext-datapackage-creator/0.0.4/ckanext-datapackage-creator-0.0.4.tar.gz

---
+++ packages/ckanext-datapackage-creator/ckanext-datapackage-creator-0.0.4.tar.gz/ckanext-datapackage-creator-0.0.4/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+from os import path
+
+
+here = path.abspath(path.dirname(__file__))
+
+
+try:
+    import pypandoc
+    readme = pypandoc.convert_file('README.md', 'rst')
+except (IOError, ImportError):
+    readme = ''
+
+
+setup(
+    name='''ckanext-datapackage-creator''',
+    version='0.0.4',
+    description='''Data Package Creator.''',
+    long_description=readme,
+    author='''Transparência Mineas Gerais''',
+    license='AGPL',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='''CKAN''',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    namespace_packages=['ckanext'],
+    install_requires=[
+        'frictionless==4.40.8',
+    ],
+    include_package_data=True,
+    package_data={
+    },
+    data_files=[],
+    entry_points={
+        'ckan.plugins': [
+            'datapackage_creator=ckanext.datapackage_creator.plugin:DatapackageCreatorPlugin'
+        ],
+    },
+)
