421f473494a86bd742616c038ac1ed99344650d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:56 +0000

    Add package tesla-admin/0.0.2/tesla_admin-0.0.2-py3-none-any.whl

---
+++ packages/tesla-admin/tesla_admin-0.0.2-py3-none-any.whl/tool/__init__.py
@@ -0,0 +1,115 @@
+import click
+import os
+# import sys
+
+
+
+
+
+
+
+@click.group()
+def supercli():
+    click.echo('''
+            Tesla Admin CLI
+            ...............
+            
+            Enjoy, ;)   
+               
+               ''')
+    pass
+
+
+@supercli.command()
+@click.option('-n', '--name', type=str, help='Name of directory', prompt='project name')
+def startproject(name):
+    click.echo(f'creating a new project {name}' )
+
+    
+    CORE_FOLDER = name + '/core/'
+    MANAGE_FILE =  name + '/manage.py'
+
+    SETTINGS_FILE = CORE_FOLDER + 'settings.py'
+    URL_FILE = CORE_FOLDER + 'urls.py'
+    os.makedirs(CORE_FOLDER)
+    with open(SETTINGS_FILE, 'a+') as s_f:
+        s_f.write('''
+                
+from tesla.static import staticfiles
+
+import os
+from pathlib import Path as Pa
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Pa(__file__).resolve().parent.parent 
+
+
+MIDDLEWARES = []
+
+staticfiles.path = os.path.join(BASE_DIR, 'static_')
+    ''')
+    with open(MANAGE_FILE, 'a+') as m_f:
+        m_f.write('''
+                   
+
+from tesla import App
+import core.settings
+import core.urls
+
+app = App(debug=True)
+
+app.middlewares.set_middlewares(core.settings.MIDDLEWARES)
+
+            ''')
+    open(URL_FILE, 'a+')
+    
+@supercli.command()
+@click.option('-n', '--name', type=str, help='Name of application', prompt='application name')
+def startapp(name):
+    click.echo(f'creating a new application {name}' )
+    APP = f'./{name}/'
+    URL_FILE = APP + 'urls.py'
+    VIEWS = APP + 'views.py'
+    MID = APP + 'middleware.py'
+    MODELS = APP + 'models.py'
+    os.makedirs(APP)
+    
+    with open(URL_FILE, 'a+') as u_f:
+        u_f.write('''
+from tesla.router import Path
+from . import views
+
+# your urls path should be here
+patterns = []
+                  ''')
+    
+    with open(VIEWS, 'a+') as v_f:
+        v_f.write('''
+from tesla.auth.decorators import login_required
+from tesla.auth.views import login
+from tesla.response import HttpResponse, Redirect, Render, JsonResponse
+from tesla.pyhtml import CT
+
+# your views
+                  ''')
+       
+    with open(MID, 'a+') as m:
+        pass
+    
+    with open(MODELS, 'a+') as m:
+        m.write(
+            '''
+from tesla.auth.modal import UserBaseModal
+from tesla.modal import Model
+from dataclasses import dataclass
+            
+            '''
+        )
+        pass 
+   
+@supercli.command()
+@click.argument('port', default=8000)        
+def serve(port = 8000):
+    click.echo('Starting the developing server')
+    os.system(f'waitress-serve --listen=*:{port} manage:app')       
+            
