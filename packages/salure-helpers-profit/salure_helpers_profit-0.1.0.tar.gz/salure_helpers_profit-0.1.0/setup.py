f142cbb0c23fbed96ddfd50c7f6daf27f89e229a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:07 +0000

    Add package salure-helpers-profit/0.1.0/salure_helpers_profit-0.1.0.tar.gz

---
+++ packages/salure-helpers-profit/salure_helpers_profit-0.1.0.tar.gz/salure_helpers_profit-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='salure_helpers_profit',
+    version='0.1.0',
+    description='Profit wrapper from Salure',
+    long_description='Profit wrapper from Salure',
+    author='D&A Salure',
+    author_email='support@salureconnnect.com',
+    packages=["salure_helpers.profit"],
+    license='Salure License',
+    install_requires=[
+        'salure-helpers-salureconnect>=1,<=1',
+        'aiohttp>=3,<=4',
+        'pandas>=1,<=1.35',
+        'requests>=2,<=3'
+    ],
+    zip_safe=False,
+)
