c284b045708bf1196e3eff846c89593e49841633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:23:28 +0000

    Add package git-multi-repo-updater/0.1.0/git_multi_repo_updater-0.1.0.tar.gz

---
+++ packages/git-multi-repo-updater/git_multi_repo_updater-0.1.0.tar.gz/git_multi_repo_updater-0.1.0/gitmultirepoupdater/tests/test_git_repository_cloning.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+from gitmultirepoupdater.cli import main
+
+
+# class CommandLineArgumentParsingTests(TestCase):
+#     def test_repositories_parsing(self):
+#         args = main(["-r", "foo"])
+#         self.assertEqual(args.repos, ["foo"])
---
+++ packages/git-multi-repo-updater/git_multi_repo_updater-0.1.0.tar.gz/git_multi_repo_updater-0.1.0/gitmultirepoupdater/tests/test_github_api.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+
+class GithubApiTests(TestCase):
+    def test_clone_github_repository(self):
+        pass
+
+    def test_create_feature_branch(self):
+        pass
+
+    def test_run_script_inside_cloned_repository(self):
+        pass
+
+    def test_commit_changes_made_to_cloned_repository(self):
+        pass
+
+    def test_push_commits(self):
+        pass
+
+    def test_create_pull_request(self):
+        pass
+
+    def test_execution_logs(self):
+        # Test that pull request link is present in the logs
+        # Test that error Tracebacks are in the logs
+        pass
