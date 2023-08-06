5f89bab163d5add8b07e8a445ffdf44a91100c82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:25 +0000

    Add package fdk-rdf-parser/2.1.16/fdk_rdf_parser-2.1.16-py3-none-any.whl

---
+++ packages/fdk-rdf-parser/fdk_rdf_parser-2.1.16-py3-none-any.whl/fdk_rdf_parser/classes/rule.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+from typing import (
+    Dict,
+    List,
+    Optional,
+)
+
+from .skos_code import ReferenceDataCode
+
+
+@dataclass
+class Rule:
+    uri: Optional[str] = None
+    identifier: Optional[str] = None
+    description: Optional[Dict[str, str]] = None
+    language: Optional[List[ReferenceDataCode]] = None
+    name: Optional[Dict[str, str]] = None
