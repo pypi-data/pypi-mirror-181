b7448189fe7d2c27f04949b84fb5e5b6d2ed31d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:41 +0000

    Add package django-reusable/7.2/django_reusable-7.2-py3-none-any.whl

---
+++ packages/django-reusable/django_reusable-7.2-py3-none-any.whl/django_reusable/urls/urls.py
@@ -0,0 +1,17 @@
+from django.urls import include
+
+from django_reusable import views
+from django_reusable.error_tracker import views as error_tracker_views
+from django.conf.urls import url
+app_name = "django_reusable"
+
+
+urlpatterns = [
+    url(r'^ajax-callback-handler/(?P<pk>\d+)/(?P<name>[\w-]+)/$',
+        views.ajax_callback_handler, name='ajax_callback_handler'),
+    url(r'^errors/', include([
+        url(r'^$', error_tracker_views.view_list, name="errors"),
+        url(r'^(?P<rhash>[\w-]+)/delete$', error_tracker_views.delete_exception, name='delete_error'),
+        url(r'^(?P<rhash>[\w-]+)$', error_tracker_views.detail, name='view_error'),
+    ])),
+]
