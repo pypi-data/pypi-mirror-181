4c37956a9d52c05e91ca6da482840483ab81e382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:36 +0000

    Add package prps/0.0.4/prps_0.0.4.tar.gz

---
+++ packages/prps/prps_0.0.4.tar.gz/prps-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='prps',
+      version='0.0.4',
+      description='Connect to Pepper via ssh and run the rock paper scissors game.',
+      long_description=readme(),
+      url="https://github.com/D7017E/Rock-Paper-Scissors",
+      author='D7017E',
+      author_email="bersim-8@student.ltu.se",
+      packages=['prps'],
+      install_requires=[
+        'paramiko',
+      ],
+      zip_safe=False)
+      
