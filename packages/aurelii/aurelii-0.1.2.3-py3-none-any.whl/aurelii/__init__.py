9fd78d3bc68506341624150caf18f658bf180537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:09 +0000

    Add package aurelii/0.1.2.3/aurelii-0.1.2.3-py3-none-any.whl

---
+++ packages/aurelii/aurelii-0.1.2.3-py3-none-any.whl/aurelii/__init__.py
@@ -0,0 +1,8 @@
+def readme():
+    with open('README.rst', 'r') as f:
+        return f.read()
+
+def info():
+    '''Prints info for Aurelii package'''
+    print("This is the Python package for Aurelii.")
+
