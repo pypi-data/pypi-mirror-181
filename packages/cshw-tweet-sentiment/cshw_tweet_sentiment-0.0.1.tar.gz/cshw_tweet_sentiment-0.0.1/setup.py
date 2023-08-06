b5d78232dbf194918da87078f9e4314e7d52d224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:08 +0000

    Add package cshw-tweet-sentiment/0.0.1/cshw_tweet_sentiment-0.0.1.tar.gz

---
+++ packages/cshw-tweet-sentiment/cshw_tweet_sentiment-0.0.1.tar.gz/cshw_tweet_sentiment-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1' 
+DESCRIPTION = 'Tweet sentiment analysis'
+LONG_DESCRIPTION = 'Made for CS501'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="cshw_tweet_sentiment", 
+        version=VERSION,
+        author="Cameron Hyaes",
+        author_email="c.cash.hayes@gmail.com",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[], # add any additional packages that 
+        # needs to be installed along with your package. Eg: 'caer'
+        
+        keywords=['python'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
