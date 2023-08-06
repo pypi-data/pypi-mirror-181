46fbebcacded9473743540cb55c982392791520b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:58 +0000

    Add package edup-utils/1.0.0/edup-utils-1.0.0.tar.gz

---
+++ packages/edup-utils/edup-utils-1.0.0.tar.gz/edup-utils-1.0.0/setup.py
@@ -0,0 +1,62 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    # This is the name of your project. The first time you publish this
+    # package, this name will be registered for you. It will determine how
+    # users can install this project, e.g.:
+    #
+    # $ pip install sampleproject
+    #
+    # And where it will live on PyPI: https://pypi.org/project/sampleproject/
+    #
+    # There are some restrictions on what makes a valid project name
+    # specification here:
+    # https://packaging.python.org/specifications/core-metadata/#name
+    name="edup-utils",  # Required
+    # Versions should comply with PEP 440:
+    # https://www.python.org/dev/peps/pep-0440/
+    #
+    # For a discussion on single-sourcing the version across setup.py and the
+    # project code, see
+    # https://packaging.python.org/guides/single-sourcing-package-version/
+    version="1.0.0",
+
+    url="https://gitlab.tinkoff.academy/bpmi-2010/projects/edup",
+    # This should be your name or the name of the organization which owns the
+    # project.
+    author="Daniel Pustotin",  # Optional
+    # This should be a valid email address corresponding to the author listed
+    # above.
+    author_email="heartsker@gmail.com",  # Optional
+
+    # You can just specify package directories manually here if your project is
+    # simple. Or you can use find_packages().
+    #
+    # Alternatively, if you just want to distribute a single Python file, use
+    # the `py_modules` argument instead as follows, which will expect a file
+    # called `my_module.py` to exist:
+    #
+    #   py_modules=["my_module"],
+    #
+    packages=find_packages(where="src"),  # Required
+    # Specify which Python versions you support. In contrast to the
+    # 'Programming Language' classifiers above, 'pip install' will check this
+    # and refuse to install the project if the version does not match. See
+    # https://packaging.python.org/guides/distributing-packages-using-setuptools/#python-requires
+    python_requires=">=3.7, <4",
+
+    # List additional URLs that are relevant to your project as a dict.
+    project_urls={
+        "Source": "https://gitlab.tinkoff.academy/bpmi-2010/projects/edup",
+    },
+)
