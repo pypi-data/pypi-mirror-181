e6d84b235a0c552fe93782ebf8618b4ac63dc2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:01 +0000

    Add package heframework/0.2.10/heframework-0.2.10.tar.gz

---
+++ packages/heframework/heframework-0.2.10.tar.gz/heframework-0.2.10/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='heframework',
+      version='0.2.10',
+      description='本次更新：修复list模块对中文的支持',
+      author='heStudio',
+      author_email='hestudio@hestudio.org',
+      url='https://gitee.com/hestudio-framework/main-windows/',
+      packages=['src',""],
+     )
+
