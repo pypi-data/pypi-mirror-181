4432c82afdd176a9d62d8978a1e96e695f9772c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:30 +0000

    Add package heframework/0.2.8/heframework-0.2.8.tar.gz

---
+++ packages/heframework/heframework-0.2.8.tar.gz/heframework-0.2.8/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='heframework',
+      version='0.2.8',
+      description='heStudio自研公共框架。',
+      author='heStudio',
+      author_email='hestudio@hestudio.org',
+      url='https://gitee.com/hestudio-framework/main-windows/',
+      packages=['src',""],
+     )
+
