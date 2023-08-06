95c632da1a7afa65b4f640c7d0a487cd80625dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:10 +0000

    Add package fdk-rdf-parser/2.1.15/fdk_rdf_parser-2.1.15.tar.gz

---
+++ packages/fdk-rdf-parser/fdk_rdf_parser-2.1.15.tar.gz/fdk_rdf_parser-2.1.15/src/fdk_rdf_parser/classes/legal_resource.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+from typing import (
+    Dict,
+    Optional,
+)
+
+
+@dataclass
+class LegalResource:
+    uri: Optional[str] = None
+    description: Optional[Dict[str, str]] = None
+    url: Optional[str] = None
