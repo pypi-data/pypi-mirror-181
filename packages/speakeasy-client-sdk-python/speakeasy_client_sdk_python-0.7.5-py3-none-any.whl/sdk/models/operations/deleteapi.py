a7c3bf4f0c728beda6df385518acc905a96ee28a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:50 +0000

    Add package speakeasy-client-sdk-python/0.7.5/speakeasy_client_sdk_python-0.7.5-py3-none-any.whl

---
+++ packages/speakeasy-client-sdk-python/speakeasy_client_sdk_python-0.7.5-py3-none-any.whl/sdk/models/operations/deleteapi.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+from typing import Optional
+from sdk.models import shared
+from sdk.models import shared
+
+
+@dataclass
+class DeleteAPIPathParams:
+    api_id: str = field(metadata={'path_param': { 'field_name': 'apiID', 'style': 'simple', 'explode': False }})
+    version_id: str = field(metadata={'path_param': { 'field_name': 'versionID', 'style': 'simple', 'explode': False }})
+    
+
+@dataclass
+class DeleteAPIRequest:
+    path_params: DeleteAPIPathParams = field()
+    
+
+@dataclass
+class DeleteAPIResponse:
+    content_type: str = field()
+    status_code: int = field()
+    error: Optional[shared.Error] = field(default=None)
+    
---
+++ packages/speakeasy-client-sdk-python/speakeasy_client_sdk_python-0.7.5-py3-none-any.whl/sdk/models/operations/queryeventlog.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+from typing import Optional
+from sdk.models import shared
+from sdk.models import shared
+
+
+@dataclass
+class QueryEventLogQueryParams:
+    filters: Optional[shared.Filters] = field(default=None, metadata={'query_param': { 'field_name': 'filters', 'serialization': 'json' }})
+    
+
+@dataclass
+class QueryEventLogRequest:
+    query_params: QueryEventLogQueryParams = field()
+    
+
+@dataclass
+class QueryEventLogResponse:
+    content_type: str = field()
+    status_code: int = field()
+    bounded_requests: Optional[list[shared.BoundedRequest]] = field(default=None)
+    error: Optional[shared.Error] = field(default=None)
+    
