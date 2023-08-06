ed6ec0369dc856bd26cb5542b746b5fd6ead1fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:15 +0000

    Add package steamship/2.2.2b0/steamship-2.2.2b0.tar.gz

---
+++ packages/steamship/steamship-2.2.2b0.tar.gz/steamship-2.2.2b0/tests/steamship_tests/utils/client.py
@@ -0,0 +1,60 @@
+from contextlib import contextmanager
+from typing import Any, Dict
+
+from steamship import Configuration, Steamship
+from steamship.data.package.package_instance import PackageInstance
+from steamship.data.plugin.plugin_instance import PluginInstance
+
+TESTING_PROFILE = "test"
+
+
+def get_steamship_client(fail_if_workspace_exists=False, **kwargs) -> Steamship:
+    # Always use the `test` profile
+    kwargs["profile"] = TESTING_PROFILE
+    return Steamship(
+        fail_if_workspace_exists=fail_if_workspace_exists, config=Configuration.parse_obj(kwargs)
+    )
+
+
+@contextmanager
+def steamship_use(
+    package_handle: str,
+    instance_handle: str = None,
+    config: Dict[str, Any] = None,
+    version: str = None,
+    fetch_if_exists: bool = True,
+    delete_workspace: bool = True,
+    **kwargs
+) -> PackageInstance:
+    # Always use the `test` profile
+    kwargs["profile"] = TESTING_PROFILE
+    instance = Steamship.use(
+        package_handle, instance_handle, config, version, fetch_if_exists, **kwargs
+    )
+    assert instance.client.config.workspace_id == instance.workspace_id
+    yield instance
+    # Clean up the workspace
+    if delete_workspace:
+        instance.client.get_workspace().delete()
+
+
+@contextmanager
+def steamship_use_plugin(
+    plugin_handle: str,
+    instance_handle: str = None,
+    config: Dict[str, Any] = None,
+    version: str = None,
+    fetch_if_exists: bool = True,
+    delete_workspace: bool = True,
+    **kwargs
+) -> PluginInstance:
+    # Always use the `test` profile
+    kwargs["profile"] = TESTING_PROFILE
+    instance = Steamship.use_plugin(
+        plugin_handle, instance_handle, config, version, fetch_if_exists, **kwargs
+    )
+    assert instance.client.config.workspace_id == instance.workspace_id
+    yield instance
+    # Clean up the workspace
+    if delete_workspace:
+        instance.client.get_workspace().delete()
