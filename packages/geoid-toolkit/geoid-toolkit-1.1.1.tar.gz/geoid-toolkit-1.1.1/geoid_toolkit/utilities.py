7f7d9a9a65aafda55b9fade5d7a941a4b14102b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:22 +0000

    Add package geoid-toolkit/1.1.1/geoid-toolkit-1.1.1.tar.gz

---
+++ packages/geoid-toolkit/geoid-toolkit-1.1.1.tar.gz/geoid-toolkit-1.1.1/geoid_toolkit/utilities.py
@@ -0,0 +1,554 @@
+#!/usr/bin/env python
+u"""
+utilities.py
+Written by Tyler Sutterley (12/2022)
+Download and management utilities for syncing time and auxiliary files
+
+PYTHON DEPENDENCIES:
+    lxml: processing XML and HTML in Python
+        https://pypi.python.org/pypi/lxml
+
+UPDATE HISTORY:
+    Updated 12/2022: functions for managing and maintaining git repositories
+    Updated 04/2022: updated docstrings to numpy documentation format
+    Updated 10/2021: using python logging for handling verbose output
+    Updated 03/2021: added sha1 option for retrieving file hashes
+    Updated 11/2020: added list function for finding files on the GFZ ICGEM
+    Updated 09/2020: copy from http and https to bytesIO object in chunks
+    Written 08/2020
+"""
+from __future__ import print_function, division
+
+import sys
+import os
+import re
+import io
+import ssl
+import ftplib
+import shutil
+import socket
+import inspect
+import hashlib
+import logging
+import warnings
+import posixpath
+import lxml.etree
+import subprocess
+import calendar,time
+if sys.version_info[0] == 2:
+    from urllib import quote_plus
+    from cookielib import CookieJar
+    import urllib2
+else:
+    from urllib.parse import quote_plus
+    from http.cookiejar import CookieJar
+    import urllib.request as urllib2
+
+# PURPOSE: get absolute path within a package from a relative path
+def get_data_path(relpath):
+    """
+    Get the absolute path within a package from a relative path
+
+    Parameters
+    ----------
+    relpath: str,
+        relative path
+    """
+    # current file path
+    filename = inspect.getframeinfo(inspect.currentframe()).filename
+    filepath = os.path.dirname(os.path.abspath(filename))
+    if isinstance(relpath,list):
+        # use *splat operator to extract from list
+        return os.path.join(filepath,*relpath)
+    elif isinstance(relpath,str):
+        return os.path.join(filepath,relpath)
+
+# PURPOSE: get the hash value of a file
+def get_hash(local, algorithm='MD5'):
+    """
+    Get the hash value from a local file or BytesIO object
+
+    Parameters
+    ----------
+    local: obj or str
+        BytesIO object or path to file
+    algorithm: str, default 'MD5'
+        hashing algorithm for checksum validation
+
+            - ``'MD5'``: Message Digest
+            - ``'sha1'``: Secure Hash Algorithm
+    """
+    # check if open file object or if local file exists
+    if isinstance(local, io.IOBase):
+        if (algorithm == 'MD5'):
+            return hashlib.md5(local.getvalue()).hexdigest()
+        elif (algorithm == 'sha1'):
+            return hashlib.sha1(local.getvalue()).hexdigest()
+    elif os.access(os.path.expanduser(local),os.F_OK):
+        # generate checksum hash for local file
+        # open the local_file in binary read mode
+        with open(os.path.expanduser(local), 'rb') as local_buffer:
+            # generate checksum hash for a given type
+            if (algorithm == 'MD5'):
+                return hashlib.md5(local_buffer.read()).hexdigest()
+            elif (algorithm == 'sha1'):
+                return hashlib.sha1(local_buffer.read()).hexdigest()
+    else:
+        return ''
+
+# PURPOSE: get the git hash value
+def get_git_revision_hash(refname='HEAD', short=False):
+    """
+    Get the git hash value for a particular reference
+
+    Parameters
+    ----------
+    refname: str, default HEAD
+        Symbolic reference name
+    short: bool, default False
+        Return the shorted hash value
+    """
+    # get path to .git directory from current file path
+    filename = inspect.getframeinfo(inspect.currentframe()).filename
+    basepath = os.path.dirname(os.path.dirname(os.path.abspath(filename)))
+    gitpath = os.path.join(basepath,'.git')
+    # build command
+    cmd = ['git', f'--git-dir={gitpath}', 'rev-parse']
+    cmd.append('--short') if short else None
+    cmd.append(refname)
+    # get output
+    with warnings.catch_warnings():
+        return str(subprocess.check_output(cmd), encoding='utf8').strip()
+
+# PURPOSE: get the current git status
+def get_git_status():
+    """Get the status of a git repository as a boolean value
+    """
+    # get path to .git directory from current file path
+    filename = inspect.getframeinfo(inspect.currentframe()).filename
+    basepath = os.path.dirname(os.path.dirname(os.path.abspath(filename)))
+    gitpath = os.path.join(basepath,'.git')
+    # build command
+    cmd = ['git', f'--git-dir={gitpath}', 'status', '--porcelain']
+    with warnings.catch_warnings():
+        return bool(subprocess.check_output(cmd))
+
+# PURPOSE: recursively split a url path
+def url_split(s):
+    """
+    Recursively split a url path into a list
+
+    Parameters
+    ----------
+    s: str
+        url string
+    """
+    head, tail = posixpath.split(s)
+    if head in ('http:','https:','ftp:','s3:'):
+        return s,
+    elif head in ('', posixpath.sep):
+        return tail,
+    return url_split(head) + (tail,)
+
+# PURPOSE: convert file lines to arguments
+def convert_arg_line_to_args(arg_line):
+    """
+    Convert file lines to arguments
+
+    Parameters
+    ----------
+    arg_line: str
+        line string containing a single argument and/or comments
+    """
+    # remove commented lines and after argument comments
+    for arg in re.sub(r'\#(.*?)$',r'',arg_line).split():
+        if not arg.strip():
+            continue
+        yield arg
+
+# PURPOSE: returns the Unix timestamp value for a formatted date string
+def get_unix_time(time_string, format='%Y-%m-%d %H:%M:%S'):
+    """
+    Get the Unix timestamp value for a formatted date string
+
+    Parameters
+    ----------
+    time_string: str
+        formatted time string to parse
+    format: str, default '%Y-%m-%d %H:%M:%S'
+        format for input time string
+    """
+    try:
+        parsed_time = time.strptime(time_string.rstrip(), format)
+    except (TypeError, ValueError):
+        pass
+    else:
+        return calendar.timegm(parsed_time)
+
+# PURPOSE: rounds a number to an even number less than or equal to original
+def even(value):
+    """
+    Rounds a number to an even number less than or equal to original
+
+    Parameters
+    ----------
+    value: float
+        number to be rounded
+    """
+    return 2*int(value//2)
+
+# PURPOSE: rounds a number upward to its nearest integer
+def ceil(value):
+    """
+    Rounds a number upward to its nearest integer
+
+    Parameters
+    ----------
+    value: float
+        number to be rounded upward
+    """
+    return -int(-value//1)
+
+# PURPOSE: make a copy of a file with all system information
+def copy(source, destination, move=False, **kwargs):
+    """
+    Copy or move a file with all system information
+
+    Parameters
+    ----------
+    source: str
+        source file
+    destination: str
+        copied destination file
+    move: bool, default False
+        remove the source file
+    """
+    source = os.path.abspath(os.path.expanduser(source))
+    destination = os.path.abspath(os.path.expanduser(destination))
+    # log source and destination
+    logging.info(f'{source} -->\n\t{destination}')
+    shutil.copyfile(source, destination)
+    shutil.copystat(source, destination)
+    if move:
+        os.remove(source)
+
+# PURPOSE: check ftp connection
+def check_ftp_connection(HOST, username=None, password=None):
+    """
+    Check internet connection with ftp host
+
+    Parameters
+    ----------
+    HOST: str
+        remote ftp host
+    username: str or NoneType
+        ftp username
+    password: str or NoneType
+        ftp password
+    """
+    # attempt to connect to ftp host
+    try:
+        f = ftplib.FTP(HOST)
+        f.login(username, password)
+        f.voidcmd("NOOP")
+    except IOError:
+        raise RuntimeError('Check internet connection')
+    except ftplib.error_perm:
+        raise RuntimeError('Check login credentials')
+    else:
+        return True
+
+# PURPOSE: list a directory on a ftp host
+def ftp_list(HOST, username=None, password=None, timeout=None,
+    basename=False, pattern=None, sort=False):
+    """
+    List a directory on a ftp host
+
+    Parameters
+    ----------
+    HOST: str or list
+        remote ftp host path split as list
+    username: str or NoneType
+        ftp username
+    password: str or NoneType
+        ftp password
+    timeout: int or NoneType, default None
+        timeout in seconds for blocking operations
+    basename: bool, default False
+        return the file or directory basename instead of the full path
+    pattern: str or NoneType, default None
+        regular expression pattern for reducing list
+    sort: bool, default False
+        sort output list
+
+    Returns
+    -------
+    output: list
+        items in a directory
+    mtimes: list
+        last modification times for items in the directory
+    """
+    # verify inputs for remote ftp host
+    if isinstance(HOST, str):
+        HOST = url_split(HOST)
+    # try to connect to ftp host
+    try:
+        ftp = ftplib.FTP(HOST[0],timeout=timeout)
+    except (socket.gaierror,IOError):
+        raise RuntimeError(f'Unable to connect to {HOST[0]}')
+    else:
+        ftp.login(username,password)
+        # list remote path
+        output = ftp.nlst(posixpath.join(*HOST[1:]))
+        # get last modified date of ftp files and convert into unix time
+        mtimes = [None]*len(output)
+        # iterate over each file in the list and get the modification time
+        for i,f in enumerate(output):
+            try:
+                # try sending modification time command
+                mdtm = ftp.sendcmd(f'MDTM {f}')
+            except ftplib.error_perm:
+                # directories will return with an error
+                pass
+            else:
+                # convert the modification time into unix time
+                mtimes[i] = get_unix_time(mdtm[4:], format="%Y%m%d%H%M%S")
+        # reduce to basenames
+        if basename:
+            output = [posixpath.basename(i) for i in output]
+        # reduce using regular expression pattern
+        if pattern:
+            i = [i for i,f in enumerate(output) if re.search(pattern,f)]
+            # reduce list of listed items and last modified times
+            output = [output[indice] for indice in i]
+            mtimes = [mtimes[indice] for indice in i]
+        # sort the list
+        if sort:
+            i = [i for i,j in sorted(enumerate(output), key=lambda i: i[1])]
+            # sort list of listed items and last modified times
+            output = [output[indice] for indice in i]
+            mtimes = [mtimes[indice] for indice in i]
+        # close the ftp connection
+        ftp.close()
+        # return the list of items and last modified times
+        return (output,mtimes)
+
+# PURPOSE: download a file from a ftp host
+def from_ftp(HOST, username=None, password=None, timeout=None,
+    local=None, hash='', chunk=8192, verbose=False, fid=sys.stdout,
+    mode=0o775):
+    """
+    Download a file from a ftp host
+
+    Parameters
+    ----------
+    HOST: str or list
+        remote ftp host path
+    username: str or NoneType
+        ftp username
+    password: str or NoneType
+        ftp password
+    timeout: int or NoneType, default None
+        timeout in seconds for blocking operations
+    local: str or NoneType, default None
+        path to local file
+    hash: str, default ''
+        MD5 hash of local file
+    chunk: int, default 8192
+        chunk size for transfer encoding
+    verbose: bool, default False
+        print file transfer information
+    fid: obj, default sys.stdout
+        open file object to print if verbose
+    mode: oct, default 0o775
+        permissions mode of output local file
+
+    Returns
+    -------
+    remote_buffer: obj
+        BytesIO representation of file
+    """
+    # create logger
+    loglevel = logging.INFO if verbose else logging.CRITICAL
+    logging.basicConfig(stream=fid, level=loglevel)
+    # verify inputs for remote ftp host
+    if isinstance(HOST, str):
+        HOST = url_split(HOST)
+    # try downloading from ftp
+    try:
+        # try to connect to ftp host
+        ftp = ftplib.FTP(HOST[0],timeout=timeout)
+    except (socket.gaierror,IOError):
+        raise RuntimeError(f'Unable to connect to {HOST[0]}')
+    else:
+        ftp.login(username,password)
+        # remote path
+        ftp_remote_path = posixpath.join(*HOST[1:])
+        # copy remote file contents to bytesIO object
+        remote_buffer = io.BytesIO()
+        ftp.retrbinary(f'RETR {ftp_remote_path}',
+            remote_buffer.write, blocksize=chunk)
+        remote_buffer.seek(0)
+        # save file basename with bytesIO object
+        remote_buffer.filename = HOST[-1]
+        # generate checksum hash for remote file
+        remote_hash = hashlib.md5(remote_buffer.getvalue()).hexdigest()
+        # get last modified date of remote file and convert into unix time
+        mdtm = ftp.sendcmd(f'MDTM {ftp_remote_path}')
+        remote_mtime = get_unix_time(mdtm[4:], format="%Y%m%d%H%M%S")
+        # compare checksums
+        if local and (hash != remote_hash):
+            # convert to absolute path
+            local = os.path.abspath(local)
+            # create directory if non-existent
+            if not os.access(os.path.dirname(local), os.F_OK):
+                os.makedirs(os.path.dirname(local), mode)
+            # print file information
+            args = (posixpath.join(*HOST),local)
+            logging.info('{0} -->\n\t{1}'.format(*args))
+            # store bytes to file using chunked transfer encoding
+            remote_buffer.seek(0)
+            with open(os.path.expanduser(local), 'wb') as f:
+                shutil.copyfileobj(remote_buffer, f, chunk)
+            # change the permissions mode
+            os.chmod(local,mode)
+            # keep remote modification time of file and local access time
+            os.utime(local, (os.stat(local).st_atime, remote_mtime))
+        # close the ftp connection
+        ftp.close()
+        # return the bytesIO object
+        remote_buffer.seek(0)
+        return remote_buffer
+
+# PURPOSE: check internet connection
+def check_connection(HOST):
+    """
+    Check internet connection with http host
+
+    Parameters
+    ----------
+    HOST: str
+        remote http host
+    """
+    # attempt to connect to http host
+    try:
+        urllib2.urlopen(HOST,timeout=20,context=ssl.SSLContext())
+    except urllib2.URLError:
+        raise RuntimeError('Check internet connection')
+    else:
+        return True
+
+# PURPOSE: download a file from a http host
+def from_http(HOST, timeout=None, context=ssl.SSLContext(),
+    local=None, hash='', chunk=16384, verbose=False, fid=sys.stdout,
+    mode=0o775):
+    """
+    Download a file from a http host
+
+    Parameters
+    ----------
+    HOST: str or list
+        remote http host path split as list
+    timeout: int or NoneType, default None
+        timeout in seconds for blocking operations
+    context: obj, default ssl.SSLContext()
+        SSL context for url opener object
+    timeout: int or NoneType, default None
+        timeout in seconds for blocking operations
+    local: str or NoneType, default None
+        path to local file
+    hash: str, default ''
+        MD5 hash of local file
+    chunk: int, default 16384
+        chunk size for transfer encoding
+    verbose: bool, default False
+        print file transfer information
+    fid: obj, default sys.stdout
+        open file object to print if verbose
+    mode: oct, default 0o775
+        permissions mode of output local file
+
+    Returns
+    -------
+    remote_buffer: obj
+        BytesIO representation of file
+    """
+    # create logger
+    loglevel = logging.INFO if verbose else logging.CRITICAL
+    logging.basicConfig(stream=fid, level=loglevel)
+    # verify inputs for remote http host
+    if isinstance(HOST, str):
+        HOST = url_split(HOST)
+    # try downloading from http
+    try:
+        # Create and submit request.
+        request = urllib2.Request(posixpath.join(*HOST))
+        response = urllib2.urlopen(request,timeout=timeout,context=context)
+    except (urllib2.HTTPError, urllib2.URLError):
+        raise Exception('Download error from {0}'.format(posixpath.join(*HOST)))
+    else:
+        # copy remote file contents to bytesIO object
+        remote_buffer = io.BytesIO()
+        shutil.copyfileobj(response, remote_buffer, chunk)
+        remote_buffer.seek(0)
+        # save file basename with bytesIO object
+        remote_buffer.filename = HOST[-1]
+        # generate checksum hash for remote file
+        remote_hash = hashlib.md5(remote_buffer.getvalue()).hexdigest()
+        # compare checksums
+        if local and (hash != remote_hash):
+            # convert to absolute path
+            local = os.path.abspath(local)
+            # create directory if non-existent
+            if not os.access(os.path.dirname(local), os.F_OK):
+                os.makedirs(os.path.dirname(local), mode)
+            # print file information
+            args = (posixpath.join(*HOST),local)
+            logging.info('{0} -->\n\t{1}'.format(*args))
+            # store bytes to file using chunked transfer encoding
+            remote_buffer.seek(0)
+            with open(os.path.expanduser(local), 'wb') as f:
+                shutil.copyfileobj(remote_buffer, f, chunk)
+            # change the permissions mode
+            os.chmod(local,mode)
+        # return the bytesIO object
+        remote_buffer.seek(0)
+        return remote_buffer
+
+# PURPOSE: list a directory on the GFZ ICGEM https server
+# http://icgem.gfz-potsdam.de
+def icgem_list(host='http://icgem.gfz-potsdam.de/tom_longtime',timeout=None,
+    parser=lxml.etree.HTMLParser()):
+    """
+    Parse the table of static gravity field models on the GFZ
+    `International Centre for Global Earth Models (ICGEM) <http://icgem.gfz-potsdam.de/>`_
+    server
+
+    Parameters
+    ----------
+    host: str
+        url for the GFZ ICGEM gravity field table
+    timeout: int or NoneType
+        timeout in seconds for blocking operations
+    parser: obj, default lxml.etree.HTMLParser()
+        HTML parser for lxml
+
+    Returns
+    -------
+    colfiles: dict
+        Static gravity field file urls mapped by field name
+    """
+    # try listing from https
+    try:
+        # Create and submit request.
+        request = urllib2.Request(host)
+        tree = lxml.etree.parse(urllib2.urlopen(request,timeout=timeout),parser)
+    except:
+        raise Exception(f'List error from {host}')
+    else:
+        # read and parse request for files
+        colfiles = tree.xpath('//td[@class="tom-cell-modelfile"]//a/@href')
+        # reduce list of files to find gfc files
+        # return the dict of model files mapped by name
+        return {re.findall('(.*?).gfc',posixpath.basename(f)).pop():url_split(f)
+            for i,f in enumerate(colfiles) if re.search('gfc$',f)}
