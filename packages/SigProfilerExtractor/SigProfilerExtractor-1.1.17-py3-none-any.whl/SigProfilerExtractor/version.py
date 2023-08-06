facc077081fa14b76dad8e4b499d895aac7a6aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:29 +0000

    Add package SigProfilerExtractor/1.1.17/SigProfilerExtractor-1.1.17-py3-none-any.whl

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.17-py3-none-any.whl/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.17'
+version = '1.1.17'
+Update = 'Add DBS test and require plotting v1.3.8 or newer.'
+    
+    
