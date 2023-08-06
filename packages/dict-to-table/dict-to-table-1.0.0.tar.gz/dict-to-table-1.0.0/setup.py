2275beea75725bf31352cfc1aec5852176e7ea59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:16 +0000

    Add package dict-to-table/1.0.0/dict-to-table-1.0.0.tar.gz

---
+++ packages/dict-to-table/dict-to-table-1.0.0.tar.gz/dict-to-table-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='dict-to-table',
+    version='1.0.0',
+    license='MIT',
+    author="Jeyendran K",
+    author_email='jeyendran785@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='',
+
+)
