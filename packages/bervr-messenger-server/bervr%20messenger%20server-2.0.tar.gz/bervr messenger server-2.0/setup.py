dad9447ff5af34f317ae9b37b5479037a0a475f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:10 +0000

    Add package bervr-messenger-server/2.0/bervr%20messenger%20server-2.0.tar.gz

---
+++ packages/bervr-messenger-server/bervr%20messenger%20server-2.0.tar.gz/bervr messenger server-2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="bervr messenger server",
+      version="2.0",
+      description="A server part for messenger",
+      author="Aleksandr Filippov",
+      author_email="bervrr@gmail.com",
+      packages=["server"]
+      )
