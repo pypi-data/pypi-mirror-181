e94bd9d2e52c9da344c5816147ffad6b40d8ef3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:33:52 +0000

    Add package mqt.qmap/1.12.1/mqt.qmap-1.12.1-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/mqt.qmap/mqt.qmap-1.12.1-cp310-cp310-macosx_11_0_arm64.whl/mqt/qmap/__init__.py
@@ -0,0 +1,46 @@
+#
+# This file is part of MQT QMAP library which is released under the MIT license.
+# See file README.md or go to http://iic.jku.at/eda/research/quantum_verification/ for more information.
+#
+
+import os
+import sys
+from pathlib import Path
+
+if sys.platform == "win32" and sys.version_info > (3, 8, 0) and "Z3_ROOT" in os.environ:
+    lib_path = Path(os.environ["Z3_ROOT"]) / "lib"
+    if lib_path.exists():
+        os.add_dll_directory(str(lib_path))
+    bin_path = Path(os.environ["Z3_ROOT"]) / "bin"
+    if bin_path.exists():
+        os.add_dll_directory(str(bin_path))
+
+from mqt.qmap.compile import compile
+from mqt.qmap.pyqmap import (
+    Arch,
+    Architecture,
+    CommanderGrouping,
+    Configuration,
+    Encoding,
+    InitialLayout,
+    Layering,
+    MappingResults,
+    Method,
+    SwapReduction,
+)
+from mqt.qmap.subarchitectures import SubarchitectureOrder
+
+__all__ = [
+    "compile",
+    "Method",
+    "InitialLayout",
+    "Layering",
+    "Arch",
+    "CommanderGrouping",
+    "SwapReduction",
+    "Encoding",
+    "Configuration",
+    "MappingResults",
+    "Architecture",
+    "SubarchitectureOrder",
+]
