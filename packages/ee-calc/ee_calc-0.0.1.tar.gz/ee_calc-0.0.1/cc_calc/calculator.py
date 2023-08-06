e83b4c6fa26c50fc021e65095346a7115df12cde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:21 +0000

    Add package ee-calc/0.0.1/ee_calc-0.0.1.tar.gz

---
+++ packages/ee-calc/ee_calc-0.0.1.tar.gz/ee_calc-0.0.1/cc_calc/calculator.py
@@ -0,0 +1,18 @@
+from cc_calc.addition import add
+from cc_calc.subtraction import subtract
+from cc_calc.multiplication import multiply
+from cc_calc.division import divide
+
+
+
+#result = add(num1=0,num2=0)
+#print(result)  
+
+#result = subtract(num1=0, num2=0)
+#print(result)  
+
+#result = multiply(num1=0, num2=0)
+#print(result) 
+
+#result = divide(num1=0, num2=0)
+#print(result)  
---
+++ packages/ee-calc/ee_calc-0.0.1.tar.gz/ee_calc-0.0.1/cc_calc/division.py
@@ -0,0 +1,7 @@
+def divide(num1, num2):
+    if num2 == 0:
+        raise ValueError("Cannot divide by zero")
+    return num1 / num2
+
+
+
