d2cd5ff19298a5fe0a9a151423cfb47a39296f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:10 +0000

    Add package jkuang-test-3/1.0.12/jkuang_test-3-1.0.12.tar.gz

---
+++ packages/jkuang-test-3/jkuang_test-3-1.0.12.tar.gz/jkuang_test-3-1.0.12/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="jkuang_test-3",
+    version="1.0.12",
+    description="test",
+    author="test",
+    packages=find_packages(),
+    python_requires='>=2.6, !=3.0.*, !=3.1.*, !=3.2.*',
+)
+
