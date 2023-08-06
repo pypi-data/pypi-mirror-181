da6b944fab443368ab9b12e11e4de00b385092eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:34:52 +0000

    Add package wrgl/0.13.4/wrgl-0.13.4-py3-none-any.whl

---
+++ packages/wrgl/wrgl-0.13.4-py3-none-any.whl/wrgl/repository_test.py
@@ -0,0 +1,211 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright © 2022 Wrangle Ltd
+
+from datetime import datetime
+import typing
+import _csv
+from unittest import TestCase
+from contextlib import contextmanager
+import io
+import tempfile
+import socket
+import os
+import csv
+
+from wrgl.diffreader import ColumnChanges
+from wrgl.repository import Repository
+from wrgl import tar
+from wrgl.vcr_test import use_vcr
+
+
+def get_open_port():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("", 0))
+    s.listen(1)
+    port = s.getsockname()[1]
+    s.close()
+    return str(port)
+
+
+class RepositoryTestCase(TestCase):
+    maxDiff = None
+    repo_uri = "http://localhost:8081"
+    client_id = "wrgl-python-sdk"
+    client_secret = "my-secret"
+
+    def setUp(self):
+        super().setUp()
+        self.repo = Repository(self.repo_uri, self.client_id, self.client_secret)
+
+    @contextmanager
+    def commit(
+        self, branch: str, message: str, primary_key: typing.List[str]
+    ) -> typing.Iterator["_csv._writer"]:
+        with tempfile.NamedTemporaryFile(mode="w", delete=False) as f:
+            writer = csv.writer(f)
+            yield writer
+        try:
+            with open(f.name, "rb") as f2:
+                self.repo.commit(branch, message, f2, primary_key)
+        finally:
+            os.remove(f.name)
+
+    @use_vcr
+    def test_authenticate(self):
+        rpt = self.repo.authenticate()
+        self.assertTrue(not not rpt)
+
+    @use_vcr
+    def test_commit(self):
+        with self.commit("test_commit", "initial commit", ["a"]) as writer:
+            writer.writerow(["a", "b", "c"])
+            writer.writerow(["1", "q", "w"])
+            writer.writerow(["2", "a", "s"])
+
+        refs = self.repo.get_refs()
+        self.assertEqual(len(refs), 1)
+        commit1 = self.repo.get_commit(refs["heads/test_commit"])
+        self.assertIsInstance(commit1.time, datetime)
+        commit2 = self.repo.get_branch("test_commit")
+        self.assertEqual(commit1, commit2)
+
+        b = io.StringIO()
+        writer = csv.writer(b)
+        writer.writerow(["a", "b", "c"])
+        writer.writerow(["1", "e", "w"])
+        writer.writerow(["2", "c", "s"])
+        cr = self.repo.commit(
+            branch="test_commit",
+            message="second commit",
+            file=io.BytesIO(b.getvalue().encode("utf8")),
+            primary_key=["a"],
+        )
+        self.assertIsNotNone(cr.sum)
+        self.assertIsNotNone(cr.table)
+
+        com_tree = self.repo.get_commit_tree(cr.sum, 2)
+        self.assertEqual(com_tree.sum, cr.sum)
+        self.assertIn(commit1.sum, com_tree.root.parents)
+        self.assertIn(commit1.sum, com_tree.root.parent_commits)
+        self.assertIsNotNone(com_tree.root.parent_commits[commit1.sum].table)
+
+        tbl = self.repo.get_table(cr.table)
+        self.assertEqual(tbl.columns, ["a", "b", "c"])
+        self.assertEqual(
+            list(self.repo.get_blocks(cr.sum)),
+            [["a", "b", "c"], ["1", "e", "w"], ["2", "c", "s"]],
+        )
+        self.assertEqual(
+            list(self.repo.get_table_blocks(cr.table)),
+            [["a", "b", "c"], ["1", "e", "w"], ["2", "c", "s"]],
+        )
+        self.assertEqual(list(self.repo.get_rows(cr.sum, [0])), [["1", "e", "w"]])
+        self.assertEqual(
+            list(self.repo.get_table_rows(cr.table, [0])), [["1", "e", "w"]]
+        )
+        dr = self.repo.diff(cr.sum, commit1.sum)
+        self.assertTrue(dr.primary_key == dr.old_primary_key)
+        self.assertGreater(len(dr.row_diff), 0)
+
+    @use_vcr
+    def test_diff_reader(self):
+        with self.commit("test_diff_reader", "initial commit", ["a"]) as writer:
+            writer.writerow(["a", "b", "c", "d"])
+            writer.writerow(["1", "q", "w", "e"])
+            writer.writerow(["2", "a", "s", "d"])
+            writer.writerow(["3", "z", "x", "c"])
+
+        with self.commit("test_diff_reader", "second commit", ["a"]) as writer:
+            writer.writerow(["a", "b", "c", "e"])
+            writer.writerow(["1", "q", "u", "r"])
+            writer.writerow(["2", "a", "s", "f"])
+            writer.writerow(["4", "y", "u", "i"])
+
+        com_tree = self.repo.get_commit_tree("heads/test_diff_reader", 2)
+        dr = self.repo.diff_reader(com_tree.sum, com_tree.root.parents[0])
+        self.assertEqual(
+            dr.column_changes,
+            ColumnChanges(
+                new_values=["a", "b", "c", "e"],
+                old_values=["a", "b", "c", "d"],
+                unchanged=set(["a", "b", "c"]),
+                added=set(["e"]),
+                removed=set(["d"]),
+            ),
+        )
+        self.assertEqual(
+            dr.pk_changes,
+            ColumnChanges(
+                new_values=["a"],
+                old_values=["a"],
+                unchanged=set(["a"]),
+                added=set([]),
+                removed=set([]),
+            ),
+        )
+        self.assertEqual(len(dr.added_rows), 1)
+        self.assertEqual(dr.added_rows.columns, ["a", "b", "c", "e"])
+        self.assertEqual(dr.added_rows.primary_key, ["a"])
+        self.assertEqual(list(dr.added_rows), [["4", "y", "u", "i"]])
+        self.assertEqual(len(dr.removed_rows), 1)
+        self.assertEqual(dr.removed_rows.columns, ["a", "b", "c", "d"])
+        self.assertEqual(dr.removed_rows.primary_key, ["a"])
+        self.assertEqual(list(dr.removed_rows), [["3", "z", "x", "c"]])
+        self.assertEqual(len(dr.modified_rows), 2)
+        self.assertEqual(dr.modified_rows.columns, ["a", "b", "c", "d", "e"])
+        self.assertEqual(dr.modified_rows.primary_key, ["a"])
+        self.assertEqual(
+            list(dr.modified_rows),
+            [
+                [("1", "1"), ("q", "q"), ("u", "w"), (None, "e"), ("r", None)],
+                [("2", "2"), ("a", "a"), ("s", "s"), (None, "d"), ("f", None)],
+            ],
+        )
+        self.assertIsNotNone(dr.data_profile)
+        self.assertEqual(dr.data_profile.old_rows_count, 3)
+        self.assertEqual(dr.data_profile.new_rows_count, 3)
+        self.assertEqual(len(dr.data_profile.columns), 5)
+
+    @use_vcr
+    def test_diff_reader_no_changes(self):
+        with self.commit(
+            "test_diff_reader_no_changes", "initial commit", ["a"]
+        ) as writer:
+            writer.writerow(["a", "b", "c", "d"])
+            writer.writerow(["1", "q", "w", "e"])
+            writer.writerow(["2", "a", "s", "d"])
+            writer.writerow(["3", "z", "x", "c"])
+
+        with self.commit(
+            "test_diff_reader_no_changes", "second commit", ["a"]
+        ) as writer:
+            writer.writerow(["a", "b", "c", "d"])
+            writer.writerow(["1", "q", "w", "e"])
+            writer.writerow(["2", "a", "s", "d"])
+            writer.writerow(["3", "z", "x", "c"])
+
+        com_tree = self.repo.get_commit_tree("heads/test_diff_reader_no_changes", 2)
+        dr = self.repo.diff_reader(com_tree.sum, com_tree.root.parents[0])
+        self.assertEqual(
+            dr.column_changes,
+            ColumnChanges(
+                new_values=["a", "b", "c", "d"],
+                old_values=["a", "b", "c", "d"],
+                unchanged=set(["a", "b", "c", "d"]),
+                added=set([]),
+                removed=set([]),
+            ),
+        )
+        self.assertEqual(
+            dr.pk_changes,
+            ColumnChanges(
+                new_values=["a"],
+                old_values=["a"],
+                unchanged=set(["a"]),
+                added=set([]),
+                removed=set([]),
+            ),
+        )
+        self.assertIsNone(dr.added_rows)
+        self.assertIsNone(dr.removed_rows)
+        self.assertIsNone(dr.modified_rows)
---
+++ packages/wrgl/wrgl-0.13.4-py3-none-any.whl/wrgl/serialize.py
@@ -0,0 +1,153 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright © 2022 Wrangle Ltd
+
+import json
+import re
+import typing
+
+import attr
+
+
+def to_camel_case(s):
+    components = s.split("_")
+    return components[0] + "".join(x.title() for x in components[1:])
+
+
+word_break_pattern = re.compile(r"([a-z])([A-Z])")
+
+
+def to_snake_case(s):
+    return word_break_pattern.sub(r"\1_\2", s).lower()
+
+
+def none_field_transformer(cls, fields):
+    return [
+        field if field.default != attr.NOTHING else field.evolve(default=None)
+        for field in fields
+    ]
+
+
+def field_transformer(namespace):
+    def transform(cls, fields):
+        results = []
+        attr.resolve_types(
+            cls, globalns=namespace, localns={cls.__name__: cls}, attribs=fields
+        )
+        for field in fields:
+            original_type = getattr(field.type, "__origin__", None)
+            if type(field.type) is str and field.type == cls.__name__:
+                field = field.evolve(type=cls)
+            if type(field.type) is type:
+                validator = attr.validators.instance_of(field.type)
+            elif original_type is list or original_type is typing.List:
+                validator = attr.validators.deep_iterable(
+                    member_validator=attr.validators.instance_of(field.type.__args__[0])
+                )
+            elif original_type is dict or original_type is typing.Dict:
+                validator = attr.validators.deep_mapping(
+                    key_validator=attr.validators.instance_of(field.type.__args__[0]),
+                    value_validator=attr.validators.instance_of(field.type.__args__[1]),
+                )
+            else:
+                raise TypeError(
+                    "unanticipated type %s (%s)"
+                    % (field.type, getattr(field.type, "__origin__"))
+                )
+            validator = attr.validators.optional(validator)
+            validator = (
+                validator
+                if field.validator is None
+                else attr.validators.and_(field.validator, validator)
+            )
+            results.append(field.evolve(default=None, validator=validator))
+        return results
+
+    return transform
+
+
+class _JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if attr.has(obj.__class__):
+            fields = attr.fields(obj.__class__)
+            return {
+                to_camel_case(field.name): getattr(obj, field.name)
+                for field in fields
+                if getattr(obj, field.name) is not None
+            }
+        return super().default(obj)
+
+
+def json_dumps(
+    inst,
+    skipkeys=False,
+    ensure_ascii=True,
+    check_circular=True,
+    allow_nan=True,
+    indent=None,
+    separators=None,
+    default=None,
+    sort_keys=False,
+    **kwargs
+):
+    return json.dumps(
+        inst,
+        skipkeys=skipkeys,
+        ensure_ascii=ensure_ascii,
+        check_circular=check_circular,
+        allow_nan=allow_nan,
+        cls=_JSONEncoder,
+        indent=indent,
+        separators=separators,
+        default=default,
+        sort_keys=sort_keys,
+        **kwargs
+    )
+
+
+def json_loads(s, serializer_cls):
+    data = json.loads(s)
+    return _deserialize(data, serializer_cls)
+
+
+def _deserialize(data, serializer_cls):
+    kwargs = dict()
+    fields_dict = attr.fields_dict(serializer_cls)
+    data = [(to_snake_case(k), v) for k, v in data.items()]
+    data_stack = [
+        (kwargs, k, fields_dict[k], v)
+        for k, v in data
+        if k in fields_dict and k != "meta"
+    ]
+    while len(data_stack) > 0:
+        parent, name, field, value = data_stack.pop()
+        if value is None:
+            parent[name] = None
+            continue
+        if type(field.type) is type:
+            if attr.has(field.type):
+                parent[name] = _deserialize(value, field.type)
+            else:
+                parent[name] = value
+        elif field.type.__origin__ is list or field.type.__origin__ is typing.List:
+            el_cls = field.type.__args__[0]
+            if type(value) is not list:
+                raise TypeError(
+                    'keyword argument "%s" should be a list of %s' % (name, el_cls)
+                )
+            if attr.has(el_cls):
+                parent[name] = [_deserialize(e, el_cls) for e in value]
+            else:
+                parent[name] = value
+        elif field.type.__origin__ is dict or field.type.__origin__ is typing.Dict:
+            el_cls = field.type.__args__[1]
+            if type(value) is not dict:
+                raise TypeError(
+                    'keyword argument "%s" should be a dict of %s' % (name, el_cls)
+                )
+            if attr.has(el_cls):
+                parent[name] = {k: _deserialize(e, el_cls) for k, e in value.items()}
+            else:
+                parent[name] = value
+        else:
+            raise TypeError("unanticipated field type %s" % field.type)
+    return serializer_cls(**kwargs)
