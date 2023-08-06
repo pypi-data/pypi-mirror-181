884e26b391e300402826f5d56bf5c361bec6b4c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:50 +0000

    Add package pyearcal/2022.0.0/pyearcal-2022.0.0-py3-none-any.whl

---
+++ packages/pyearcal/pyearcal-2022.0.0-py3-none-any.whl/pyearcal/l10n/__init__.py
@@ -0,0 +1,18 @@
+from .default import DefaultLocale
+
+from .czech import CzechLocale
+from .slovak import SlovakLocale
+from .italian import ItalianLocale
+
+
+def get_locale(locale):
+    if locale == "en":
+        return DefaultLocale()
+    elif locale == "cs":
+        return CzechLocale()
+    elif locale == "sk":
+        return SlovakLocale()
+    elif locale == "it":
+        return ItalianLocale()
+    else:
+        raise ValueError("Unknown locale")
