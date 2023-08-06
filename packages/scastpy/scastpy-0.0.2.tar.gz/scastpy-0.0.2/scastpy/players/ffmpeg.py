3ebdd65f7488cf354a396ecf35aff8f43558e48e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:35 +0000

    Add package scastpy/0.0.2/scastpy-0.0.2.tar.gz

---
+++ packages/scastpy/scastpy-0.0.2.tar.gz/scastpy-0.0.2/scastpy/players/ffmpeg.py
@@ -0,0 +1,84 @@
+import subprocess
+import os
+import threading
+
+from urllib.parse import urlparse
+from scastpy.utils.logging import logger
+from scastpy.players.base import Player
+
+
+class FFMpeg(Player):
+    name = 'ffmpeg'
+
+    uri = None
+    filename = None
+    output_directory = None
+
+    def __init__(self, output_directory='.', *args, **kwargs):
+        try:
+            subprocess.Popen(['ffmpeg', '-version'], stderr=subprocess.DEVNULL,
+                             stdout=subprocess.DEVNULL)
+        except Exception as e:
+            logger.error('ffmpeg command not found, this player maybe not work')
+            logger.debug(e)
+
+        super().__init__(*args, **kwargs)
+        self.output_directory = output_directory
+
+    def stop(self):
+        pass
+
+    def pause(self):
+        pass
+
+    def get_volume(self):
+        return 0
+
+    def set_volume(self, volume):
+        pass
+
+    def get_transport_info(self):
+        pass
+
+    def get_position_info(self):
+        pass
+
+    def set_uri(self, uri):
+        self.uri = uri
+        self.filename = os.path.basename(urlparse(uri).path)
+        logger.info('ffmpeg: {}'.format(uri))
+
+    def play(self):
+        logger.info('ffmpeg: starting download video ...')
+        if self.uri is None or self.filename is None:
+            raise Exception('media uri or output filename not be set')
+
+        # download m3u8 as mp4 file
+        _, ext = os.path.splitext(self.filename)
+        if ext == '.m3u8' or not ext:
+            self.filename += '.mp4'
+
+        output_path = os.path.join(self.output_directory, self.filename)
+
+        # rename if output file exists
+        index = 1
+        while True:
+            if not os.path.exists(output_path):
+                break
+            fn, ext = os.path.splitext(output_path)
+            output_path = fn + '-' + str(index) + ext
+            index += 1
+
+        command = ['ffmpeg', '-i', self.uri, '-c', 'copy', output_path]
+
+        def run():
+            logger.info('ffmpeg: execute command "{}"'.format(' '.join(command)))
+            popen = subprocess.Popen(command,
+                                     stdout=subprocess.PIPE, universal_newlines=True)
+            for stdout_line in iter(popen.stdout.readline, ""):
+                logger.info('ffmpeg: {}'.format(stdout_line))
+
+            popen.stdout.close()
+            popen.wait()
+
+        threading.Thread(target=run).start()
---
+++ packages/scastpy/scastpy-0.0.2.tar.gz/scastpy-0.0.2/scastpy/utils/templates.py
@@ -0,0 +1,47 @@
+import os
+
+from scastpy import __version__
+
+BASE_DIR = os.path.join(os.path.dirname(__file__), '..')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+with open(os.path.join(TEMPLATE_DIR, 'description.xml'), 'r') as f:
+    DESC_TEMPLATE = f.read()
+
+
+SSDP_NOTIFY_TEMPLATE = '''NOTIFY * HTTP/1.1\r
+HOST: 239.255.255.250:1900\r
+CACHE-CONTROL: max-age=66\r
+LOCATION: {location}\r
+OPT: "http://schemas.upnp.org/upnp/1/0/"; ns=01\r
+NT: {st}\r
+NTS: ssdp:alive\r
+SERVER: ScastPy/{VERSION} HTTP/1.0\r
+X-User-Agent: ssdpy\r
+USN: {usn}\r
+\r
+'''.replace('{VERSION}', __version__)
+
+
+SSDP_RESPONSE_TEMPLATE = '''HTTP/1.1 200 OK\r
+CACHE-CONTROL: max-age=66\r
+DATE: {{DATE}}\r
+EXT:\r
+LOCATION: {location}\r
+OPT: "http://schemas.upnp.org/upnp/1/0/"; ns=01\r
+SERVER: ScastPy/{VERSION} HTTP/1.0\r
+X-User-Agent: scastpy\r
+ST: {st}\r
+USN: {usn}\r
+\r
+'''.replace('{VERSION}', __version__)
+
+RESPONSE_TEMPLATE = '''<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/"
+  s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+  <s:Body>
+    <u:{ACTION}Response xmlns:u="{NS}">
+        {RESPONSE}
+    </u:{ACTION}Response>
+  </s:Body>
+</s:Envelope>'''
+
