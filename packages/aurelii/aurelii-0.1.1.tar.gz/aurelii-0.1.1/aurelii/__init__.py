90bc471d07d8ee1ae38a14c93b20cd4f8eb51236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:59 +0000

    Add package aurelii/0.1.1/aurelii-0.1.1.tar.gz

---
+++ packages/aurelii/aurelii-0.1.1.tar.gz/aurelii-0.1.1/aurelii/__init__.py
@@ -0,0 +1,7 @@
+def readme():
+    with open('README.rst', 'r') as f:
+        return f.read()
+
+def info():
+    '''Prints info for Aurelii package'''
+    print("This is the Python package for Aurelii.")
