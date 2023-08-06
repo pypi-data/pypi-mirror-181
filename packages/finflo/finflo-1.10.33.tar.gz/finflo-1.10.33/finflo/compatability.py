a158719906ffa48807b4059b636407b91ba59900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:09 +0000

    Add package finflo/1.10.33/finflo-1.10.33.tar.gz

---
+++ packages/finflo/finflo-1.10.33.tar.gz/finflo-1.10.33/finflo/compatability.py
@@ -0,0 +1,13 @@
+import django
+from .exception import VersionError 
+
+
+def check_version_compatibility():
+    if django.VERSION >= (3, 0):
+        pass
+    else:
+        raise VersionError("finflo works with django 3.0 and higher , please upgrade to the latest version \
+        of django")
+    return check_version_compatibility
+
+
