8eee086639e21693d622824fc9b5e6406acad3bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:19:30 +0000

    Add package audio-diffusion-pytorch/0.0.97/audio-diffusion-pytorch-0.0.97.tar.gz

---
+++ packages/audio-diffusion-pytorch/audio-diffusion-pytorch-0.0.97.tar.gz/audio-diffusion-pytorch-0.0.97/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="audio-diffusion-pytorch",
+    packages=find_packages(exclude=[]),
+    version="0.0.97",
+    license="MIT",
+    description="Audio Diffusion - PyTorch",
+    long_description_content_type="text/markdown",
+    author="Flavio Schneider",
+    author_email="archinetai@protonmail.com",
+    url="https://github.com/archinetai/audio-diffusion-pytorch",
+    keywords=["artificial intelligence", "deep learning", "audio generation"],
+    install_requires=[
+        "tqdm",
+        "torch>=1.6",
+        "data-science-types>=0.2",
+        "einops>=0.4",
+        "einops-exts>=0.0.3",
+        "audio-encoders-pytorch",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
