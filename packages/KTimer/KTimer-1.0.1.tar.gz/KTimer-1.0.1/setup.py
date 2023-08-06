2a0a263bdbeb523b6229bb13cf456cce39969862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:57 +0000

    Add package KTimer/1.0.1/KTimer-1.0.1.tar.gz

---
+++ packages/KTimer/KTimer-1.0.1.tar.gz/KTimer-1.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="KTimer",
+    version="1.0.1",
+    author="Osman Onur KUZUCU",
+    author_email="kuzucu48@gmail.com",
+    description="Scheduled event driven library for Python Coders",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    company="KZC Software Inc.",
+    url="",
+    packages=["KTimer"],
+    install_requires=['ksubscribe'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
