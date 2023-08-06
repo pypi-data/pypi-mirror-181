ff5f683b66ce16ce987663e26ff4fe99e5121bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:40 +0000

    Add package dirlib/1.0.0/dirlib-1.0.0.tar.gz

---
+++ packages/dirlib/dirlib-1.0.0.tar.gz/dirlib-1.0.0/dirlib/config.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from pathlib import Path
+
+def user_config_dir(platform:str = sys.platform):
+    dir = ""
+    match platform:
+        case "win32":
+            dir = os.getenv("AppData")
+            if dir is None:
+                dir = os.getenv("UserProfile")
+                if dir is None:
+                    raise 'neither %AppData% nor %UserProfile% are defined'
+            return dir
+
+        case "darwin":
+            dir = os.getenv("HOME")
+            if dir is None:
+                raise '$HOME is not defined'
+            dir = Path(dir)
+            dir.joinpath("/Libary/Application Support")
+            return str(dir)
+
+        case "linux":
+            dir = os.getenv("XDG_CONFIG_HOME")
+            if dir is None:
+                dir = os.getenv("HOME")
+                if dir is None:
+                    raise 'neither $XDG_CONFIG_HOME nor $HOME are defined'
+                dir = Path(dir)
+                dir.joinpath("./config")
+            return str(dir)
+
+        case _:
+            raise "Your platform is not supproted. Currently, dirlib supports Windows, Unix and macOS"
