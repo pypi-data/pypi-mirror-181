d544015cf18bb702df8e625183f7546fb55acf1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:16 +0000

    Add package distri-package/1.11/distri_package-1.11.tar.gz

---
+++ packages/distri-package/distri_package-1.11.tar.gz/distri_package-1.11/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='distri_package',
+      version='1.11',
+      description='Gaussian and Binomial distributions',
+      packages=['distri_package'],
+      readme="README.md",
+      long_description="""The purpose of this package is to help users analyze Binomial and Gaussian distributions. \n
+            Install \n
+            To pip install type: \n
+            `pip install distri_pack` \n \n
+
+            Files \n
+            Binomialdistribution.py - Binomial distribution class for calculating and visualizing a Binomial distribution. \n
+            Gaussiandistribution.py - Gaussian distribution class for calculating and visualizing a Gaussian distribution. \n
+            Generaldistribution.py - Generic distribution class for calculating and visualizing a probability distribution. \n""",
+      long_description_content_type='text/markdown',
+      zip_safe=False)
+
+    
