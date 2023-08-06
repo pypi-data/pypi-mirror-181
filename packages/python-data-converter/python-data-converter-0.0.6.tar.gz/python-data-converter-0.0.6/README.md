a8095bd4a3e9ad6638b289368869fc11dbce0788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:15 +0000

    Add package python-data-converter/0.0.6/python-data-converter-0.0.6.tar.gz

---
+++ packages/python-data-converter/python-data-converter-0.0.6.tar.gz/python-data-converter-0.0.6/README.md
@@ -0,0 +1,76 @@
+# [Python Data Converter](https://github.com/app-generator/devtool-python-converter)
+
+**Converter library** for `CSV`, `OpenAPI`, `Pandas DF`, `URLs` using a simple **Drag & Drop UI** - provided by [AppSeed](https://appseed.us/).
+
+> [EULA License](./LICENSE.md): 
+
+- **Free** for `students`, `solo-developers` (personal use), `NGOs`, and eLearning activities
+- [Unrestricted usage](./LICENSE.md#lifetime-license) allowed via `one-time payment` - [$299](https://appseed.gumroad.com/l/devtool-python-converter) (managed by GUMROAD) 
+
+<br />
+
+## Video Presentation 
+
+The material explains how to use this conversion tool written in Python to manipulate and convert information into different formats. The tool uses a simple UI able to convert OpenAPI descriptors, CSV, and DataFrames into Python Models definition (allows editing), Data tables, and charts. 
+
+<br />
+
+[![Python Data Converter - Converter library for CSV, OpenAPI, Pandas DF, URLs using a simple Drag & Drop UI - EULA license.](https://user-images.githubusercontent.com/51070104/207289612-000891c6-7c4d-487c-9599-7aac96928f0b.jpg)](https://www.youtube.com/watch?v=87qvYSvjGOk)
+
+<br />
+
+### `Product Roadmap`
+
+| Status | Delivery | Input | Output | Info | 
+| --- | --- | --- | --- | --- |
+| DONE | **Drop 1** | **OpenAPI** `JSON` | `DB Model` | DB Model(s) extraction |
+| NA | Drop 2 | **OpenAPI** `Yaml` | `DB Model` | DB Model(s) extraction |
+| NA | `Drop 2` | `Swagger UI` URL | `DB Model` | DB Model(s) extraction |
+| --- | --- | --- | --- | --- |
+| DONE | **Drop 1** | **CSV** | `DB Model` | DB Model |
+| DONE | **Drop 1** | **CSV** | `DataTables` | Paginated Data View (vanilla JS) |
+| DONE | **Drop 1** | **CSV** | `Charts` | Data to visualisation  |
+| NA | **Drop 1** | **CSV** | `Export` (with filters) | CSV, PDF  |
+| NA | `Drop 2` | **CSV** URL | `DB Model` | DB Model |
+| NA | `Drop 2` | **CSV** URL | `DataTables` | Paginated Data View (vanilla JS) |
+| NA | `Drop 2` | **CSV** URL | `Charts` | Data to visualisation  |
+| NA | `Drop 2` | **CSV** URL | `Export` (with filters) | CSV, PDF  |
+| --- | --- | --- | --- | --- |
+| DONE | **Drop 1** | **Pandas DF** | `DB Model` | DB Model |
+| DONE | **Drop 1** | **Pandas DF** | `DataTables` | Paginated Data View (vanilla JS) |
+| DONE | **Drop 1** | **Pandas DF** | `Charts` | Data to visualisation  |
+| NA | **Drop 1** | **Pandas DF** | `Export` (with filters) | CSV, PDF  |
+| --- | --- | --- | --- | --- |
+| NA | `Drop 2` | **DBMS** `Remote URL` | `DB Model` | Models Introspection |
+| NA | `Drop 2` | **DBMS** `Remote URL` | `DataTables` | Paginated Data View (vanilla JS) |
+| NA | `Drop 2` | **DBMS** `Remote URL` | `Charts` | Data to visualisation  |
+| NA | `Drop 2` | **DBMS** `Remote URL` | `Export` (with filters) | CSV, PDF  |
+
+<br />
+
+## Build from sources
+
+```bash
+$ # Clone the sources
+$ git clone https://github.com/app-generator/devtool-python-converter.git
+$ cd devtool-python-converter
+$
+$ # Virtualenv modules installation (Unix based systems)
+$ virtualenv env
+$ source env/bin/activate
+$
+$ # Install requirements
+$ pip3 install -r requirements.txt
+$
+$ # Set the FLASK_APP environment variable
+$ (Unix/Mac) export FLASK_APP=run.py
+$ (Windows) set FLASK_APP=run.py
+$ (Powershell) $env:FLASK_APP = ".\run.py"
+$
+$ flask run 
+```
+
+<br />
+
+---
+[Python Data Converter](https://github.com/app-generator/devtool-python-converter) - Tool provided by [AppSeed](https://appseed.us).
---
+++ packages/python-data-converter/python-data-converter-0.0.6.tar.gz/python-data-converter-0.0.6/app/views.py
@@ -0,0 +1,163 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+import json
+import math
+import os
+import sys
+import logging
+# Flask modules
+from jinja2 import TemplateNotFound
+from flask import render_template, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+# App modules
+from app import app, ALLOWED_EXTENSIONS
+from py_data_converter import *
+from py_data_converter.common import get_flask_model, get_django_model
+from py_data_converter.converter_csv import convert_csv_to_django_models, convert_csv_to_flask_models
+from py_data_converter.converter_openapi import convert_openapi_json_to_django_models, \
+    convert_openapi_json_to_flask_models, convert_openapi_yaml_to_django_models, convert_openapi_yaml_to_flask_models
+from py_data_converter.converter_pandas import convert_pandas_to_flask_models, convert_pandas_to_django_models
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def get_type(filename):
+    return filename.rsplit('.', 1)[1].lower()
+
+
+# App main route + generic
+# routing
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+
+        # front
+
+        data = request.form
+        post_type = data['type']
+        if post_type == 'file':
+            # check if the post request has the file part
+            file = request.files['file']
+            # If the user does not select a file, the browser submits an
+            # empty file without a filename.
+            if file.filename == '':
+                flash('No selected file')
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                output_desired = data['output']
+                # front
+                flask_response = ''
+                django_response = ''
+                input_type = get_type(filename)
+                print(output_desired)
+                if output_desired == 'Flask':
+                    if input_type == 'csv':
+                        flask_response = convert_csv_to_flask_models(app.config['UPLOAD_FOLDER'], filename)
+                    elif input_type == 'json':
+                        flask_response = convert_openapi_json_to_flask_models(app.config['UPLOAD_FOLDER'], filename)
+                    elif input_type == 'yaml':
+                        flask_response = convert_openapi_yaml_to_flask_models(app.config['UPLOAD_FOLDER'], filename)
+                        os.remove(app.config['UPLOAD_FOLDER'] + "\\" + 'converted_to_json.json')
+
+                    elif input_type == 'pkl':
+                        flask_response = convert_pandas_to_flask_models(app.config['UPLOAD_FOLDER'], filename)
+                        os.remove(app.config['UPLOAD_FOLDER'] + "\\" + filename[:-4] + '.csv')
+                    data = {'flask': flask_response}
+                    os.remove(app.config['UPLOAD_FOLDER'] + "\\" + filename)
+
+                    return data
+                elif output_desired == 'Django':
+                    if input_type == 'csv':
+                        django_response = convert_csv_to_django_models(app.config['UPLOAD_FOLDER'], filename)
+                    elif input_type == 'json':
+                        django_response = convert_openapi_json_to_django_models(app.config['UPLOAD_FOLDER'], filename)
+                    elif input_type == 'yaml':
+                        django_response = convert_openapi_yaml_to_django_models(app.config['UPLOAD_FOLDER'], filename)
+                        os.remove(app.config['UPLOAD_FOLDER'] + "\\" + 'converted_to_json.json')
+
+                    elif input_type == 'pkl':
+                        django_response = convert_pandas_to_flask_models(app.config['UPLOAD_FOLDER'], filename)
+                        os.remove(app.config['UPLOAD_FOLDER'] + "\\" + filename[:-4] + '.csv')
+                    data = {'django': django_response}
+                    os.remove(app.config['UPLOAD_FOLDER'] + "\\" + filename)
+                    return data
+                elif output_desired == 'DataTable':
+                    if input_type == 'csv':
+                        csv_file = pd.read_csv(app.config['UPLOAD_FOLDER'] + "\\" + filename)
+                    elif input_type == 'pkl':
+                        csv_file = pd.read_pickle(app.config['UPLOAD_FOLDER'] + "\\" + filename)
+                    else:
+                        flash('input file is not supported!')
+                        os.remove(app.config['UPLOAD_FOLDER'] + "\\" + filename)
+                        return redirect(request.url)
+                    os.remove(app.config['UPLOAD_FOLDER'] + "\\" + filename)
+                    headings = [row for row in csv_file.head()]
+                    return render_template('datatb/datatb.html', **{
+                        'model_name': 'model_name',
+                        'headings': headings,
+                        'data': [[val for val in record[1]] for record in csv_file.iterrows()],
+                    })
+                else:
+                    if input_type == 'csv':
+                        django_response = convert_csv_to_django_models(app.config['UPLOAD_FOLDER'], filename)
+                        flask_response = convert_csv_to_flask_models(app.config['UPLOAD_FOLDER'], filename)
+                    elif input_type == 'json':
+                        django_response = convert_openapi_json_to_django_models(app.config['UPLOAD_FOLDER'],
+                                                                                filename)
+                        flask_response = convert_openapi_json_to_flask_models(app.config['UPLOAD_FOLDER'], filename)
+                    elif input_type == 'yaml':
+                        django_response = convert_openapi_yaml_to_django_models(app.config['UPLOAD_FOLDER'],
+                                                                                filename)
+                        flask_response = convert_openapi_yaml_to_flask_models(app.config['UPLOAD_FOLDER'], filename)
+                        os.remove(app.config['UPLOAD_FOLDER'] + "\\" + 'converted_to_json.json')
+                    elif input_type == 'pkl':
+                        flask_response = convert_pandas_to_flask_models(app.config['UPLOAD_FOLDER'], filename)
+                        django_response = convert_pandas_to_django_models(app.config['UPLOAD_FOLDER'], filename)
+                        os.remove(app.config['UPLOAD_FOLDER'] + "\\" + filename[:-4] + '.csv')
+                    os.remove(app.config['UPLOAD_FOLDER'] + "\\" + filename)
+                    data = {'django': django_response, 'flask': flask_response}
+                    return data
+
+    elif request.method == 'GET':
+        # front
+        return render_template('converter/index.html')
+
+
+import pandas as pd
+
+
+@app.route('/datatb', methods=['POST'])
+def dynamic_datatb():
+    file = request.files['file']
+    # If the user does not select a file, the browser submits an
+    # empty file without a filename.
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        input_type = get_type(filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        if input_type == 'csv':
+            csv_file = pd.read_csv(app.config['UPLOAD_FOLDER'] + "\\" + filename)
+        elif input_type == 'pkl':
+            csv_file = pd.read_pickle(app.config['UPLOAD_FOLDER'] + "\\" + filename)
+        else:
+            flash('input file is not supported!')
+            os.remove(app.config['UPLOAD_FOLDER'] + "\\" + filename)
+            return redirect(request.url)
+        os.remove(app.config['UPLOAD_FOLDER'] + "\\" + filename)
+        headings = [row for row in csv_file.head()]
+        return render_template('datatb/datatb.html', **{
+            'model_name': 'model_name',
+            'headings': headings,
+            'data': [[val for val in record[1]] for record in csv_file.iterrows()],
+        })
