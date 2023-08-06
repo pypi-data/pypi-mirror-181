78d7e2591b9077c21ab9eabdcb47c9b11d7ff879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:56 +0000

    Add package heframework/0.2.11/heframework-0.2.11.tar.gz

---
+++ packages/heframework/heframework-0.2.11.tar.gz/heframework-0.2.11/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='heframework',
+      version='0.2.11',
+      description='本次更新：修复show在不传入标题无法运行的问题',
+      author='heStudio',
+      author_email='hestudio@hestudio.org',
+      url='https://gitee.com/hestudio-framework/main-windows/',
+      packages=['src',""],
+     )
+
---
+++ packages/heframework/heframework-0.2.11.tar.gz/heframework-0.2.11/src/list.py
@@ -0,0 +1,34 @@
+# heStudio Framework List
+
+import json
+
+
+class list:
+    def __init__(self, json_file, info):
+        self.code = None
+        if not json_file:
+            self.code = 25000
+            self.error = 25002
+        else:
+            self.file = json_file
+            if not info:
+                self.code = 25000
+                self.error = 25001
+            else:
+                self.info = info
+
+    def list(self):
+        if self.code == 25000:
+            # # 该代码仅供测试使用
+            # print(self.error)
+            return self.error
+        else:
+            db = json.load(open(self.file, encoding="utf-8"))
+            num = 0
+            for name in db["name"]:
+                num += 1
+                print(str(num), ":", name)
+            choose = input(str(self.info))
+            commands = db["command"]
+            exec(commands[int(int(choose)-1)])
+            return 20000
