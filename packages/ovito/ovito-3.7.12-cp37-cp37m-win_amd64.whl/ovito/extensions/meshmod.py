ce8fd52c8acbbd424fa320a1ad4d27b335452886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:25 +0000

    Add package ovito/3.7.12/ovito-3.7.12-cp37-cp37m-win_amd64.whl

---
+++ packages/ovito/ovito-3.7.12-cp37-cp37m-win_amd64.whl/ovito/extensions/meshmod.py
@@ -0,0 +1,8 @@
+# Load dependencies.
+import ovito.extensions.pyscript
+import ovito.extensions.stdmod
+import ovito.extensions.stdobj
+import ovito.extensions.mesh
+
+# Load the C extension module.
+import ovito.plugins.MeshModPython
