3a49eb3fcc9b131ea5751edba4b0cd9eb477d79e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:07 +0000

    Add package unavoids/1.0/unavoids-1.0.tar.gz

---
+++ packages/unavoids/unavoids-1.0.tar.gz/unavoids-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='unavoids',
+    version='1.0',    
+    description='UNAVOIDS: Unsupervised and Nonparametric Approach for Visualizing Outliers and Invariant Detection Scoring',
+    url='https://github.com/isotlaboratory/UNAVOIDS-Code',
+    author='Yousef, Waleed A. and Traoré, Issa and Briguglio, William',
+    author_email='wyousef@uvic.ca',
+    license='GNU GENERAL PUBLIC LICENSE',
+    packages=['unavoids'],
+    install_requires=[],
+
+    classifiers=[
+    ],
+)
