af901ede80b4ec9cc8b3a049a3900b1a200ea848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:20:47 +0000

    Add package vandelay/0.1/vandelay-0.1.tar.gz

---
+++ packages/vandelay/vandelay-0.1.tar.gz/vandelay-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='vandelay',
+      version='0.1',
+      description='vandelay',
+      author='Hao Wu',
+      license='MIT',
+      packages=['vandelay'],
+      zip_safe=False)
+
