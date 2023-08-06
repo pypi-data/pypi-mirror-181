0aec455fdac6f46c50dac9d6ff5f25972ac0259e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:25 +0000

    Add package texplotlibx/0.9.3/texplotlibx-0.9.3-py3-none-any.whl

---
+++ packages/texplotlibx/texplotlibx-0.9.3-py3-none-any.whl/texplotlibx/curve_data.py
@@ -0,0 +1,45 @@
+from collections.abc import Iterable, Callable
+from .attribute import _tpl_attribute_function, _tpl_attribute_variable
+from .curve_label import _tpl_curve_label
+
+class _tpl_curve_data:
+  __counter: int = 0
+
+  @staticmethod
+  def __sort(x: Iterable, y: Iterable, xerr: Iterable = None, yerr: Iterable = None):
+    def __subsort(m: Iterable):
+      return zip(*sorted(zip(*m)))
+    if xerr is None and yerr is None:
+      x, y = __subsort((x, y))
+    elif xerr is None and yerr is not None:
+      x, y, yerr = __subsort((x, y, yerr))
+    elif xerr is not None and yerr is None:
+      x, y, xerr = __subsort((x, y, xerr))
+    else:
+      x, y, xerr, yerr = __subsort((x, y, xerr, yerr))
+    return x, y, xerr, yerr
+
+  def __init__(self, x: Iterable, y: Iterable, xerror: Iterable = None, yerror: Iterable = None,
+               label: str = None, color: str = None, opacity: str = None, yfunc: Callable = None,
+               sortdata: bool = True, mark: str = None):
+    if yfunc is not None:
+      y = map(yfunc, y)
+      if yerror is not None:
+        yerror = map(yfunc, yerror)
+    self.x = x
+    self.y = y
+    self.xerror = xerror
+    self.yerror = yerror
+    self.mark = _tpl_attribute_variable(token="mark", value=mark)
+
+    if sortdata:
+      self.x, self.y, self.xerror, self.yerror = self.__class__.__sort(self.x, self.y, self.xerror, self.yerror)
+
+    self.label = _tpl_curve_label(
+        token="addlegendentry", value=label,
+        suppress_semicolon=True
+    )
+    self.tablename = "table%d" % self.__class__.__counter
+    self.__class__.__counter += 1
+    self.color = _tpl_attribute_variable(token="color", value=color)
+    self.opacity = _tpl_attribute_variable(token="opacity", value=opacity)
---
+++ packages/texplotlibx/texplotlibx-0.9.3-py3-none-any.whl/texplotlibx/legend.py
@@ -0,0 +1,25 @@
+from .attribute import _tpl_attribute_variable
+
+
+class _tpl_legend:
+  def __init__(self):
+    self.pos = _tpl_attribute_variable(token="legend pos")
+
+  @staticmethod
+  def __expand(value: str) -> str:
+    if value == 'n':
+      return 'north'
+    if value == 's':
+      return 'south'
+    if value == 'e':
+      return 'east'
+    if value == 'w':
+      return 'west'
+    if value == 'o':
+      return 'outer'
+
+  def _set_position(self, value: str):
+    if len(value) <= 3:
+      expanded = [self.__class__.__expand(s) for s in value]
+      value = ' '.join(expanded)
+    self.pos.set_value(value)
