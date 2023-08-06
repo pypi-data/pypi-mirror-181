831b268526e91eaab6b211571070aca712f10257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:20 +0000

    Add package jupyter-security/0.0.2/jupyter_security-0.0.2.tar.gz

---
+++ packages/jupyter-security/jupyter_security-0.0.2.tar.gz/jupyter_security-0.0.2/src/index.ts
@@ -0,0 +1,85 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { MainAreaWidget, ICommandPalette } from '@jupyterlab/apputils';
+
+import { ILauncher } from '@jupyterlab/launcher';
+
+import { reactIcon } from '@jupyterlab/ui-components';
+
+import { requestAPI } from './handler';
+
+import { CounterWidget } from './widget';
+
+import '../style/index.css';
+
+/**
+ * The command IDs used by the react-widget plugin.
+ */
+namespace CommandIDs {
+  export const create = 'create-react-widget';
+}
+
+/**
+ * Initialization data for the @datalayer/jupyter-security extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '@datalayer/jupyter-security:plugin',
+  autoStart: true,
+  requires: [ICommandPalette],
+  optional: [ISettingRegistry, ILauncher],
+  activate: (
+    app: JupyterFrontEnd,
+    palette: ICommandPalette,
+    settingRegistry: ISettingRegistry | null,
+    launcher: ILauncher
+  ) => {
+    const { commands } = app;
+    const command = CommandIDs.create;
+    commands.addCommand(command, {
+      caption: 'Show Jupyter AWS',
+      label: 'Jupyter AWS',
+      icon: (args: any) => reactIcon,
+      execute: () => {
+        const content = new CounterWidget();
+        const widget = new MainAreaWidget<CounterWidget>({ content });
+        widget.title.label = 'Jupyter AWS';
+        widget.title.icon = reactIcon;
+        app.shell.add(widget, 'main');
+      }
+    });
+    const category = 'Jupyter AWS';
+    palette.addItem({ command, category, args: { origin: 'from palette' } });
+    if (launcher) {
+      launcher.add({
+        command
+      });
+    }
+    console.log('JupyterLab extension @datalayer/jupyter-security is activated!');
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('@datalayer/jupyter-security settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for @datalayer/jupyter-security.', reason);
+        });
+    }
+    requestAPI<any>('get_example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The datalayer server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
