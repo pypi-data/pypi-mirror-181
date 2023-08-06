934765347aeca36e889366399857a9b4eba700b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:27 +0000

    Add package bayes-nets-sample/0.1/bayes_nets_sample-0.1.tar.gz

---
+++ packages/bayes-nets-sample/bayes_nets_sample-0.1.tar.gz/bayes_nets_sample-0.1/tests/test_help.py
@@ -0,0 +1,12 @@
+## To Get more information about the BayesNets Class as well as its methods:
+### run the following command:
+
+import unittest
+from bayesNetsSample import BayesNets
+
+class TestHelp(unittest.TestCase):
+
+	def test_help(self):
+		help(BayesNets)
+
+unittest.main()
