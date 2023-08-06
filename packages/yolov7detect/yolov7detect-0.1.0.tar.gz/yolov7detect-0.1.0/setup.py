fa79303d0b9bcb4ef4580c99dc7eeef280c9268b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:20 +0000

    Add package yolov7detect/0.1.0/yolov7detect-0.1.0.tar.gz

---
+++ packages/yolov7detect/yolov7detect-0.1.0.tar.gz/yolov7detect-0.1.0/setup.py
@@ -0,0 +1,46 @@
+import io
+import os
+
+import setuptools
+
+
+def get_long_description():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    with io.open(os.path.join(base_dir, "README.md"), encoding="utf-8") as f:
+        return f.read()
+
+
+def get_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
+
+setuptools.setup(
+    name="yolov7detect",
+    version="0.1.0",
+    author="kadirnar",
+    license="MIT",
+    description="Packaged version of the Yolov7 repository",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kadirnar/yolov7-pip",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=get_requirements(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    keywords="machine-learning, deep-learning, pytorch, vision, image-classification, object-detection, yolov7, detector, yolov5",
+)
