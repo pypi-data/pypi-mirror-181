16b4095a94a8c4b30172b24f6b9d1579b3429d82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:49 +0000

    Add package yoakecli/0.0.2/yoakecli-0.0.2-py3-none-any.whl

---
+++ packages/yoakecli/yoakecli-0.0.2-py3-none-any.whl/yoakecli/simpleRequest.py
@@ -0,0 +1,76 @@
+import json, typing, urllib.request, urllib.error, urllib.parse
+from email.message import Message
+
+class Response(typing.NamedTuple):
+    body: str
+    headers: Message
+    status: int
+    error_count: int = 0
+
+    def json(self) -> typing.Any:
+        try:
+            output = json.loads(self.body)
+        except json.JSONDecodeError:
+            output = ""
+        return output
+
+def request(
+    url: str,
+    data: dict = None,
+    params: dict = None,
+    headers: dict = None,
+    method: str = "GET",
+    data_as_json: bool = True,
+    error_count: int = 0,
+) -> Response:
+    if not url.casefold().startswith("http"):
+        raise urllib.error.URLError("Incorrect and possibly insecure protocol in url")
+    method = method.upper()
+    request_data = None
+    headers = headers or {}
+    data = data or {}
+    params = params or {}
+    headers = {
+        "accept": "*/*",
+        'connection': 'keep-alive',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+        **headers
+    }
+
+    if method == "GET":
+        params = {**params, **data}
+        data = None
+
+    if params:
+        url += "?" + urllib.parse.urlencode(params, doseq=True, safe="/")
+
+    if data:
+        if data_as_json:
+            request_data = json.dumps(data).encode()
+            headers["Content-Type"] = "application/json; charset=UTF-8"
+        else:
+            request_data = urllib.parse.urlencode(data).encode()
+
+    httprequest = urllib.request.Request(
+        url, data=request_data, headers=headers, method=method
+    )
+
+    try:
+        with urllib.request.urlopen(httprequest) as httpresponse:
+            response = Response(
+                headers=httpresponse.headers,
+                status=httpresponse.status,
+                body=   httpresponse.read().decode('utf-8'),
+            )
+    except urllib.error.HTTPError as e:
+        response = Response(
+            body=str(e.reason),
+            headers=e.headers,
+            status=e.code,
+            error_count=error_count + 1,
+        )
+    except Exception as e:
+        print('Kết nối với server không thành công!')
+        exit()
+
+    return response
