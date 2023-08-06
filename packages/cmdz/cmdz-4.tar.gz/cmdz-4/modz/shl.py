f7f5c0636c46767026d7c22d3dd95f404180147c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:26 +0000

    Add package cmdz/4/cmdz-4.tar.gz

---
+++ packages/cmdz/cmdz-4.tar.gz/cmdz-4/modz/shl.py
@@ -0,0 +1,19 @@
+# This file is placed in the Public Domain.
+# pylint: disable=C0116,C0413,W0212,C0301,W0613
+
+
+"shell"
+
+
+import time
+
+
+from cmdz import Cfg, Command, Console, Wd, printable, setcompleter
+
+
+def shl(event):
+    setcompleter(Command.cmd)
+    date = time.ctime(time.time()).replace("  ", " ")
+    print("%s started at %s %s" % (Cfg.name.upper(), date, printable(Cfg, "console,debug,verbose,wait", plain=True)))
+    cli = Console()
+    cli.start()
