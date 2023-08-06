a9138efa7980cd55eafa60f46d8be16e0249bfef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:23 +0000

    Add package TBWW/0.3.4/TBWW-0.3.4.tar.gz

---
+++ packages/TBWW/TBWW-0.3.4.tar.gz/TBWW-0.3.4/README.md
@@ -0,0 +1,4 @@
+# TBWW
+TBWW - Telegram Bot Wrapper Wrapper, a package designed to simplify Telegram bot setup
+
+Package versions `<=0.3.0` are for Python 2. From `0.3.1` onwards, this library targets Python 3.
---
+++ packages/TBWW/TBWW-0.3.4.tar.gz/TBWW-0.3.4/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+import sys, os
+
+if sys.argv[0] == "publish" or sys.argv[1] == "publish":
+    os.system("python3 setup.py sdist")
+    os.system("twine upload dist/*")
+    sys.exit(0)
+
+setup(
+    name="TBWW",
+    version="0.3.4",
+    description="Telegram Bot Wrapper Wraper.\n\nPackage versions <=0.3.0 target Python 2. Versions >=0.3.1 target Python 3.",
+    license="GNU GPL 3.0",
+    install_requires=["python-telegram-bot"],
+    py_modules = ["tbww"]
+)
