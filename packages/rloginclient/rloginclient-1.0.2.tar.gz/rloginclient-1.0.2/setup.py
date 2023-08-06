4264e2d3b9c03a4c5aee651ff10714192e8164f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:28 +0000

    Add package rloginclient/1.0.2/rloginclient-1.0.2.tar.gz

---
+++ packages/rloginclient/rloginclient-1.0.2.tar.gz/rloginclient-1.0.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Author:zhang yechong
+
+
+from distutils.core import setup
+
+setup(
+    name='rloginclient',
+    version='1.0.2',
+    description='Rlogin客户端',
+    author='Zhang Yechong',
+    author_email='641878711@qq.com',
+    py_modules=['rlogin.rlogin_client']
+)
