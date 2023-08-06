38cce87c13756e11486d5dcbc5602ac71c19eba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:16 +0000

    Add package Person-Anirban-details/0.1/Person_Anirban_details-0.1.tar.gz

---
+++ packages/Person-Anirban-details/Person_Anirban_details-0.1.tar.gz/Person_Anirban_details-0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1'
+DESCRIPTION = 'Testing the basics of PyPi packages.'
+LONG_DESCRIPTION = 'Tried making a package and got errors. This is more of a Hello World of packages.'
+
+# Setting up
+setup(
+    name="Person_Anirban_details",
+    version=VERSION,
+    author="Anirban Saha",
+    author_email="<mailme@anirbansaha.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'Anirban Saha'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
