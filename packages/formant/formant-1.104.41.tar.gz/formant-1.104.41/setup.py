7f5053ae0a0f20918bcac7a927594cbf21c8bb5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:47 +0000

    Add package formant/1.104.41/formant-1.104.41.tar.gz

---
+++ packages/formant/formant-1.104.41.tar.gz/formant-1.104.41/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="formant",
+    version="1.104.41",
+    author="Formant",
+    author_email="eng@formant.io",
+    description="Formant python package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://formant.io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "grpcio>=1.37.0",
+        "grpcio-status>=1.37.0",
+        "protobuf>=3.15.8, <=3.19.4",
+        "typing-extensions>=3.7.4.2",
+        "requests>=2.25.1",
+        "python-lzf==0.2.4",
+        "jsonschema==3.2.0",
+    ],
+    python_requires=">=2.7",
+)
