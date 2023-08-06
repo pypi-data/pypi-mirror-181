d447f19a0b059bcdf0f4ea3360924126208a8d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:47 +0000

    Add package pyniel/0.3/pyniel-0.3.tar.gz

---
+++ packages/pyniel/pyniel-0.3.tar.gz/pyniel-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pyniel",
+    description='Python tools for Yours Truly',
+    author='Daniel Dugas',
+    version='0.3',
+    packages=find_packages(),
+)
