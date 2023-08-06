9506b04db889a3dce994bb5c953deaa49fbf71ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:43 +0000

    Add package trajectory-alignment/1.3/trajectory%20alignment-1.3.tar.gz

---
+++ packages/trajectory-alignment/trajectory%20alignment-1.3.tar.gz/trajectory alignment-1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup( name = 'trajectory alignment' ,
+		version = '1.3' ,
+		description = 'Utilities to Align and Average Trajectories' ,
+		author = 'Andrea Picco',
+		author_email = 'andrea.picco@unige.ch',
+		url = 'http://apicco.github.io/trajectory_alignment/',
+		download_url = 'https://github.com/apicco/trajectory_alignment/archive/master.zip',
+		packages = [ 'trajalign' ],
+		license = 'The software is distributed under the terms of the GNU General Public License Version 3, June 2007. Trajalign is a free software and comes with ABSOLUTELY NO WARRANTY. You are welcome to redistribute the software. However, we appreciate is use of such software would result in citations of Picco, A., Kaksonen, M., _Precise tracking of the dynamics of multiple proteins in endocytic events_,  Methods in Cell Biology, Vol. 139, pages 51-68 (2017) http://www.sciencedirect.com/science/article/pii/S0091679X16301546'
+
+		)
