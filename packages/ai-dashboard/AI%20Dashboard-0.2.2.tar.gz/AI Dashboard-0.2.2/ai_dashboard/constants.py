f20fbe74904e0e96022023b5bef8ec6242ea0230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:40 +0000

    Add package ai-dashboard/0.2.2/AI%20Dashboard-0.2.2.tar.gz

---
+++ packages/ai-dashboard/AI%20Dashboard-0.2.2.tar.gz/AI Dashboard-0.2.2/ai_dashboard/constants.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+BASE_URL = "https://api.us-east-1.relevance.ai/latest"
+
+WELCOME_MESSAGE = "Welcome to RelevanceAI AI Dashboard. Logged in as {}."
+
+ONE_MB = 2**20
+
+
+class Colours(Enum):
+    INDIGO = "INDIGO"
+    TEAL = "TEAL"
+    PINK = "PINK"
+    VIOLET = "VIOLET"
+    YELLOW = "YELLOW"
+    BLUE = "BLUE"
