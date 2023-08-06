5ad43dcb8d3c6d42d73c5edd46d7f261c59c8ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:42 +0000

    Add package lnschema_core/0.23.2/lnschema_core-0.23.2.tar.gz

---
+++ packages/lnschema_core/lnschema_core-0.23.2.tar.gz/lnschema_core-0.23.2/lnschema_core/dev/id.py
@@ -0,0 +1,58 @@
+"""IDs.
+
+See: https://github.com/laminlabs/lamin-notes/blob/main/docs/2022/ids.ipynb
+
+Base generators:
+
+.. autosummary::
+   :toctree: .
+
+   base26
+   base62
+   base64
+
+Entity-related generators:
+
+.. autosummary::
+   :toctree: .
+
+   schema
+   user
+   instance
+   storage
+   dobject
+   run
+   usage
+   notebook
+   pipeline
+   project
+   secret
+"""
+from ._id import (  # noqa
+    base26,
+    base62,
+    base64,
+    dobject,
+    dset,
+    instance,
+    notebook,
+    pipeline,
+    project,
+    run,
+    schema,
+    secret,
+    storage,
+    usage,
+    user,
+)
+
+id_base62 = base62  # backward compat
+id_base26 = base26  # backward compat
+id_schema_module = schema  # backward compat
+id_dobject = dobject  # backward compat
+id_run = run  # backward compat
+id_user = user  # backward compat
+id_usage = usage  # backward compat
+id_secret = secret  # backward compat
+id_instance = instance  # backward compat
+id_storage = storage  # backward compat
