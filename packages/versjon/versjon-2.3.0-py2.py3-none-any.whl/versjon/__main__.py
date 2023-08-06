e83e6e78ccd18c50cea8c3c9e3059a7b0e06a47a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:16 +0000

    Add package versjon/2.3.0/versjon-2.3.0-py2.py3-none-any.whl

---
+++ packages/versjon/versjon-2.3.0-py2.py3-none-any.whl/versjon/__main__.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+# encoding: utf-8
+
+
+import click
+import colorama
+import sys
+
+from .versjon_tool import run
+
+
+@click.command()
+@click.option("-d", "--docs_path", default=".")
+@click.option("-e", "--exclude_pattern", default=None)
+@click.option("-S", "--stable-version", default=None, help="Overrides the stable version")
+@click.option("-v", "--verbose", is_flag=True)
+@click.option("-i", "--no_index", is_flag=True)
+@click.option("-s", "--no_stable_index", is_flag=True)
+@click.option("-u", "--user_templates", default=None)
+def cli(docs_path, exclude_pattern, stable_version, verbose, no_index, no_stable_index, user_templates):
+    try:
+        run(
+            docs_path=docs_path,
+            exclude_pattern=exclude_pattern,
+            stable_version=stable_version,
+            no_index=no_index,
+            no_stable_index=no_stable_index,
+            user_templates=user_templates,
+        )
+    except Exception as e:
+
+        if verbose:
+            # We just propagate the exception out
+            raise
+
+        colorama.init()
+        print(colorama.Fore.RED + str(e))
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    cli()
