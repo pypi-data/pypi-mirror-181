f46bb20bafb966af3b4d5f8d375841b1e13e4114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:10 +0000

    Add package asf-search/6.0.1/asf_search-6.0.1-py3-none-any.whl

---
+++ packages/asf-search/asf_search-6.0.1-py3-none-any.whl/asf_search/__init__.py
@@ -0,0 +1,29 @@
+# backport of importlib.metadata for python < 3.8
+from importlib_metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError as e:
+    print('package is not installed!\n'
+          'Install in editable/develop mode via (from the top of this repo):\n'
+          '   python3 -m pip install -e .\n'
+          'Or, to just get the version number use:\n'
+          '   python setup.py --version')
+    raise PackageNotFoundError("Install with 'python3 -m pip install -e .' to use") from e
+
+from .ASFSession import ASFSession
+from .ASFProduct import ASFProduct
+from .ASFSearchResults import ASFSearchResults
+from .ASFSearchOptions import ASFSearchOptions, validators
+from .exceptions import *
+from .constants import *
+from .health import *
+from .search import *
+from .download import *
+from .CMR import *
+from .baseline import *
+from .WKT import validate_wkt
+from .export import *
+
+REPORT_ERRORS=True 
+"""Enables automatic search error reporting to ASF, send any questions to uso@asf.alaska.edu"""
