593c4e38c3c3d76708e99aea290f1375df1dbf5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:57:07 +0000

    Add package contextchecker/0.0.4/contextchecker-0.0.4.tar.gz

---
+++ packages/contextchecker/contextchecker-0.0.4.tar.gz/contextchecker-0.0.4/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+     name='contextchecker',
+     author='Zephania Reuben',
+     author_email='zreuben815@gmail.com',
+     license='MIT',
+     description='A Python Package for checking possibility of certain words to appear in similar context',
+     version='0.0.4'
+ )
