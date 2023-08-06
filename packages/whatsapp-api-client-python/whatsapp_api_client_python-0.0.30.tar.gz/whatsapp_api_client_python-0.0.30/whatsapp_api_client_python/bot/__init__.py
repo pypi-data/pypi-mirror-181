dd76be1e06f48c3e003881b9fce590c5ba052dec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:29 +0000

    Add package whatsapp-api-client-python/0.0.30/whatsapp_api_client_python-0.0.30.tar.gz

---
+++ packages/whatsapp-api-client-python/whatsapp_api_client_python-0.0.30.tar.gz/whatsapp_api_client_python-0.0.30/whatsapp_api_client_python/bot/__init__.py
@@ -0,0 +1,10 @@
+from .bot import AbstractBot, Bot
+from .handlers import AbstractHandler, Handler, MessageHandler
+
+__all__ = [
+    "AbstractBot",
+    "Bot",
+    "AbstractHandler",
+    "Handler",
+    "MessageHandler"
+]
