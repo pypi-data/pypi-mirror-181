9a7ebca5eaa4309af3fce402e1042ef82d3c2ddd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:13 +0000

    Add package qpsolvers/2.7.0/qpsolvers-2.7.0.tar.gz

---
+++ packages/qpsolvers/qpsolvers-2.7.0.tar.gz/qpsolvers-2.7.0/qpsolvers/solution.py
@@ -0,0 +1,240 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2022 Stéphane Caron and the qpsolvers contributors.
+#
+# This file is part of qpsolvers.
+#
+# qpsolvers is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Output from a QP solver.
+"""
+
+from dataclasses import dataclass, field
+from typing import Optional
+
+import numpy as np
+
+from .problem import Problem
+
+
+@dataclass(frozen=False)
+class Solution:
+
+    """
+    Solution provided by a QP solver to a given problem.
+
+    Attributes
+    ----------
+    problem :
+        Quadratic program the solution corresponds to.
+
+    obj :
+        Value of the primal objective at the solution (``None`` if no solution
+        was found).
+
+    x :
+        Solution vector of the primal quadratic program (``None`` if no
+        solution was found).
+
+    y :
+        Dual multipliers for equality constraints (``None`` if no solution was
+        found). The dimension of :math:`y` is equal to the number of equality
+        constraints. The values :math:`y_i` can be either positive or negative.
+
+    z :
+        Dual multipliers for linear inequality constraints (``None`` if no
+        solution was found). The dimension of :math:`z` is equal to the number
+        of inequalities. The value :math:`z_i` for inequality :math:`i` is
+        always positive.
+
+        - If :math:`z_i > 0`, the inequality is active at the solution:
+          :math:`G_i x = h_i`.
+        - If :math:`z_i = 0`, the inequality is inactive at the solution:
+          :math:`G_i x < h_i`.
+
+    z_box :
+        Dual multipliers for box inequality constraints (``None`` if no
+        solution was found). The sign of :math:`z_{box,i}` depends on the
+        active bound:
+
+        - If :math:`z_{box,i} < 0`, then the lower bound :math:`lb_i = x_i` is
+          active at the solution.
+        - If :math:`z_{box,i} = 0`, then neither the lower nor the upper bound
+          are active and :math:`lb_i < x_i < ub_i`.
+        - If :math:`z_{box,i} > 0`, then the upper bound :math:`x_i = ub_i` is
+          active at the solution.
+
+    extras :
+        Other outputs, specific to each solver.
+    """
+
+    problem: Problem
+    extras: dict = field(default_factory=dict)
+    obj: Optional[float] = None
+    x: Optional[np.ndarray] = None
+    y: Optional[np.ndarray] = None
+    z: Optional[np.ndarray] = None
+    z_box: Optional[np.ndarray] = None
+
+    @property
+    def is_empty(self) -> bool:
+        """
+        Check whether the solution is empty.
+        """
+        return self.x is None
+
+    def primal_residual(self) -> float:
+        """
+        Compute the primal residual of the solution:
+
+        .. math::
+
+            r_p := \\max(\\| A x_k - b \\|_\\infty, [G x_k - h]_+),
+
+        Returns
+        -------
+        :
+            Primal residual if it is defined, ``np.inf`` otherwise.
+
+        Notes
+        -----
+        See for instance [tolerances]_ for an overview of optimality conditions
+        and why this residual will be zero at the optimum.
+        """
+        _, _, G, h, A, b, lb, ub = self.problem.unpack()
+        if self.x is None:
+            return np.inf
+        x = self.x
+        return max(
+            [
+                0.0,
+                np.max(G.dot(x) - h) if G is not None else 0.0,
+                np.linalg.norm(A.dot(x) - b, np.inf) if A is not None else 0.0,
+                np.max(lb - x) if lb is not None else 0.0,
+                np.max(x - ub) if ub is not None else 0.0,
+            ]
+        )
+
+    def dual_residual(self) -> float:
+        """
+        Compute the dual residual of the solution:
+
+        .. math::
+
+            r_d := \\| P x_k + q + A^T y_k + G^T z_k \\|_\\infty
+
+        Returns
+        -------
+        :
+            Dual residual if it is defined, ``np.inf`` otherwise.
+
+        Notes
+        -----
+        See for instance [tolerances]_ for an overview of optimality conditions
+        and why this residual will be zero at the optimum.
+        """
+        P, q, G, _, A, _, lb, ub = self.problem.unpack()
+        if self.x is None:
+            return np.inf
+        zeros = np.zeros(self.x.shape)
+        Px = P.dot(self.x)
+
+        ATy = zeros
+        if A is not None:
+            if self.y is None:
+                return np.inf
+            ATy = A.T.dot(self.y)
+
+        GTz = zeros
+        if G is not None:
+            if self.z is None:
+                return np.inf
+            GTz = G.T.dot(self.z)
+
+        z_box = zeros
+        if lb is not None or ub is not None:
+            if self.z_box is None:
+                return np.inf
+            z_box = self.z_box
+
+        p = np.linalg.norm(Px + q + GTz + ATy + z_box, np.inf)
+        return p  # type: ignore
+
+    def duality_gap(self) -> float:
+        """
+        Compute the duality gap of the solution:
+
+        .. math::
+
+            r_g := | x_k^T P x_k + q^T x_k + b^T y_k + h^T z_k |
+
+        Returns
+        -------
+        :
+            Duality gap if it is defined, ``np.inf`` otherwise.
+
+        Notes
+        -----
+        See for instance [tolerances]_ for an overview of optimality conditions
+        and why this gap will be zero at the optimum.
+        """
+        P, q, _, h, _, b, lb, ub = self.problem.unpack()
+        if self.x is None:
+            return np.inf
+        xPx = self.x.T.dot(P.dot(self.x))
+        qx = q.dot(self.x)
+
+        hz = 0.0
+        if h is not None:
+            if self.z is None:
+                return np.int
+            hz = h.dot(self.z)
+
+        by = 0.0
+        if b is not None:
+            if self.y is None:
+                return np.inf
+            by = b.dot(self.y)
+
+        lb_z_box = 0.0
+        ub_z_box = 0.0
+        if self.z_box is not None:
+            if lb is not None:
+                lb_z_box = lb.dot(np.minimum(self.z_box, 0.0))
+            if ub is not None:
+                ub_z_box = ub.dot(np.maximum(self.z_box, 0.0))
+        return abs(xPx + qx + hz + by + lb_z_box + ub_z_box)
+
+    def is_optimal(self, eps_abs: float) -> bool:
+        """
+        Check whether the solution is indeed optimal.
+
+        Parameters
+        ----------
+        eps_abs :
+            Absolute tolerance for the primal residual, dual residual and
+            duality gap.
+
+        Notes
+        -----
+        See for instance [tolerances]_ for an overview of optimality conditions
+        in quadratic programming.
+        """
+        return (
+            self.primal_residual() < eps_abs
+            and self.dual_residual() < eps_abs
+            and self.duality_gap() < eps_abs
+        )
---
+++ packages/qpsolvers/qpsolvers-2.7.0.tar.gz/qpsolvers-2.7.0/tests/test_conversions.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016-2022 Stéphane Caron and the qpsolvers contributors.
+#
+# This file is part of qpsolvers.
+#
+# qpsolvers is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option) any
+# later version.
+#
+# qpsolvers is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with qpsolvers. If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from numpy import allclose, array, eye
+from qpsolvers.conversions import linear_from_box_inequalities
+
+
+class TestConversions(unittest.TestCase):
+
+    """
+    Test fixture for box to linear inequality conversion.
+    """
+
+    def setUp(self):
+        self.G = array([[1.0, 2.0, 1.0], [2.0, 0.0, 1.0], [-1.0, 2.0, -1.0]])
+        self.h = array([3.0, 2.0, -2.0]).reshape((3,))
+        self.lb = array([-1.0, -1.0, -1.0])
+        self.ub = array([1.0, 1.0, 1.0])
+
+    def __test_linear_from_box_inequalities(self, G, h, lb, ub):
+        G2, h2 = linear_from_box_inequalities(G, h, lb, ub)
+        m = G.shape[0] if G is not None else 0
+        k = lb.shape[0]
+        self.assertTrue(allclose(G2[m : m + k, :], -eye(k)))
+        self.assertTrue(allclose(h2[m : m + k], -lb))
+        self.assertTrue(allclose(G2[m + k : m + 2 * k, :], eye(k)))
+        self.assertTrue(allclose(h2[m + k : m + 2 * k], ub))
+
+    def test_concatenate_bounds(self):
+        G, h, lb, ub = self.G, self.h, self.lb, self.ub
+        self.__test_linear_from_box_inequalities(G, h, lb, ub)
+
+    def test_pure_bounds(self):
+        self.__test_linear_from_box_inequalities(None, None, self.lb, self.ub)
