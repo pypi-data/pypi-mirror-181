f79f14e4f2afe2d5cc6cfc6b1dc3552f63e09e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:48 +0000

    Add package ansys-fluent-core/0.12.dev8/ansys-fluent-core-0.12.dev8.tar.gz

---
+++ packages/ansys-fluent-core/ansys-fluent-core-0.12.dev8.tar.gz/ansys-fluent-core-0.12.dev8/src/ansys/fluent/core/solver/settings_232/__init__.py
@@ -0,0 +1,9 @@
+#
+# This is an auto-generated file.  DO NOT EDIT!
+#
+
+"""A package providing Fluent's Settings Objects in Python."""
+from ansys.fluent.core.solver.flobject import *
+
+SHASH = "df8571d0dcb77b505c3511261d6cfd8fef930d8a1242e2b686aa9e0624878a22"
+from .root import root
