b8f3ecf9cefa9bd09b64a599b15e8600cd0adbaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:58 +0000

    Add package PeerAnnot/0.0.1.post5/PeerAnnot-0.0.1.post5-py3-none-any.whl

---
+++ packages/PeerAnnot/PeerAnnot-0.0.1.post5-py3-none-any.whl/peerannot/runners/__init__.py
@@ -0,0 +1,31 @@
+import click
+
+from peerannot import __version__
+from peerannot.runners.run import run
+from peerannot.runners.datasets import datasets
+
+SOURCES = [run, datasets]
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
+
+
+@click.command(
+    name="peerannot",
+    cls=click.CommandCollection,
+    sources=SOURCES,
+    context_settings=CONTEXT_SETTINGS,
+    invoke_without_command=True,
+)
+@click.option("--version", is_flag=True, help="Print version")
+@click.pass_context
+def peerannot(ctx, version=False):
+    if version:
+        output = __version__
+        output = f"PeerAnnot version {output}"
+        click.echo(output)
+        raise SystemExit(0)
+    if ctx.invoked_subcommand is None:
+        print(peerannot.get_help(ctx))
+
+
+if __name__ == "__main__":
+    peerannot()
