15f4c5231ed4ae742498a1091222808b55320741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:45 +0000

    Add package Draw-Reaction-zhiyix2/0.1.0/Draw_Reaction_zhiyix2-0.1.0-py3-none-any.whl

---
+++ packages/Draw-Reaction-zhiyix2/Draw_Reaction_zhiyix2-0.1.0-py3-none-any.whl/Draw_Reaction/__init__.py
@@ -0,0 +1,13 @@
+# __init__.py
+
+from importlib import resources
+try:
+    import tkinter
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+# Version of the Draw_Reaction package
+__version__ = "0.1.0"
+
+_cfg = tomllib.loads(resources.read_text("reader", "config.toml"))
+URL = _cfg["feed"]["url"]
