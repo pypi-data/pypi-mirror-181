761bca3a3fce111af501224ce7ef9d7002a0cbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:50 +0000

    Add package steam-tools-ng/2.0.2/steam_tools_ng-2.0.2-py3-none-any.whl

---
+++ packages/steam-tools-ng/steam_tools_ng-2.0.2-py3-none-any.whl/steam_tools_ng/core/cardfarming.py
@@ -0,0 +1,291 @@
+#!/usr/bin/env python
+#
+# Lara Maia <dev@lara.monster> 2015 ~ 2022
+#
+# The Steam Tools NG is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# The Steam Tools NG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#
+import aiohttp
+import asyncio
+import atexit
+import contextlib
+import random
+import sys
+import time
+from pathlib import Path
+from psutil import Popen, NoSuchProcess
+from subprocess import call
+from typing import AsyncGenerator, Dict, Optional, Any
+
+from stlib import webapi, client, universe, community
+from . import utils
+from .. import i18n, config
+
+_ = i18n.get_translation
+
+
+# FIXME: Workaround for keyboard lag when running games on GUI on Windows
+# FIXME: https://gitlab.gnome.org/GNOME/gtk/-/issues/2015
+# FIXME: L36:79
+def killall(process: 'Popen') -> None:
+    if not process.is_running():
+        return
+
+    for child in process.children(recursive=True):
+        with contextlib.suppress(NoSuchProcess):
+            child.kill()
+            child.wait()
+
+    process.kill()
+    process.wait()
+
+
+class SteamAPIExecutorWorkaround:
+    def __init__(self, appid: int, *args: Any, **kwargs: Any) -> None:
+        self.process = None
+        self.appid = appid
+        self._is_running = False
+
+        if sys.platform == 'win32':
+            if getattr(sys, 'frozen', False):
+                executor_path = [str(Path(sys.executable).parent / 'steam-api-executor.exe'), str(self.appid)]
+            else:
+                executor_path = [sys.executable, '-m', 'steam_tools_ng.steam_api_executor', str(self.appid)]
+
+            self.process = Popen(executor_path, creationflags=0x08000000)
+            self._is_running = True
+            atexit.register(killall, self.process)
+        else:
+            self.executor = _SteamAPIExecutor(appid, *args, **kwargs)
+
+    def is_running(self) -> bool:
+        if sys.platform == 'win32':
+            return self._is_running
+        else:
+            return self.executor.is_running()
+
+    def shutdown(self, *args: Any, **kwargs: Any) -> None:
+        if sys.platform == 'win32':
+            killall(self.process)
+            self._is_running = False
+        else:
+            self.executor.shutdown(*args, **kwargs)
+
+
+_SteamAPIExecutor = client.SteamAPIExecutor
+client.SteamAPIExecutor = SteamAPIExecutorWorkaround
+
+# FIXME: L36:79
+# FIXME: ---- #
+
+executors = {}
+
+
+def safe_exit(*args: Any) -> None:
+    for executor in executors.values():
+        executor.shutdown()
+
+
+async def while_has_cards(
+        steamid: universe.SteamId,
+        badge: community.Badge,
+) -> AsyncGenerator[utils.ModuleData, None]:
+    webapi_session = webapi.SteamWebAPI.get_session(0)
+    community_session = community.Community.get_session(0)
+
+    while badge.cards != 0:
+        first_wait = config.parser.getint("cardfarming", "first_wait")
+        default_wait = config.parser.getint("cardfarming", "default_wait")
+        min_wait = config.parser.getint("cardfarming", "min_wait")
+
+        try:
+            game_list = await webapi_session.get_owned_games(steamid, appids_filter=[badge.appid])
+            game_info = game_list[0]
+        except aiohttp.ClientError:
+            yield utils.ModuleData(error=_("Check your connection. (server down?)"), info=_("Waiting Changes"))
+            await asyncio.sleep(10)
+            continue
+
+        if game_info.playtime_forever * 60 >= first_wait:
+            wait_offset = random.randint(default_wait, int(default_wait / 100 * 125))
+        else:
+            wait_offset = first_wait - game_info.playtime_forever * 60
+
+        try:
+            executor = client.SteamAPIExecutor(badge.appid)
+        except AttributeError:
+            yield utils.ModuleData(action='ignore', info=_("Invalid game id {}. Ignoring.").format(badge.appid))
+            break
+        except ProcessLookupError:
+            yield utils.ModuleData(error=_("Steam Client is not running."), info=_("Waiting Changes"))
+            await asyncio.sleep(15)
+            continue
+
+        for past_time in range(wait_offset):
+            current_time = round((wait_offset - past_time) / 60)
+            current_time_size = _('minutes')
+
+            if current_time <= 1:
+                current_time = wait_offset - past_time
+                current_time_size = _('seconds')
+
+            yield utils.ModuleData(
+                display=str(badge.appid),
+                info=_("{} {} for {}").format(current_time, current_time_size, badge.name),
+                status=_("Running {}").format(badge.name),
+                level=(past_time, wait_offset),
+                raw_data=executor,
+                action="check",
+            )
+            await asyncio.sleep(1)
+
+        executor.shutdown()
+        wait_offset = random.randint(min_wait, int(min_wait / 100 * 125))
+        for past_time in range(wait_offset):
+            yield utils.ModuleData(
+                display=str(badge.appid),
+                info=_('Updating drops for {}').format(badge.name),
+                status=_("Game paused"),
+                level=(past_time, wait_offset),
+            )
+            await asyncio.sleep(1)
+
+        while True:
+            try:
+                cards = await community_session.get_card_drops_remaining(steamid, badge.appid)
+            except aiohttp.ClientError:
+                yield utils.ModuleData(error=_("Check your connection. (server down?)"), info=_("Waiting Changes"))
+                await asyncio.sleep(10)
+            except community.BadgeError:
+                yield utils.ModuleData(error=_("Steam Server is busy"), info=_("Waiting Changes"))
+                await asyncio.sleep(20)
+            else:
+                break
+
+        # noinspection PyProtectedMember
+        badge = badge._replace(cards=cards)
+
+    utils.ModuleData(
+        display=str(badge.appid),
+        info=_("{} ({})").format(_("Done"), badge.name),
+    )
+
+
+async def main(steamid: universe.SteamId, custom_game_id: int = 0) -> AsyncGenerator[utils.ModuleData, None]:
+    asyncio.current_task().add_done_callback(safe_exit)
+
+    reverse_sorting = config.parser.getboolean("cardfarming", "reverse_sorting")
+    max_concurrency = config.parser.getint("cardfarming", "max_concurrency")
+    invisible = config.parser.getboolean("cardfarming", "invisible")
+    community_session = community.Community.get_session(0)
+
+    try:
+        badges = sorted(
+            await community_session.get_badges(steamid),
+            key=lambda badge_: badge_.cards,  # type: ignore
+            reverse=reverse_sorting
+        )
+    except aiohttp.ClientError:
+        yield utils.ModuleData(error=_("Check your connection. (server down?)"), info=_("Waiting Changes"))
+        await asyncio.sleep(10)
+        return
+
+    if not badges or (custom_game_id and custom_game_id not in [badge.appid for badge in badges]):
+        yield utils.ModuleData(error=_("No more cards to drop."), info=_("Waiting Changes"))
+        await asyncio.sleep(random.randint(500, 1200))
+        return
+
+    generators = {}
+
+    if invisible:
+        call(f'{config.file_manager} "steam://friends/status/invisible"')
+
+    for badge in badges:
+        yield utils.ModuleData(
+            display=str(badge.appid),
+            status=_("Loading {}").format(badge.name),
+        )
+
+        if custom_game_id and badge.appid != custom_game_id:
+            yield utils.ModuleData(info=_("Skipping {}").format(badge.appid))
+            continue
+
+        generators[badge.appid] = while_has_cards(steamid, badge)
+
+    tasks: Dict[int, Optional[asyncio.Task[Any]]] = {}
+    semaphore = asyncio.Semaphore(max_concurrency)
+    last_update = 0
+
+    while True:
+        for appid in generators.keys():
+            progress_coro = anext(generators[appid])
+            assert asyncio.iscoroutine(progress_coro)
+
+            if appid not in tasks:
+                if semaphore.locked():
+                    break
+
+                await semaphore.acquire()
+                tasks[appid] = asyncio.create_task(progress_coro)
+
+            if not tasks[appid]:
+                continue
+
+            current_task = tasks[appid]
+            assert isinstance(current_task, asyncio.Task)
+
+            if current_task.done():
+                semaphore.release()
+
+                if current_task.exception():
+                    if isinstance(current_task.exception(), StopAsyncIteration):
+                        tasks[appid] = None
+                        continue
+
+                    current_exception = current_task.exception()
+                    assert isinstance(current_exception, BaseException)
+                    raise current_exception
+
+                await semaphore.acquire()
+                tasks[appid] = asyncio.create_task(progress_coro)
+
+        if not any(tasks.values()):
+            break
+
+        await asyncio.wait([task for task in tasks.values() if task], return_when=asyncio.FIRST_COMPLETED)
+
+        for appid, task in tasks.items():
+            if task and task.done() and not task.exception():
+                global executors
+                data: utils.ModuleData = task.result()
+
+                if data.action == 'check':
+                    executors[appid] = data.raw_data
+
+                if int(time.time()) > last_update + 3:
+                    current_running_limit = len(tasks)
+                    total_remaining = len(generators) - len([task for task in tasks.values() if not task])
+                    running_executors = [executor for executor in executors.values() if executor.is_running()]
+
+                    if current_running_limit > total_remaining:
+                        current_running_limit = total_remaining
+
+                    yield utils.ModuleData(
+                        display=' : '.join([str(executor.appid) for executor in running_executors]),
+                        info=f'{data.info} +{len(running_executors)-1}',
+                        status=_('Running {} from {} remaining').format(current_running_limit, total_remaining),
+                        level=data.level,
+                        raw_data=running_executors,
+                        action=data.action,
+                    )
+                    last_update = int(time.time())
---
+++ packages/steam-tools-ng/steam_tools_ng-2.0.2-py3-none-any.whl/steam_tools_ng/core/steamtrades.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+#
+# Lara Maia <dev@lara.monster> 2015 ~ 2022
+#
+# The Steam Tools NG is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# The Steam Tools NG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+#
+import aiohttp
+import asyncio
+import logging
+import random
+from typing import AsyncGenerator
+
+from stlib import plugins, login
+from . import utils
+from .. import i18n, config
+
+_ = i18n.get_translation
+log = logging.getLogger(__name__)
+
+
+async def main() -> AsyncGenerator[utils.ModuleData, None]:
+    yield utils.ModuleData(status=_("Loading"))
+
+    if plugins.has_plugin("steamtrades"):
+        steamtrades = plugins.get_plugin("steamtrades")
+        steamtrades_session = steamtrades.Main.get_session(0)
+    else:
+        raise ImportError(_("Unable to find Steamtrades plugin"))
+
+    trade_ids = config.parser.get("steamtrades", "trade_ids")
+    wait_min = config.parser.getint("steamtrades", "wait_min")
+    wait_max = config.parser.getint("steamtrades", "wait_max")
+
+    if not trade_ids:
+        yield utils.ModuleData(error=_("No trade ID found"), info=_("Waiting Changes"))
+        await asyncio.sleep(5)
+        return
+
+    trades = [trade.strip() for trade in trade_ids.split(',')]
+
+    try:
+        await steamtrades_session.do_login()
+    except aiohttp.ClientError:
+        yield utils.ModuleData(error=_("Check your connection. (server down?)"))
+        await asyncio.sleep(15)
+        return
+    except steamtrades.TooFast:
+        yield utils.ModuleData(error=_("Unable to login. Trying again in 15 seconds"))
+        await asyncio.sleep(15)
+        return
+    except steamtrades.UserSuspended:
+        yield utils.ModuleData(error=_("User is suspended."))
+        await asyncio.sleep(18000)
+        return
+    except steamtrades.PrivateProfile:
+        yield utils.ModuleData(error=_("Your profile must be public to use steamtrades."), info=_("Waiting Changes"))
+        await asyncio.sleep(30)
+        return
+    except steamtrades.UserLevelError:
+        yield utils.ModuleData(error=_("You must be level 1 or greater to use steamtrades."))
+        await asyncio.sleep(300)
+        return
+    except login.LoginError:
+        yield utils.ModuleData(error=_("User is not logged in. Trying again in 30 seconds"))
+        await asyncio.sleep(30)
+        return
+
+    bumped = False
+
+    for trade_id in trades:
+        try:
+            trade_info = await steamtrades_session.get_trade_info(trade_id)
+        except (IndexError, aiohttp.ClientResponseError):
+            yield utils.ModuleData(error=_("Unable to find trade id"))
+            bumped = False
+            break
+        except aiohttp.ClientError:
+            yield utils.ModuleData(error=_("Check your connection. (server down?)"))
+            bumped = False
+            break
+
+        yield utils.ModuleData(display=trade_info.id, info=trade_info.title)
+        max_ban_wait = random.randint(5, 15)
+        for past_time in range(max_ban_wait):
+            try:
+                yield utils.ModuleData(level=(past_time, max_ban_wait))
+            except KeyError:
+                yield utils.ModuleData(level=(0, 0))
+
+            await asyncio.sleep(1)
+
+        try:
+            if await steamtrades_session.bump(trade_info):
+                yield utils.ModuleData(display=trade_id, info=_("Bumped!"))
+                bumped = True
+            else:
+                yield utils.ModuleData(display=trade_id, error=_("Unable to bump"))
+                await asyncio.sleep(5)
+                continue
+        except aiohttp.ClientError:
+            yield utils.ModuleData(error=_("Check your connection. (server down?)"))
+            await asyncio.sleep(10)
+            bumped = False
+            break
+        except steamtrades.NoTradesError:
+            yield utils.ModuleData(error=_("No trades available to bump"))
+            await asyncio.sleep(15)
+            continue
+        except steamtrades.TradeNotReadyError as exception:
+            wait_min = exception.time_left * 60
+            wait_max = wait_min + 400
+            bumped = True
+        except steamtrades.TradeClosedError as exception:
+            yield utils.ModuleData(error=_("Trade {}({}) is closed").format(exception.title, exception.id))
+            await asyncio.sleep(5)
+            continue
+        except login.LoginError:
+            yield utils.ModuleData(error=_("Login is lost. Trying to relogin."))
+            await asyncio.sleep(5)
+            bumped = False
+            break
+
+    if not bumped:
+        await asyncio.sleep(10)
+        return
+
+    wait_offset = random.randint(wait_min, wait_max)
+    log.debug(_("Setting wait_offset from steamtrades to %s"), wait_offset)
+    for past_time in range(wait_offset):
+        yield utils.ModuleData(
+            info=_("Waiting more {} minutes").format(round(wait_offset / 60)),
+            level=(past_time, wait_offset),
+        )
+
+        await asyncio.sleep(1)
