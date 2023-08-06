eb30c7b707bd9681f79ff12f262e7b69d5d2d8ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:53:40 +0000

    Add package tensortrax/0.2.6/tensortrax-0.2.6.tar.gz

---
+++ packages/tensortrax/tensortrax-0.2.6.tar.gz/tensortrax-0.2.6/tensortrax/math/__init__.py
@@ -0,0 +1,35 @@
+r"""
+ _                            
+| |                          ████████╗██████╗  █████╗ ██╗  ██╗
+| |_ ___ _ __  ___  ___  _ __╚══██╔══╝██╔══██╗██╔══██╗╚██╗██╔╝
+| __/ _ \ '_ \/ __|/ _ \| '__|  ██║   ██████╔╝███████║ ╚███╔╝ 
+| ||  __/ | | \__ \ (_) | |     ██║   ██╔══██╗██╔══██║ ██╔██╗ 
+ \__\___|_| |_|___/\___/|_|     ██║   ██║  ██║██║  ██║██╔╝ ██╗
+                                ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝  
+"""
+
+from . import _linalg as linalg
+from . import _special as special
+from ._math_tensor import (
+    sin,
+    cos,
+    tan,
+    sinh,
+    cosh,
+    tanh,
+    exp,
+    log,
+    log10,
+    dot,
+    ddot,
+    trace,
+    transpose,
+    sum,
+    sqrt,
+    einsum,
+    matmul,
+    diagonal,
+    ravel,
+    reshape,
+)
+from . import _math_array as array
