fccdc85abf7e0bd37715680df71710124d8f08e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:34:45 +0000

    Add package python-okx/0.1.1/python_okx-0.1.1-py3-none-any.whl

---
+++ packages/python-okx/python_okx-0.1.1-py3-none-any.whl/okx/TimeUtils.py
@@ -0,0 +1,16 @@
+import time
+
+import requests
+
+
+def get_server_time():
+    url = "https://www.okx.com/api/v5/public/time"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()['data'][0]['ts']
+    else:
+        return ""
+
+
+def get_local_timestamp():
+    return int(time.time())
