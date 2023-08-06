086a053e6aee5414f01dabb165a998be7589ec9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:41 +0000

    Add package akerbp.mlops/2.5.7a5/akerbp.mlops-2.5.7a5-py3-none-any.whl

---
+++ packages/akerbp.mlops/akerbp.mlops-2.5.7a5-py3-none-any.whl/akerbp/mlops/core/logger_config.py
@@ -0,0 +1,28 @@
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            "datefmt": "%d/%m/%Y %H:%M:%S",
+        }
+    },
+    "handlers": {
+        "default": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "standard",
+            "stream": "ext://sys.stdout",
+        },
+        "warnings": {
+            "class": "logging.StreamHandler",
+            "level": "WARNING",
+            "formatter": "standard",
+            "stream": "ext://sys.stdout",
+        },
+    },
+    "loggers": {
+        "": {"handlers": ["default"], "level": "INFO"},
+        "py.warnings": {"handlers": ["warnings"], "level": "WARNING"},
+    },
+}
