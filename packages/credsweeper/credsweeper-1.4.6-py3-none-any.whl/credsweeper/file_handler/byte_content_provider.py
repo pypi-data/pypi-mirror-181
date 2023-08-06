4837cf0b737d94c061552d03a30007148af1ce77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:30:32 +0000

    Add package credsweeper/1.4.6/credsweeper-1.4.6-py3-none-any.whl

---
+++ packages/credsweeper/credsweeper-1.4.6-py3-none-any.whl/credsweeper/file_handler/byte_content_provider.py
@@ -0,0 +1,34 @@
+from typing import List, Optional
+
+from credsweeper.file_handler.analysis_target import AnalysisTarget
+from credsweeper.file_handler.content_provider import ContentProvider
+from credsweeper.utils import Util
+
+
+class ByteContentProvider(ContentProvider):
+    """Allow to scan byte sequence.
+
+    Parameters:
+        content: byte sequence to be scanned.Would be automatically split into an array of lines in a new
+          line character is present
+        file_path: optional string. Might be specified if you know true file name lines was taken from
+
+    """
+
+    def __init__(
+            self,  #
+            content: bytes,  #
+            file_path: Optional[str] = None,  #
+            file_type: Optional[str] = None,  #
+            info: Optional[str] = None) -> None:
+        super().__init__(file_path=file_path, file_type=file_type, info=info)
+        self.lines = Util.decode_bytes(content)
+
+    def get_analysis_target(self) -> List[AnalysisTarget]:
+        """Return lines to scan.
+
+        Return:
+            list of analysis targets based on every row in a content
+
+        """
+        return self.lines_to_targets(self.lines)
