a150a1ee0d85a2a0ac2a697fb25ade81ee8c4111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:33 +0000

    Add package nl-service-metadata-generator/0.2.0/nl_service_metadata_generator-0.2.0-py3-none-any.whl

---
+++ packages/nl-service-metadata-generator/nl_service_metadata_generator-0.2.0-py3-none-any.whl/nl_service_metadata_generator/constants.py
@@ -0,0 +1,12 @@
+SERVICE_TEMPLATE = "iso19119_nl_profile_2.1.0.xml"
+CODELIST_JSON_FILE = "data/json/codelists.json"
+SERVICE_METADATA_SCHEMA = (
+    "data/schema/schemas.opengis.net/csw/2.0.2/profiles/apiso/1.0.0/apiso.xsd"
+)
+TEMPLATES_DIR = "data/templates"
+DEFAULT_CSW_ENDPOINT = "https://nationaalgeoregister.nl/geonetwork/srv/dut/csw"
+QUALITY_SERVICE_CONFORMANCE = {
+    "qosPerformance": 1,
+    "qosAvailability": 99,
+    "qosCapacity": 10,
+}
