15ede38459f818d447d9524580c9c61dec253128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:34 +0000

    Add package esi-utils-textformat/1.0.0/esi-utils-textformat-1.0.0.tar.gz

---
+++ packages/esi-utils-textformat/esi-utils-textformat-1.0.0.tar.gz/esi-utils-textformat-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="esi-utils-textformat",
+    version="1.0.0",
+    description="USGS Earthquake Impact Utilities",
+    author="Mike Hearne",
+    author_email="mhearne@usgs.gov",
+    url="https://code.usgs.gov/ghsc/esi/esi-utils-textformat",
+    packages=[
+        "esi_utils_textformat",
+    ],
+)
