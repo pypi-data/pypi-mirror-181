3e738ce9e8b74741ae7e320eaa63ad76e5e6654f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:37:59 +0000

    Add package bayes-nets-sample/1.2/bayes_nets_sample-1.2.tar.gz

---
+++ packages/bayes-nets-sample/bayes_nets_sample-1.2.tar.gz/bayes_nets_sample-1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'bayes_nets_sample',
+    version = '1.2',
+    license = 'MIT',
+    description = 'A class to represent a Bayes Network. And apply all the different sampling strategies to the calculate a given query.',
+    author = "Srajan Chourasia",
+    author_email = 'srajanstark.ash@gmail.com',
+    packages = find_packages('bayes'),
+    package_dir = {'': 'bayes'},
+    url = 'https://github.com/srajan-kiyotaka/bayesNetSample',
+    download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+    keywords = ['bayes'],
+    install_requires = [
+          'random',
+      ],
+    classifiers = [
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Student_Teachers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3.9',      #Specify which python versions that you want to support
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
