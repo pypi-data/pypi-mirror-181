0fd1c44e5cb395314c6b85f480803cbb155be3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:27 +0000

    Add package trellisdata/0.1.26/trellisdata-0.1.26-py3-none-any.whl

---
+++ packages/trellisdata/trellisdata-0.1.26-py3-none-any.whl/trellisdata/database_trigger.py
@@ -0,0 +1,262 @@
+import json
+import yaml
+import neo4j
+import base64
+import logging
+
+class TriggerController:
+
+    def __init__(self, trigger_document):
+        """Manage database trigger activation and get query parameters.
+
+        Inspired by docs: https://pyyaml.org/wiki/PyYAMLDocumentation
+
+        args:
+            trigger_document (str): YAML formatted str with tagged !DatabaseTrigger documents
+        """
+        triggers = yaml.load_all(trigger_document, Loader=yaml.FullLoader)
+
+        self.node_triggers = {}
+        self.relationship_triggers = {}
+        self.supported_patterns = ["node", "relationship"]
+
+        # Separate triggers by type
+        for trigger in triggers:
+
+            self._validate_trigger_content(trigger)
+
+            if trigger.pattern == 'node':
+                if trigger.start['label'] in self.node_triggers.keys():
+                    self.node_triggers[trigger.start['label']].append(trigger)
+                else:
+                    self.node_triggers[trigger.start['label']] = [trigger]
+            elif trigger.pattern == 'relationship':
+                if trigger.start['label'] in self.relationship_triggers.keys():
+                    self.relationship_triggers[trigger.start['label']].append(trigger)
+                else:
+                    self.relationship_triggers[trigger.start['label']] = [trigger]
+            else:
+                raise ValueError(f"{trigger.pattern} is not a supported trigger pattern.")
+
+    def evaluate_trigger_conditions(self, query_response):
+        """
+        args:
+            query_response (trellisdata.QueryResponseReader)
+        """
+
+        response_pattern = self._determine_result_pattern(query_response)
+
+        if response_pattern == 'node':
+            logging.info("#> Evaluating node triggers.")
+            activated_triggers = self._evaluate_node_triggers(query_response)
+        elif response_pattern == 'relationship':
+            logging.info("#> Evaluating relationship triggers.")
+            activated_triggers = self._evaluate_relationship_triggers(query_response)
+        else:
+            raise ValueError(f"#> Response pattern '{response_pattern}' is not a supported trigger pattern.")
+        return activated_triggers
+
+    def _determine_result_pattern(self, query_response):
+        len_nodes = len(query_response.nodes)
+        len_relationships = len(query_response.relationship)
+
+        #if len_nodes == 1 and len_relationships == 0:
+        if len_nodes == 1 and not query_response.relationship:
+            return "node"
+        elif len_nodes == 0 and query_response.relationship:
+        #elif len_nodes == 2 and len_relationships == 1:
+            return "relationship"
+        else:
+            raise ValueError(f"Number of nodes ({len_nodes}) and relationships ({len_relationships}) " +
+                              "does not fit a recognized return pattern.")
+
+    def _evaluate_node_triggers(self, query_response):
+        """
+        When evaluating node triggers, the only criterion is 
+        whether the node label matches the trigger label.
+        """
+        node = query_response.nodes[0]
+        node_labels = node['labels']
+
+        # New logic to handle multiple labels
+        start_triggers = []
+        for label in node_labels:
+            label_triggers = self.node_triggers.get(label)
+            if label_triggers:
+                start_triggers.extend(label_triggers)
+        # Remove duplicates
+        candidate_triggers = set(start_triggers)
+
+        activated_triggers = []
+        for trigger in candidate_triggers:
+            node_properties = node['properties']
+
+            query_parameters = self._get_node_trigger_parameters(
+                                    trigger,
+                                    node_properties)
+            trigger_tuple = (trigger, query_parameters)
+            activated_triggers.append(trigger_tuple)
+        return activated_triggers
+
+    def _evaluate_relationship_triggers(self, query_response):
+        """
+        When evaluating relationships triggers, the criteria are...
+        """
+        
+        # Not relevant anymore; only (1) relationship can be passed and
+        # presence of relationship is checked upstream.
+        """
+        if len(query_response.relationship) == 0:
+            return None
+        elif len(query_response.relationship) > 1:
+            raise ValueError(
+                "Multiple relationships provided, " +
+                "only single relationship patterns supported.")
+        """
+
+        relationship = query_response.relationship
+        start_labels = relationship['start_node']['labels']
+
+        # Get relevant triggers
+        start_triggers = []
+        for label in start_labels:
+            label_triggers = self.relationship_triggers.get(label)
+            if label_triggers:
+                start_triggers.extend(label_triggers)
+        candidate_triggers = set(start_triggers)
+
+        trigger_names = [trigger.name for trigger in candidate_triggers]
+        logging.info(f"#> Triggers that match start node '{start_labels}': {trigger_names}.")
+
+        end_labels = relationship['end_node']['labels']
+        relationship_type = relationship['type']
+
+        activated_triggers = []
+        for trigger in candidate_triggers:
+            #if (start_label == trigger.start['label'] # This is redundant
+            #        and end_label == trigger.end['label'] 
+            #        and relationship_type == trigger.relationship['type']):
+            if (
+                trigger.relationship['type'] == relationship_type and
+                trigger.end['label'] in end_labels):
+
+                start_properties = relationship['start_node']['properties']
+                end_properties = relationship['end_node']['properties']
+                rel_properties = relationship['properties']
+
+                # TODO: Get query parameters for relationship triggers
+                parameters = self._get_relationship_trigger_parameters(
+                    trigger,
+                    start_properties,
+                    end_properties,
+                    rel_properties)
+                trigger_tuple = (trigger, parameters)
+                activated_triggers.append(trigger_tuple)
+            else:
+                logging.debug(f"#> {trigger.name} not activated. " +
+                    f"Triple relationship {relationship_type} does not match {trigger.relationship['type']} " +
+                    f"or trigger label '{trigger.end['label']}' not in end node labels: {end_labels}.")
+        return activated_triggers
+
+    def _get_relationship_trigger_parameters(
+                                             self,
+                                             trigger,
+                                             start_properties,
+                                             end_properties,
+                                             rel_properties):
+
+        start_parameters = trigger.start.get('properties')
+        end_parameters = trigger.end.get('properties')
+        rel_parameters = trigger.relationship.get('properties')
+
+        parameters = {}
+        if start_parameters:
+            parameters = self._get_parameter_values(start_parameters, start_properties, parameters, trigger.start['label'])
+        if end_parameters:
+            parameters = self._get_parameter_values(end_parameters, end_properties, parameters, trigger.end['label'])
+        if rel_parameters:
+            parameters = self._get_parameter_values(rel_parameters, rel_properites, parameters, trigger.relationship['type'])
+        return parameters
+
+    def _get_node_trigger_parameters(
+                                     self,
+                                     trigger,
+                                     node_properties):
+        node_parameters = trigger.start.get('properties')
+
+        parameters = {}
+        if node_parameters:
+            parameters = self._get_parameter_values(
+                                        node_parameters,
+                                        node_properties,
+                                        parameters,
+                                        trigger.start['label'])
+        return parameters
+
+    def _get_parameter_values(self, parameter_mapping, properties, parameters, entity_label):
+        # Parameters describe values that must be provided to the activated query.
+        # Properties come from database response entities (node or relationship).
+        for property_name, parameter_name in parameter_mapping.items():
+            try:
+                parameters[parameter_name] = properties[property_name]
+            except KeyError:
+                raise KeyError(f"{entity_label} is missing property {property_name}.")
+        return parameters
+
+    def _validate_trigger_content(self, trigger):
+        if not hasattr(trigger, "name"):
+            raise AttributeError(f"#> Trigger is not named.")
+
+        logging.debug(f"#> Validating trigger: {trigger.name}.")
+        
+        if not hasattr(trigger, "pattern"):
+            raise AttributeError(f"#> Trigger is missing an activation pattern.")
+    
+        if not trigger.pattern in self.supported_patterns:
+            raise ValueError(f"#> Trigger pattern '{trigger.pattern}' not in supported patterns: {self.supported_patterns}.")
+        
+        try:
+            label = trigger.start['label']
+            if not label:
+                raise ValueError(f"#> Trigger start node missing label.")
+        except:
+            raise ValueError(f"#> Trigger start node missing label.")
+
+        if not hasattr(trigger, "query"):
+            raise AttributeError(f"#> Trigger is missing query.")
+
+        if hasattr(trigger, "relationship") or hasattr(trigger, "end"):
+            try:
+                rel_type = trigger.relationship["type"]
+                if not rel_type:
+                    raise ValueError(f"#> Trigger relationship type missing.")
+            except:
+                raise ValueError(f"#> Trigger relationship type missing.")
+
+            try:
+                label = trigger.end["label"]
+                if not label:
+                    raise ValueError(f"#> Trigger end node missing label.")
+            except:
+                raise ValueError(f"#> Trigger end node missing label.")
+
+
+class DatabaseTrigger(yaml.YAMLObject):
+    """
+    Inspired by docs: https://pyyaml.org/wiki/PyYAMLDocumentation
+    """
+    yaml_tag = u'!DatabaseTrigger'
+
+    def __init__(
+                 self, 
+                 pattern, 
+                 start, 
+                 query, 
+                 end = 0, 
+                 relationship = 0):
+        self.pattern = pattern
+        self.start = start
+        self.query = query
+
+        self.end = end
+        self.relationship = relationship
---
+++ packages/trellisdata/trellisdata-0.1.26-py3-none-any.whl/trellisdata/utils.py
@@ -0,0 +1,244 @@
+import json
+import pytz
+import random
+import hashlib
+
+from anytree import Node, RenderTree
+from anytree.search import find
+
+from datetime import datetime
+
+class TaxonomyParser:
+    """
+    This class is a wrapper on a Tree class from the anytree library to hold
+    different data hierarchies read from a JSON representation
+
+    Source: https://towardsdatascience.com/represent-hierarchical-data-in-python-cd36ada5c71a
+    """
+
+    def __init__(self, level_prefix = "L"):
+        self.prefix = level_prefix
+        self.nodes = {}
+        self.root_key = None
+
+    def find_by_name(self, name) -> Node:
+        """
+        Retrieve a node by its unique identifier name
+        """
+        root = self.nodes[self.root_key]
+        node = find(root, lambda node: node.name == name)
+        return node
+   
+    def read_from_json(self, fname):
+        """
+        Read the taxonomy from a JSON file given as input
+        """
+        
+        self.nodes = {}
+        try:
+            with open(fname, "r") as f:
+                data = json.load(f)
+                n_levels = len(list(data.keys()))
+
+                # read the root node
+                root = data[f"{self.prefix}0"][0]
+                name = root["name"]
+                _ = root.pop("name")
+                
+                self.nodes[name] = Node(name, **root)
+                self.root_key = name
+
+                # populate the tree
+                for k in range(1, n_levels):
+                    
+                    key = f"{self.prefix}{k}"
+                    nodes = data[key]
+
+                    for n in nodes:
+                        try:
+                            assert "name" in n
+                            name = n["name"]
+                            _ = n.pop("name")
+                            parent = n["parent"]
+                            _ = n.pop("parent")
+                            
+                            self.nodes[name] = Node(
+                                name,
+                                parent=self.nodes[parent],
+                                **n
+                            )
+                        except AssertionError:
+                            print(f"Malformed node representation: {n}")
+                        except KeyError:
+                            print(f"Detected a dangling node: {n['name']}")
+
+        except (FileNotFoundError, KeyError):
+            raise Exception("Not existent or malformed input JSON file")
+
+    def read_from_string(self, data_str):
+        """
+        Read the taxonomy from a JSON file given as input
+        """
+        
+        self.nodes = {}
+        try:
+            data = json.loads(data_str)
+            n_levels = len(list(data.keys()))
+
+            # read the root node
+            root = data[f"{self.prefix}0"][0]
+            name = root["name"]
+            _ = root.pop("name")
+            
+            self.nodes[name] = Node(name, **root)
+            self.root_key = name
+
+            # populate the tree
+            for k in range(1, n_levels):
+                
+                key = f"{self.prefix}{k}"
+                nodes = data[key]
+
+                for n in nodes:
+                    try:
+                        assert "name" in n
+                        name = n["name"]
+                        _ = n.pop("name")
+                        parent = n["parent"]
+                        _ = n.pop("parent")
+                        
+                        self.nodes[name] = Node(
+                            name,
+                            parent=self.nodes[parent],
+                            **n
+                        )
+                    except AssertionError:
+                        print(f"Malformed node representation: {n}")
+                    except KeyError:
+                        print(f"Detected a dangling node: {n['name']}")
+
+        except (KeyError):
+            raise Exception("Malformed input JSON string")
+
+
+class Struct:
+    # https://stackoverflow.com/questions/6866600/how-to-parse-read-a-yaml-file-into-a-python-object
+    def __init__(self, **entries):
+        self.__dict__.update(entries)
+
+
+def _get_datetime_stamp():
+    now = datetime.now()
+    datestamp = now.strftime("%y%m%d-%H%M%S-%f")[:-3]
+    return datestamp
+
+def make_unique_task_id(nodes):
+    """ Create pretty-unique hash value based on input node 
+        properties. Ignore labels and node Ids.
+        Inspiration for hashing algorithm: https://www.geeksforgeeks.org/ways-sort-list-dictionaries-values-python-using-lambda-function/
+
+        Args:
+            nodes (list): List of node dictionaries output from Neo4j Graph
+        
+        Returns:
+            task_id (str): Combination of datetime stamp and nodes hash.
+                Format: '{date}-{time}-{time}-{nodes hash}'
+                Example: '221214-162717-045-5d709493'
+            trunc_nodes_hash (str): Hash value created from 
+                combined properties of all nodes.
+    """
+    nodes_properties = []
+    for node in nodes:
+        nodes_properties.append(node['properties'])
+    sorted_nodes = sorted(nodes_properties, key = lambda i: i['id'])
+    nodes_str = json.dumps(sorted_nodes, sort_keys=True, ensure_ascii=True, default=str)
+    nodes_hash = hashlib.sha256(nodes_str.encode('utf-8')).hexdigest()
+    print(nodes_hash)
+    trunc_nodes_hash = str(nodes_hash)[:8]
+    datetime_stamp = _get_datetime_stamp()
+    task_id = f"{datetime_stamp}-{trunc_nodes_hash}"
+    return(task_id, trunc_nodes_hash)
+
+def publish_to_pubsub_topic(publisher, project_id,  topic, message):
+    """Convert dictionary to JSON and publish to Pub/Sub topic.
+
+    Args:
+        publisher (pubsub.PublisherClient): Pub/Sub client
+        project_id (str): Google Cloud Project ID
+        topic (str): Pub/Sub topic name
+        message (dict): Dictionary with header and body fields.
+
+    Returns:
+        result (???)
+    """
+
+    topic_path = publisher.topic_path(project_id, topic)
+    # https://stackoverflow.com/questions/11875770/how-to-overcome-datetime-datetime-not-json-serializable/36142844#36142844
+    json_message = json.dumps(message, indent=4, sort_keys=True, default=str).encode('utf-8')
+    result = publisher.publish(topic_path, data=json_message).result()
+    return result
+
+def publish_str_to_topic(publisher, project_id, topic, str_data):
+    topic_path = publisher.topic_path(project_id, topic)
+    message = str_data.encode('utf-8')
+    result = publisher.publish(topic_path, data=message).result()
+    return result
+
+def convert_timestamp_to_rfc_3339(timestamp):
+    # Load time in RFC 3339 format
+    # Description of RFC 3339: http://henry.precheur.org/python/rfc3339.html
+    # Pub/Sub message example: https://cloud.google.com/functions/docs/writing/background#functions-writing-background-hello-pubsub-python
+    try:
+        rfc3339_time = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S.%fZ')
+    except ValueError as exception:
+        try:
+            rfc3339_time = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
+        except:
+            return ValueError
+    except:
+        return ValueError
+    return rfc3339_time
+
+def get_seconds_from_epoch(datetime_obj):
+    """Get datetime as total seconds from epoch.
+
+    Provides datetime in easily sortable format
+
+    Args:
+        datetime_obj (datetime): Datetime.
+    Returns:
+        (float): Seconds from epoch
+    """
+    from_epoch = datetime_obj - datetime(1970, 1, 1, tzinfo=pytz.UTC)
+    from_epoch_seconds = from_epoch.total_seconds()
+    return from_epoch_seconds
+
+def make_standard_time_fields():
+    """
+    Return
+        (dict): Times in iso (str) and from-epoch (int) formats
+    """
+    datetime_created = datetime.now(pytz.UTC)
+
+    time_created_epoch = get_seconds_from_epoch(datetime_created)
+    time_created_iso = datetime_created.isoformat()
+
+    time_fields = {
+                   'timeCreatedEpoch': time_created_epoch,
+                   'timeCreatedIso': time_created_iso,
+    }
+    return time_fields
+
+def get_datetime_iso8601(date_string):
+    """ Convert ISO 8601 date strings to datetime objects.
+
+    Google datetime format: https://tools.ietf.org/html/rfc3339
+    ISO 8601 standard format: https://en.wikipedia.org/wiki/ISO_8601
+
+    Args:
+        date_string (str): Date in ISO 8601 format
+    Returns
+        (datetime.datetime): Datetime objects
+    """
+    return iso8601.parse_date(date_string)
+
