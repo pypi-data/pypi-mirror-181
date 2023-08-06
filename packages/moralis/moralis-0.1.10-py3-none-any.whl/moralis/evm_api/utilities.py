7baa9e79b1b2183611ffd2d2ae5e26b923f4d68a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:28 +0000

    Add package moralis/0.1.10/moralis-0.1.10-py3-none-any.whl

---
+++ packages/moralis/moralis-0.1.10-py3-none-any.whl/moralis/evm_api/utilities.py
@@ -0,0 +1,11 @@
+from .version import __version__
+
+
+def get_common_headers():
+    """Get the headers for the requests."""
+    return {
+        "x-moralis-platform": 'Python SDK',
+        "x-moralis-build-target": 'python',
+        "x-moralis-build-target": __version__,
+    }
+
