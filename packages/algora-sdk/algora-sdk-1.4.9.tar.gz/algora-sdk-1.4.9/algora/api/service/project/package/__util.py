555afd520eaa3ab3757816cb8452899ad33e0a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:11 +0000

    Add package algora-sdk/1.4.9/algora-sdk-1.4.9.tar.gz

---
+++ packages/algora-sdk/algora-sdk-1.4.9.tar.gz/algora-sdk-1.4.9/algora/api/service/project/package/__util.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+from algora.api.service.project.package.model import ProjectPackageRequest
+
+
+def _get_project_package_request_info(id: str) -> dict:
+    return {
+        'endpoint': f"config/project/package/{id}"
+    }
+
+
+def _get_project_packages_request_info() -> dict:
+    return {
+        'endpoint': f"config/project/package"
+    }
+
+
+def _create_project_package_request_info(request: ProjectPackageRequest) -> dict:
+    return {
+        'endpoint': f"config/project/package",
+        'json': request.request_dict()
+    }
+
+
+def _update_project_package_request_info(id: str, request: ProjectPackageRequest) -> dict:
+    return {
+        'endpoint': f"config/project/package/{id}",
+        'json': request.request_dict()
+    }
+
+
+def _delete_project_package_request_info(id: str) -> dict:
+    return {
+        'endpoint': f"config/project/package/{id}"
+    }
