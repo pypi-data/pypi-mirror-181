4994e1c54adc6c4f54ce5344b0d9b02f4052de36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:56:26 +0000

    Add package live-trading-indicators/0.5.3/live_trading_indicators-0.5.3.tar.gz

---
+++ packages/live-trading-indicators/live_trading_indicators-0.5.3.tar.gz/live_trading_indicators-0.5.3/src/live_trading_indicators/__init__.py
@@ -0,0 +1,29 @@
+import logging.config
+from .exceptions import *
+from .indicator_data import *
+from .indicators_set import Indicators, help
+from .timeframe import Timeframe
+from .config import config_get_default, config_load, config_save
+from .constants import TIME_TYPE, PRICE_TYPE, VOLUME_TYPE
+
+
+def config(actions=None, **kwargs):
+
+    if actions == 'set_default':
+        config = config_get_default()
+    else:
+        config = config_load()
+
+    if len(kwargs):
+
+        config.update(kwargs)
+
+        for settings_name, settings_value in kwargs.items():
+            if settings_value is None:
+                del config[settings_name]
+
+    if len(kwargs) or actions is not None:
+        config_save(config)
+
+    return config
+
