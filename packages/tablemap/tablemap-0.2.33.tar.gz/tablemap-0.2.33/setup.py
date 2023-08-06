1e0775040cd0ac552b224cfd85e6aa693cecd7ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:51 +0000

    Add package tablemap/0.2.33/tablemap-0.2.33.tar.gz

---
+++ packages/tablemap/tablemap-0.2.33.tar.gz/tablemap-0.2.33/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='tablemap',
+    version='0.2.33',
+    description='sql not knowing sql',
+    long_description='',
+    url='https://github.com/nalssee/tablemap.git',
+    author='nalssee',
+    author_email='jinisrolling@gmail.com',
+    license='MIT',
+)
