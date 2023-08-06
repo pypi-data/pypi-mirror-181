f95d7d0da9075c5260c7bf7c7fa40b25b41fccd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:38 +0000

    Add package pyMSDtorch/0.1.5/pyMSDtorch-0.1.5.tar.gz

---
+++ packages/pyMSDtorch/pyMSDtorch-0.1.5.tar.gz/pyMSDtorch-0.1.5/pyMSDtorch/core/networks/__init__.py
@@ -0,0 +1,10 @@
+"""
+All pyMSDNet models
+"""
+from . import AggNet, MSDNet, TUNet, SMSNet, SparseNet
+
+__all__ = ['AggNet',
+           'MSDNet',
+           'TUNet',
+           'SMSNet',
+           'SparseNet']
