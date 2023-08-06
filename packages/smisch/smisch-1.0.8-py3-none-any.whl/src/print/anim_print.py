13b4d0d043edd9d626788a94d37c0352a6f915cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:31 +0000

    Add package smisch/1.0.8/smisch-1.0.8-py3-none-any.whl

---
+++ packages/smisch/smisch-1.0.8-py3-none-any.whl/src/print/anim_print.py
@@ -0,0 +1,9 @@
+import time
+
+def animate_print(text,delay=0.05):
+    custom_text = []
+    for i in text:
+        custom_text.append(i)
+        time.sleep(delay)
+        print("".join(custom_text),end="\r")
+    print("\n\n")
