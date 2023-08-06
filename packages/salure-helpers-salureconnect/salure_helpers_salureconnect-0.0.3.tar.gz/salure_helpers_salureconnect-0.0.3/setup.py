4f429611d4bd38cfc5e0a266754f4bf9477cdef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:09 +0000

    Add package salure-helpers-salureconnect/0.0.3/salure_helpers_salureconnect-0.0.3.tar.gz

---
+++ packages/salure-helpers-salureconnect/salure_helpers_salureconnect-0.0.3.tar.gz/salure_helpers_salureconnect-0.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name='salure_helpers_salureconnect',
+    version='0.0.3',
+    description='SalureConnect wrapper from Salure',
+    long_description='SalureConnect wrapper from Salure',
+    author='D&A Salure',
+    author_email='support@salureconnnect.com',
+    packages=["salure_helpers.salureconnect"],
+    license='Salure License',
+    zip_safe=False,
+)
