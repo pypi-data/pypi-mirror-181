4c4b8828861968cc8e6805e600691b66ab0e2b50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:34:42 +0000

    Add package napari-conidie/0.0.1/napari-conidie-0.0.1.tar.gz

---
+++ packages/napari-conidie/napari-conidie-0.0.1.tar.gz/napari-conidie-0.0.1/src/napari_conidie/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.1"
+from ._widget import process_function_segmentation,save_modification,quantitative_data_for_all
+
+__all__ = (
+    "process_function_segmentation",
+    "save_modification",
+    "quantitative_data_for_all",
+)
