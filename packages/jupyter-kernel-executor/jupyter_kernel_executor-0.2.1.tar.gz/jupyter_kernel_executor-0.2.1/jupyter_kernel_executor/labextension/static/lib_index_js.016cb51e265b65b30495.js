e5179c5324132062214835624f874f0141a367b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:19 +0000

    Add package jupyter-kernel-executor/0.2.1/jupyter_kernel_executor-0.2.1.tar.gz

---
+++ packages/jupyter-kernel-executor/jupyter_kernel_executor-0.2.1.tar.gz/jupyter_kernel_executor-0.2.1/jupyter_kernel_executor/labextension/static/lib_index_js.016cb51e265b65b30495.js
@@ -0,0 +1,30 @@
+"use strict";
+(self["webpackChunkjupyter_kernel_executor"] = self["webpackChunkjupyter_kernel_executor"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/**
+ * Initialization data for the jupyter_kernel_executor extension.
+ */
+const plugin = {
+    id: 'jupyter_kernel_executor:plugin',
+    autoStart: true,
+    activate: (app) => {
+        console.log('JupyterLab extension jupyter_kernel_executor is activated!');
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.016cb51e265b65b30495.js.map
