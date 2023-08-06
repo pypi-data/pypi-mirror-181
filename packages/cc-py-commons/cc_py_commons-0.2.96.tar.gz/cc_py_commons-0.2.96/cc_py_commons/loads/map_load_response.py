f1d0635684b55d0ab1351a5c6b121c8f77cb4ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:17 +0000

    Add package cc-py-commons/0.2.96/cc_py_commons-0.2.96.tar.gz

---
+++ packages/cc-py-commons/cc_py_commons-0.2.96.tar.gz/cc_py_commons-0.2.96/cc_py_commons/loads/map_load_response.py
@@ -0,0 +1,20 @@
+from datetime import date
+from cc_py_commons.loads.load_schema import LoadSchema
+
+def execute(freight_hub_json):
+  freight_hub_json_copy = freight_hub_json.copy()
+  pickup_date = freight_hub_json.get('pickupDate')
+
+  if pickup_date:
+    freight_hub_json_copy['pickupDate'] = date.fromtimestamp(int(pickup_date)/1000).isoformat()
+
+  delivery_date = freight_hub_json.get('deliveryDate')
+
+  if delivery_date:
+    freight_hub_json_copy['deliveryDate'] = date.fromtimestamp(int(delivery_date)/1000).isoformat()
+
+  available_time = freight_hub_json.get('availableTime')
+  if available_time:
+    freight_hub_json_copy['availableTime'] = date.fromtimestamp(int(available_time) / 1000).isoformat()
+
+  return LoadSchema().load(freight_hub_json_copy)
