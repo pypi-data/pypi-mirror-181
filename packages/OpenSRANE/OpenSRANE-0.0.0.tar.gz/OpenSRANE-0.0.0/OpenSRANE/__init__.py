1576ca6fab7dc8233119fe2ac5f112ae8acd3371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:48 +0000

    Add package OpenSRANE/0.0.0/OpenSRANE-0.0.0.tar.gz

---
+++ packages/OpenSRANE/OpenSRANE-0.0.0.tar.gz/OpenSRANE-0.0.0/OpenSRANE/__init__.py
@@ -0,0 +1,24 @@
+# /* ****************************************************************** **
+# **    OpeSRANE - Open Software for Risk Assessment of Natech Events   **
+# **                                                                    **
+# **                                                                    **
+# **                                                                    **
+# ** (C) Copyright 2022, Bijan Sayyafzadeh                              **
+# **                                                                    **
+# ** All Rights Reserved.                                               **
+# **                                                                    **
+# ** Commercial use of this program without express permission of the   **
+# ** owner (Bijan SayyafZadeh and following Developers), is             **
+# ** strictly prohibited.  See file 'COPYRIGHT'  in main directory      **
+# ** for information on usage and redistribution,  and for a            **
+# ** DISCLAIMER OF ALL WARRANTIES.                                      **
+# **                                                                    **
+# ** Developed by:                                                      **
+# **   Bijan SayyafZadeh (B.Sayyaf@yahoo.com)                           **
+# **   MehDi Sharifi                                                    **
+# **   Abdolreza Sarvghad Moghaddam                                     **
+# **   Eslam Kashi                                                      **
+# **                                                                    **
+# ** ****************************************************************** */
+
+
