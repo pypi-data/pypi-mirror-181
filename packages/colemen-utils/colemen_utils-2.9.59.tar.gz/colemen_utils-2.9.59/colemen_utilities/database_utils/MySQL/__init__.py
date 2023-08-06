92289df08fd77ac8570cfe239cb4bfedafe71bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:57 +0000

    Add package colemen-utils/2.9.59/colemen_utils-2.9.59.tar.gz

---
+++ packages/colemen-utils/colemen_utils-2.9.59.tar.gz/colemen_utils-2.9.59/colemen_utilities/database_utils/MySQL/__init__.py
@@ -0,0 +1,22 @@
+# pylint: disable=missing-function-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=line-too-long
+# pylint: disable=unused-import
+
+
+
+# import colemen_utilities.database_utils.MySQL.MySQLDatabase as mysqlDB
+# import colemen_utilities.database_utils.MySQL.Column as Column
+# import colemen_utilities.database_utils.MySQL.CacheFile as cacheFile
+from colemen_utilities.database_utils.MySQL.Column import *
+from colemen_utilities.database_utils.MySQL.CacheFile import *
+from colemen_utilities.database_utils.MySQL.SelectQuery import *
+from colemen_utilities.database_utils.MySQL.UpdateQuery import *
+from colemen_utilities.database_utils.MySQL.DeleteQuery import *
+
+from colemen_utilities.database_utils.MySQL.MySQLDatabase import MySQLDatabase as MySQL
+
+
+
+
+
