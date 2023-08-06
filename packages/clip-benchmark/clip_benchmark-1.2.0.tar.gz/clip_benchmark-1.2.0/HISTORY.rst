b4902b74b6089949372ddaa7c85e60ac0b01d7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:01 +0000

    Add package clip-benchmark/1.2.0/clip_benchmark-1.2.0.tar.gz

---
+++ packages/clip-benchmark/clip_benchmark-1.2.0.tar.gz/clip_benchmark-1.2.0/HISTORY.rst
@@ -0,0 +1,24 @@
+## History
+
+### 1.2.0
+
+* Added support for loading webdatasets
+
+### 1.1.0
+
+* Added better support for multilingual eval
+* Added better support for linear probing
+* Added support for CuPL prompts
+
+### 1.0.1
+
+* pypi description as markdown
+
+### 1.0.0
+
+* Actual first release on PyPI.
+
+
+### 0.1.0
+
+* First release on PyPI.
---
+++ packages/clip-benchmark/clip_benchmark-1.2.0.tar.gz/clip_benchmark-1.2.0/tests/test_clip_benchmark.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""Tests for `clip_benchmark` package."""
+
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+from clip_benchmark.cli import run
+
+class base_args:
+    dataset="dummy"
+    split="test"
+    model="ViT-B-32-quickgelu"
+    pretrained="laion400m_e32"
+    task="zeroshot_classification"
+    amp=False
+    num_workers=4
+    batch_size=64
+    dataset_root="root"
+    output="result.json"
+    verbose=True
+    root="root"
+    annotation_file=""
+    seed=0
+    skip_load=False
+    language="en"
+    model_cache_dir=None
+    cupl=False
+    save_clf=None
+    load_clfs=[]
+
+def test_base():
+    run(base_args)
+
