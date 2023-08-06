918f4eeed95c3309d94e783a89329025c53762bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:17 +0000

    Add package rubi/1.0.4/rubi-1.0.4.tar.gz

---
+++ packages/rubi/rubi-1.0.4.tar.gz/rubi-1.0.4/rubi/contracts/__init__.py
@@ -0,0 +1,7 @@
+from .router import RubiconRouter, RubiconRouterSigner
+from .market import RubiconMarket, RubiconMarketSigner
+from .house import BathHouse, BathHouseSigner
+from .bath import BathToken, BathTokenSigner
+from .aid import FactoryAid, FactoryAidSigner, MarketAid, MarketAidSigner
+
+
