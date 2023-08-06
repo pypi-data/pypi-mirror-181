b5047ace24d9d3c4edd044ab30eb5a646558db8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:40 +0000

    Add package mbapy/0.0.8/mbapy-0.0.8.tar.gz

---
+++ packages/mbapy/mbapy-0.0.8.tar.gz/mbapy-0.0.8/mbapy/file.py
@@ -0,0 +1,10 @@
+import chardet
+
+
+def detect_byte_coding(bits:bytes):
+    adchar = chardet.detect(bits[:(1000 if len(bits) > 1000 else len(bits))])['encoding']
+    if adchar == 'gbk' or adchar == 'GBK' or adchar == 'GB2312':
+        true_text = bits.decode('GB2312', "ignore")
+    else:
+        true_text = bits.decode('utf-8', "ignore")
+    return true_text
