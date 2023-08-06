131529fa3ddce6b2d6b6e20510c5441b21ab77e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:24 +0000

    Add package pyxcp/0.19.3/pyxcp-0.19.3-cp39-cp39-win_amd64.whl

---
+++ packages/pyxcp/pyxcp-0.19.3-cp39-cp39-win_amd64.whl/pyxcp/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Universal Calibration Protocol for Python"""
+from .master import Master
+from .transport import Can
+from .transport import Eth
+from .transport import SxI
+from .transport import Usb
+
+# if you update this manually, do not forget to update .bumpversion.cfg
+__version__ = "0.19.3"
