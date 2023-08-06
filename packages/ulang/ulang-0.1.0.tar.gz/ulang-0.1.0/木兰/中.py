c1c74f9d31df67b5301263215d0be1f2e6d5c54d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:51 +0000

    Add package ulang/0.1.0/ulang-0.1.0.tar.gz

---
+++ packages/ulang/ulang-0.1.0.tar.gz/ulang-0.1.0/木兰/中.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+import ast
+import getopt
+import os
+import sys
+
+from 木兰.交互 import 开始交互
+from 木兰.分析器.词法分析器 import 分词器
+from 木兰.分析器.语法分析器 import 语法分析器
+from 木兰.功用.反馈信息 import 反馈信息
+from 木兰.环境 import 创建全局变量
+from 木兰.生成 import 木兰, python
+
+
+def 用途(程序):
+    介绍 = '''使用方法: %s 源码文件
+选项:
+ --版本,         -版   显示版本
+ --python变木兰, -兰   将 Python 源码转换为木兰源码
+ --语法树,       -树   语法树信息
+'''
+    sys.stderr.write(介绍 % os.path.basename(程序))
+    sys.exit(-1)
+
+
+def 中(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    try:
+        选项, 参数 = getopt.getopt(
+            argv[1:],
+            '兰版树',
+            [
+                "语法树",
+                "python变木兰",
+                '版本',
+                'dump-python'
+            ]
+        )
+    except getopt.GetoptError as e:
+        try:
+            sys.stderr.write(str(e) + '\n')
+            用途(argv[0])
+        finally:
+            e = None
+            del e
+
+    版本 = False
+    python变木兰 = False
+    语法树 = False
+    生成python代码 = False
+    for 某项, 值 in 选项:
+        if 某项 in ('-版', '--版本'):
+            版本 = True
+        elif 某项 in ("-兰", "--python变木兰"):
+            python变木兰 = True
+        elif 某项 in ("-树", "--语法树"):
+            语法树 = True
+        elif 某项 == '--dump-python':
+            生成python代码 = True
+
+    if 版本:
+        from 木兰 import __版本__
+        sys.stderr.write('%s\n' % __版本__)
+        sys.exit()
+
+    if len(sys.argv) == 1:
+        sys.exit(开始交互())
+
+    if len(参数) > 0:
+        源码文件 = 参数[0]
+    with open(源码文件, 'r', encoding='utf-8') as f:
+        源码 = f.read()
+
+    if python变木兰:
+        语法树节点 = ast.parse(源码, 源码文件)
+        print(木兰.转换(语法树节点))
+        return
+    try:
+        分析器 = 语法分析器(分词器)
+        节点 = 分析器.分析(源码, 源码文件)
+
+        if 生成python代码:
+            print(python.代码生成器().得到源码(节点))
+            return
+
+        if 语法树:
+            print(ast.dump(节点, True, True))
+            # print(语法树相关.格式化节点(节点, 1))
+            return
+        # 参考：https://docs.python.org/3.7/library/functions.html?highlight=compile#compile
+        可执行码 = compile(节点, 源码文件, 'exec')
+
+        环境变量 = 创建全局变量(文件名=源码文件)
+
+        exec(可执行码, 环境变量)
+
+    except SyntaxError as 语法错误:
+        sys.stderr.write(f"语法错误: {语法错误}\n")
+    except TypeError as 类型错误:
+        sys.stderr.write(f"类型错误: {类型错误}\n")
+    except ValueError as 语法错误:
+        sys.stderr.write(f"语法错误: {语法错误}\n")
+    except Exception as e:
+        try:
+            sys.stderr.write('%s\n' % 反馈信息(e, 源码文件))
+        finally:
+            e = None
+            del e
