fdb00010faadb886aec45e6701a1b88aa1d9288e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:26:56 +0000

    Add package cbra/0.165.0/cbra-0.165.0.tar.gz

---
+++ packages/cbra/cbra-0.165.0.tar.gz/cbra-0.165.0/cbra/ext/service/__init__.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2022 Cochise Ruhulessin
+#
+# All rights reserved. No warranty, explicit or implicit, provided. In
+# no event shall the author(s) be liable for any claim or damages.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+import fastapi
+
+from .clientkeysendpoint import ClientKeysEndpoint
+from .personaldatahandler import PersonalDataHandler
+from .resource import Resource
+from .service import Service
+from .serviceclient import ServiceClient
+from .serviceresource import ServiceResource
+
+
+__all__: list[str] = [
+    'ClientKeysEndpoint',
+    'Resource',
+    'PersonalDataHandler',
+    'PersonalDataService',
+    'Service',
+    'ServiceClient',
+    'ServiceResource'
+]
+
+
+CurrentService: ServiceClient = fastapi.Depends(ServiceClient.current)
+
+PersonalDataService: PersonalDataHandler = fastapi.Depends()
