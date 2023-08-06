28045e915e6c0fa679555111b807a12c4be171c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:44 +0000

    Add package kukur/0.1.7/kukur-0.1.7-py3-none-any.whl

---
+++ packages/kukur/kukur-0.1.7-py3-none-any.whl/kukur/source/arrow.py
@@ -0,0 +1,160 @@
+"""Common logic for the file formats supported by pyarrow."""
+
+# SPDX-FileCopyrightText: 2021 Timeseer.AI
+# SPDX-License-Identifier: Apache-2.0
+
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Generator
+
+import pyarrow as pa
+import pyarrow.compute
+import pyarrow.types
+
+from kukur import Metadata, SeriesSelector
+from kukur.base import SeriesSearch
+from kukur.exceptions import InvalidDataError
+from kukur.loader import Loader
+from kukur.source.quality import QualityMapper
+
+
+class BaseArrowSource(ABC):
+    """Base class for pyarrow file format data sources."""
+
+    __loader: Loader
+    __data_format: str
+    __quality_mapper: QualityMapper
+    __sort_by_timestamp: bool = False
+
+    def __init__(
+        self,
+        data_format: str,
+        loader: Loader,
+        quality_mapper: QualityMapper,
+        *,
+        sort_by_timestamp: bool = False,
+    ):
+        """Create a new data source."""
+        self.__loader = loader
+        self.__data_format = data_format
+        self.__quality_mapper = quality_mapper
+        self.__sort_by_timestamp = sort_by_timestamp
+
+    @abstractmethod
+    def read_file(self, file_like) -> pa.Table:
+        """Read the given file-like object using the pyarrow format reader."""
+        ...
+
+    @abstractmethod
+    def get_file_extension(self) -> str:
+        """Return the file extension for the supported pyarrow format."""
+        ...
+
+    def search(self, selector: SeriesSearch) -> Generator[Metadata, None, None]:
+        """ArrowSource does not support searching for time series."""
+
+    # pylint: disable=no-self-use
+    def get_metadata(self, selector: SeriesSelector) -> Metadata:
+        """Feather currently always returns empty metadata."""
+        return Metadata(selector)
+
+    def get_data(
+        self, selector: SeriesSelector, start_date: datetime, end_date: datetime
+    ) -> pa.Table:
+        """Read data in one of the predefined formats.
+
+        The complete file will be loaded in an Arrow table during processing.
+        """
+        data = self.__read_all_data(selector)
+        if self.__sort_by_timestamp is True:
+            data = data.sort_by("ts")
+        # pylint: disable=no-member
+        on_or_after = pyarrow.compute.greater_equal(data["ts"], pa.scalar(start_date))
+        before = pyarrow.compute.less(data["ts"], pa.scalar(end_date))
+        return data.filter(pyarrow.compute.and_(on_or_after, before))
+
+    def __read_all_data(self, selector: SeriesSelector) -> pa.Table:
+        if self.__data_format == "pivot":
+            return self._read_pivot_data(selector)
+
+        if self.__data_format == "dir":
+            return self._read_directory_data(selector)
+
+        return self._read_row_data(selector)
+
+    def _read_pivot_data(self, selector: SeriesSelector) -> pa.Table:
+        all_data = self.read_file(self.__loader.open())
+        if selector.name not in all_data.column_names:
+            raise InvalidDataError(f'column "{selector.name}" not found')
+        data = all_data.select([0, selector.name]).rename_columns(["ts", "value"])
+        schema = pa.schema(
+            [
+                ("ts", pa.timestamp("us", "utc")),
+                ("value", _get_value_schema_type(data)),
+            ]
+        )
+        return data.cast(schema)
+
+    def _read_row_data(self, selector: SeriesSelector) -> pa.Table:
+        columns = ["series name", "ts", "value"]
+        if self.__quality_mapper.is_present():
+            columns.append("quality")
+
+        all_data = self.read_file(self.__loader.open()).rename_columns(columns)
+        schema = pa.schema(
+            [
+                ("ts", pa.timestamp("us", "utc")),
+                ("value", _get_value_schema_type(all_data)),
+            ]
+        )
+        if self.__quality_mapper.is_present():
+            schema = schema.append(pa.field("quality", pa.int8()))
+            kukur_quality_values = self._map_quality(all_data["quality"])
+            all_data = all_data.set_column(
+                3, "quality", pa.array(kukur_quality_values, type=pa.int8())
+            )
+
+        # pylint: disable=no-member
+        data = all_data.filter(
+            pyarrow.compute.equal(all_data["series name"], pa.scalar(selector.name))
+        ).drop(["series name"])
+
+        return data.cast(schema)
+
+    def _read_directory_data(self, selector: SeriesSelector) -> pa.Table:
+        columns = ["ts", "value"]
+        if self.__quality_mapper.is_present():
+            columns.append("quality")
+
+        data = self.read_file(
+            self.__loader.open_child(
+                f"{selector.tags['series name']}.{self.get_file_extension()}"
+            )
+        ).rename_columns(columns)
+        schema = pa.schema(
+            [
+                ("ts", pa.timestamp("us", "utc")),
+                ("value", _get_value_schema_type(data)),
+            ]
+        )
+        if self.__quality_mapper.is_present():
+            schema = schema.append(pa.field("quality", pa.int8()))
+            kukur_quality_values = self._map_quality(data["quality"])
+            data = data.set_column(
+                2, "quality", pa.array(kukur_quality_values, type=pa.int8())
+            )
+        return data.cast(schema)
+
+    def _map_quality(self, quality_data: pa.array) -> pa.Table:
+        return [
+            self.__quality_mapper.from_source(source_quality_value.as_py())
+            for source_quality_value in quality_data
+        ]
+
+
+def _get_value_schema_type(data: pa.Table):
+    value_type = pa.float64()
+    if len(data) > 0:
+        if pyarrow.types.is_string(data["value"][0].type):
+            value_type = pa.string()
+    return value_type
---
+++ packages/kukur/kukur-0.1.7-py3-none-any.whl/kukur/source/csv/csv.py
@@ -0,0 +1,282 @@
+"""
+csv contains the CSV data source for Timeseer.
+
+Three formats are supported:
+- row based, with many series in one file containing one line per data point
+- directory based, with one file per series
+- pivot, with many series as columns in one file
+"""
+
+# SPDX-FileCopyrightText: 2021 Timeseer.AI
+# SPDX-License-Identifier: Apache-2.0
+
+import csv
+
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Any, Dict, Generator, List, Optional
+
+import pyarrow as pa
+import pyarrow.csv
+import pyarrow.compute
+
+from kukur import Dictionary, Metadata, SeriesSearch, SeriesSelector
+
+from kukur.loader import Loader, from_config as loader_from_config
+from kukur.exceptions import InvalidDataError, InvalidSourceException, KukurException
+from kukur.metadata import fields
+from kukur.source.metadata import MetadataMapper, MetadataValueMapper
+from kukur.source.quality import QualityMapper
+
+
+class InvalidMetadataError(KukurException):
+    """Raised when the metadata is invalid."""
+
+    def __init__(self, message: str):
+        KukurException.__init__(self, f"invalid metadata: {message}")
+
+
+def from_config(
+    config: Dict[str, Any],
+    metadata_mapper: MetadataMapper,
+    metadata_value_mapper: MetadataValueMapper,
+    quality_mapper: QualityMapper,
+):
+    """Create a new CSV data source from the given configuration dictionary."""
+    loaders = CSVLoaders()
+    if "path" in config:
+        loaders.data = loader_from_config(config, "path", files_as_path=True)
+    if "metadata" in config:
+        loaders.metadata = loader_from_config(config, "metadata", "r")
+    if "dictionary_dir" in config:
+        loaders.dictionary = loader_from_config(config, "dictionary_dir", "r")
+    data_format = config.get("format", "row")
+    metadata_fields: List[str] = config.get("metadata_fields", [])
+    if len(metadata_fields) == 0:
+        metadata_fields = config.get("fields", [])
+    mappers = CSVMappers(metadata_mapper, metadata_value_mapper, quality_mapper)
+    return CSVSource(data_format, metadata_fields, loaders, mappers)
+
+
+@dataclass
+class CSVLoaders:
+    """Data loaders for CSV sources."""
+
+    data: Optional[Loader] = None
+    metadata: Optional[Loader] = None
+    dictionary: Optional[Loader] = None
+
+
+@dataclass
+class CSVMappers:
+    """Value mappers for CSV sources."""
+
+    metadata: MetadataMapper
+    metadata_values: MetadataValueMapper
+    quality: QualityMapper
+
+
+class CSVSource:
+    """A CSV data source."""
+
+    __loaders: CSVLoaders
+    __data_format: str
+    __metadata_fields: List[str]
+    __mappers: CSVMappers
+
+    def __init__(
+        self,
+        data_format: str,
+        metadata_fields: List[str],
+        loaders: CSVLoaders,
+        mappers: CSVMappers,
+    ):
+        """Create a new CSV data source."""
+        self.__loaders = loaders
+        self.__metadata_fields = metadata_fields
+        self.__data_format = data_format
+        self.__mappers = mappers
+
+    def search(self, selector: SeriesSearch) -> Generator[Metadata, None, None]:
+        """Search for series matching the given selector."""
+        if self.__loaders.metadata is None:
+            return
+        with self.__loaders.metadata.open() as metadata_file:
+            reader = csv.DictReader(metadata_file)
+            for row in reader:
+                if self.__mappers.metadata.from_kukur("series name") not in row:
+                    raise InvalidMetadataError('column "series name" not found')
+                series_name = row[self.__mappers.metadata.from_kukur("series name")]
+                metadata = None
+                if "series name" in selector.tags:
+                    if series_name == selector.name:
+                        metadata = Metadata(
+                            SeriesSelector.from_tags(
+                                selector.source, selector.tags, selector.field
+                            )
+                        )
+                else:
+                    selector_tags = selector.tags.copy()
+                    selector_tags["series name"] = series_name
+                    metadata = Metadata(
+                        SeriesSelector.from_tags(
+                            selector.source, selector_tags, selector.field
+                        )
+                    )
+
+                if metadata is not None:
+                    field_names = [field for field, _ in metadata.iter_names()]
+                    if len(self.__metadata_fields) > 0:
+                        field_names = self.__metadata_fields
+                    for field in field_names:
+                        if self.__mappers.metadata.from_kukur(field) in row:
+                            try:
+                                value = row[self.__mappers.metadata.from_kukur(field)]
+                                metadata.coerce_field(
+                                    field,
+                                    self.__mappers.metadata_values.from_source(
+                                        field, value
+                                    ),
+                                )
+                            except ValueError:
+                                pass
+                    dictionary_name = metadata.get_field(fields.DictionaryName)
+                    if dictionary_name is not None:
+                        metadata.set_field(
+                            fields.Dictionary, self.__get_dictionary(dictionary_name)
+                        )
+                    yield metadata
+
+    def get_metadata(self, selector: SeriesSelector) -> Metadata:
+        """Read metadata, taking any configured metadata mapping into account."""
+        metadata = Metadata(selector)
+        if self.__loaders.metadata is None:
+            return metadata
+
+        with self.__loaders.metadata.open() as metadata_file:
+            reader = csv.DictReader(metadata_file)
+            for row in reader:
+                if self.__mappers.metadata.from_kukur("series name") not in row:
+                    raise InvalidMetadataError('column "series name" not found')
+                if (
+                    row[self.__mappers.metadata.from_kukur("series name")]
+                    != selector.name
+                ):
+                    continue
+                field_names = [field for field, _ in metadata.iter_names()]
+                if len(self.__metadata_fields) > 0:
+                    field_names = self.__metadata_fields
+                for field in field_names:
+                    if self.__mappers.metadata.from_kukur(field) in row:
+                        try:
+                            value = row[self.__mappers.metadata.from_kukur(field)]
+                            metadata.coerce_field(
+                                field,
+                                self.__mappers.metadata_values.from_source(
+                                    field, value
+                                ),
+                            )
+                        except ValueError:
+                            pass
+
+            dictionary_name = metadata.get_field(fields.DictionaryName)
+            if dictionary_name is not None:
+                metadata.set_field(
+                    fields.Dictionary, self.__get_dictionary(dictionary_name)
+                )
+
+        return metadata
+
+    def __get_dictionary(self, set_name: str) -> Optional[Dictionary]:
+        if self.__loaders.dictionary is None:
+            return None
+        if not self.__loaders.dictionary.has_child(f"{set_name}.csv"):
+            return None
+        with self.__loaders.dictionary.open_child(f"{set_name}.csv") as mapping_file:
+            reader = csv.reader(mapping_file)
+            mapping = {int(row[0]): row[1] for row in reader}
+            return Dictionary(mapping)
+
+    def get_data(
+        self, selector: SeriesSelector, start_date: datetime, end_date: datetime
+    ) -> pa.Table:
+        """Read data in one of the predefined formats.
+
+        The complete CSV file will be loaded in an Arrow table during processing.
+        """
+        data = self.__read_all_data(selector)
+        # pylint: disable=no-member
+        on_or_after = pyarrow.compute.greater_equal(data["ts"], pa.scalar(start_date))
+        before = pyarrow.compute.less(data["ts"], pa.scalar(end_date))
+        return data.filter(pyarrow.compute.and_(on_or_after, before))
+
+    def __read_all_data(self, selector: SeriesSelector) -> pa.Table:
+        if self.__loaders.data is None:
+            raise InvalidSourceException("No data path configured.")
+        if self.__data_format == "pivot":
+            return _read_pivot_data(self.__loaders.data, selector)
+
+        if self.__data_format == "dir":
+            return self._read_directory_data(self.__loaders.data, selector)
+
+        return self._read_row_data(self.__loaders.data, selector)
+
+    def _read_row_data(self, loader: Loader, selector: SeriesSelector) -> pa.Table:
+        columns = ["series name", "ts", "value"]
+        if self.__mappers.quality.is_present():
+            columns.append("quality")
+        read_options = pyarrow.csv.ReadOptions(column_names=columns)
+        convert_options = pyarrow.csv.ConvertOptions(
+            column_types={"ts": pa.timestamp("us", "utc")},
+        )
+        all_data = pyarrow.csv.read_csv(
+            loader.open(), read_options=read_options, convert_options=convert_options
+        )
+        if self.__mappers.quality.is_present():
+            kukur_quality_values = self._map_quality(all_data["quality"])
+            all_data = all_data.set_column(
+                3, "quality", pa.array(kukur_quality_values, type=pa.int8())
+            )
+
+        # pylint: disable=no-member
+        data = all_data.filter(
+            pyarrow.compute.equal(all_data["series name"], pa.scalar(selector.name))
+        )
+        return data.drop(["series name"])
+
+    def _read_directory_data(
+        self, loader: Loader, selector: SeriesSelector
+    ) -> pa.Table:
+        columns = ["ts", "value"]
+        if self.__mappers.quality.is_present():
+            columns.append("quality")
+        read_options = pyarrow.csv.ReadOptions(column_names=columns)
+        convert_options = pyarrow.csv.ConvertOptions(
+            column_types={"ts": pa.timestamp("us", "utc")},
+        )
+        all_data = pyarrow.csv.read_csv(
+            loader.open_child(f"{selector.tags['series name']}.csv"),
+            read_options=read_options,
+            convert_options=convert_options,
+        )
+        if self.__mappers.quality.is_present():
+            kukur_quality_values = self._map_quality(all_data["quality"])
+            return all_data.set_column(
+                2, "quality", pa.array(kukur_quality_values, type=pa.int8())
+            )
+        return all_data
+
+    def _map_quality(self, quality_data: pa.array) -> pa.Table:
+        return [
+            self.__mappers.quality.from_source(source_quality_value.as_py())
+            for source_quality_value in quality_data
+        ]
+
+
+def _read_pivot_data(loader: Loader, selector: SeriesSelector) -> pa.Table:
+    all_data = pyarrow.csv.read_csv(loader.open())
+    if selector.name not in all_data.column_names:
+        raise InvalidDataError(f'column "{selector.name}" not found')
+    columns = ["ts", "value"]
+    schema = pa.schema([("ts", pa.timestamp("us", "utc")), ("value", pa.float64())])
+    return all_data.select([0, selector.name]).rename_columns(columns).cast(schema)
