90b319d0278c5c24ab2dddf45f9cf1a2b0bd2916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:20:03 +0000

    Add package configdict/2.5.2/configdict-2.5.2.tar.gz

---
+++ packages/configdict/configdict-2.5.2.tar.gz/configdict-2.5.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+readme = open('README.rst').read()
+version = (2, 5, 2)
+
+setup(
+    name='configdict',
+    python_requires=">=3.8",
+    version=".".join(map(str, version)),
+    description='A supercharged dict used as configuration',
+    long_description=readme,
+    author='Eduardo Moguillansky',
+    author_email='eduardo.moguillansky@gmail.com',
+    url='https://github.com/gesellkammer/configdict',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "setuptools",
+        "appdirs",
+        "PyYAML",
+        "watchdog",
+        "fuzzywuzzy"
+    ],
+    license="BSD",
+    zip_safe=False,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.8'
+    ],
+)
