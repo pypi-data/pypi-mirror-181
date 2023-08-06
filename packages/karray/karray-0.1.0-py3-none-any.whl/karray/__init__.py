d49ac004eabb7bd0eb81e4f7bcff285bef228a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:56 +0000

    Add package karray/0.1.0/karray-0.1.0-py3-none-any.whl

---
+++ packages/karray/karray-0.1.0-py3-none-any.whl/karray/__init__.py
@@ -0,0 +1,12 @@
+# karray
+# copyright 2022, Carlos Gaete-Morales, DIW-Berlin 
+"""
+    karray
+    copyright 2022, Carlos Gaete-Morales, DIW-Berlin 
+"""
+__version__ = (0, 1, 0)
+__author__ = 'Carlos Gaete-Morales'
+
+
+from .array import Long, Array, numpy_to_long, concat, from_feather
+from .setting import settings
