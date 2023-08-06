e3fbcf477fc14625482b0bcdc676b12dd1fa9a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:01 +0000

    Add package zpp-ManagedFile/1.0.0/zpp_ManagedFile-1.0.0-py3-none-any.whl

---
+++ packages/zpp-ManagedFile/zpp_ManagedFile-1.0.0-py3-none-any.whl/zpp_ManagedFile/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '1.0.0'
+__author__ = 'ZephyrOff  (Alexandre Pajak)'
+__descriptor__ = 'Système de fichier managé pour le contrôle des actions sur un fichier'
+__lastupdate__ = '15/12/2022'
+__releasenote__ = 'First update'
+
+from .ManagedFile import *
