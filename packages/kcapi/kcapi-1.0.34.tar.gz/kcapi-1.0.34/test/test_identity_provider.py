3779dc7db7c233a25ca64fc02ded80c6b0997515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:42 +0000

    Add package kcapi/1.0.34/kcapi-1.0.34.tar.gz

---
+++ packages/kcapi/kcapi-1.0.34.tar.gz/kcapi-1.0.34/test/test_identity_provider.py
@@ -0,0 +1,43 @@
+import unittest, time
+from .testbed import KcBaseTestCase
+import json
+
+
+def load_sample(fname):
+    f = open(fname)
+    file1 = json.loads(f.read())
+    f.close()
+    return file1
+
+
+class TestingUserAPI(KcBaseTestCase):
+
+    def test_identity_provider_adding_saml_and_oid_IDP_providers(self):
+        idp1 = self.kc.build('identity-provider', self.REALM)
+        idp = self.kc.build('idp', self.REALM)
+        saml = load_sample('./test/payloads/idp_saml.json')
+        oid = load_sample('./test/payloads/idp_oid.json')
+        state1 = idp.create(saml).isOk()
+        self.assertTrue(state1)
+
+        state2 = idp1.create(oid).isOk()
+        self.assertTrue(state2)
+
+        oidc_ip = idp1.findFirstByKV('alias', 'oidc666')
+        saml_ip = idp1.findFirstByKV('alias', 'saml666')
+
+        self.assertEqual(saml_ip['alias'], saml['alias'], 'SAML alias should match')
+        self.assertEqual(oidc_ip['alias'], oid['alias'], 'SAML alias should match')
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.testbed.createRealms()
+        cls.testbed.createUsers()
+        cls.testbed.createClients()
+        cls.REALM = cls.testbed.REALM
+        cls.kc = cls.testbed.getKeycloak()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/kcapi/kcapi-1.0.34.tar.gz/kcapi-1.0.34/test/testbed.py
@@ -0,0 +1,113 @@
+from kcapi import OpenID, Keycloak
+import os
+import json
+import unittest
+from vcr_unittest import VCRTestCase
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+def readFromJSON(filename):
+    with open(filename) as json_file:
+        return json.load(json_file)
+
+class TestBed:
+    def __init__(self, realm = None, username = None, password = None, endpoint = None): 
+
+        self.USER = os.environ['KC_USER']
+        self.PASSWORD = os.environ['KC_PASSWORD']
+        self.REALM = os.environ['KC_REALM']
+        self.ENDPOINT = os.environ['KC_ENDPOINT']
+
+        self.groupName = 'DC'
+        self.roleNames = ['level-1', 'level-2', 'level-3'] 
+
+        # TODO this is not intercepted by vcrpy.
+        token = OpenID.createAdminClient(self.USER, self.PASSWORD, url=self.ENDPOINT).getToken()
+        self.kc = Keycloak(token, self.ENDPOINT)
+        self.master_realm = self.kc.admin()
+        self.realm = self.REALM 
+        self.token = token
+
+    def deleteRealms(self):
+        realm = self.realm
+        if self.master_realm.existByKV("id", realm):
+            self.master_realm.removeFirstByKV("id", realm)
+
+    def createRealms(self):
+        realm = self.realm
+        self.master_realm.create({"enabled": "true", "id": realm, "realm": realm})
+
+    def createGroups(self):
+        group = self.kc.build('groups', self.realm)
+        g_creation_state = group.create({"name": self.groupName}).isOk()
+        self.createRoles()
+
+
+    def createRoles(self):
+        roles = self.kc.build('roles', self.realm)
+        for role in self.roleNames:
+            roles.create({"name": role}).isOk() 
+
+
+    def createClients(self):
+        realm = self.realm
+        client = {"enabled":True,
+                  "attributes":{},
+                  "redirectUris":[],
+                  "clientId":"dc",
+                  "protocol":"openid-connect", 
+                  "directAccessGrantsEnabled":True
+                  }
+
+        clients = self.kc.build('clients', realm)
+        if not clients.create(client).isOk(): 
+            raise Exception('Cannot create Client')
+
+    def createUsers(self):
+        realm = self.realm
+        test_users = [
+                {"enabled":'true',"attributes":{},"username":"batman","firstName":"Bruce", "lastName":"Wayne", "emailVerified":""}, 
+                {"enabled":'true',"attributes":{},"username":"superman","firstName":"Clark", "lastName":"Kent", "emailVerified":""}, 
+                {"enabled":'true',"attributes":{},"username":"aquaman","firstName":"AAA%", "lastName":"Corrupt", "emailVerified":""}
+        ]
+
+        users = self.kc.build('users', realm)
+        
+        for usr in test_users: 
+            users.create(usr).isOk()
+
+    def goodBye(self): 
+        state = self.master_realm.remove(self.realm).ok()
+        if not state:
+            raise Exception("Cannot delete the realm -> " + self.realm )
+
+    def getKeycloak(self):
+        return self.kc
+    def getAdminRealm(self):
+        return self.master_realm
+
+
+class KcBaseTestCase(VCRTestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.testbed = TestBed()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.testbed.goodBye()
+        super().tearDownClass()
+        return True
+
+    def _get_vcr_kwargs(self):
+        kwargs = super()._get_vcr_kwargs()
+        # https://vcrpy.readthedocs.io/en/latest/usage.html#record-modes
+        # "once" or "new_episodes" makes sense - which is better?
+        # "new_episodes" is more auto-magic, use it.
+        # BUT - authentiaction is in setUp(), is not recorded, so each time a diffrent access token is send,
+        # and VCR is not used.
+        # We use "none".
+        vcrpy_record_mode = os.environ.get("VCRPY_RECORD_MODE", "none")
+        kwargs['record_mode'] = vcrpy_record_mode
+        return kwargs
