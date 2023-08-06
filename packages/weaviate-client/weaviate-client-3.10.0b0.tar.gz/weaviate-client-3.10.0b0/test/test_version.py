135a9210d07c30cd3f675fb7e496570d0763788e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:53 +0000

    Add package weaviate-client/3.10.0b0/weaviate-client-3.10.0b0.tar.gz

---
+++ packages/weaviate-client/weaviate-client-3.10.0b0.tar.gz/weaviate-client-3.10.0b0/test/test_version.py
@@ -0,0 +1,11 @@
+import unittest
+import weaviate
+
+
+class TestVersion(unittest.TestCase):
+    def test_version(self):
+        """
+        Test the `__version__` global variable.
+        """
+
+        self.assertEqual(weaviate.__version__, "3.10.0b0", "Check if the version is set correctly!")
