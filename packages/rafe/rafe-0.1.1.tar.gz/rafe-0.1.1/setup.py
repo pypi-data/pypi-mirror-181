9b15229e1c40b08ba34d58e13fcde513aab87292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:59 +0000

    Add package rafe/0.1.1/rafe-0.1.1.tar.gz

---
+++ packages/rafe/rafe-0.1.1.tar.gz/rafe-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import sys
+from setuptools import setup
+
+import rafe
+
+assert sys.argv[1] in ('develop', 'sdist')
+
+
+setup(
+    name = "rafe",
+    version = rafe.__version__,
+    author = "Ilan Schnell",
+    url = "https://github.com/Quansight/rafe",
+    license = "BSD",
+    description = "Reproducible Artifacts for Environments",
+    long_description = open('README.md').read(),
+    packages = ['rafe', 'examples'],
+    entry_points = {'console_scripts': [
+        'build = rafe.build:main',
+    ]},
+    package_data = {'examples': ['*/*']},
+)
