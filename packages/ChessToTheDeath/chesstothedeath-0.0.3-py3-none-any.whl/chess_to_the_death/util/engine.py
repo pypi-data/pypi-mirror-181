d975ec9bcad4c35f1863cdb8a0bcb1394dfa8595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:35 +0000

    Add package ChessToTheDeath/0.0.3/chesstothedeath-0.0.3-py3-none-any.whl

---
+++ packages/ChessToTheDeath/chesstothedeath-0.0.3-py3-none-any.whl/chess_to_the_death/util/engine.py
@@ -0,0 +1,290 @@
+import numpy as np
+from chess_to_the_death.util.pieces import *
+from chess_to_the_death.util.player import Player
+
+
+def bothWayDic(dic: dict) -> dict:
+    """
+    takes a dictionary and adds every (value, key) entry,
+    essentially flipping the given entries.
+    """
+    dic.update(dict(map(reversed, dic.items())))
+    return dic
+
+
+DIMENSION = (8, 8)
+flipDic = bothWayDic({0: 7, 1: 6, 2: 5, 3: 4})
+pieceTranslateDic = bothWayDic({'p': 1, 'b': 2, 'n': 3, 'r': 4, 'q': 5, 'k': 6})
+
+
+class GameState:
+    player_turn = True # True -> 'white', False -> 'black'
+    board: np.ndarray = None
+    white_casualties, black_casualties = [], []
+
+    def __init__(self, image_size):
+        self.image_size = image_size
+        
+        white_pieces = []
+        white_pieces.append(Rook(  'r', 0, 7, Player.PLAYER_W, image_size))
+        white_pieces.append(Rook(  'r', 7, 7, Player.PLAYER_W, image_size))
+        white_pieces.append(Knight('n', 1, 7, Player.PLAYER_W, image_size))
+        white_pieces.append(Knight('n', 6, 7, Player.PLAYER_W, image_size))
+        white_pieces.append(Bishop('b', 2, 7, Player.PLAYER_W, image_size))
+        white_pieces.append(Bishop('b', 5, 7, Player.PLAYER_W, image_size))
+        white_pieces.append(Queen( 'q', 3, 7, Player.PLAYER_W, image_size))
+        white_pieces.append(King(  'k', 4, 7, Player.PLAYER_W, image_size))
+        white_pieces.append(Pawn(  'p', 0, 6, Player.PLAYER_W, image_size))
+        white_pieces.append(Pawn(  'p', 1, 6, Player.PLAYER_W, image_size))
+        white_pieces.append(Pawn(  'p', 2, 6, Player.PLAYER_W, image_size))
+        white_pieces.append(Pawn(  'p', 3, 6, Player.PLAYER_W, image_size))
+        white_pieces.append(Pawn(  'p', 4, 6, Player.PLAYER_W, image_size))
+        white_pieces.append(Pawn(  'p', 5, 6, Player.PLAYER_W, image_size))
+        white_pieces.append(Pawn(  'p', 6, 6, Player.PLAYER_W, image_size))
+        white_pieces.append(Pawn(  'p', 7, 6, Player.PLAYER_W, image_size))
+
+        black_pieces = []
+        black_pieces.append(Rook(  'r', 0, 0, Player.PLAYER_B, image_size))
+        black_pieces.append(Rook(  'r', 7, 0, Player.PLAYER_B, image_size))
+        black_pieces.append(Knight('n', 1, 0, Player.PLAYER_B, image_size))
+        black_pieces.append(Knight('n', 6, 0, Player.PLAYER_B, image_size))
+        black_pieces.append(Bishop('b', 2, 0, Player.PLAYER_B, image_size))
+        black_pieces.append(Bishop('b', 5, 0, Player.PLAYER_B, image_size))
+        black_pieces.append(Queen( 'q', 3, 0, Player.PLAYER_B, image_size))
+        black_pieces.append(King(  'k', 4, 0, Player.PLAYER_B, image_size))
+        black_pieces.append(Pawn(  'p', 0, 1, Player.PLAYER_B, image_size))
+        black_pieces.append(Pawn(  'p', 1, 1, Player.PLAYER_B, image_size))
+        black_pieces.append(Pawn(  'p', 2, 1, Player.PLAYER_B, image_size))
+        black_pieces.append(Pawn(  'p', 3, 1, Player.PLAYER_B, image_size))
+        black_pieces.append(Pawn(  'p', 4, 1, Player.PLAYER_B, image_size))
+        black_pieces.append(Pawn(  'p', 5, 1, Player.PLAYER_B, image_size))
+        black_pieces.append(Pawn(  'p', 6, 1, Player.PLAYER_B, image_size))
+        black_pieces.append(Pawn(  'p', 7, 1, Player.PLAYER_B, image_size))
+
+        self.pieces: list[Piece] = white_pieces + black_pieces
+        self.createBoard()
+        
+        print(self.__repr__())
+
+    def currentPlayer(self) -> str:
+        """
+        returns 'white' or 'black'.
+        """
+        return Player.OPTIONS[self.player_turn]
+
+    def getPiece(self, col: int, row: int) -> Piece:
+        """
+        Takes x,y (col,row) coordinates andd returns
+        the corresponding piece standing on that position.
+        """
+        for piece in self.pieces:
+            if piece.cell_col == col and piece.cell_row == row:
+                return piece
+        return None
+
+    def selectablePiece(self, piece: Piece) -> bool:
+        """
+        Returns a boolean, whether or not the selected Piece
+        belongs no the current team-color.
+        """
+        return piece._player == self.currentPlayer()
+
+    def isEmptyCell(self, col: int, row: int) -> bool:
+        return not self.getPiece(col, row)
+
+    def promotePiece(self, piece: Piece, newPieceName: str) -> None:
+        """
+        Takes an existing Piece and promotes it to another Piece-Type
+        corresponding to the given 'newPieceName' identifier.
+        (e.g. promotePiece(Pawn(...), 'q') replaces the Pawn with a new Queen)
+        This should only happen to Pawn-pieces, checks havee to be made beforehand.
+        """
+        if newPieceName == 'r':
+            promotedPiece = Rook(
+                'r', piece.cell_col, piece.cell_row, piece._player, self.image_size)
+        elif newPieceName == 'n':
+            promotedPiece = Knight(
+                'n', piece.cell_col, piece.cell_row, piece._player, self.image_size)
+        elif newPieceName == 'b':
+            promotedPiece = Bishop(
+                'b', piece.cell_col, piece.cell_row, piece._player, self.image_size)
+        else:
+            promotedPiece = Queen(
+                'q', piece.cell_col, piece.cell_row, piece._player, self.image_size)
+        self.pieces.append(promotedPiece)
+        self.pieces.remove(piece)
+
+    def promotePawnOption(self, piece: Piece) -> bool:
+        """
+        Checks whether or not a piece can be promoted.
+        It has to be a pawn and it has to have reached the top
+        of the board.
+        """
+        return piece._name == 'p' and piece.cell_row == 0
+
+    def move(self, piece: Piece, to_col: int, to_row: int, options_move: list) -> bool:
+        """
+            moves a 'piece' to the new coordinates 'to_col','to_row'
+            if it is a valid move. Also checks for castling.
+            Returns True if the move is valid and has been taken.
+        """
+        if not self.isEmptyCell(to_col, to_row):
+            return False
+        if not (to_col, to_row) in options_move:
+            return False
+        leftCastle, rook = self.getCastleOptionLeft(piece)
+        if leftCastle == (to_col, to_row):
+            rook.move(to_col+1, to_row)
+        rightCastle, rook = self.getCastleOptionRight(piece)
+        if rightCastle == (to_col, to_row):
+            rook.move(to_col-1, to_row)
+        piece.move(to_col, to_row)
+        return True
+
+    def attack(self, piece: Piece, to_col: int, to_row: int, options_attack: list) -> bool:
+        """
+            'piece' attacks another piece at the coordinates 'to_col','to_row'
+            if it is a valid attack. 
+            Returns True if the attack is valid and has been taken.
+        """
+        if self.isEmptyCell(to_col, to_row):
+            return False
+        if not (to_col, to_row) in options_attack:
+            return False
+        attacked_piece = self.getPiece(to_col, to_row)
+        attacked_piece.health -= piece.damage
+        if attacked_piece.health <= 0:
+            piece.move(attacked_piece.cell_col, attacked_piece.cell_row)
+            self.pieces.remove(attacked_piece)
+            print("Dead:", attacked_piece)
+            if attacked_piece._player == Player.PLAYER_W:
+                self.white_casualties.append(attacked_piece)
+            else:
+                self.black_casualties.append(attacked_piece)
+        return True
+
+    def playerWon(self) -> str:
+        """
+        Checks if a player has won, by successfully defeating the
+        enemy king.
+        Returns 'white' or 'black' according to the team that won,
+        or returns an empty string no team has won yet.
+        """
+        currentEnemyPlayer = Player.OPTIONS[not self.player_turn]
+        for piece in self.pieces:
+            if piece._player == currentEnemyPlayer and piece._name == 'k':
+                return ''
+        return self.currentPlayer()
+
+    def getOptions(self, piece: Piece) -> tuple:
+        """
+        Returns a tuple of lists containing all valid moves
+        and attacks of a given 'piece'.
+        """
+        if not piece:
+            return ([], [])
+        options_move, options_attack = piece.getOptions(self.board)
+        leftCastle, _ = self.getCastleOptionLeft(piece)
+        rightCastle, _ = self.getCastleOptionRight(piece)
+        if leftCastle:
+            options_move.append(leftCastle)
+        if rightCastle:
+            options_move.append(rightCastle)
+
+        return (options_move, options_attack)
+
+    def getCastleOptionLeft(self, piece: Piece) -> tuple:
+        """
+        Takes a 'piece' and checks if it has
+        the option to castle of the left side.
+        If so the function returns a tuple containing the position
+        to which the piece can castle, aswell as the rook included
+        in the move.
+        """
+        if piece._name != 'k' or not piece.firstMove:
+            return (None, None)
+        if (self.board[piece.cell_row, piece.cell_col-1] != 0) or (
+                self.board[piece.cell_row, piece.cell_col-2] != 0):
+            return (None, None)
+        if (self.currentPlayer() == Player.PLAYER_W):
+            if (self.board[piece.cell_row, piece.cell_col-3] != 0):
+                return (None, None)
+            if (self.board[piece.cell_row, piece.cell_col-4] == pieceTranslateDic['r']) and (
+                    self.getPiece(piece.cell_col-4, piece.cell_row).firstMove):
+                return ((piece.cell_col-2, piece.cell_row), self.getPiece(piece.cell_col-4, piece.cell_row))
+        if (self.currentPlayer() == Player.PLAYER_B):
+            if (self.board[piece.cell_row, piece.cell_col-3] == -pieceTranslateDic['r']) and (
+                    self.getPiece(piece.cell_col-3, piece.cell_row).firstMove):
+                return ((piece.cell_col-2, piece.cell_row), self.getPiece(piece.cell_col-3, piece.cell_row))
+        return (None, None)
+
+    def getCastleOptionRight(self, piece: Piece):
+        """
+        Takes a 'piece' and checks if it has
+        the option to castle of the right side.
+        If so the function returns a tuple containing the position
+        to which the piece can castle, aswell as the rook included
+        in the move.
+        """
+        if piece._name != 'k' or not piece.firstMove:
+            return (None, None)
+        if (self.board[piece.cell_row, piece.cell_col+1] != 0) or (
+                self.board[piece.cell_row, piece.cell_col+1] != 0):
+            return (None, None)
+        if (self.currentPlayer() == Player.PLAYER_W):
+            if (self.board[piece.cell_row, piece.cell_col+3] == pieceTranslateDic['r']) and (
+                    self.getPiece(piece.cell_col+3, piece.cell_row).firstMove):
+                return ((piece.cell_col+2, piece.cell_row), self.getPiece(piece.cell_col+3, piece.cell_row))
+        if (self.currentPlayer() == Player.PLAYER_B):
+            if (self.board[piece.cell_row, piece.cell_col+3] != 0):
+                return (None, None)
+            if (self.board[piece.cell_row, piece.cell_col+4] == -pieceTranslateDic['r']) and (
+                    self.getPiece(piece.cell_col+4, piece.cell_row).firstMove):
+                return ((piece.cell_col+2, piece.cell_row), self.getPiece(piece.cell_col+4, piece.cell_row))
+        return (None, None)
+
+    def flipBoard(self) -> None:
+        """
+        flips the gameboard, by mirroring the
+        x,y coordinates of all pieces at the center
+        of the board.
+        """
+        for piece in self.pieces:
+            piece.cell_col = flipDic[piece.cell_col]
+            piece.cell_row = flipDic[piece.cell_row]
+
+    def createBoard(self) -> None:
+        """
+        Creates the gameboard as a numpy.ndarray representation.
+        Each piece has a unique number identifier stored in 'pieceTranslateDic'.
+        e.g.: startingPosition:
+        [[-4. -3. -2. -5. -6. -2. -3. -4.]       
+        [-1. -1. -1. -1. -1. -1. -1. -1.]       
+        [ 0.  0.  0.  0.  0.  0.  0.  0.]       
+        [ 0.  0.  0.  0.  0.  0.  0.  0.]       
+        [ 0.  0.  0.  0.  0.  0.  0.  0.]       
+        [ 0.  0.  0.  0.  0.  0.  0.  0.]       
+        [ 1.  1.  1.  1.  1.  1.  1.  1.]       
+        [ 4.  3.  2.  5.  6.  2.  3.  4.]]
+        """
+        self.board = np.zeros(DIMENSION)
+        for piece in self.pieces:
+            self.board[piece.cell_row, piece.cell_col] = pieceTranslateDic[piece._name] * (
+                1 if piece._player == "white" else -1
+            )
+
+    def nextTurn(self):
+        """
+        flips the board at switches to thee team whose
+        turn it is at the moment.
+        """
+        self.player_turn = not self.player_turn
+        self.flipBoard()
+        self.createBoard()
+        print(self.__repr__())
+
+    def __str__(self) -> str:
+        return self.board.__str__()
+
+    def __repr__(self) -> str:
+        return self.__str__()
---
+++ packages/ChessToTheDeath/chesstothedeath-0.0.3-py3-none-any.whl/chess_to_the_death/util/gui.py
@@ -0,0 +1,258 @@
+import pygame
+from itertools import product
+
+import chess_to_the_death.util.engine as engine
+import chess_to_the_death.util.fpsClock as fpsClock
+from chess_to_the_death.util.loader import loadImage
+from chess_to_the_death.util.pieces import Piece # only for type-hints
+
+BOARD_SIZE = (1024, 1024)
+CELL_SIZE = (BOARD_SIZE[0] // engine.DIMENSION[0],
+             BOARD_SIZE[1] // engine.DIMENSION[1])
+HALF_CELL_SIZE = (CELL_SIZE[0]//2, CELL_SIZE[1]//2)
+IMAGE_OFFSET = 24
+IMG_SIZE = tuple([size - (2*IMAGE_OFFSET) for size in CELL_SIZE])
+MAX_FPS = 30
+COLORS = [(230, 230, 230), #"#E6E6E6" -> WHITE / CELL
+          ( 32,  33,  36), #"#202124" -> DARK_GRAY / CELL
+          (255,   0,   0), #"#FF0000" -> RED / HEALTH
+          (  0,   0, 255), #"#0000FF" -> BLUE / SELECTED
+          (  0, 255,   0), #"#00FF00" -> GREEN / MOVABLE
+          (255,   0,   0), #"#FF0000" -> RED / ATTACKABLE
+          ( 46, 149, 153), #"#2E9599" -> TEAL / TEXT
+          (  0,   0,   0)] #"#000000" -> BLACK / REDO
+
+
+class Holder:
+    selectedCell: Piece = None
+    winner: str = ''
+    options_move, options_attack = [], []
+    attack_icon: pygame.Surface = None
+    fps: fpsClock = None
+
+
+def drawBoard(mainScreen: pygame.Surface) -> None:
+    """
+    Draws the black and white background cells.
+    """
+    for x in product(range(engine.DIMENSION[0]), range(engine.DIMENSION[1])):
+        pygame.draw.rect(mainScreen, COLORS[sum(x) % 2],
+                         pygame.Rect(x[0] * CELL_SIZE[0],
+                                     x[1] * CELL_SIZE[1],
+                                     *CELL_SIZE))
+
+
+def highlightCell(mainScreen: pygame.Surface, x: int, y: int, color: tuple) -> None:
+    """
+    Highlights a single cell at the coordinates x,y
+    with the 'color' given as a tuple (R, G, B).
+    """
+    highlight = pygame.Surface(CELL_SIZE)
+    highlight.set_alpha(75)
+    highlight.fill(color)
+    mainScreen.blit(highlight, (x * CELL_SIZE[0], y * CELL_SIZE[1]))
+
+
+def highlightCells(mainScreen: pygame.Surface, piece: Piece, options_move: list, options_attack: list) -> None:
+    """
+    Highlights the selected cell.
+    Highlights the valid moves of the selected cell.
+    Highlights the valid attacks of the selected cell.
+    """
+    if piece:
+        highlightCell(mainScreen, piece.cell_col, piece.cell_row, COLORS[3])
+    for option in options_move:
+        highlightCell(mainScreen, option[0], option[1], COLORS[4])
+    for option in options_attack:
+        highlightCell(mainScreen, option[0], option[1], COLORS[5])
+
+
+def drawPieces(mainScreen: pygame.Surface, gameState: engine.GameState, attack_icon: pygame.Surface) -> None:
+    """
+    Draws all pieces of the engine.board, including their health- and attackvalues
+    """
+    for piece in gameState.pieces:
+        mainScreen.blit(piece.image,
+                        pygame.Rect(piece.cell_col * CELL_SIZE[0] + IMAGE_OFFSET,
+                                    piece.cell_row * CELL_SIZE[1] + IMAGE_OFFSET,
+                                    *IMG_SIZE))
+        mainScreen.blit(attack_icon, (piece.cell_col * CELL_SIZE[0], piece.cell_row * CELL_SIZE[1]))
+        font = pygame.font.SysFont("Verdana", 16)
+        mainScreen.blit(font.render(str(piece.damage), True, COLORS[6]),
+                        (piece.cell_col * CELL_SIZE[0] + 25,
+                         piece.cell_row * CELL_SIZE[1]))
+        pygame.draw.rect(mainScreen, COLORS[2],
+                         pygame.Rect(
+                             piece.cell_col * CELL_SIZE[0] + (CELL_SIZE[0]//10),
+                             (piece.cell_row + 1) * CELL_SIZE[1] - IMAGE_OFFSET + (CELL_SIZE[0]//10),
+                             (piece.health * (CELL_SIZE[0] - (CELL_SIZE[0]//5)))//piece.maxHealth,
+                             IMAGE_OFFSET // 3))
+        font = pygame.font.SysFont("Verdana", 8)
+        mainScreen.blit(font.render(str(piece.health) + "/" + str(piece.maxHealth), True, COLORS[6]),
+                        (piece.cell_col * CELL_SIZE[0] + (CELL_SIZE[0]//10),
+                         (piece.cell_row + 1) * CELL_SIZE[1] - IMAGE_OFFSET))
+
+
+def drawWinner(mainScreen: pygame.Surface, winner: str) -> None:
+    """
+    If a player has won this function will display the win-message
+    on the 'mainScreen'.
+    """
+    if not winner:
+        return
+    font = pygame.font.SysFont("Verdana", 64)
+    text = font.render(winner.upper() + " WON!", True, COLORS[6])
+    text_size = (text.get_width(), text.get_height())
+    text_location = pygame.Rect(0, 0, *BOARD_SIZE).move(BOARD_SIZE[0] / 2 - text_size[0] / 2,
+                                                        BOARD_SIZE[1] / 2 - text_size[1] / 2)
+    mainScreen.blit(text, text_location)
+    font = pygame.font.SysFont("Verdana", 32)
+    text = font.render("Play Again (R)", True, COLORS[7])
+    text_location = pygame.Rect(0, 0, *BOARD_SIZE).move(BOARD_SIZE[0] / 2 - text.get_width() / 2,
+                                                        BOARD_SIZE[1] / 2 + text_size[1] / 2)
+    mainScreen.blit(text, text_location)
+
+
+def drawGame(mainScreen: pygame.Surface, gameState: engine.GameState, holder: Holder) -> None:
+    drawBoard(mainScreen)
+    highlightCells(mainScreen, holder.selectedCell,  holder.options_move,  holder.options_attack)
+    drawPieces(mainScreen, gameState,  holder.attack_icon)
+    drawWinner(mainScreen,  holder.winner)
+
+
+def renderGame(mainScreen: pygame.Surface, gameState: engine.GameState, holder: Holder) -> None:
+    """
+    Draws the Game, the Fps-display and updates the screen.
+    """
+    drawGame(mainScreen, gameState, holder)
+    holder.fps.render(mainScreen)
+    pygame.display.flip()
+
+
+def drawPromoteOptions(mainScreen: pygame.Surface, piece: Piece, promoteOptions: list) -> None:
+    """
+    If a pawn can be promoted this function will draw the four promoteOptions on the
+    cell at which the pawn stands.
+    """
+    pygame.draw.rect(mainScreen, COLORS[(piece.cell_col+piece.cell_row) % 2],
+                     pygame.Rect(piece.cell_col * CELL_SIZE[0],
+                                 piece.cell_row * CELL_SIZE[1],
+                                 *CELL_SIZE))
+    for row in promoteOptions:
+        for option in row:
+            mainScreen.blit(option[0], pygame.Rect(*option[1]))
+
+
+def choosePromoteOptions(mainScreen: pygame.Surface, gameState: engine.GameState, holder: Holder) -> bool:
+    """
+    Takes over the main Loop, until the player has decided to which piece he/she wants to
+    promote the pawn (holder.selectedCell).
+    
+    Returns a boolean in case the game is being quit.
+    """
+    piece = holder.selectedCell
+    currentPlayer = gameState.currentPlayer()
+    promoteOptions = [['n', 'b'], ['r', 'q']]
+    promoteOptionsDimensions = [[(loadImage(currentPlayer+'n', HALF_CELL_SIZE),
+                                  (piece.cell_col * CELL_SIZE[0],
+                                   piece.cell_row * CELL_SIZE[1],
+                                   *HALF_CELL_SIZE)),
+                                 (loadImage(currentPlayer+'b', HALF_CELL_SIZE),
+                                  (piece.cell_col * CELL_SIZE[0] + HALF_CELL_SIZE[0],
+                                     piece.cell_row * CELL_SIZE[1],
+                                     *HALF_CELL_SIZE))],
+                                [(loadImage(currentPlayer+'r', HALF_CELL_SIZE),
+                                  (piece.cell_col * CELL_SIZE[0],
+                                    piece.cell_row *
+                                   CELL_SIZE[1] + HALF_CELL_SIZE[1],
+                                    *HALF_CELL_SIZE)),
+                                 (loadImage(currentPlayer+'q', HALF_CELL_SIZE),
+                                    (piece.cell_col * CELL_SIZE[0] + HALF_CELL_SIZE[0],
+                                     piece.cell_row *
+                                     CELL_SIZE[1] + HALF_CELL_SIZE[1],
+                                     *HALF_CELL_SIZE))]]
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                col, row = pygame.mouse.get_pos()
+                col = (col*2) // CELL_SIZE[0]
+                row = (row*2) // CELL_SIZE[1]
+                if (col//2 == holder.selectedCell.cell_col) and (
+                        row//2 == holder.selectedCell.cell_row):
+                    col = col-(2*holder.selectedCell.cell_col)
+                    row = row-(2*holder.selectedCell.cell_row)
+                    gameState.promotePiece(holder.selectedCell, promoteOptions[row][col])
+                    return True
+        drawGame(mainScreen, gameState, holder)
+        drawPromoteOptions(mainScreen, holder.selectedCell, promoteOptionsDimensions)
+        holder.fps.render(mainScreen)
+        pygame.display.flip()
+
+
+def newGame(holder: Holder) -> engine.GameState:
+    """
+    Resets all values and starts/returns a new engine.GameState.
+    """
+    holder.selectedCell, holder.winner = None, None
+    holder.options_move, holder.options_attack = [], []
+    return engine.GameState(IMG_SIZE)
+
+
+def mainGUI():
+    holder = Holder()
+    pygame.init()
+    pygame.display.set_caption('Chess to the Death')
+    pygame.display.set_icon(loadImage("blackp", (20, 20)))
+    mainScreen = pygame.display.set_mode(BOARD_SIZE)
+
+    holder.fps = fpsClock.FPS(MAX_FPS, BOARD_SIZE[0]-30, 0)
+    gameState = newGame(holder)
+    holder.attack_icon = loadImage("damage", (20, 20))
+
+    running = True
+    while running:
+        renderGame(mainScreen, gameState, holder)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN and not holder.winner:
+                col, row = pygame.mouse.get_pos()
+                col = col // CELL_SIZE[0]
+                row = row // CELL_SIZE[1]
+                piece = gameState.getPiece(col, row)
+                print("Selected:", piece, col, row)
+                if not holder.selectedCell:
+                    if piece and gameState.selectablePiece(piece):
+                        holder.selectedCell = piece
+                        holder.options_move, holder.options_attack = gameState.getOptions(piece)
+                else:
+                    if (gameState.move(holder.selectedCell, col, row, holder.options_move)) or (
+                            gameState.attack(holder.selectedCell, col, row, holder.options_attack)):
+                        if gameState.promotePawnOption(holder.selectedCell):
+                            print("Choose Pawn Promotion...")
+                            running = choosePromoteOptions(mainScreen, gameState, holder)
+                            print("Pawn promoted!")
+                        if running:
+                            holder.selectedCell = None
+                            holder.options_move, holder.options_attack = [], []
+                            holder.winner = gameState.playerWon()
+                            if not holder.winner:
+                                gameState.createBoard()
+                                renderGame(mainScreen, gameState, holder)
+                                pygame.time.delay(250)
+                                gameState.nextTurn()
+                    elif piece and gameState.selectablePiece(piece):
+                        if piece == holder.selectedCell:
+                            piece = None
+                        holder.selectedCell = piece
+                        holder.options_move, holder.options_attack = gameState.getOptions(piece)
+            if event.type == pygame.KEYDOWN and holder.winner:
+                if pygame.key.name(event.key) == 'r':
+                    print("Restarting...")
+                    gameState = newGame(holder)
+
+    print("GoodBye!")
