8cb9d23d9f91b7e48fd13485805061a0f9380bee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:33 +0000

    Add package fmake/0.0.2/fmake-0.0.2.tar.gz

---
+++ packages/fmake/fmake-0.0.2.tar.gz/fmake-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fmake", 
+    version="0.0.2",
+    author="Richard Peschke",
+    author_email="peschke@hawaii.edu",
+    description="build scripts for firmware projects",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'pandas',
+          'numpy',
+          'matplotlib',
+          'wget',
+          'openpyxl'
+    ],
+    python_requires='>=3.8',
+    
+    entry_points = {
+        'console_scripts': ['fmake=vhdl_build_system.main_vhdl_make:main_vhdl_make'],
+    }
+)
---
+++ packages/fmake/fmake-0.0.2.tar.gz/fmake-0.0.2/vhdl_build_system/__init__.py
@@ -0,0 +1,11 @@
+import vhdl_build_system.vhdl_run_vivado
+import vhdl_build_system.vhdl_make_simulation
+import vhdl_build_system.vhdl_make_test_bench
+
+import vhdl_build_system.vhdl_merge_split_test_cases
+
+import vhdl_build_system.Convert2CSV 
+
+import vhdl_build_system.make_build_system
+
+import vhdl_build_system.run_ise 
