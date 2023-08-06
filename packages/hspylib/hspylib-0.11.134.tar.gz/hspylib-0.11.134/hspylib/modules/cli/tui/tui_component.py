a496e196a4aae9d5ccffe97e2258bc8b76fb95b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:23:56 +0000

    Add package hspylib/0.11.134/hspylib-0.11.134.tar.gz

---
+++ packages/hspylib/hspylib-0.11.134.tar.gz/hspylib-0.11.134/hspylib/modules/cli/tui/tui_component.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+from typing import TypeVar, Generic, Optional, List, Any
+
+from hspylib.core.tools.commons import sysout
+from hspylib.core.tools.text_tools import ensure_endswith, elide_text
+from hspylib.modules.cli.icons.font_awesome.awesome import Awesome
+from hspylib.modules.cli.keyboard import Keyboard
+from hspylib.modules.cli.tui.tui_preferences import TUIPreferences
+
+T = TypeVar("T")
+
+
+class TUIComponent(Generic[T], ABC):
+
+    @staticmethod
+    def _draw_line(line_fmt: str, max_columns: int, *args: Any) -> None:
+        """TODO"""
+        sysout(ensure_endswith(elide_text(line_fmt.format(*args), max_columns), "%NC%"))
+
+    def __init__(self):
+        self.prefs: TUIPreferences = TUIPreferences.INSTANCE or TUIPreferences()
+        self.done: bool = False
+        self.require_render: bool = True
+
+    @abstractmethod
+    def execute(self, title: str) -> Optional[T | List[T]]:
+        """TODO"""
+
+    def _draw_line_color(self, is_selected: bool = False, set_bg_color: bool = True) -> Awesome:
+        """TODO"""
+        if is_selected:
+            selector = self.prefs.selected
+            if set_bg_color:
+                sysout(self.prefs.sel_bg_color.code, end="")
+            sysout(self.prefs.highlight_color.code, end="")
+        else:
+            selector = self.prefs.unselected
+            sysout(self.prefs.text_color.code, end="")
+
+        return selector
+
+    @abstractmethod
+    def _render(self) -> None:
+        """TODO"""
+
+    @abstractmethod
+    def _navbar(self, *args) -> str:
+        """TODO"""
+
+    @abstractmethod
+    def _handle_keypress(self) -> Keyboard:
+        """TODO"""
