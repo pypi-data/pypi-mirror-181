4e9adccd8c366f282e0a9e7293cb17ab1f70f352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:28 +0000

    Add package PeerAnnot/0.0.1.post4/PeerAnnot-0.0.1.post4-py3-none-any.whl

---
+++ packages/PeerAnnot/PeerAnnot-0.0.1.post4-py3-none-any.whl/peerannot/models/template.py
@@ -0,0 +1,10 @@
+from peerannot.helpers.converters import Converter
+
+
+class CrowdModel:
+    def __init__(self, answers):
+        self.converter = Converter(answers)
+        self.answers = self.converter.transform()
+        self.answers = dict(
+            sorted({int(k): v for k, v in self.answers.items()}.items())
+        )
