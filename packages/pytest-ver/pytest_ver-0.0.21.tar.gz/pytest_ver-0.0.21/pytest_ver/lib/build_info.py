b75a185e0d711e94098625929c2a7c9ecd15c80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:20 +0000

    Add package pytest-ver/0.0.21/pytest_ver-0.0.21.tar.gz

---
+++ packages/pytest-ver/pytest_ver-0.0.21.tar.gz/pytest_ver-0.0.21/pytest_ver/lib/build_info.py
@@ -0,0 +1,15 @@
+class BuildInfo:  # pylint: disable=too-few-public-methods
+    python_version = '3.9.0'
+    os_name = 'posix'
+    git_sha = 'eb42ad8fea001376509bede05ece5311970429c6'
+    git_branch = 'master'
+    git_uncommitted = [
+        ' M pytest_ver/lib/constants.py',
+        'M  requirements.txt',
+        'M  setup.py',
+        'M  todo.md',
+        'A  tools/__init__.py',
+        'D  tools/release/gen_build_info.py',
+    ]
+    git_unpushed = [
+    ]
