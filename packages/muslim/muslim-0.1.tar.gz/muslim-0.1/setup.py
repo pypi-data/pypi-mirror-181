1a6e2a931c2102faea10882920f58a53a6aeb1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:22 +0000

    Add package muslim/0.1/muslim-0.1.tar.gz

---
+++ packages/muslim/muslim-0.1.tar.gz/muslim-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='muslim',
+    version='0.1',
+    license='CC0 1.0 Universal',
+    author="Harsh Avinash",
+    author_email='harsh.avinash.official@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Harsh-Avinash/pip-install-muslim',
+    keywords='Muslims in python',
+    install_requires=[
+        'scikit-learn',
+        'pandas',
+        'numpy'
+    ],
+
+)
