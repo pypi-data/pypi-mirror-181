f26891a6f19d646aaab8fd3c0531715938ba4754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:11 +0000

    Add package vule-magics-python2/0.0.3/vule-magics-python2-0.0.3.tar.gz

---
+++ packages/vule-magics-python2/vule-magics-python2-0.0.3.tar.gz/vule-magics-python2-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import os
+import setuptools
+
+with open( os.path.join(os.path.abspath(os.path.join(__file__, os.pardir)), "scripts/__VERSION__"), "r" ) as f:
+    __VERSION__ =  f.read()
+
+setuptools.setup(
+    name="vule-magics-python2",
+    version=__VERSION__,
+    author="Le Tuan Vu",
+    author_email="ltnv24@gmail.com",
+    description="Vu's custom magic commands",
+    long_description="",
+    long_description_content_type="text/markdown",
+    packages=['vule_sparkmagic', 'scripts'],
+    package_data={"scripts": ["__VERSION__"]},
+    include_package_data=True,
+    install_requires=['ipython', 'pyspark'],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
