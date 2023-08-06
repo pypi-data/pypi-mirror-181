a3742ad0dfaf26d98cdc2399818ab838675521e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:04 +0000

    Add package admcheck/0.0.8/admcheck-0.0.8.tar.gz

---
+++ packages/admcheck/admcheck-0.0.8.tar.gz/admcheck-0.0.8/admcheck/main.py
@@ -0,0 +1,9 @@
+from admcheck import *
+import ctypes, os
+
+def is_Admin():
+    try:
+        is_admin = (os.getuid() == 0)
+    except AttributeError:
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+    return is_admin
