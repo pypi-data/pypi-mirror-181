2e8ca92a5c363fc083fad644efaa179231639a2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:02 +0000

    Add package Python-Quelert/1.0.4/Python-Quelert-1.0.4.tar.gz

---
+++ packages/Python-Quelert/Python-Quelert-1.0.4.tar.gz/Python-Quelert-1.0.4/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='Python-Quelert',
+	version='1.0.4',
+	author='Georgy2008',
+	author_email='zadvornow2908@gmail.com',
+	description='Python Quelert and qw',
+	packages=['Quelert'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
