2cf3171fa19d5b9e6a1ab6b2f9773e67d6ce5c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:57 +0000

    Add package smisch/1.0.5/smisch-1.0.5.tar.gz

---
+++ packages/smisch/smisch-1.0.5.tar.gz/smisch-1.0.5/smisch.py
@@ -0,0 +1,31 @@
+import os,time,string,random
+
+from src.print.custom_prints import *
+def clear_console(type="line"):
+    if type == "line":
+        print(100*" ",end="\r")
+    elif type == "console":
+        for i in range(100):
+            print()
+def animate_input(text,delay=0.05):
+    clear_console("console")
+    custom_text = []
+    for i in text:
+        custom_text.append(i)
+        time.sleep(delay)
+        if len(text) != len(custom_text):
+            print("".join(custom_text),end="\r")
+        else:
+            s = input(f"{text}")
+            clear_console("console")
+            return s
+def ask_input(text,type="str"):
+    while True:
+        if type == "str":
+            a = animate_input(text,0.01)
+            return a
+        elif type == "int":
+            try:
+                a = int(animate_input(text,0.01))
+                return a 
+            except: pass
