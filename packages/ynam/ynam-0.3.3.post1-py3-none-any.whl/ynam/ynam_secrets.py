0cc0b529f8378a629ef4cf5020c37f6cc380b9a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:19 +0000

    Add package ynam/0.3.3.post1/ynam-0.3.3.post1-py3-none-any.whl

---
+++ packages/ynam/ynam-0.3.3.post1-py3-none-any.whl/ynam/ynam_secrets.py
@@ -0,0 +1,44 @@
+import json
+import os
+from dataclasses import dataclass
+
+from .ynam_parser import arg
+
+
+@dataclass
+class Secrets():
+
+    ynab_api_key: str = None
+    ynab_account_id: str = None
+    ynab_budget_id: str = None
+
+    mint_username: str = None
+    mint_password: str = None
+    mint_mfa_seed: str = None
+
+
+def loadSecrets():
+    fileSecrets = {}
+    path = arg('config_file')
+    if os.path.exists(path):
+        with open(path) as file:
+            fileSecrets = json.load(file)
+
+    for key in {
+            k: v
+            for k, v in vars(Secrets).items() if not k.startswith('_')
+    }:
+        if arg(key) is not None:
+            fileSecrets[key] = arg(key)
+    return fileSecrets
+
+
+def updateStash(key, value):
+    if value != '':
+        secrets = {**loadSecrets()}
+        secrets[key] = value
+        with open(arg('config_file'), 'w+') as file:
+            file.write(json.dumps(secrets, indent=2))
+
+
+stash = Secrets(**loadSecrets())
