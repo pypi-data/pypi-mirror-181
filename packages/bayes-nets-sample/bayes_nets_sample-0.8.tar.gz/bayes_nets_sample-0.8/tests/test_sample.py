8be52f3baace3b75a511d79d63a021a531b89e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:34 +0000

    Add package bayes-nets-sample/0.8/bayes_nets_sample-0.8.tar.gz

---
+++ packages/bayes-nets-sample/bayes_nets_sample-0.8.tar.gz/bayes_nets_sample-0.8/tests/test_sample.py
@@ -0,0 +1,9 @@
+import unittest
+from bayes.sample_input_txt import SampleInputTXT
+
+class TestHelp(unittest.TestCase):
+
+	def test_help(self):
+		self.assertEqual(SampleInputTXT().generate_sample(), True)
+
+unittest.main()
