59d7df494e29f5891e1b8049fe99e2778ba13142
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:24 +0000

    Add package rofunc/0.0.1/rofunc-0.0.1.0.tar.gz

---
+++ packages/rofunc/rofunc-0.0.1.0.tar.gz/rofunc-0.0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rofunc",
+    version="0.0.1.0",
+    description='The Full Process Python Package for Robot Learning from Demonstration',
+    author="Junjia Liu",
+    author_email="jjliu@mae.cuhk.edu.hk",
+    url='https://github.com/Skylark0924/Rofunc',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['matplotlib', 'pandas', 'tqdm', 'pillow', 'pytransform3d', 'numpy', 'pynput',
+                      'nestle', 'omegaconf', 'hydra-core', 'opencv-python', 'neurokit2', 'skrl', 'gdown', 'dm_tree',
+                      'openpyxl', 'pytz', 'urdfpy', 'pin', 'shutup', 'elegantrl', 'ray[rllib]==2.2.0'],
+                      # 'pbdlib @ https://github.com/Skylark0924/Rofunc/releases/download/v0.0.0.7.1/pbdlib-0.1-py3-none-any.whl',
+                      # 'isaacgym @ https://github.com/Skylark0924/Rofunc/releases/download/v0.0.0.9/isaacgym-1.0rc4-py3-none-any.whl',
+                      # 'pyzed @ https://github.com/Skylark0924/Rofunc/releases/download/v0.0.0.9/pyzed-3.7-cp37-cp37m-linux_x86_64.whl'],
+    python_requires=">3.6,<3.9",
+    keywords=['robotics', 'learning from demonstration'],
+    license='MIT',
+    entry_points={
+        'console_scripts': [
+            'rf=rofunc._main:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
