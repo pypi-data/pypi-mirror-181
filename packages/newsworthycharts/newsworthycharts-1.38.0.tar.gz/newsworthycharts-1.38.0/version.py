0fac6fb4f79fb05b3e605d945b98110aa116c8bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:46 +0000

    Add package newsworthycharts/1.38.0/newsworthycharts-1.38.0.tar.gz

---
+++ packages/newsworthycharts/newsworthycharts-1.38.0.tar.gz/newsworthycharts-1.38.0/version.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+name = "newsworthycharts"
+
+short_version = "1.38.0"
+long_version = short_version
+
+short_desc = """\
+Create charts and publish on Amazon S3.\
+"""
+authors = u"Jens Finnäs and Leo Wallentin, J++ Stockholm"
+year = date.today().year
+copyright = "%s, %s" % (year, authors)
+email = "stockholm@jplusplus.org"
+
+version = long_version
