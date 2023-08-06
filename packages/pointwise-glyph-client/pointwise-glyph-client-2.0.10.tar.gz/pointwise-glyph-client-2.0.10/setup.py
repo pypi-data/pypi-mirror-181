17f28baed290154404b3858417b40013ea13cac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:46 +0000

    Add package pointwise-glyph-client/2.0.10/pointwise-glyph-client-2.0.10.tar.gz

---
+++ packages/pointwise-glyph-client/pointwise-glyph-client-2.0.10.tar.gz/pointwise-glyph-client-2.0.10/setup.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='pointwise-glyph-client',
+      version='2.0.10',
+      description='Glyph client in Python with Python-like API to Pointwise Glyph Server',
+      url='http://github.com/pointwise/GlyphClientPython',
+      install_requires=['numpy'],
+      packages=['pointwise', 'pointwise.glyphapi'],
+      classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 2.7'],
+      author='Cadence Design Systems, Inc.',
+      author_email='pw-support@cadence.com')
