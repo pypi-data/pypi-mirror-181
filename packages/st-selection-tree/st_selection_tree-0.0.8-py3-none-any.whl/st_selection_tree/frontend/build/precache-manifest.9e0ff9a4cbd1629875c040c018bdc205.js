492a478c793451760537cc57cd4062b8b6aacac5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:54:29 +0000

    Add package st-selection-tree/0.0.8/st_selection_tree-0.0.8-py3-none-any.whl

---
+++ packages/st-selection-tree/st_selection_tree-0.0.8-py3-none-any.whl/st_selection_tree/frontend/build/precache-manifest.9e0ff9a4cbd1629875c040c018bdc205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb02efabd3862fe6f4055ead5a15d6fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad87df40d4abe1b35d8f",
+    "url": "./static/js/2.246defa6.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.246defa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4ba6d1b103a39f6377b",
+    "url": "./static/js/main.1f01f351.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
