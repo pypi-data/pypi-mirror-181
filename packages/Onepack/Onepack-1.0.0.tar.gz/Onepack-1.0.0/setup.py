0490ac8e637bd7914402d320c7407916deab1731
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:24 +0000

    Add package Onepack/1.0.0/Onepack-1.0.0.tar.gz

---
+++ packages/Onepack/Onepack-1.0.0.tar.gz/Onepack-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Onepack',
+    version='1.0.0',
+    packages=['onepack'],
+    include_package_data=True,
+    install_requires=[
+        'django',
+    ],
+)
