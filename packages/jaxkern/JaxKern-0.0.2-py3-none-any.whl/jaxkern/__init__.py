ca5aec820e70717bbf229348021631e758c8eea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:08 +0000

    Add package jaxkern/0.0.2/JaxKern-0.0.2-py3-none-any.whl

---
+++ packages/jaxkern/JaxKern-0.0.2-py3-none-any.whl/jaxkern/__init__.py
@@ -0,0 +1,23 @@
+"""JaxKern."""
+from .kernels import (
+    RBF,
+    GraphKernel,
+    Matern12,
+    Matern32,
+    Matern52,
+    Polynomial,
+    ProductKernel,
+    SumKernel,
+)
+
+__version__ = "0.0.2"
+__all__ = [
+    "RBF",
+    "GraphKernel",
+    "Matern12",
+    "Matern32",
+    "Matern52",
+    "Polynomial",
+    "ProductKernel",
+    "SumKernel",
+]
