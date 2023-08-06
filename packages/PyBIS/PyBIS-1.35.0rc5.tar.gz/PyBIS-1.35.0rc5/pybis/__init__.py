fa96fdbb48ffb449686fafaeaa4f12f73783508c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:57 +0000

    Add package PyBIS/1.35.0rc5/PyBIS-1.35.0rc5.tar.gz

---
+++ packages/PyBIS/PyBIS-1.35.0rc5.tar.gz/PyBIS-1.35.0rc5/pybis/__init__.py
@@ -0,0 +1,9 @@
+name = "pybis"
+__author__ = "Swen Vermeul • ID SIS • ETH Zürich"
+__email__ = "swen@ethz.ch"
+__version__ = "1.35.0"
+
+from . import pybis
+from .pybis import DataSet
+from .pybis import Openbis
+from .pybis import Sample
