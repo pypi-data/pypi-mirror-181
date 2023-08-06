e941d194ea8d4f948b0b5dddd49ffe9e94ae88d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:23 +0000

    Add package upbge-stubs/0.3.1.27.dev3709618445/upbge-stubs-0.3.1.27.dev3709618445.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev3709618445.tar.gz/upbge-stubs-0.3.1.27.dev3709618445/ramp_blend_items/__init__.pyi
@@ -0,0 +1,75 @@
+"""
+
+
+Ramp Blend Items
+^^^^^^^^^^^^^^^^
+
+:MIX:             
+  Mix.
+
+.. _rna-enum-ramp-blend-items:
+
+======================================================================
+
+:DARKEN:          
+  Darken.
+
+:MULTIPLY:        
+  Multiply.
+
+:BURN:            
+  Color Burn.
+
+======================================================================
+
+:LIGHTEN:         
+  Lighten.
+
+:SCREEN:          
+  Screen.
+
+:DODGE:           
+  Color Dodge.
+
+:ADD:             
+  Add.
+
+======================================================================
+
+:OVERLAY:         
+  Overlay.
+
+:SOFT_LIGHT:      
+  Soft Light.
+
+:LINEAR_LIGHT:    
+  Linear Light.
+
+======================================================================
+
+:DIFFERENCE:      
+  Difference.
+
+:SUBTRACT:        
+  Subtract.
+
+:DIVIDE:          
+  Divide.
+
+======================================================================
+
+:HUE:             
+  Hue.
+
+:SATURATION:      
+  Saturation.
+
+:COLOR:           
+  Color.
+
+:VALUE:           
+  Value.
+
+"""
+
+import typing
