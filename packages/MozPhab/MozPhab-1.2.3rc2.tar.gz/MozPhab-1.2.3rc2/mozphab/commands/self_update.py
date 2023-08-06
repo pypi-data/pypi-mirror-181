821c241208f6fc39726f300b063d6663e504d15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:17 +0000

    Add package MozPhab/1.2.3rc2/MozPhab-1.2.3rc2.tar.gz

---
+++ packages/MozPhab/MozPhab-1.2.3rc2.tar.gz/MozPhab-1.2.3rc2/mozphab/commands/self_update.py
@@ -0,0 +1,30 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from mozphab.environment import MOZPHAB_VERSION
+from mozphab.logger import logger
+from mozphab.spinner import wait_message
+from mozphab.updater import check_for_updates, self_upgrade
+
+
+def self_update(_):
+    """`self-update` command, updates this package"""
+    new_version = check_for_updates(force_check=True)
+
+    if not new_version:
+        logger.info(
+            f"You are running the latest version of `moz-phab`, {MOZPHAB_VERSION}."
+        )
+        return
+
+    with wait_message(f"Upgrading to version {new_version}"):
+        self_upgrade()
+
+    logger.info(f"Upgraded to version {new_version}")
+
+
+def add_parser(parser):
+    update_parser = parser.add_parser("self-update", help="Update review script")
+    update_parser.set_defaults(func=self_update, needs_repo=False)
