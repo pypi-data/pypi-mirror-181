c8c8cb625dbe072b4d01d3aa226148aac152271b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:57 +0000

    Add package fitterpp/0.0.4/fitterpp-0.0.4-py3-none-any.whl

---
+++ packages/fitterpp/fitterpp-0.0.4-py3-none-any.whl/fitterpp/__init__.py
@@ -0,0 +1,6 @@
+from fitterpp.constants import METHOD_LEASTSQ, \
+    METHOD_DIFFERENTIAL_EVOLUTION,  \
+    METHOD_BOTH, METHOD_FITTER_DEFAULTS, MAX_NFEV
+from fitterpp.fitterpp import Fitterpp
+from fitterpp.util import dictToParameters
+from fitterpp import constants
