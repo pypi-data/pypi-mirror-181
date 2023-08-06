33c7f5ad8803b7d5083580b56134242aed0a8668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:57 +0000

    Add package streamlit-sparrow-labeling/0.1.1/streamlit_sparrow_labeling-0.1.1-py3-none-any.whl

---
+++ packages/streamlit-sparrow-labeling/streamlit_sparrow_labeling-0.1.1-py3-none-any.whl/streamlit_sparrow_labeling/frontend/build/precache-manifest.c87c3fa2a6f512fc2099fce7d92aceeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d56039885ee735806153b24e925bf3a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b5e94cdbf0ebdc7d87c3",
+    "url": "./static/css/main.41cd645d.chunk.css"
+  },
+  {
+    "revision": "9d22068522b7fcc3478b",
+    "url": "./static/js/2.2a12cbe3.chunk.js"
+  },
+  {
+    "revision": "f1fb474ae9ccd77a5fa1bda403be7c43",
+    "url": "./static/js/2.2a12cbe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e94cdbf0ebdc7d87c3",
+    "url": "./static/js/main.744b77a8.chunk.js"
+  },
+  {
+    "revision": "56d913b25ac97136b09b",
+    "url": "./static/js/runtime-main.b5044f72.js"
+  }
+]);
