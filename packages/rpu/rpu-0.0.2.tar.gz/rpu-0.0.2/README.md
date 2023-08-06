0113493c416bf1543a3bb5a9f2bb00a16f25701c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:23 +0000

    Add package rpu/0.0.2/rpu-0.0.2.tar.gz

---
+++ packages/rpu/rpu-0.0.2.tar.gz/rpu-0.0.2/README.md
@@ -0,0 +1,25 @@
+# RPU
+
+**R**andom **P**ython **U**tilities is a library that contains stuff that (I know I) and you probably are tired of adding to all of your projects. But with rpu you can just add it as a requirement and no more copy pasting! Main benefit of using rpu over copy pasting is that if there is a bug found with the code, instead of having to fix it in all of your projects it can just be fixed in the library and then its fixed for everyone.
+
+Our docs are located at rpu.cibere.dev. View the <a href="https://rpu.cibere.dev/stable/index.html">stable docs here</a>, and <a href="https://rpu.cibere.dev/latest/index.html">latest docs here</a>
+
+## Key Features
+
+### Grammar
+
+- Plural
+- possessive
+
+### Iterables
+
+- chunk
+- get
+
+### async
+
+- run_in_exectuor
+
+### CLI
+
+- command infrustructure
---
+++ packages/rpu/rpu-0.0.2.tar.gz/rpu-0.0.2/rpu/__main__.py
@@ -0,0 +1,52 @@
+import platform
+import sys
+import webbrowser
+
+import rpu
+
+from .cli import ConsoleClient
+
+client = ConsoleClient()
+
+
+@client.command(
+    name="version",
+    description="gives you the version of rpu you are running",
+    brief="gives you the version of rpu your using",
+    aliases=["v"],
+)
+def cmd_version():
+    print(rpu.__version__)
+
+
+@client.command(
+    name="docs",
+    description="opens rpu's documentation. If your using alpha/beta, latest docs will be brought up. If your using final then stable docs will be brought up.",
+    brief="opens rpus docs",
+    aliases=["d"],
+)
+def cmd_docs():
+    version = "stable" if rpu.version_info.releaselevel == "final" else "latest"
+
+    print(f"Opening the {version} docs in your browser")
+    webbrowser.open(f"https://rpu.cibere.dev/{version}/index")
+
+
+@client.command(
+    name="system-info",
+    description="gives you system information. Specifically rpu version, python version, and os",
+    brief="gives you system info",
+    aliases=["os", "s"],
+)
+def cmd_system_info():
+    info = {}
+
+    info["ciberedev.py"] = rpu.__version__
+    info["python"] = sys.version.split(" ")[0]
+    info["OS"] = platform.platform()
+
+    nl = "\n"
+    print(nl.join([f"{item}: {info[item]}" for item in info]))
+
+
+client.run()
