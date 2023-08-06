75d975b7c35dcf6c8a0ce42559a0a7128ee5b2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:17:27 +0000

    Add package pytrevl/0.2.0/pytrevl-0.2.0-py3-none-any.whl

---
+++ packages/pytrevl/pytrevl-0.2.0-py3-none-any.whl/pytrevl/__init__.py
@@ -0,0 +1,22 @@
+'''
+Import modules to shorten import path to use i.e.:
+
+pytrevl.Dashboard()
+
+instead of
+
+pytrevl.dashboard.Dashboard()
+'''
+
+from .dashboard import Dashboard
+from .components import PieChart, BarChart, LineChart, DonutChart, CustomChart
+from .cube import Cube
+from .filter import Filter
+from .style import Style
+from .api.cube_query import CubeQuery
+
+
+# Version and Author
+
+__version__ = '0.2.0'
+__author__ = 'Trendence Institut'
