8755ec4747cb67ce101ed0816d4725738d6130eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:31:47 +0000

    Add package bemserver-api/0.5.0/bemserver-api-0.5.0.tar.gz

---
+++ packages/bemserver-api/bemserver-api-0.5.0.tar.gz/bemserver-api-0.5.0/bemserver_api/resources/timeseries/schemas.py
@@ -0,0 +1,36 @@
+"""Timeseries API schemas"""
+import marshmallow as ma
+import marshmallow_sqlalchemy as msa
+
+from bemserver_core.model import Timeseries
+
+from bemserver_api import AutoSchema, Schema, SortField
+
+
+class TimeseriesSchema(AutoSchema):
+    class Meta(AutoSchema.Meta):
+        model = Timeseries
+
+    id = msa.auto_field(dump_only=True)
+    name = msa.auto_field(validate=ma.validate.Length(1, 80))
+
+
+class TimeseriesPutSchema(TimeseriesSchema):
+    class Meta(TimeseriesSchema.Meta):
+        exclude = ("campaign_id", "campaign_scope_id")
+
+
+class TimeseriesQueryArgsSchema(Schema):
+    sort = SortField(("name",))
+    name = ma.fields.Str()
+    in_name = ma.fields.Str()
+    unit_symbol = ma.fields.Str()
+    campaign_id = ma.fields.Int()
+    campaign_scope_id = ma.fields.Int()
+    user_id = ma.fields.Int()
+    site_id = ma.fields.Int()
+    building_id = ma.fields.Int()
+    storey_id = ma.fields.Int()
+    space_id = ma.fields.Int()
+    zone_id = ma.fields.Int()
+    event_id = ma.fields.Int()
