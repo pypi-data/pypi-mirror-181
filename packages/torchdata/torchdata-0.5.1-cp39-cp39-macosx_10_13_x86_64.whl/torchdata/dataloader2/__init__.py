3700e38c7dc18b37eb6c09fee0c7f475af94eeeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:10 +0000

    Add package torchdata/0.5.1/torchdata-0.5.1-cp39-cp39-macosx_10_13_x86_64.whl

---
+++ packages/torchdata/torchdata-0.5.1-cp39-cp39-macosx_10_13_x86_64.whl/torchdata/dataloader2/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+from .dataloader2 import DataLoader2, DataLoader2Iterator
+from .error import PauseIteration
+from .reading_service import (
+    CheckpointableReadingServiceInterface,
+    DistributedReadingService,
+    MultiProcessingReadingService,
+    PrototypeMultiProcessingReadingService,
+    ReadingServiceInterface,
+)
+from .shuffle_spec import ShuffleSpec
+
+__all__ = [
+    "CheckpointableReadingServiceInterface",
+    "DataLoader2",
+    "DataLoader2Iterator",
+    "DistributedReadingService",
+    "MultiProcessingReadingService",
+    "PauseIteration",
+    "PrototypeMultiProcessingReadingService",
+    "ReadingServiceInterface",
+    "ShuffleSpec",
+]
+
+assert __all__ == sorted(__all__)
