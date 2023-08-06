7a5ec01dd0e30989eefb5bff3bb8820ae6939eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:01 +0000

    Add package a-pandas-ex-lookupdict/0.10/a_pandas_ex_lookupdict-0.10.tar.gz

---
+++ packages/a-pandas-ex-lookupdict/a_pandas_ex_lookupdict-0.10.tar.gz/a_pandas_ex_lookupdict-0.10/a_pandas_ex_lookupdict/__init__.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from pandas.core.frame import DataFrame
+from typing import Union
+
+
+def get_lookup_dict(df, as_index: list, as_values: Union[list, None] = None) -> dict:
+    if not isinstance(as_index, list):
+        as_index = [as_index]
+    if not as_values:
+        as_values = [x for x in df.columns.to_list() if x not in as_index]
+    elif not isinstance(as_values, list):
+        as_index = [as_values]
+    nparr = df[as_values].__array__()
+    indexnumber = df.drop_duplicates(subset=as_index, keep="last").index.__array__()
+    colors = df.drop_duplicates(subset=as_index, keep="last")[as_index].__array__()
+    if 0 not in indexnumber:
+        indexnumber = np.concatenate([np.array([0]), indexnumber])
+    return {
+        tuple(colors[ini]): nparr[x:y]
+        for ini, x, y in zip(range(len(indexnumber)), indexnumber[:], indexnumber[1:])
+    }
+
+
+def pd_add_lookup_dict():
+    DataFrame.d_get_lookup_dict = get_lookup_dict
+
+
