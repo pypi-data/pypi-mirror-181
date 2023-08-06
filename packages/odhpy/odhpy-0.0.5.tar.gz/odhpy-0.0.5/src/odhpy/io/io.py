c2b0a95234865bdcdaaca83aeab8c4ffb84e8151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:31 +0000

    Add package odhpy/0.0.5/odhpy-0.0.5.tar.gz

---
+++ packages/odhpy/odhpy-0.0.5.tar.gz/odhpy-0.0.5/src/odhpy/io/io.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from odhpy import utils
+
+def read_ts_csv(filename, date_format=r"%d/%m/%Y", **kwargs):
+    """Reads a daily timeseries csv into a DataFrame, and sets the index to the Date.
+    Assumed there is a column named "Date"
+
+    Args:
+        filename (_type_): _description_
+        date_format (str, optional): defaults to "%d/%m/%Y" as per Fors. Other common formats include "%Y-%m-%d", "%Y/%m/%d".
+
+    Returns:
+        _type_: _description_
+    """
+    df = pd.read_csv(filename, **kwargs)
+    df = utils.set_index_dt(df)
+    return df
