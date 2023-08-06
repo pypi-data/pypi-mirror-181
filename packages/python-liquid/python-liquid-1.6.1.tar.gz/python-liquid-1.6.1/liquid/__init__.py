46238331135f7062751597d9ae2852dae352cfb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:05 +0000

    Add package python-liquid/1.6.1/python-liquid-1.6.1.tar.gz

---
+++ packages/python-liquid/python-liquid-1.6.1.tar.gz/python-liquid-1.6.1/liquid/__init__.py
@@ -0,0 +1,33 @@
+# flake8: noqa
+# pylint: disable=useless-import-alias,missing-module-docstring
+
+__version__ = "1.6.1"
+
+try:
+    from markupsafe import escape as escape
+    from markupsafe import Markup as Markup
+    from markupsafe import soft_str
+except ImportError:
+    from liquid.exceptions import escape as escape  # type: ignore
+    from liquid.exceptions import Markup as Markup  # type: ignore
+
+    # pylint: disable=invalid-name
+    soft_str = str  # type: ignore
+
+from .mode import Mode as Mode
+from .token import Token
+from .expression import Expression
+
+from .loaders import FileSystemLoader as FileSystemLoader
+from .loaders import DictLoader as DictLoader
+from .loaders import ChoiceLoader as ChoiceLoader
+
+from .context import Context as Context
+from .context import Undefined as Undefined
+from .context import DebugUndefined as DebugUndefined
+from .context import StrictUndefined as StrictUndefined
+from .context import StrictDefaultUndefined as StrictDefaultUndefined
+from .context import is_undefined as is_undefined
+
+from .environment import Environment as Environment
+from .environment import Template as Template
