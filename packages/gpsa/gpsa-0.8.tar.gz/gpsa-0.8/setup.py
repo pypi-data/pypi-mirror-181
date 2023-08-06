0179808b5899c3fd7973f736d4b2e1e8d76b0aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:24:27 +0000

    Add package gpsa/0.8/gpsa-0.8.tar.gz

---
+++ packages/gpsa/gpsa-0.8.tar.gz/gpsa-0.8/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gpsa",
+    version="0.8",
+    description="Gaussian Process Spatial Alignment (GPSA)",
+    url="https://github.com/andrewcharlesjones/spatial-alignment",
+    download_url="https://github.com/andrewcharlesjones/spatial-alignment/archive/refs/tags/v0.8.tar.gz",
+    author="Andy Jones",
+    author_email="ajones788@gmail.com",
+    packages=find_packages(),
+)
