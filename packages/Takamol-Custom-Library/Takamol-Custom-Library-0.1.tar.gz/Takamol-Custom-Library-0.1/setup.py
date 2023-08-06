5f28dc163b943b8c8376f14be9e3d51730a7f2d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:47 +0000

    Add package Takamol-Custom-Library/0.1/Takamol-Custom-Library-0.1.tar.gz

---
+++ packages/Takamol-Custom-Library/Takamol-Custom-Library-0.1.tar.gz/Takamol-Custom-Library-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='Takamol-Custom-Library',
+    version='0.1',
+    license='',
+    author="Haitham Al-Mughrabi",
+    author_email='haitham.almughrabi@wareefunited.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='Takamol Custom Library',
+    install_requires=[
+        'selenium',
+        'webdriver-manager'
+    ],
+
+)
