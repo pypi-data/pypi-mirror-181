c2aa5e6e7861a5c4a41ae435f17222ee709331ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:11 +0000

    Add package aria2p/0.11.3/aria2p-0.11.3-py3-none-any.whl

---
+++ packages/aria2p/aria2p-0.11.3-py3-none-any.whl/aria2p/cli/commands/remove.py
@@ -0,0 +1,45 @@
+"""Command to remove downloads."""
+
+from __future__ import annotations
+
+import sys
+
+from aria2p.api import API
+from aria2p.client import ClientException
+
+
+def remove(api: API, gids: list[str] | None = None, do_all: bool = False, force: bool = False) -> int:
+    """
+    Remove subcommand.
+
+    Arguments:
+        api: The API instance to use.
+        gids: The GIDs of the downloads to remove.
+        do_all: Pause all downloads if True.
+        force: Force pause or not (see API.remove).
+
+    Returns:
+        int: 0 if all success, 1 if one failure.
+    """
+    if do_all:
+        if api.remove_all():
+            return 0
+        return 1
+
+    try:
+        downloads = api.get_downloads(gids)
+    except ClientException as error:
+        print(str(error), file=sys.stderr)
+        return 1
+
+    ok = True
+    result = api.remove(downloads, force=force)
+
+    if all(result):
+        return 0 if ok else 1
+
+    for item in result:
+        if isinstance(item, ClientException):
+            print(item, file=sys.stderr)
+
+    return 1
---
+++ packages/aria2p/aria2p-0.11.3-py3-none-any.whl/aria2p/types.py
@@ -0,0 +1,8 @@
+"""Complex type annotations."""
+
+from __future__ import annotations
+
+from pathlib import Path
+from typing import Union
+
+PathOrStr = Union[Path, str]
