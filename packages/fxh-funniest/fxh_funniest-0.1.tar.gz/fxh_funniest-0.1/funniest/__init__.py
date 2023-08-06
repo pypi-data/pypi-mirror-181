a1b3fcd29a213e52639c2a3b5a42aae5a100c2fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:23 +0000

    Add package fxh-funniest/0.1/fxh_funniest-0.1.tar.gz

---
+++ packages/fxh-funniest/fxh_funniest-0.1.tar.gz/fxh_funniest-0.1/funniest/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+    return "this is the funniest joke I can find for you!"
---
+++ packages/fxh-funniest/fxh_funniest-0.1.tar.gz/fxh_funniest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="fxh_funniest",
+      version="0.1",
+      description="The funniest joke in the world",
+      url="http://github.com/fp/funniest",
+      author_email="huang.fp@gmail.com",
+      author="fangping",
+      license="MIT",
+      packages=['funniest'],
+      zip_safe=False)
