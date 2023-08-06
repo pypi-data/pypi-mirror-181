c123fb80529894e277ec303c23a175256d648cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:06 +0000

    Add package django-user-sessions/2.0.0/django-user-sessions-2.0.0.tar.gz

---
+++ packages/django-user-sessions/django-user-sessions-2.0.0.tar.gz/django-user-sessions-2.0.0/tests/settings.py
@@ -0,0 +1,64 @@
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+
+SECRET_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'user_sessions',
+    'tests',
+]
+
+MIDDLEWARE = (
+    'user_sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+)
+
+ROOT_URLCONF = 'tests.urls'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
+}
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+GEOIP_PATH = os.path.join(os.path.dirname(BASE_DIR), 'GeoLite2-City.mmdb')
+SESSION_ENGINE = 'user_sessions.backends.db'
+
+LOGIN_URL = '/admin/'
+LOGOUT_REDIRECT_URL = '/'
+
+SILENCED_SYSTEM_CHECKS = ['admin.E406', 'admin.E409', 'admin.E410']
