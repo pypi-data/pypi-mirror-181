a80a25b5fe87cb2510a80ea27d90c76f9a14cbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:51 +0000

    Add package python-logging-discord-handler/0.1.4/python_logging_discord_handler-0.1.4.tar.gz

---
+++ packages/python-logging-discord-handler/python_logging_discord_handler-0.1.4.tar.gz/python_logging_discord_handler-0.1.4/README.md
@@ -0,0 +1,207 @@
+[![PyPI version](https://badge.fury.io/py/python-logging-discord-handler.svg)](https://badge.fury.io/py/python-logging-discord-handler)
+
+[![Automated test suite](https://github.com/tradingstrategy-ai/python-logging-discord-handler/actions/workflows/tests.yml/badge.svg)](https://github.com/tradingstrategy-ai/python-logging-discord-handler/actions/workflows/tests.yml)
+
+[![Documentation Status](https://readthedocs.org/projects/python-logging-discord-handler/badge/?version=latest)](https://python-logging-discord-handler.readthedocs.io/en/latest/?badge=latest)
+
+# Python logging handler for Discord
+
+Redirect your Python log output to Discord using [Python logging subsystem](https://docs.python.org/3/howto/logging.html) and [Discord Webhook library](https://github.com/lovvskillz/python-discord-webhook).
+
+![Example screenshot](https://raw.githubusercontent.com/tradingstrategy-ai/python-logging-discord-handler/master/docs/source/_static/example_screenshot.png)
+
+# Use cases
+
+- Easily share logs with non-technical colleagues
+- Get notified on server-side errors
+- Follow your batch job processes easily
+- Good for businesses and communities that have their messaging set up in Discord 
+
+# Features
+
+- Minimum or no changes to a Python application needed
+- Optional color coding of messages using [Discord embeds](https://discordjs.guide/popular-topics/embeds.html#embed-preview)
+- Optional emoticons on messages using Unicode
+- Discord rate limiting friendly for burst of logs
+- [Documentation](https://python-logging-discord-handler.readthedocs.io/)
+- Special handling for long log messages like tracebacks to deal with Discord's 2000 character max message length
+
+# Requirements
+
+- Python 3.8+
+
+# Installation 
+
+[With pip](https://pypi.org/project/pip/):
+
+```shell
+pip install python-logging-discord-handler
+```
+
+[With Poetry](https://python-poetry.org/):
+
+```shell
+poetry add python-logging-discord-handler
+```
+
+# Usage
+
+This example logs both to Discord and standard output. 
+
+First you need to 
+
+```python
+import logging
+
+from discord_logging.handler import DiscordHandler
+
+# See instructions below how to get a Webhook URL
+webhook_url = # ...
+logger = logging.getLogger()
+
+stream_format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+discord_format = logging.Formatter("%(message)s")
+
+discord_handler = DiscordHandler(
+    "Hello World Bot", 
+    webhook_url, 
+    avatar_url="https://i0.wp.com/www.theterminatorfans.com/wp-content/uploads/2012/09/the-terminator3.jpg?resize=900%2C450&ssl=1")
+
+#discord_handler = DiscordHandler("Happy Bot", webhook_url, emojis={})
+discord_handler.setFormatter(discord_format)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(stream_format)
+
+# Add the handlers to the Logger
+logger.addHandler(discord_handler)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
+
+logger.info("This is an info message")
+logger.debug("A debug message - usually not that interesting")
+logger.error("Very nasty error messgae!")
+```
+
+[Find more examples in the examples.py source code](https://github.com/tradingstrategy-ai/python-logging-discord-handler/blob/master/discord_logging/examples.py).
+
+# How to get Discord webhook URL
+
+1. Go to *Edit channel* (gear) in Discord
+2. Choose *Integrations*
+3. Choose *View webhooks* -> *New*
+4. Copy URL
+
+## Webhook URL security
+
+It is recommend that you store the webhook URL outside your source code to avoid damage in hacks or similar security incidents.
+
+In Linux/macOS shell you can do on the command line:
+
+```shell
+export DISCORD_TEST_WEBHOOK_URL=<your webhook URL here>
+```
+
+For long term configuration, you can create a file storing environment variables outside your source code tree, like in your home directory. Store the `export` commands there.
+
+```shell
+# Text editor for a secrets.env file in your home directory on Linux
+nano ~/secrets.env  
+```
+
+In your Linux shell session, you can then read this file and make environment variables effective using [source](https://superuser.com/a/46149) command in your shell:
+
+```shell
+# Reads secrets.env and executes all commands there and makes them effective
+# in the current shell session
+source ~/secrets.env
+```
+
+Then you can read the environment variable in your Python code: 
+
+```python
+import os
+
+webhook_url = os.environ["DISCORD_TEST_WEBHOOK_URL"]
+```
+
+# Discord limitations
+
+- Max 2000 characters per message. See API documentation how to work around this limitation with different options. By default the bottom most lines of the log message, like a traceback, are shown.
+- Discord embeds, those that give you a logging level color bar on the left, have very hard time to deal with long lines. Embeds are disabled for long lines by default.
+
+## Log output formatting logic
+
+The log message are converted to Discord embeds with the following logic
+
+- Single line log messsages are converted to embed titles
+- For multi line log messages, the first line is the embed title and the following lines are the embed description
+- Long lines or long messages cannot be convert to embeds, instead they use [Discord Markdown code formattiong](https://support.discord.com/hc/en-us/articles/210298617-Markdown-Text-101-Chat-Formatting-Bold-Italic-Underline-) to preserve the readability of the output
+- A special `message_break_char` can be assigned to manually split long messages 
+
+# Colours and emoticons
+
+Logging messages can be decorated with colours and emoticons.
+
+![Emoji screenshot](https://raw.githubusercontent.com/tradingstrategy-ai/python-logging-discord-handler/master/docs/source/_static/emoji_example.png)
+
+
+Here are the defaults:
+
+```python
+#: The default log level colors as hexacimal, converted int
+DEFAULT_COLOURS = {
+    None: 2040357,  # Unknown log level
+    logging.CRITICAL: 14362664,  # Red
+    logging.ERROR: 14362664,  # Red
+    logging.WARNING: 16497928,  # Yellow
+    logging.INFO: 2196944,  # Blue
+    logging.DEBUG: 8947848,  # Gray
+}
+
+
+#: The default log emojis as
+DEFAULT_EMOJIS = {
+    None: "",  # Unknown log level
+    logging.CRITICAL: "🆘",
+    logging.ERROR: "❌",
+    logging.WARNING: "⚠️",
+    logging.INFO: "",
+    logging.DEBUG: "",
+}
+```
+
+Emoticons are disabled by default as they often make the output a bit too colourful and harder to read.
+
+# Testing and development
+
+## Manual tests
+
+Inspect how logging output looks in Discord.
+
+- Checkout this Git repository
+- Set up a dummy Discord channel
+- Get its webhook URL
+
+```shell
+poetry install -E docs 
+export DISCORD_TEST_WEBHOOK_URL=...
+python discord_logging/examples.py
+```
+
+This will dump some messages to your Discord.
+
+## Automated tests
+
+Run:
+
+```shell
+pytest
+```
+
+# History
+
+[Originally created for Trading Strategy](https://tradingstrategy.ai) to follow trading bot activity.
+
+# License 
+
+MIT
---
+++ packages/python-logging-discord-handler/python_logging_discord_handler-0.1.4.tar.gz/python_logging_discord_handler-0.1.4/discord_logging/handler.py
@@ -0,0 +1,233 @@
+"""Python logging support for Discord."""
+import logging
+import sys
+from typing import Optional, List
+
+from discord_webhook import DiscordEmbed, DiscordWebhook
+
+#: The default log level colors as hexacimal, converted int
+from requests import Response
+
+DEFAULT_COLOURS = {
+    None: 2040357,  # Unknown log level
+    logging.CRITICAL: 14362664,  # Red
+    logging.ERROR: 14362664,  # Red
+    logging.WARNING: 16497928,  # Yellow
+    logging.INFO: 2196944,  # Blue
+    logging.DEBUG: 8947848,  # Gray
+}
+
+
+#: The default log emojis as
+DEFAULT_EMOJIS = {
+    None: "",  # Unknown log level
+    logging.CRITICAL: "🆘",
+    logging.ERROR: "❌",
+    logging.WARNING: "⚠️",
+    logging.INFO: "",
+    logging.DEBUG: "",
+}
+
+
+class DiscordHandler(logging.Handler):
+    """Output logs to Discord chat.
+
+    A handler class which writes logging records, appropriately formatted,
+    to a Discord Server using webhooks.
+    """
+
+    def __init__(self,
+                 service_name: str,
+                 webhook_url: str,
+                 colours=DEFAULT_COLOURS,
+                 emojis=DEFAULT_EMOJIS,
+                 avatar_url: Optional[str]=None,
+                 rate_limit_retry: bool=True,
+                 embed_line_wrap_threshold: int=60,
+                 message_break_char: Optional[str]=None,
+                 discord_timeout: float=5.0):
+        """
+
+        :param service_name: Shows at the bot username in Discord.
+        :param webhook_url: Channel webhook URL. See README for details.
+        :param colours: Log level to Discord embed color mapping
+        :param emojis:
+            Log level to emoticon decoration mapping.
+            If present this is appended as a prefix to the first line of the log.
+        :param avatar_url: Bot profile picture
+        :param rate_limit_retry: Try to recover when Discord server tells us to slow down
+        :param embed_line_wrap_threshold:
+            How many characters a text line can contain until we go to "long line" output format.
+        :param message_break_char:
+            If given, manually split log entry text to several Discord messages by this character.
+            Useful if your application output long custom messages.
+            For example, you can use ellipsis `…` in your log message to split in several Discord
+            messages to overcome the 2000 character limitation.
+        :param discord_timeout:
+            How many seconds to wait before giving up on Discord request.
+        """
+
+        logging.Handler.__init__(self)
+        self.webhook_url = webhook_url
+        self.service_name = service_name
+        self.colours = colours
+        self.emojis = emojis
+        self.rate_limit_retry = rate_limit_retry
+        self.avatar_url = avatar_url
+        self.reentry_barrier = False
+        self.embed_line_wrap_threshold = embed_line_wrap_threshold
+        self.message_break_char = message_break_char
+        self.discord_timeout = discord_timeout
+
+    def should_format_as_code_block(self, record: logging.LogRecord, msg: str) -> bool:
+        """Figure out whether we want to use code block formatting in Discord.
+
+        Check for new lines and long lines in the log message.
+        """
+
+        if "\n" not in msg:
+            if len(msg) > self.embed_line_wrap_threshold:
+                return True
+
+        return "\n" in msg
+
+    def clip_content(self, content: str, max_len=1900, clip_to_end=True) -> str:
+        """Make sure the text fits to a Discord message.
+
+        Discord max message length is 2000 chars.
+        """
+        if len(content) > max_len - 5:
+            if clip_to_end:
+                return "..." + content[-max_len:]
+            else:
+                return content[0:max_len] + "..."
+        else:
+            return content
+
+    def split_by_break_character(self, content: str) -> List[str]:
+        """Split the inbound log message to several Discord messages.
+
+        Uses manual break character method if set. If not set,
+        then do nothing.
+        """
+        if self.message_break_char:
+            return content.split(self.message_break_char)
+        else:
+            return [content]
+
+    def attempt_to_report_failure(self, resp: Response, orignal: DiscordWebhook):
+        """Attempt to report a failure to deliver a log message.
+
+        Usually this happens if we pass content to Discord the server does not like.
+
+        More information
+
+        - https://stackoverflow.com/questions/53935198/in-my-discord-webhook-i-am-getting-the-error-embeds-0
+        """
+        # Output to the stderr, as it is not safe to use logging here
+        #
+        print(f"Discord webhook request failed: {resp.status_code}: {resp.content}. Payload content was: {orignal.content}, embeds: {orignal.embeds}", file=sys.stderr)
+        # Attempt to warn user about log failure
+        discord = DiscordWebhook(
+            url=self.webhook_url,
+            username=self.service_name,
+            rate_limit_retry=self.rate_limit_retry,
+            avatar_url=self.avatar_url,
+            timeout=self.discord_timeout,
+        )
+        discord.content = f"Failed to deliver log message: {resp.status_code}: {resp.content}"
+        discord.execute()
+
+    def emit(self, record: logging.LogRecord):
+        """Send a log entry to Discord."""
+
+        if self.reentry_barrier:
+            # Don't let Discord and request internals to cause logging
+            # and thus infinite recursion. This is because the underlying
+            # requests package itself uses logging.
+            return
+
+        self.reentry_barrier = True
+
+        try:
+
+            # About the Embed footer trick
+            # https://stackoverflow.com/a/65543555/315168
+
+            try:
+                # Run internal log message formatting that will expand %s, %d and such
+                inbound_msg = self.format(record)
+
+                # Choose colour and emoji for this log record
+                colour = self.colours.get(record.levelno) or self.colours[None]
+                emoji = self.emojis.get(record.levelno, "")
+                if emoji:
+                    # Add some space before the next char
+                    emoji += " "
+
+                split_msgs = self.split_by_break_character(inbound_msg)
+
+                for msg in split_msgs:
+
+                    if not msg:
+                        # Don't attempt to deliver empty messages
+                        continue
+
+                    # Start preparing the webhook payload for this messgae
+                    discord = DiscordWebhook(
+                        url=self.webhook_url,
+                        username=self.service_name,
+                        rate_limit_retry=self.rate_limit_retry,
+                        avatar_url=self.avatar_url,
+                        timeout=self.discord_timeout,
+                    )
+
+                    # Should we use Markdown code blocks for ths message?
+                    if self.should_format_as_code_block(record, msg):
+
+                        try:
+                            first, remainder = msg.split("\n", maxsplit=1)
+                        except ValueError:
+                            first = msg
+                            remainder = ""
+
+                        # Find our longest line lenght
+                        max_line_length = max([len(l) for l in msg.split("\n")])
+
+                        # Truncate the message to its last 2000 chars / bottom lines
+                        clipped = self.clip_content(remainder)
+
+                        if max_line_length > self.embed_line_wrap_threshold:
+                            # We have long lines, need to use Markdown
+                            clipped_msg = self.clip_content(msg)
+                            discord.content = f"```{emoji}{clipped_msg}```"
+                        else:
+                            # We have a clipped content, but short lines
+                            embed = DiscordEmbed(title=f"{emoji}{first}", description=clipped, color=colour)
+                            discord.add_embed(embed)
+
+                    else:
+                        # This message should be straight forward
+                        if emoji:
+                            title = f"{emoji}{msg}"
+                        else:
+                            title = msg
+                        embed = DiscordEmbed(title=title, color=colour)
+                        discord.add_embed(embed)
+
+                    # Can be one or list of responses,
+                    #  bad API design
+                    resp = discord.execute()
+                    assert isinstance(resp, Response), f"Discord webhook replies: {resp}"
+                    if resp.status_code != 200:
+                        self.attempt_to_report_failure(resp, discord)
+
+            except Exception as e:
+                # We cannot use handleError here, because Discord request may cause
+                # infinite recursion when Discord connection fails and
+                # it tries to log.
+                # We fall back to writing the error to stderr
+                print(f"Error from Discord logger {e}", file=sys.stderr)
+                self.handleError(record)
+        finally:
+            self.reentry_barrier = False
