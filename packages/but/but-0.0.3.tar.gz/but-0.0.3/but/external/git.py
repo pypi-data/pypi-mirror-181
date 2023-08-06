3fbf75546531ee3ab23bc01f6a5c866a5f987db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:44 +0000

    Add package but/0.0.3/but-0.0.3.tar.gz

---
+++ packages/but/but-0.0.3.tar.gz/but-0.0.3/but/external/git.py
@@ -0,0 +1,67 @@
+import re
+from subprocess import check_call, check_output
+
+
+def git_check_available():
+    try:
+        result = check_output(['git', '--version'], encoding='utf-8')
+    except FileNotFoundError:
+        raise RuntimeError('Cannot run `git` (FileNotFoundError)')
+
+    version = re.match(r'git version (.+?)$', result, re.MULTILINE)
+    if version is None:
+        raise RuntimeError(f'Cannot understand `git --version`, got {result!r}')
+
+    version_tuple = tuple(version.group(1).split('.'))
+    if version_tuple[0] != '2':
+        raise RuntimeError(f'Expected git version 2, got {version.group(1)!r}')
+
+    return version_tuple
+
+
+def git_clean_out():
+    try:
+        check_call(['git', 'clean', '--force', '--quiet', '-x', 'out'])
+    except FileNotFoundError:
+        raise RuntimeError('Cannot run `git` (FileNotFoundError)')
+
+
+def git_log_created(path):
+    try:
+        result = check_output(
+            [
+                'git',
+                'log',
+                '--follow',
+                '-1',
+                '--pretty=format:%at',
+                '--diff-filter=A',
+                '--',
+                path,
+            ],
+            encoding='utf-8',
+        )
+    except FileNotFoundError:
+        raise RuntimeError('Cannot run `git` (FileNotFoundError)')
+
+    return 0 if result == '' else int(result)
+
+
+def git_log_updated(path):
+    try:
+        result = check_output(
+            [
+                'git',
+                'log',
+                '--follow',
+                '-1',
+                '--pretty=format:%at',
+                '--',
+                path,
+            ],
+            encoding='utf-8',
+        )
+    except FileNotFoundError:
+        raise RuntimeError('Cannot run `git` (FileNotFoundError)')
+
+    return 0 if result == '' else int(result)
