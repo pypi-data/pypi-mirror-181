09c65b97f6e405b543d3d7cca5d02e12f134a6f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:04 +0000

    Add package emo-connect-python/0.1.0/emo-connect-python-0.1.0.tar.gz

---
+++ packages/emo-connect-python/emo-connect-python-0.1.0.tar.gz/emo-connect-python-0.1.0/README.md
@@ -0,0 +1,33 @@
+# emo-connect-python
+Python library for connecting and interacting with an EMO pet
+
+## Usage
+```bash
+pip install emo-connect-python
+```
+## Example
+```python
+import asyncio
+import json
+import logging
+
+from emoconnect.EmoConnectManager import EmoConnectManager
+from emoconnect.ble.request import StateRequest
+
+logging.basicConfig(format='[EMO] %(asctime)s %(levelname)s %(message)s')
+logger = logging.getLogger()
+# can be changed to logging.DEBUG for debugging issues
+logger.setLevel(logging.INFO)
+
+
+async def main():
+    ecm = EmoConnectManager()
+    await ecm.connectToEmo()
+
+    state = await ecm.sendRequest(StateRequest.everything())
+    print(state)
+
+    await ecm.disconnect()
+
+asyncio.run(main())
+```
---
+++ packages/emo-connect-python/emo-connect-python-0.1.0.tar.gz/emo-connect-python-0.1.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='emo-connect-python',
+    version='0.1.0',
+    packages=find_packages(
+        include=['src/emoconnect*'],
+        exclude=['src/examples*', 'src/tests*'],
+    ),
+    author='Clark Scheff',
+    author_email='clark@scheffsblend.com',
+    description='Python library for connecting and interacting with an EMO pet',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/0xD34D/emo-connect-python',
+    install_requires=[
+        'bleak',
+        'bleak-retry-connector',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+    ],
+)
