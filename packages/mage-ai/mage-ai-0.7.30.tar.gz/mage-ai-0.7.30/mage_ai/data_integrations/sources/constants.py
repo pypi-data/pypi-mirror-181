a172d3f431b6b9fe163164668fe55f83f451f431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:23 +0000

    Add package mage-ai/0.7.30/mage-ai-0.7.30.tar.gz

---
+++ packages/mage-ai/mage-ai-0.7.30.tar.gz/mage-ai-0.7.30/mage_ai/data_integrations/sources/constants.py
@@ -0,0 +1,17 @@
+SQL_SOURCES = [
+    dict(name='BigQuery'),
+    dict(name='MySQL'),
+    dict(name='PostgreSQL'),
+    dict(name='Redshift'),
+]
+
+SOURCES = sorted([
+    dict(name='Api'),
+    dict(name='Amplitude'),
+    dict(name='Chargebee'),
+    dict(name='Google Search Console'),
+    dict(name='Google Sheets'),
+    dict(name='Intercom'),
+    dict(name='Salesforce'),
+    dict(name='Stripe'),
+] + SQL_SOURCES, key=lambda x: x['name'])
