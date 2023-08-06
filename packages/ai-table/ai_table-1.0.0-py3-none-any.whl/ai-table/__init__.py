43bc06a6a10d2ab6b27a1c4c15711b096dcf6aee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:46:59 +0000

    Add package ai-table/1.0.0/ai_table-1.0.0-py3-none-any.whl

---
+++ packages/ai-table/ai_table-1.0.0-py3-none-any.whl/ai-table/__init__.py
@@ -0,0 +1,5 @@
+from .dist.core import clean_results, read_data
+from .dist.core import check_separation_data, check_recognition_data
+from .dist.core import shapes_separation
+from .dist.core import shapes_recognition
+from .dist.core import show_separation_results, show_recognition_results
