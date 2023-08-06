917614bca652d542e3ae007c299061b885de97fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:55 +0000

    Add package alteredai/0.0.3/alteredai-0.0.3-py3-none-any.whl

---
+++ packages/alteredai/alteredai-0.0.3-py3-none-any.whl/alteredai/AlteredAiDataLoader.py
@@ -0,0 +1,6 @@
+class AlteredAiDataLoader:
+    def __init__(self):
+        print("AlteredAiDataLoader Loaded Successfully")
+    
+    def funDataLoader(self):
+        print("This is function from DataLoader Class")
