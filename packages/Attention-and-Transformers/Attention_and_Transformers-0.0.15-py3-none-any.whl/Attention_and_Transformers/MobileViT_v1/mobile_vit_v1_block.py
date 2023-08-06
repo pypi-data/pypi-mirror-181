5e7fb9522fd15cfc5d96be307734bdad296c2121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:01:23 +0000

    Add package Attention-and-Transformers/0.0.15/Attention_and_Transformers-0.0.15-py3-none-any.whl

---
+++ packages/Attention-and-Transformers/Attention_and_Transformers-0.0.15-py3-none-any.whl/Attention_and_Transformers/MobileViT_v1/mobile_vit_v1_block.py
@@ -0,0 +1,231 @@
+from typing import Union
+
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Layer, Dropout, Dense, LayerNormalization, Concatenate
+
+from .BaseLayers import ConvLayer
+from .multihead_self_attention_2D import MultiHeadSelfAttentionEinSum2D as MHSA
+
+
+class Transformer(Layer):
+    def __init__(
+        self,
+        num_heads: int = 4,
+        embedding_dim: int = 90,
+        qkv_bias: bool = True,
+        mlp_ratio: float = 2.0,
+        linear_drop: float = 0.2,
+        attention_drop: float = 0.2,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self.norm_1 = LayerNormalization(epsilon=1e-6)
+        self.norm_2 = LayerNormalization(epsilon=1e-6)
+
+        self.attn = MHSA(
+            num_heads=num_heads,
+            embedding_dim=embedding_dim,
+            qkv_bias=qkv_bias,
+            attention_drop=attention_drop,
+            linear_drop=linear_drop,
+        )
+
+        hidden_features = int(embedding_dim * mlp_ratio)
+
+        self.mlp = Sequential(
+            layers=[
+                Dense(hidden_features, activation="swish"),
+                Dropout(linear_drop),
+                Dense(embedding_dim),
+                Dropout(linear_drop),
+            ]
+        )
+
+    def call(self, x):
+
+        x = x + self.attn(self.norm_1(x))
+        x = x + self.mlp(self.norm_2(x))
+
+        return x
+
+
+# https://github.com/apple/ml-cvnets/blob/84d992f413e52c0468f86d23196efd9dad885e6f/cvnets/modules/mobilevit_block.py#L186
+def unfolding(nn, patch_h: int = 2, patch_w: int = 2):
+    """
+    ### Notations (wrt paper) ###
+        B/b = batch
+        P/p = patch_size
+        N/n = number of patches
+        D/d = embedding_dim
+
+    H, W
+    [                            [
+        [1, 2, 3, 4],     Goal      [1, 3, 9, 11],
+        [5, 6, 7, 8],     ====>     [2, 4, 10, 12],
+        [9, 10, 11, 12],            [5, 7, 13, 15],
+        [13, 14, 15, 16],           [6, 8, 14, 16]
+    ]                            ]
+    """
+
+    B, H, W, D = tf.shape(nn)[0], tf.shape(nn)[1], tf.shape(nn)[2], tf.shape(nn)[3]
+    patch_area = int(patch_h * patch_w)
+
+    num_patch_h, num_patch_w = int(tf.math.ceil(H / patch_h)), int(tf.math.ceil(W / patch_w))
+    num_patches = num_patch_h * num_patch_w
+
+    interpolate = False
+
+    if ((num_patch_h * patch_h) != H) or ((num_patch_w * patch_w) != W):
+        nn = tf.image.resize(nn, [num_patch_h * patch_h, num_patch_w * patch_w], method="bilinear")
+        interpolate = True
+
+    # [B, H, W, D] --> [B*nh, ph, nw, pw*D]
+    reshaped_fm = tf.reshape(nn, (B * num_patch_h, patch_h, num_patch_w, patch_w * D))
+
+    # [B*nh, ph, nw, pw*D] --> [B*nh, nw, ph, pw*D]
+    transposed_fm = tf.transpose(reshaped_fm, perm=[0, 2, 1, 3])
+
+    # [B*nh, nw, ph, pw*D] --> [B, N, P, D]
+    reshaped_fm = tf.reshape(transposed_fm, (B, num_patches, patch_area, D))
+
+    # [B, N, P, D] --> [B, P, N, D]
+    transposed_fm = tf.transpose(reshaped_fm, perm=[0, 2, 1, 3])
+
+    info_dict = {
+        "orig_size": (H, W),
+        "batch_size": B,
+        "dim": D,
+        "interpolate": interpolate,
+        "num_patches_w": num_patch_w,
+        "num_patches_h": num_patch_h,
+    }
+
+    return transposed_fm, info_dict
+
+
+# https://github.com/apple/ml-cvnets/blob/84d992f413e52c0468f86d23196efd9dad885e6f/cvnets/modules/mobilevit_block.py#L233
+def folding(nn, info_dict: dict, patch_h: int = 2, patch_w: int = 2):
+    """
+    ### Notations (wrt paper) ###
+        B/b = batch
+        P/p = patch_size
+        N/n = number of patches
+        D/d = embedding_dim
+    """
+
+    B = info_dict["batch_size"]
+    D = info_dict["dim"]
+    num_patch_h = info_dict["num_patches_h"]
+    num_patch_w = info_dict["num_patches_w"]
+
+    # [B, P, N D] --> [B, N, P, D]
+    nn = tf.transpose(nn, perm=(0, 2, 1, 3))
+
+    # [B, N, P, D] --> [B*nh, nw, ph, pw*D]
+    nn = tf.reshape(nn, (B * num_patch_h, num_patch_w, patch_h, patch_w * D))
+
+    # [B*nh, nw, ph, pw*D] --> [B*nh, ph, nw, pw*D]
+    nn = tf.transpose(nn, perm=(0, 2, 1, 3))
+
+    # [B*nh, ph, nw, pw*D] --> [B, nh*ph, nw, pw, D] --> [B, H, W, C]
+    nn = tf.reshape(nn, (B, num_patch_h * patch_h, num_patch_w * patch_w, D))
+
+    if info_dict["interpolate"]:
+        nn = tf.image.resize(nn, size=info_dict["orig_size"])
+    return nn
+
+
+class MobileViT_v1_Block(Layer):
+    def __init__(
+        self,
+        out_filters: int = 64,
+        embedding_dim: int = 90,
+        patch_size: Union[int, tuple] = (2, 2),
+        transformer_repeats: int = 2,
+        num_heads: int = 4,
+        attention_drop: float = 0.0,
+        linear_drop: float = 0.0,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self.out_filters = out_filters
+        self.embedding_dim = embedding_dim
+        self.patch_size_h, self.patch_size_w = patch_size if isinstance(patch_size, tuple) else (patch_size // 2, patch_size // 2)
+        self.transformer_repeats = transformer_repeats
+        self.num_heads = num_heads
+
+        # local_feature_extractor 1 and 2
+        self.local_features_1 = ConvLayer(num_filters=self.out_filters, kernel_size=3, strides=1, use_bn=True, use_activation=True)
+        self.local_features_2 = ConvLayer(
+            num_filters=self.embedding_dim, kernel_size=1, strides=1, use_bn=False, use_activation=False, use_bias=False
+        )
+
+        layers = [
+            Transformer(
+                embedding_dim=self.embedding_dim,
+                num_heads=self.num_heads,
+                linear_drop=linear_drop,
+                attention_drop=attention_drop,
+            )
+            for _ in range(self.transformer_repeats)
+        ]
+
+        layers.append(LayerNormalization(epsilon=1e-6))
+
+        # Repeated transformer blocks
+        self.transformer_blocks = Sequential(layers=layers)
+
+        # Fusion blocks
+        self.local_features_3 = ConvLayer(num_filters=self.out_filters, kernel_size=1, strides=1, use_bn=True, use_activation=True)
+        self.concat = Concatenate(axis=-1)
+        self.fuse_local_global = ConvLayer(num_filters=self.out_filters, kernel_size=3, strides=1, use_bn=True, use_activation=True)
+
+    def call(self, x):
+        local_representation = self.local_features_1(x)
+        local_representation = self.local_features_2(local_representation)
+
+        # Transformer as Convolution Steps
+        # --------------------------------
+        # # Unfolding
+        unfolded, info_dict = unfolding(local_representation, patch_h=self.patch_size_h, patch_w=self.patch_size_w)
+
+        # # Infomation sharing/mixing --> global representation
+        global_representation = self.transformer_blocks(unfolded)
+
+        # # Folding
+        folded = folding(global_representation, info_dict=info_dict, patch_h=self.patch_size_h, patch_w=self.patch_size_w)
+        # # --------------------------------
+
+        # Fusion
+        local_mix = self.local_features_3(folded)
+        fusion = self.concat([local_mix, x])
+        fusion = self.fuse_local_global(fusion)
+
+        return fusion
+
+
+if __name__ == "__main__":
+    batch = 2
+    H = W = 32
+    C = 96
+    P = 2 * 2
+    L = 4
+    embedding_dim = 144
+
+    mvitblk = MobileViT_v1_Block(
+        out_filters=C,
+        embedding_dim=embedding_dim,
+        patch_size=P,
+        transformer_repeats=L,
+        attention_drop=0.0,
+        linear_drop=0.0,
+    )
+
+    inputs = tf.random.normal((batch, H, W, C))
+
+    out = mvitblk(inputs)
+    print("inputs.shape", inputs.shape)
+    print("out.shape", out.shape)
---
+++ packages/Attention-and-Transformers/Attention_and_Transformers-0.0.15-py3-none-any.whl/Attention_and_Transformers/ViT/multihead_self_attention.py
@@ -0,0 +1,295 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Layer, Dropout, Dense
+from tensorflow.keras.layers import EinsumDense
+
+
+tf.random.set_seed(1)
+tf.keras.utils.set_random_seed(1)
+
+
+class MultiHeadSelfAttentionEinSum(Layer):
+    """
+    Apply Multi-Head Self Attention.
+
+    Parameters
+    ----------
+    num_heads: int = 2
+        Number of heads
+
+    embedding_dim: int = 64
+        Size of embedding dimension i.e. D or Dmodel
+
+    projection_dim: int = None
+        Dimension for each head
+
+    qkv_bias: bool = True
+        Use bias in query, keys and values projection layer
+
+    attention_drop: float = 0.2
+        Dropout rate for the attention matrix
+
+    linear_drop: float = 0.2
+        Dropout rate for the final linear projection
+
+    """
+
+    def __init__(
+        self,
+        num_heads: int = 2,
+        embedding_dim: int = 64,
+        projection_dim: int = None,
+        qkv_bias: bool = True,
+        attention_drop: float = 0.2,
+        linear_drop: float = 0.2,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self.num_heads = num_heads
+        self.embedding_dim = embedding_dim
+        self.projection_dim = projection_dim if projection_dim else self.embedding_dim // self.num_heads
+        self.scale = self.projection_dim**0.5
+        self.qkv_bias = qkv_bias
+
+        self.use_attention_drop = attention_drop > 0.0
+        self.use_linear_drop = linear_drop > 0.0
+
+        if self.use_attention_drop:
+            self.attn_dropout = Dropout(attention_drop)
+
+        if self.use_linear_drop:
+            self.linear_dropout = Dropout(linear_drop)
+
+        ##### Notations #####
+        # B/b: batch
+        # T/t: number of tokens
+        # E/e: embedding dimension
+        # N/n: number of heads
+        # P/p: projection dimension
+
+        # Shape:(B, T, E) * (E, N, P * 3) --> (B, T, N, P * 3)
+        # self.W_QKV = EinsumDense(
+        #     "bte,enp->btnp",
+        #     output_shape=[None, self.num_heads, 3 * self.projection_dim],
+        #     bias_axes="np" if self.qkv_bias else None,
+        # )
+
+        # New Shape: (B, T, E) * (E, N, P * 3) --> (B, N, T, P * 3)
+        self.W_QKV = EinsumDense(
+            "bte,enp->bntp",
+            output_shape=[self.num_heads, None, 3 * self.projection_dim],
+            bias_axes="np" if self.qkv_bias else None,
+        )
+
+        # Shape: (B, N, T, P) * (P, N, E) --> (B, T, E)
+        self.Wo = EinsumDense("bntp,pne->bte", output_shape=[None, self.embedding_dim], bias_axes="e" if self.qkv_bias else None)
+
+    def call(self, inputs):
+        # inputs --> Shape: (B, T, E)
+
+        # Old -- Transpose dimensions after operation
+        # output_tensor = self.W_QKV(inputs)  # Shape: (B, T, N, P * 3)
+        # output_tensor = tf.einsum("btnp->bntp", output_tensor)  # Shape: (B, N, T, P * 3)
+
+        # New -- Simulatenously transpose the dimension
+        output_tensor = self.W_QKV(inputs)  # Shape: (B, N, T, P * 3)
+
+        # Shape: (B, N, T, P) * 3
+        q, k, v = tf.split(output_tensor, num_or_size_splits=3, axis=-1)
+
+        # Shape: (B, N, T, P) * (B, N, T, P) --> (B, N, T, T)
+        attention_matrix = tf.einsum("...ij, ...kj-> ...ik", q, k)
+
+        # Shape: (B, N, T, T)
+        attention_matrix = tf.nn.softmax(tf.math.divide(attention_matrix, self.scale), axis=-1)
+        if self.use_attention_drop:
+            attention_matrix = self.attn_dropout(attention_matrix)
+
+        # Shape: (B, N, T, T) * (B, N, T, P) --> (B, N, T, P)
+        weighted_values = tf.einsum("...ij,...jk->...ik", attention_matrix, v)
+
+        final = self.Wo(weighted_values)  # Shape: (B, N, T, P) * (P, N, E) --> (B, T, E)
+        if self.use_linear_drop:
+            final = self.linear_dropout(final)
+
+        return final
+
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "num_heads": self.num_heads,
+                "embedding_dim": self.embedding_dim,
+                "projection_dim": self.projection_dim,
+                "qkv_bias": self.qkv_bias,
+            }
+        )
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+
+class MultiHeadSelfAttention_basic(Layer):
+    """
+    Apply Multi-Head Self Attention.
+
+    Parameters
+    ----------
+    num_heads: int = 2
+        Number of heads
+
+    embedding_dim: int = 64
+        Size of embedding dimension i.e. D or Dmodel
+
+    projection_dim: int = None
+        Dimension for each head
+
+    qkv_bias: bool = True
+        Use bias in query, keys and values projection layer
+
+    attention_drop: float = 0.2
+        Dropout rate for the attention matrix
+
+    linear_drop: float = 0.2
+        Dropout rate for the final linear projection
+    """
+
+    def __init__(
+        self,
+        num_heads: int = 2,
+        embedding_dim: int = 64,
+        projection_dim: int = None,
+        qkv_bias: bool = True,
+        attention_drop: float = 0.2,
+        linear_drop: float = 0.2,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self.num_heads = num_heads
+        self.embedding_dim = embedding_dim
+        self.projection_dim = projection_dim if projection_dim else self.embedding_dim // self.num_heads
+        self.scale = self.projection_dim**0.5
+        self.heads_merge_dimension = self.projection_dim * self.num_heads
+        self.qkv_bias = qkv_bias
+
+        self.qkv_W = Dense(units=3 * self.num_heads * self.projection_dim, name="W_expand_project", use_bias=self.qkv_bias)
+        self.final_linear_project = Dense(units=self.embedding_dim, name="final_project", use_bias=self.qkv_bias)
+
+        self.use_attention_drop = attention_drop > 0.0
+        self.use_linear_drop = linear_drop > 0.0
+
+        if self.use_attention_drop:
+            self.attn_dropout = Dropout(attention_drop)
+
+        if self.use_linear_drop:
+            self.linear_dropout = Dropout(linear_drop)
+
+    def call(self, input_mat):
+
+        batch_dims = tf.shape(input_mat)[0]
+        input_dims = tf.shape(input_mat)[1]
+
+        Q_K_V = self.qkv_W(input_mat)  # Shape: (#B, #tokens, #projection_dim *3)
+        # tf.print(tf.shape(Q_K_V))
+        # Shape: (#B, #tokens, #heads, #projection_dim *3)
+        Q_K_V_reshape = tf.reshape(Q_K_V, (batch_dims, input_dims, self.num_heads, 3 * self.projection_dim))
+        # tf.print(tf.shape(Q_K_V_reshape))
+
+        Q_K_V_transpose = tf.transpose(Q_K_V_reshape, perm=(0, 2, 1, 3))  # Shape: (#B, #heads, #tokens, #projection_dim * 3)
+        # tf.print(tf.shape(Q_K_V_transpose))
+
+        q, k, v = tf.split(Q_K_V_transpose, num_or_size_splits=3, axis=-1)
+        # print("q:", q.shape, "k:", k.shape, "v:", v.shape)
+
+        # Shape: (#B, #heads, #tokens, #tokens)
+        attention_matrix = tf.nn.softmax(q @ tf.transpose(k, perm=(0, 1, 3, 2)) / self.scale, axis=-1)
+        # tf.print("attention_matrix:", tf.shape(attention_matrix))
+        if self.use_attention_drop:
+            attention_matrix = self.attn_dropout(attention_matrix)
+
+        weighted_values = attention_matrix @ v  # Shape: (#B, #heads, #tokens, #projection_dim)
+        # tf.print("Reweighting inputs:", tf.shape(weighted_values))
+
+        weighted_values = tf.transpose(weighted_values, perm=(0, 2, 1, 3))  # Shape: (#B, #tokens, #heads, #projection_dim)
+        # tf.print("bring head and dim together", tf.shape(final_out))
+
+        weighted_values = tf.reshape(weighted_values, (batch_dims, input_dims, self.heads_merge_dimension))
+        # tf.print("Concatenating values from all heads:", tf.shape(weighted_values))
+
+        mhsa_output = self.final_linear_project(weighted_values)
+        if self.use_linear_drop:
+            final = self.linear_dropout(final)
+        # tf.print("mhsa_output:", tf.shape(mhsa_output))
+
+        return mhsa_output
+
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "num_heads": self.num_heads,
+                "embedding_dim": self.embedding_dim,
+                "projection_dim": self.projection_dim,
+                "qkv_bias": self.qkv_bias,
+            }
+        )
+        return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(**config)
+
+
+if __name__ == "__main__":
+    batch_dims = 2
+    input_dims = 4
+    embedding_dim = 64
+    num_heads = 8
+    use_bias = False
+
+    proj_dim = embedding_dim // num_heads
+
+    print("Normal")
+    lal = MultiHeadSelfAttention_basic(num_heads=num_heads, embedding_dim=embedding_dim, projection_dim=proj_dim, qkv_bias=use_bias, name="mhsa")
+
+    inputs = tf.random.normal((batch_dims, input_dims, embedding_dim))
+    _ = lal(inputs)
+
+    model = tf.keras.Sequential()
+
+    model.add(tf.keras.layers.InputLayer(input_shape=(None, embedding_dim)))
+    model.add(lal)
+    # model.summary()
+
+    print(model.count_params())
+
+    print("Einsum")
+    lal = MultiHeadSelfAttentionEinSum(
+        num_heads=num_heads,
+        embedding_dim=embedding_dim,
+        projection_dim=proj_dim,
+        qkv_bias=use_bias,
+        name="mhsa",
+    )
+
+    inputs = tf.random.normal((batch_dims, input_dims, embedding_dim))
+    _ = lal(inputs)
+
+    model = tf.keras.Sequential()
+
+    model.add(tf.keras.layers.InputLayer(input_shape=(None, embedding_dim)))
+    model.add(lal)
+    # model.summary()
+
+    print(model.count_params())
+
+    print("Tensorflow default")
+    layer = tf.keras.layers.MultiHeadAttention(num_heads=num_heads, key_dim=proj_dim, use_bias=use_bias)
+    inputs = tf.keras.Input(shape=(None, embedding_dim))
+    output_tensor = layer(inputs, inputs)
+    model = tf.keras.Model(inputs, output_tensor)
+    print(model.count_params())
+    # model.summary()
