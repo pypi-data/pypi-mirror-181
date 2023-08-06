bdb35ed0ab17a5568ee032073a72ba694e643068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:02:32 +0000

    Add package endpoint-wrapper/1.0.12/endpoint-wrapper-1.0.12.tar.gz

---
+++ packages/endpoint-wrapper/endpoint-wrapper-1.0.12.tar.gz/endpoint-wrapper-1.0.12/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+
+    long_description = fh.read()
+
+setuptools.setup(
+    install_requires=[
+        'confluent-kafka'
+    ],
+    name="endpoint-wrapper",
+    packages=['endpoint_wrapper', 'endpoint_wrapper.util'],
+    version="1.0.12",
+    author="Raymond Pang",
+    author_email="raymondpang365@gmail.com",
+    description="A helper that wraps your service with messaging protocols",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/raymondpang365/endpoint-wrapper",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    download_url="https://codeload.github.com/raymondpang365/endpoint-wrapper/tar.gz/refs/tags/1.0.0",
+)
