75328bb24a3fb455cfbb2a5e1f50c1564b315046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:00 +0000

    Add package komle-witslm-client/0.3.4.1/komle-witslm-client-0.3.4.1.tar.gz

---
+++ packages/komle-witslm-client/komle-witslm-client-0.3.4.1.tar.gz/komle-witslm-client-0.3.4.1/komle_witslm_client/bindings/v1411/update/_gsr.py
@@ -0,0 +1,84 @@
+# komle_witslm_client/bindings/v1411/update/_gsr.py
+# -*- coding: utf-8 -*-
+# PyXB bindings for NM:5aaaf474613b31707b26b95522eb65602c2f6b28
+# Generated 2022-10-17 03:43:38.839613 by PyXB version 1.2.6 using Python 3.10.4.final.0
+# Namespace http://www.isotc211.org/2005/gsr [xmlns:gsr]
+
+from __future__ import unicode_literals
+import pyxb
+import pyxb.binding
+import pyxb.binding.saxer
+import io
+import pyxb.utils.utility
+import pyxb.utils.domutils
+import sys
+import pyxb.utils.six as _six
+# Unique identifier for bindings created at the same time
+_GenerationUID = pyxb.utils.utility.UniqueIdentifier('urn:uuid:bad9cb73-4c52-4f57-9441-277e82f301b6')
+
+# Version of PyXB used to generate the bindings
+_PyXBVersion = '1.2.6'
+
+# A holder for module-level binding classes so we can access them from
+# inside class definitions where property names may conflict.
+_module_typeBindings = pyxb.utils.utility.Object()
+
+# Import bindings for namespaces imported into schema
+import komle_witslm_client.bindings.v1411.update._nsgroup as _ImportedBinding_bindingsv1411update__nsgroup
+
+# NOTE: All namespace declarations are reserved within the binding
+Namespace = pyxb.namespace.NamespaceForURI('http://www.isotc211.org/2005/gsr', create_if_missing=True)
+Namespace.configureCategories(['typeBinding', 'elementBinding'])
+
+def CreateFromDocument (xml_text, fallback_namespace=None, location_base=None, default_namespace=None):
+    """Parse the given XML and use the document element to create a
+    Python instance.
+
+    @param xml_text An XML document.  This should be data (Python 2
+    str or Python 3 bytes), or a text (Python 2 unicode or Python 3
+    str) in the L{pyxb._InputEncoding} encoding.
+
+    @keyword fallback_namespace An absent L{pyxb.Namespace} instance
+    to use for unqualified names when there is no default namespace in
+    scope.  If unspecified or C{None}, the namespace of the module
+    containing this function will be used, if it is an absent
+    namespace.
+
+    @keyword location_base: An object to be recorded as the base of all
+    L{pyxb.utils.utility.Location} instances associated with events and
+    objects handled by the parser.  You might pass the URI from which
+    the document was obtained.
+
+    @keyword default_namespace An alias for @c fallback_namespace used
+    in PyXB 1.1.4 through 1.2.6.  It behaved like a default namespace
+    only for absent namespaces.
+    """
+
+    if pyxb.XMLStyle_saxer != pyxb._XMLStyle:
+        dom = pyxb.utils.domutils.StringToDOM(xml_text)
+        return CreateFromDOM(dom.documentElement)
+    if fallback_namespace is None:
+        fallback_namespace = default_namespace
+    if fallback_namespace is None:
+        fallback_namespace = Namespace.fallbackNamespace()
+    saxer = pyxb.binding.saxer.make_parser(fallback_namespace=fallback_namespace, location_base=location_base)
+    handler = saxer.getContentHandler()
+    xmld = xml_text
+    if isinstance(xmld, _six.text_type):
+        xmld = xmld.encode(pyxb._InputEncoding)
+    saxer.parse(io.BytesIO(xmld))
+    instance = handler.rootObject()
+    return instance
+
+def CreateFromDOM (node, fallback_namespace=None, default_namespace=None):
+    """Create a Python instance from the given DOM node.
+    The node tag must correspond to an element declaration in this module.
+
+    @deprecated: Forcing use of DOM interface is unnecessary; use L{CreateFromDocument}."""
+    if fallback_namespace is None:
+        fallback_namespace = default_namespace
+    if fallback_namespace is None:
+        fallback_namespace = Namespace.fallbackNamespace()
+    return pyxb.binding.basis.element.AnyCreateFromDOM(node, fallback_namespace)
+
+from komle_witslm_client.bindings.v1411.update._nsgroup import SC_CRS_PropertyType # {http://www.isotc211.org/2005/gsr}SC_CRS_PropertyType
