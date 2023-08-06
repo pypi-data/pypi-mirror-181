f3f09e6762ded754c53812c914faf27f13adaa8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:46 +0000

    Add package p360-export/1.4.0/p360_export-1.4.0-py3-none-any.whl

---
+++ packages/p360-export/p360_export-1.4.0-py3-none-any.whl/p360_export/exceptions/data_picker.py
@@ -0,0 +1,14 @@
+class DataPickerException(Exception):
+    pass
+
+
+class EmptyColumnMappingException(DataPickerException):
+    pass
+
+
+class InvalidFacebookColumnException(DataPickerException):
+    pass
+
+
+class UserIdMappingMissingException(DataPickerException):
+    pass
