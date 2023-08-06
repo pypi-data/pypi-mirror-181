72e800e2114d8cf383856c876efb19cecabff018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:45 +0000

    Add package polars-lts-cpu/0.15.6/polars_lts_cpu-0.15.6.tar.gz

---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.15.6.tar.gz/polars_lts_cpu-0.15.6/local_dependencies/polars/tests/it/core/mod.rs
@@ -0,0 +1,11 @@
+mod groupby;
+mod joins;
+mod list;
+#[cfg(feature = "pivot")]
+mod pivot;
+#[cfg(feature = "rolling_window")]
+mod rolling_window;
+mod series;
+mod utils;
+
+use polars::prelude::*;
