4ff6d99d952b0dc7f3b2f8b0d10d8dae0fb5b87b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:35 +0000

    Add package django-user-auth-all/0.1/django_user_auth_all-0.1-py3-none-any.whl

---
+++ packages/django-user-auth-all/django_user_auth_all-0.1-py3-none-any.whl/user/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import *
+# from django.contrib.auth.models import Permission
+# from django.contrib.auth.models import Group
+
+
+
+# admin.site.register(Group)
+
+admin.site.register(User)
+# admin.site.register(Permission)
---
+++ packages/django-user-auth-all/django_user_auth_all-0.1-py3-none-any.whl/user/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path,include
+from .views import *
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register('users',UserViewSet)
+router.register('permission',PermissionViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('user_role/',UserRoleApi.as_view()),
+    path('update_profile/',update_profile),
+    path('change_password/',change_password),
+    path('auth/',LoginView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+]
