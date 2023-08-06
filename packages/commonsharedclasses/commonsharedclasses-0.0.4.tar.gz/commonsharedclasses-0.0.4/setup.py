00b832b2a895d9107061af7963ad8cc7c467dabf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:52 +0000

    Add package commonsharedclasses/0.0.4/commonsharedclasses-0.0.4.tar.gz

---
+++ packages/commonsharedclasses/commonsharedclasses-0.0.4.tar.gz/commonsharedclasses-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='commonsharedclasses',
+    version='0.0.4',
+    description='This package will contain all common classes, version 0.0.3 updates:- getSharedLakehouseSqlCursor function added in tenant_lakehouse_query class',
+    author='Ankit Kumar',
+    packages=['commonsharedclasses'],
+    zip_safe=False
+)
