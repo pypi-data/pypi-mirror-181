04aa0d507382ff1ff825cd635099df90810abc34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:47 +0000

    Add package ypkpathway/4.0.0a6/ypkpathway-4.0.0a6-py3-none-any.whl

---
+++ packages/ypkpathway/ypkpathway-4.0.0a6-py3-none-any.whl/ypkpathway/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""docstring."""
+
+__author__ = "Björn Johansson"
+__copyright__ = "Copyright 2013 - 2022 Björn Johansson"
+__credits__ = ["Björn Johansson"]
+__license__ = "BSD"
+__maintainer__ = "Björn Johansson"
+__email__ = "bjornjobb@gmail.com"
+
+from ._version import version as __version__
+
+from .code import element_cloning, TranscriptionalUnit, PathWay
+
+__all__ = ["element_cloning", "TranscriptionalUnit", "PathWay"]
