9911171bf27e28dee85403c8df9a9604b8d9f091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:03 +0000

    Add package vfbLib/0.2.0/vfbLib-0.2.0-py2.py3-none-any.whl

---
+++ packages/vfbLib/vfbLib-0.2.0-py2.py3-none-any.whl/vfbLib/ufo/guides.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+import logging
+
+from typing import TYPE_CHECKING, List
+from vfbLib.ufo.typing import UfoGuide
+
+if TYPE_CHECKING:
+    from vfbLib.typing import (
+        GuideDict,
+        GuidePropertyList,
+    )
+
+
+logger = logging.getLogger(__name__)
+
+
+def get_master_guides(mm_guides: GuideDict, master_index: int) -> List[UfoGuide]:
+    # Concatenate guidlines for both directions and extract coords for
+    # master_index
+    guides = []
+    for d in ("h", "v"):
+        direction_mm_guides = mm_guides[d]
+        for master_guide in direction_mm_guides[master_index]:
+            guide = UfoGuide(angle=0, x=0, y=0)
+            if d == "h":
+                guide["y"] = master_guide["pos"]
+            else:
+                guide["x"] = master_guide["pos"]
+            angle = master_guide["angle"]
+            if d == "v":
+                angle = 90 - angle
+            if angle < 0:
+                angle += 360
+            if angle > 360:
+                angle -= 360
+            guide["angle"] = round(angle, 2)
+            guides.append(guide)
+    return guides
+
+
+def apply_guide_properties(
+    guides: List[UfoGuide], properties: GuidePropertyList
+) -> None:
+    # Update the guides with names and colors from properties
+    for prop in properties:
+        guide_index = prop["index"]
+        assert isinstance(guide_index, int)
+        guide = guides[guide_index - 1]  # index is 1-based
+        if "color" in prop:
+            color = prop["color"]
+            assert isinstance(color, str)
+            r = int(color[1:3], 16) / 0xFF
+            try:
+                g = int(color[3:5], 16) / 0xFF
+            except ValueError:
+                g = 0
+            try:
+                b = int(color[5:7], 16) / 0xFF
+            except ValueError:
+                b = 0
+            guide["color"] = f"{r:0.4f},{g:0.4f},{b:0.4f},1"
+        if "name" in prop:
+            guide["name"] = prop["name"]
