f52c6d1bceb32b9d373c659c3ab47ab76426253b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:11 +0000

    Add package SAMTEST/2.1.0/SAMTEST-2.1.0.tar.gz

---
+++ packages/SAMTEST/SAMTEST-2.1.0.tar.gz/SAMTEST-2.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SAMTEST',
+    version='2.1.0',
+    description='A simple library for demonstrating how to share a Python package',
+    # author='Your Name',
+    # author_email='your_email@example.com',
+    # url='https://github.com/your-username/my_library',
+    packages=['SAMTEST'],
+    install_requires=[]
+)
