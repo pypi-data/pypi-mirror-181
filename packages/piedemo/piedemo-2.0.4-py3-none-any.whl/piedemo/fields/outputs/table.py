2c3e6ece5121278a252db4d5e3f37f7c5fb2da20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:52 +0000

    Add package piedemo/2.0.4/piedemo-2.0.4-py3-none-any.whl

---
+++ packages/piedemo/piedemo-2.0.4-py3-none-any.whl/piedemo/fields/outputs/table.py
@@ -0,0 +1,31 @@
+import io
+import base64
+from .base import OutputField
+import pandas as pd
+from typing import Optional
+
+
+class OutputTableField(OutputField):
+    def __init__(self, name):
+        super(OutputTableField, self).__init__(name=name)
+        self.table: Optional[pd.DataFrame] = None
+
+    def set_output(self, data: pd.DataFrame):
+        self.table = data
+
+    def generate(self):
+        columns = self.table.columns.tolist()
+        return {
+            "card": "OutputTableCard",
+            "data": {
+                "name": self.name,
+                "headers": columns,
+                "rows": [[str(self.table[c][i]) for c in columns] for i in range(len(self.table))]
+            }
+        }
+
+    def clear(self):
+        self.table = None
+
+    def __repr__(self):
+        return "OutputTableField(%s)" % self.name
