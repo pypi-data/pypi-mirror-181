dca910ca3790bf3c4189114d0f429d0f43bf7514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:33:14 +0000

    Add package hbsst/1.0.1/hbsst-1.0.1.tar.gz

---
+++ packages/hbsst/hbsst-1.0.1.tar.gz/hbsst-1.0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hbsst',
+      version='1.0.1',
+      description='本工具将百度站长API提交的命令集成优化，改成利于使用的Python函数，方便程序调用。',
+      author='heStudio',
+      author_email='hestudio@hestudio.org',
+      url='https://gitee.com/heStudio/baidu-search-submission-tool/',
+      packages=[''],
+     )
