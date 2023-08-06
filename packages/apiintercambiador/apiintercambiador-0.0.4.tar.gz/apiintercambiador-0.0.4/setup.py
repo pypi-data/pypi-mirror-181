463c1aa10bf6b16d5065d93a4553581eca786b21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:14 +0000

    Add package apiintercambiador/0.0.4/apiintercambiador-0.0.4.tar.gz

---
+++ packages/apiintercambiador/apiintercambiador-0.0.4.tar.gz/apiintercambiador-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from ensurepip import version
+import pathlib
+from setuptools import find_packages, setup
+
+setup(
+    name='apiintercambiador',
+    version='0.0.4',
+    description='Librería para transferir información entre API Intercambiador y las fuentes de datos.' ,
+    long_description=(pathlib.Path(__file__).parent / "README.md").read_text(encoding='utf-8'),
+    long_description_content_type="text/markdown",
+    author='Marc Güell Jiménez',
+    author_email='mguell@tridenia.com',
+    url='',
+    license='MIT',
+    py_modules=["apiintercambiador"],
+    package_dir={'':'src'},
+    install_requires=[
+        'requests'
+    ]
+)
