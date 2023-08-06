13f99d9d6b1b27ab7495b7c666e9c1ca2af3fc89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:52 +0000

    Add package datasciencetbpy/0.0.2/datasciencetbpy-0.0.2.tar.gz

---
+++ packages/datasciencetbpy/datasciencetbpy-0.0.2.tar.gz/datasciencetbpy-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+    name= 'datasciencetbpy',
+    version= '0.0.2',
+    license= 'MIT',
+    description= 'Librería para limpieza y visualización de datos, y Machine learning',
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    long_description_content_type="text/markdown",
+    author= 'Clase Data Science The Bridge Septiembre 2022',
+    url= 'https://github.com/LLBF/Proyecto_libreria_DS',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        ],
+    python_requires= '>=3.7',
+    install_requires= ['numpy', 'pandas', 'matplotlib', 'plotly', 'scikit-learn', 'imbalanced-learn', 'seaborn', 'folium',
+                       'selenium', 'requests', 'pillow', 'opencv-python', 'unidecode'],
+    packages=find_packages()
+)
