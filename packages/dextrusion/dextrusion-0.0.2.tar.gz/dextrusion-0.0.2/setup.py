85f960a9b77f9b0fe54fe7b90c076a6bb241ee52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:15:08 +0000

    Add package dextrusion/0.0.2/dextrusion-0.0.2.tar.gz

---
+++ packages/dextrusion/dextrusion-0.0.2.tar.gz/dextrusion-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dextrusion',
+    version='0.0.2',
+    description='DeXtrusion: automatic detection of cell extrusion in epithelial tissu',
+      author='Gaëlle Letort and Alexis Villars',
+      url='https://gitlab.pasteur.fr/gletort/dextrusion',
+      package_dir={'':'src'},
+      packages=find_packages('src'),
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "opencv-python",
+        "tifffile>=2022.2.2",
+        "roifile",
+        "scikit-image",
+        "scikit-learn",
+        "scipy",
+        "tensorflow==2.8", 
+        "protobuf==3.19",
+        "ipython"
+    ],
+)
+
