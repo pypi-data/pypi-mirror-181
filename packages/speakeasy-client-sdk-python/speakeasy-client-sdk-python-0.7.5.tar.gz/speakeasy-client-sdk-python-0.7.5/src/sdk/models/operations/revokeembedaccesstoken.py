e5ecc4b6c2dadc0588d0031d904551a484b7382d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:50 +0000

    Add package speakeasy-client-sdk-python/0.7.5/speakeasy-client-sdk-python-0.7.5.tar.gz

---
+++ packages/speakeasy-client-sdk-python/speakeasy-client-sdk-python-0.7.5.tar.gz/speakeasy-client-sdk-python-0.7.5/src/sdk/models/operations/revokeembedaccesstoken.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+from typing import Optional
+from sdk.models import shared
+from sdk.models import shared
+
+
+@dataclass
+class RevokeEmbedAccessTokenPathParams:
+    token_id: str = field(metadata={'path_param': { 'field_name': 'tokenID', 'style': 'simple', 'explode': False }})
+    
+
+@dataclass
+class RevokeEmbedAccessTokenRequest:
+    path_params: RevokeEmbedAccessTokenPathParams = field()
+    
+
+@dataclass
+class RevokeEmbedAccessTokenResponse:
+    content_type: str = field()
+    status_code: int = field()
+    error: Optional[shared.Error] = field(default=None)
+    
