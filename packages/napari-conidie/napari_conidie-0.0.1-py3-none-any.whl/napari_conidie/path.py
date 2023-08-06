0d528df7c802e3fb343462c66cbf897c51ce1e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:34:42 +0000

    Add package napari-conidie/0.0.1/napari_conidie-0.0.1-py3-none-any.whl

---
+++ packages/napari-conidie/napari_conidie-0.0.1-py3-none-any.whl/napari_conidie/path.py
@@ -0,0 +1,11 @@
+import os.path
+
+homedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+base_dir = "."
+
+def get_base_dir():
+    return os.path.abspath(os.path.join(homedir, base_dir))
+
+def get_models_dir():
+    return os.path.join(get_base_dir(), "napari_conidie")
