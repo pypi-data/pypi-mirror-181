894555e94068cae68ef5aec451bcfed63d111536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:05 +0000

    Add package mpqcv/0.0.7/mpqcv-0.0.7.tar.gz

---
+++ packages/mpqcv/mpqcv-0.0.7.tar.gz/mpqcv-0.0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+setup(
+    name='mpqcv',
+    version='0.0.7',
+    description='CV for MPQ',
+    author='MPQ',#作者
+    author_email='miaopeiqi@163.com',
+    url='https://github.com/miaopeiqi',
+    #packages=find_packages(),
+    packages=['mpqcv'],  #这里是所有代码所在的文件夹名称
+    install_requires=[''],
+)
