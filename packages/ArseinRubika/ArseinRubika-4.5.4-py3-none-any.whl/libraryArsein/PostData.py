841d6cccc86dd9a1da8d6ded496abce66ba9329f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:36 +0000

    Add package ArseinRubika/4.5.4/ArseinRubika-4.5.4-py3-none-any.whl

---
+++ packages/ArseinRubika/ArseinRubika-4.5.4-py3-none-any.whl/libraryArsein/PostData.py
@@ -0,0 +1,34 @@
+import aiohttp
+import libraryArsein.encoder
+from libraryArsein.encoder import encoderjson
+import json
+from json import dumps, loads
+import time
+from time import sleep
+
+get_Errors = False
+
+async def http(s,auth,js):
+	enc = encoderjson(auth)
+	async with aiohttp.ClientSession() as session:
+		async with session.post(s, data=dumps({"api_version":"5","auth": auth,"data_enc":enc.encrypt(dumps(js))}) , headers = {'Content-Type': 'application/json'}) as response:
+			Post =  await response.text()
+			if type(Post) == dict:
+				if "status_det" in Post.keys():
+					if Post.get("status_det") == 'INVALID_AUTH':
+						get_Errors = True
+					else:
+						get_Errors = False
+						return Post
+
+async def httpfiles(s,dade,head):
+	async with aiohttp.ClientSession() as session:
+		async with session.post(s, data= dade  , headers = head) as response:
+			Post =  await response.text()
+			if type(Post) == dict:
+				if "status_det" in Post.keys():
+					if Post.get("status_det") == 'INVALID_AUTH':
+						get_Errors = True
+					else:
+						get_Errors = False
+						return Post
