7a502e25d4e5608952e5ba4248523887f21dd203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:20 +0000

    Add package Xplique/0.4.3/Xplique-0.4.3.tar.gz

---
+++ packages/Xplique/Xplique-0.4.3.tar.gz/Xplique-0.4.3/xplique/metrics/__init__.py
@@ -0,0 +1,8 @@
+"""
+Explanations Metrics module
+"""
+
+from .fidelity import MuFidelity, Deletion, Insertion, DeletionTS, InsertionTS, \
+    DeletionTab, InsertionTab
+from .stability import AverageStability
+from .representativity import MeGe
