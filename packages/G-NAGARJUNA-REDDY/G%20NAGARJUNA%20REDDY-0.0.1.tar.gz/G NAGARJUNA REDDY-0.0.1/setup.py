15ce68944e7166f99f75f3cc8e1aa2f5a51b548e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:26 +0000

    Add package G-NAGARJUNA-REDDY/0.0.1/G%20NAGARJUNA%20REDDY-0.0.1.tar.gz

---
+++ packages/G-NAGARJUNA-REDDY/G%20NAGARJUNA%20REDDY-0.0.1.tar.gz/G NAGARJUNA REDDY-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'NAGARJUNA'
+LONG_DESCRIPTION = 'A package to perform arithmetic operations'
+
+# Setting up
+setup(
+    name="G NAGARJUNA REDDY",
+    version=VERSION,
+    author="Nagarjuna",
+    author_email="nagarjunagolamari@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['add', 'sub', 'mathematics', 'python tutorial', 'Nagarjuna'],
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent"
+    ]
+)
