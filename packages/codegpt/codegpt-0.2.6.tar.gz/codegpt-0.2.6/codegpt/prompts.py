b6de667314f807c56cf3d11883a3f3af63b8780b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:57 +0000

    Add package codegpt/0.2.6/codegpt-0.2.6.tar.gz

---
+++ packages/codegpt/codegpt-0.2.6.tar.gz/codegpt-0.2.6/codegpt/prompts.py
@@ -0,0 +1,27 @@
+prompts = {
+    "comment": "Add or update comments according to the given language's standards. Add or update function, module, or class level comments if they're appropriate.",
+    "varnames": "Change variable names, but nothing else, to make the code more readable. For example, instead of using 'x' and 'y', use 'width' and 'height'.",
+    "ugh": "Do anything you can to make this code more readable. Add comments, change variable and function names, add whitespace, whatever. Add a readme to explain what the code does and where it could be improved.",
+    "bugs": """Find any bugs you can, note them in comments prefixed with BUG:
+
+Before:
+def divide(a, b):
+    return a / b
+
+After:
+def divide(a, b):
+    # BUG: This function should check for division by zero.
+    return a / b
+""",
+    "vulns": """Find any vulnerabilities you can, note them in comments prefixed with VULN:
+
+Before:
+def set_username(username):
+    this.username = username
+
+After:
+def set_username(username):
+    # VULN: This function should validate the input to prevent injection attacks.
+    this.username = username
+""",
+}
