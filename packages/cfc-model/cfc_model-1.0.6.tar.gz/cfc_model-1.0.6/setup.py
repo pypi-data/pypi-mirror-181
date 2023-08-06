0fb8b6f3bfe44d92c3a9eaf7f074be38f29b912a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:45:49 +0000

    Add package cfc-model/1.0.6/cfc_model-1.0.6.tar.gz

---
+++ packages/cfc-model/cfc_model-1.0.6.tar.gz/cfc_model-1.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import setuptools
+
+setup(
+    name='cfc_model',
+    version='1.0.6',    
+    description='An easy-to-use api for the closed-form continuous models in tensorflow and pytorch.',
+    url='https://github.com/nightvision04/CfC',
+    author='Daniel Scott',
+    author_email='danscottlearns@gmail.com',
+    install_requires=['pandas',
+                      'numpy',                     
+                      ],
+    
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+    ],
+    py_modules=['cfc_model'],
+)
