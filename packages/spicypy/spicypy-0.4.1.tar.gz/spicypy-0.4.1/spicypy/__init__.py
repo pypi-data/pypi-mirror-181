8d529db672d90429f2282d11c026a62bc1b3f623
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:01 +0000

    Add package spicypy/0.4.1/spicypy-0.4.1.tar.gz

---
+++ packages/spicypy/spicypy-0.4.1.tar.gz/spicypy-0.4.1/spicypy/__init__.py
@@ -0,0 +1,17 @@
+"""
+Init module.
+
+Artem Basalaev <artem[dot]basalaev[at]physik.uni-hamburg.de>,
+Christian Darsow-Fromm <cdarsowf[at]physnet.uni-hamburg.de>
+"""
+
+import logging as log
+import sys
+
+from importlib import metadata
+
+try:
+    __version__ = metadata.version(__name__)
+except metadata.PackageNotFoundError:
+    __version__ = "unknown"
+    log.warning("Version not known, importlib.metadata is not working correctly.")
