968ccf5e1db9ebc93530f926aad7d8dfafd651e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:15 +0000

    Add package datamol/0.8.8/datamol-0.8.8.tar.gz

---
+++ packages/datamol/datamol-0.8.8.tar.gz/datamol-0.8.8/datamol/types.py
@@ -0,0 +1,8 @@
+from rdkit import Chem
+from rdkit.Chem import rdChemReactions
+
+Mol = Chem.rdchem.Mol
+BondType = Chem.rdchem.BondType
+ChemicalReaction = rdChemReactions.ChemicalReaction
+Atom = Chem.rdchem.Atom
+Bond = Chem.rdchem.Bond
