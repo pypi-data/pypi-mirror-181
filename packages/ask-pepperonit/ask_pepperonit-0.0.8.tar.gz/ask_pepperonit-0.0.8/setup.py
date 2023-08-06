73794d779f33fd14d1e41b63777c38a8ed7cf24e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:46:28 +0000

    Add package ask-pepperonit/0.0.8/ask_pepperonit-0.0.8.tar.gz

---
+++ packages/ask-pepperonit/ask_pepperonit-0.0.8.tar.gz/ask_pepperonit-0.0.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ask_pepperonit',
+    version='0.0.8',
+    description='Say Hello!',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=["ask_pepperonit"],
+    package_dir={'': 'src'},
+    url="https://github.com/D7017E/Web_Interaction_Package",
+    author="Oscar Lundberg",
+    author_email="jensoscarlundberg95@gmail.com",
+    install_requires = [
+        "wikipedia ~= 1.4.0",
+        "search_google ~= 1.2.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
