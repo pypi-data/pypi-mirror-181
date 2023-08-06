e13377dc736a828ddd2f0df9e0b60284f0c703dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:20:06 +0000

    Add package titiler.core/0.10.1/titiler.core-0.10.1.tar.gz

---
+++ packages/titiler.core/titiler.core-0.10.1.tar.gz/titiler.core-0.10.1/titiler/core/__init__.py
@@ -0,0 +1,11 @@
+"""titiler.core"""
+
+__version__ = "0.10.1"
+
+from . import dependencies, errors, factory, routing  # noqa
+from .factory import (  # noqa
+    BaseTilerFactory,
+    MultiBandTilerFactory,
+    MultiBaseTilerFactory,
+    TilerFactory,
+)
