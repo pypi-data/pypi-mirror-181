451f579df8d09a051530088cd6dbab359005e0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:53 +0000

    Add package notecoin/0.24.8/notecoin-0.24.8-py3-none-any.whl

---
+++ packages/notecoin/notecoin-0.24.8-py3-none-any.whl/notecoin/base/tables/__init__.py
@@ -0,0 +1,4 @@
+from notecoin.base.tables.base import (AccountTask, BaseTask, MarketTask,
+                                       Ticker24HTask)
+from notecoin.base.tables.kline import KlineData
+from notecoin.base.tables.trade import TradeData
