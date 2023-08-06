47279958f4b3f97352c2137db0c0b895f153ff09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:51 +0000

    Add package jetnet/0.2.2/jetnet-0.2.2-py3-none-any.whl

---
+++ packages/jetnet/jetnet-0.2.2-py3-none-any.whl/jetnet/__init__.py
@@ -0,0 +1,13 @@
+# __init__
+
+# IMPORTANT: evaluation has to be imported first since energyflow must be imported before torch
+# See https://github.com/pkomiske/EnergyFlow/issues/24
+import jetnet.evaluation
+import jetnet.datasets
+import jetnet.losses
+import jetnet.utils
+
+import jetnet.datasets.utils
+import jetnet.datasets.normalisations
+
+__version__ = "0.2.2"
