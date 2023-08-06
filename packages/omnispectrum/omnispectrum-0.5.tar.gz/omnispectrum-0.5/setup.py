8c6be219497a5fdf61adec5976bc8db1fa084a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:24 +0000

    Add package omnispectrum/0.5/omnispectrum-0.5.tar.gz

---
+++ packages/omnispectrum/omnispectrum-0.5.tar.gz/omnispectrum-0.5/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='omnispectrum',
+    version = 0.5,
+    description=(
+        '全向地震动反应谱生成程序'
+    ),
+    long_description=open('README.md', encoding = 'utf-8').read(),
+    long_description_content_type="text/markdown",
+    author = 'Guochang Li',
+    author_email = 'liguochangli@gmail.com',
+    url = 'http://umr.ink',
+    license = 'Apache 3.0',
+    packages = ['omnispectrum'],
+    platforms = ["all"],
+    install_requires = [
+        'pandas',
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'tqdm',
+        'python-docx'
+    ],
+)
