0940c2640aea3e992d69b3a7bf783e86143a7f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:19:44 +0000

    Add package reapermc/1.5.0/reapermc-1.5.0-py3-none-any.whl

---
+++ packages/reapermc/reapermc-1.5.0-py3-none-any.whl/reapermc/__init__.py
@@ -0,0 +1,17 @@
+__version__ = "1.5.0"
+
+
+from beet import Context
+from beet.contrib.load import load
+from bolt import bolt
+
+
+def beet_default(ctx: Context):
+    ctx.require(
+        load(
+            data_pack={"data/reapermc/modules": "@reapermc/modules"},
+        ),
+        bolt(entrypoint=["*:main", "reapermc:core"]),
+        "bolt_expressions.plugin",
+        "reapermc.plugins.nbt_literals",
+    )
