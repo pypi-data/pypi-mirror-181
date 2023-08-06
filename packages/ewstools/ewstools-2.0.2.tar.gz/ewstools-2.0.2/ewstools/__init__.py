6bc22146bc880fdc4199c1b7d99fe552bf51ffa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:17:39 +0000

    Add package ewstools/2.0.2/ewstools-2.0.2.tar.gz

---
+++ packages/ewstools/ewstools-2.0.2.tar.gz/ewstools-2.0.2/ewstools/__init__.py
@@ -0,0 +1,10 @@
+# __init__.py
+name = "ewstools"
+
+# Import relevant modules
+from . import core
+from . import helpers
+from . import models
+
+# Import specific classes and functions
+from .core import TimeSeries
