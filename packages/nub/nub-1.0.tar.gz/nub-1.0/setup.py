a50cf2aacbe0e4f4e42cef0d1c28d109c9e68875
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:14 +0000

    Add package nub/1.0/nub-1.0.tar.gz

---
+++ packages/nub/nub-1.0.tar.gz/nub-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+name='nub',
+version='1.0',
+description='The Head First Python Search Tools',
+author='Tamerlan',
+author_email='a.tamerlan2005@mail.ru',
+url='https://github.com/tamehunter/vsearch',
+py_modules=['nub'],
+)
