389b739cde0df08636ab7d604e67bdb3f4cf2e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:56 +0000

    Add package pyeqeq/0.0.10/pyeqeq-0.0.10-cp38-cp38-macosx_11_0_arm64.whl

---
+++ packages/pyeqeq/pyeqeq-0.0.10-cp38-cp38-macosx_11_0_arm64.whl/pyeqeq/settings.py
@@ -0,0 +1,6 @@
+import pkg_resources
+
+IONIZATION_DATA_PATH = pkg_resources.resource_filename(
+    "pyeqeq", "data/ionizationdata.dat"
+)
+CHARGE_DATA_PATH = pkg_resources.resource_filename("pyeqeq", "data/chargecenters.dat")
