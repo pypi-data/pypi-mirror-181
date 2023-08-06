0fe0f72c197e787b8574c7e1b03fe1c1656e3e95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:35 +0000

    Add package omars-analysis/0.1.0/omars_analysis-0.1.0.tar.gz

---
+++ packages/omars-analysis/omars_analysis-0.1.0.tar.gz/omars_analysis-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['omars_analysis']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.23.5,<2.0.0', 'scipy>=1.9.3,<2.0.0']
+
+setup_kwargs = {
+    'name': 'omars-analysis',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Mohammed Saif Ismail Hameed',
+    'author_email': 'saifismailh@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
