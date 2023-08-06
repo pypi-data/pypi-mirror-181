66852f82c088fb393ec1b20970ac0bb37f777d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:20 +0000

    Add package rdfhash/0.3.2/rdfhash-0.3.2.tar.gz

---
+++ packages/rdfhash/rdfhash-0.3.2.tar.gz/rdfhash-0.3.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from os import path, getcwd
+
+setup(
+    name="rdfhash",
+    version="0.3.2",
+    author="Neil Graham",
+    author_email="grahamneiln@gmail.com",
+    url="https://github.com/NeilGraham/rdfhash",
+    license_files="LICENSE.txt",
+    description="De-duplicate RDF triples w/ a SPARQL query. Subjects taken from SELECT are replaced by the hash of their triples '{predicate} {object}.\n' pairs sorted.",
+    long_description=open(path.join(getcwd(), "README.md")).read()
+    # Replace local links to 'docs/' to Github page 'docs/'.
+    .replace(
+        "](docs/",
+        "](https://github.com/NeilGraham/rdfhash/blob/master/docs/",
+    ),
+    long_description_content_type="text/markdown",
+    packages=["rdfhash"],
+    package_dir={"rdfhash": "package"},
+    entry_points={"console_scripts": ["rdfhash = rdfhash.__main__:run"]},
+    python_requires=">=3.6",
+    install_requires=[
+        "pytest >= 7.1.2",
+        "rdflib >= 6.1.1",
+    ],
+)
