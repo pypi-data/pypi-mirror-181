e83a34377d87845e97c81a70c6c82b2f765d5afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:33:12 +0000

    Add package ucb-client/22.0.12/ucb-client-22.0.12.tar.gz

---
+++ packages/ucb-client/ucb-client-22.0.12.tar.gz/ucb-client-22.0.12/swagger_client/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.billing_api import BillingApi
+from swagger_client.api.builds_api import BuildsApi
+from swagger_client.api.buildtargets_api import BuildtargetsApi
+from swagger_client.api.config_api import ConfigApi
+from swagger_client.api.credentials_api import CredentialsApi
+from swagger_client.api.orgs_api import OrgsApi
+from swagger_client.api.projects_api import ProjectsApi
+from swagger_client.api.public_api import PublicApi
+from swagger_client.api.shares_api import SharesApi
+from swagger_client.api.status_api import StatusApi
+from swagger_client.api.userdevices_api import UserdevicesApi
+from swagger_client.api.users_api import UsersApi
+from swagger_client.api.webhooks_api import WebhooksApi
