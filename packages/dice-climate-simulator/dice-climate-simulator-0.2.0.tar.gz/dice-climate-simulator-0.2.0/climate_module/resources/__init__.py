4a71f03b312443aed3912de033a1232dec3c0092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:48 +0000

    Add package dice-climate-simulator/0.2.0/dice-climate-simulator-0.2.0.tar.gz

---
+++ packages/dice-climate-simulator/dice-climate-simulator-0.2.0.tar.gz/dice-climate-simulator-0.2.0/climate_module/resources/__init__.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2022 International Institute for Applied Systems Analysis.
+# All rights reserved. The use and distribution terms for this software
+# are covered by the MIT License (http://opensource.org/licenses/MIT)
+# which can be found in the file LICENSE at the root of this distribution.
+# By using this software in any fashion, you are agreeing to be bound by
+# the terms of this license.
+# You must not remove this notice, or any other, from this software.
+"""Data resources.
+
+hansel FFI.csv: net CO2 emissions in BaU from Hansel et al. (2020).
+
+SSP markers ratio non_CO2 to CO2.csv: ratio of non-CO2 to CO2 forcings
+in marker SSP scenarios (SSP Database).
+
+References
+----------
+.. [1] Hansel, M., Drupp, M., Johansson, D., Nesje, F., Azar, C., Freeman, M.,
+    Groom, B., & Sterner, T. (2020). Climate Economics Support for the UN
+    Climate Targets. Nature Climate Change, 10: 781-789.
+    https://doi.org/10.1038/s41558-020-0833-x
+.. [2] [SSP Database] https://tntcat.iiasa.ac.at/SspDb/
+
+"""
+
+__author__ = "Anna Shchiptsova"
+__copyright__ = "Copyright (c) 2022 IIASA"
+__license__ = "MIT"
