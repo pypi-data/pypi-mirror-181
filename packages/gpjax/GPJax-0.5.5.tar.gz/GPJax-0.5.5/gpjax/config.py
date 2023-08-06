6fd04c1049cba7c1a322e18b324f3699b6f9dabf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:20 +0000

    Add package gpjax/0.5.5/GPJax-0.5.5.tar.gz

---
+++ packages/gpjax/GPJax-0.5.5.tar.gz/GPJax-0.5.5/gpjax/config.py
@@ -0,0 +1,88 @@
+# Copyright 2022 The GPJax Contributors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+import distrax as dx
+import jax.numpy as jnp
+import jax.random as jr
+import tensorflow_probability.substrates.jax.bijectors as tfb
+from ml_collections import ConfigDict
+
+__config = None
+
+FillTriangular = dx.Chain(
+    [
+        tfb.FillTriangular(),
+    ]
+)  # TODO: Dan to chain methods.
+Identity = dx.Lambda(forward=lambda x: x, inverse=lambda x: x)
+Softplus = dx.Lambda(
+    forward=lambda x: jnp.log(1 + jnp.exp(x)),
+    inverse=lambda x: jnp.log(jnp.exp(x) - 1.0),
+)
+
+
+def get_defaults() -> ConfigDict:
+    """Construct and globally register the config file used within GPJax.
+
+    Returns:
+        ConfigDict: A `ConfigDict` describing parameter transforms and default values.
+    """
+    config = ConfigDict(type_safe=False)
+    config.key = jr.PRNGKey(123)
+
+    # Covariance matrix stabilising jitter
+    config.jitter = 1e-6
+
+    # Default bijections
+    config.transformations = transformations = ConfigDict()
+    transformations.positive_transform = Softplus
+    transformations.identity_transform = Identity
+    transformations.triangular_transform = FillTriangular
+
+    # Default parameter transforms
+    transformations.alpha = "positive_transform"
+    transformations.lengthscale = "positive_transform"
+    transformations.variance = "positive_transform"
+    transformations.smoothness = "positive_transform"
+    transformations.shift = "positive_transform"
+    transformations.obs_noise = "positive_transform"
+    transformations.latent = "identity_transform"
+    transformations.basis_fns = "identity_transform"
+    transformations.offset = "identity_transform"
+    transformations.inducing_inputs = "identity_transform"
+    transformations.variational_mean = "identity_transform"
+    transformations.variational_root_covariance = "triangular_transform"
+    transformations.natural_vector = "identity_transform"
+    transformations.natural_matrix = "identity_transform"
+    transformations.expectation_vector = "identity_transform"
+    transformations.expectation_matrix = "identity_transform"
+
+    global __config
+    if not __config:
+        __config = config
+    return __config
+
+
+def add_parameter(param_name: str, bijection: dx.Bijector) -> None:
+    """Add a parameter and its corresponding transform to GPJax's config file.
+
+    Args:
+        param_name (str): The name of the parameter that is to be added.
+        bijection (dx.Bijector): The bijection that should be used to unconstrain the parameter's value.
+    """
+    lookup_name = f"{param_name}_transform"
+    get_defaults()
+    __config.transformations[lookup_name] = bijection
+    __config.transformations[param_name] = lookup_name
---
+++ packages/gpjax/GPJax-0.5.5.tar.gz/GPJax-0.5.5/setup.py
@@ -0,0 +1,76 @@
+import io
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+# Get version number (function from GPyTorch)
+def read(*names, **kwargs):
+    with io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8"),
+    ) as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+version = find_version("gpjax", "__init__.py")
+readme = open("README.md").read()
+
+
+REQUIRES = [
+    "jax<0.4.0",
+    "jaxlib<0.4.0",
+    "optax",
+    "jaxutils",
+    "jaxkern",
+    "chex",
+    "distrax>=0.1.2",
+    "tqdm>=4.0.0",
+    "ml-collections==0.1.0",
+    "jaxtyping>=0.0.2",
+    "jaxlinop>=0.0.3",
+    "deprecation",
+]
+
+EXTRAS = {
+    "dev": [
+        "black",
+        "isort",
+        "pylint",
+        "flake8",
+        "pytest",
+        "networkx",
+        "pytest-cov",
+    ],
+    "cuda": ["jax[cuda]"],
+}
+
+
+setup(
+    name="GPJax",
+    version=version,
+    author="Thomas Pinder",
+    author_email="t.pinder2@lancaster.ac.uk",
+    packages=find_packages(".", exclude=["tests"]),
+    license="LICENSE",
+    description="Didactic Gaussian processes in Jax.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Documentation": "https://gpjax.readthedocs.io/en/latest/",
+        "Source": "https://github.com/thomaspinder/GPJax",
+    },
+    install_requires=REQUIRES,
+    tests_require=EXTRAS["dev"],
+    extras_require=EXTRAS,
+    keywords=["gaussian-processes jax machine-learning bayesian"],
+)
