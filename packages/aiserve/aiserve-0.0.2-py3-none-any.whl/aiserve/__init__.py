078ca3f9463b54f295446670707373803c3a8297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:30 +0000

    Add package aiserve/0.0.2/aiserve-0.0.2-py3-none-any.whl

---
+++ packages/aiserve/aiserve-0.0.2-py3-none-any.whl/aiserve/__init__.py
@@ -0,0 +1,16 @@
+# __init__.py
+# Copyright (C) 2022 (hhkim94@naver.com)
+#
+
+__version__ = '0.0.1'
+
+# from ._internal import load_config
+#
+# load_config()
+
+from .main import *
+
+__all__ = [
+    "__version__",
+    "hello"
+]
---
+++ packages/aiserve/aiserve-0.0.2-py3-none-any.whl/aiserve/_internal/__init__.py
@@ -0,0 +1,7 @@
+# def load_config():
+
+    # from .containers import ApplicationContainer
+    #
+    # container = ApplicationContainer()
+    # container.check_dependencies()
+    # container.init_resources()
