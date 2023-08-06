f2932c59e0baf47597d86e5eafcec78e1860f797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:44:21 +0000

    Add package smol-k8s-lab/0.10.3/smol_k8s_lab-0.10.3-py3-none-any.whl

---
+++ packages/smol-k8s-lab/smol_k8s_lab-0.10.3-py3-none-any.whl/smol_k8s_lab/k8s_distros/kind.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3.11
+"""
+       Name: kind
+DESCRIPTION: create or delete a kind cluster
+     AUTHOR: github.com/jessebot/smol-k8s-lab
+    LICENSE: GNU AFFERO GENERAL PUBLIC LICENSE Version 3
+"""
+import logging as log
+from os import path
+from shutil import which
+from ..console_logging import sub_header
+from ..env_config import PWD
+from ..subproc import subproc
+
+
+def install_kind_cluster():
+    """
+    Run installation process for kind and create cluster
+    returns True
+    """
+    # make sure kind is installed first, and if not, install it
+    if not which("kind"):
+        msg = ("ʕ•́ᴥ•̀ʔ [b]kind[/b] is [warn]not installed[/warn]. "
+               "[i]We'll install it for you.[/i] ʕᵔᴥᵔʔ")
+        sub_header(msg)
+        log.debug("Installing kind with brew...")
+        subproc(['brew install kind'], spinner=True)
+
+    # then use our pre-configured kind file to install a small cluster
+    full_path = path.join(PWD, '../distros/kind/kind_cluster_config.yaml')
+    log.debug("Creating a kind cluster...")
+    subproc([f"kind create cluster --config={full_path}"])
+
+    return True
+
+
+def delete_kind_cluster():
+    """
+    delete kind cluster
+    returns True
+    """
+    subproc(['kind delete cluster'])
+    return True
