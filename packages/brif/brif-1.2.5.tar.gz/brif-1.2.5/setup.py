0584487eaa65375676559fce6373daba5c5ede6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:31 +0000

    Add package brif/1.2.5/brif-1.2.5.tar.gz

---
+++ packages/brif/brif-1.2.5.tar.gz/brif-1.2.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import platform
+
+if platform.system() == 'Windows':
+    extra_flags = ['/openmp']
+    extra_link = []
+elif platform.system() != 'Darwin':
+    extra_flags = ['-fopenmp']
+    extra_link = ['-lgomp']
+else:
+    extra_flags = []
+    extra_link = []
+
+setup(name='brif',
+      version="1.2.5",
+      ext_modules=[
+          Extension('brifc',
+                    ['pybrif.c','brif.c'],
+                    extra_compile_args = extra_flags,
+                    extra_link_args = extra_link
+                    )
+          ]
+      )
