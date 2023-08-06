b1dd3d9d3b25eb12841a9aba817542e97eb947c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:35 +0000

    Add package osmium/3.6.0rc1/osmium-3.6.0rc1-cp36-cp36m-macosx_10_9_x86_64.whl

---
+++ packages/osmium/osmium-3.6.0rc1-cp36-cp36m-macosx_10_9_x86_64.whl/osmium/version.py
@@ -0,0 +1,15 @@
+"""
+Version information.
+"""
+
+# the major version
+pyosmium_major = '3.6'
+# current release (Pip version)
+pyosmium_release = '3.6.0rc1'
+
+# libosmium version shipped with the Pip release
+libosmium_version = '2.18.0'
+# protozero version shipped with the Pip release
+protozero_version = '1.7.1'
+# pybind11 version shipped with the Pip release
+pybind11_version = '2.10.1'
