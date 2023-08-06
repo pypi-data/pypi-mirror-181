5aac48f56e14100a6f176727ea870e16110f9dbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:21:00 +0000

    Add package localcosmos-server/0.12.2/localcosmos_server-0.12.2.tar.gz

---
+++ packages/localcosmos-server/localcosmos_server-0.12.2.tar.gz/localcosmos_server-0.12.2/localcosmos_server/utils.py
@@ -0,0 +1,33 @@
+import re
+
+
+def get_domain_name(request):
+    setup_domain_name = request.get_host().split(request.get_port())[0].split(':')[0]
+    return setup_domain_name
+
+from datetime import datetime, timezone, timedelta
+def datetime_from_cron(cron):
+    # fromtimestamp takes seconds
+
+    delta_minutes = 0 - cron['cron'].get('timezone_offset', 0)
+    
+    tz = timezone(
+        timedelta(minutes=delta_minutes)
+    )
+
+    utc = cron['cron']['timestamp']/1000
+
+    local = utc + (delta_minutes * 60)
+
+    timestamp = datetime.fromtimestamp(local, tz=tz)
+
+    return timestamp
+
+def api_filter_endpoints_hook(endpoints):
+    # for (path, path_regex, method, callback) in endpoints:
+    #      pass
+    # drop html endpoints
+    endpoints = [endpoint for endpoint in endpoints if not endpoint[0].endswith("{format}")]
+    exposed_endpoints = [endpoint for endpoint in endpoints if re.match('/api/(user|app|password|online-content|token)/', endpoint[0])]
+
+    return exposed_endpoints
