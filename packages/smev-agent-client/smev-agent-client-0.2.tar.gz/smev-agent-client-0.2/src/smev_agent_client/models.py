cffb4f3ca21b180c78d685f111d6ab0717479b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:18 +0000

    Add package smev-agent-client/0.2/smev-agent-client-0.2.tar.gz

---
+++ packages/smev-agent-client/smev-agent-client-0.2.tar.gz/smev-agent-client-0.2/src/smev_agent_client/models.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+from django.db.models.base import Model
+from django.db.models.fields import DateTimeField
+from django.db.models.fields import TextField
+from django.db.models.indexes import Index
+
+from .logging.base import Entry as EntryModel
+
+
+class Entry(Model):
+
+    class Meta:
+        verbose_name = verbose_name_plural = 'Журнал запросов'
+        indexes = [
+            Index(fields=['date_time']),
+        ]
+
+    request = TextField(
+        verbose_name=EntryModel.request.title
+    )
+    response = TextField(
+        verbose_name=EntryModel.response.title,
+        null=True, blank=True
+    )
+    error = TextField(
+        verbose_name=EntryModel.error.title,
+        null=True, blank=True
+    )
+    date_time = DateTimeField(
+        verbose_name=EntryModel.date_time.title
+    )
