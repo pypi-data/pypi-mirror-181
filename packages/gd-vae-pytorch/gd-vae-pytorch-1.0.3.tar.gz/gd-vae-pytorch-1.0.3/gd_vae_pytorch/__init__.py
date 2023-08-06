af3a472a71a43d88d125739526bf24c6c0550c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:56 +0000

    Add package gd-vae-pytorch/1.0.3/gd-vae-pytorch-1.0.3.tar.gz

---
+++ packages/gd-vae-pytorch/gd-vae-pytorch-1.0.3.tar.gz/gd-vae-pytorch-1.0.3/gd_vae_pytorch/__init__.py
@@ -0,0 +1,3 @@
+# global initialization 
+name="gd_vae"; # package name
+__version__="1.0.2"; # package version
---
+++ packages/gd-vae-pytorch/gd-vae-pytorch-1.0.3.tar.gz/gd-vae-pytorch-1.0.3/setup.py
@@ -0,0 +1,44 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gd-vae-pytorch",
+    version="1.0.3",
+    author="Ryan Lopez and Paul J. Atzberger",
+    author_email="atzberg@gmail.com",
+    description="Geometric Dynamic Variational Autoencoders (GD-VAEs).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://atzberger.org/",
+    packages=['gd_vae_pytorch', 'gd_vae_pytorch.tests'],
+    #install_requires=['numpy','matplotlib','scikit-learn','torch'],
+    #install_requires=['numpy >= 1.19.0','matplotlib >= 3.0.0','scikit-learn >= 1.0.0','torch >= 1.9.0'],
+    #packages=setuptools.find_packages(include=['gd-vae-pytorch']),
+    #setup_requires=["numpy>=1.16","scipy>=1.3","matplotlib>=3.0"],
+    #install_requires=["numpy>=1.16","scipy>=1.3","matplotlib>=3.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        #"License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
+# print a message after install 
+##ss="";
+#ss+="PyTorch implementation of GD-VAEs.\n";
+#ss+="\n"
+#ss+="NOTE: The package is still being packaged for pip. \n";
+#ss+="Please sign-up below for Google-Form for mailing list \n";
+#ss+="announcing soon this code release: \n";
+#ss+="https://forms.gle/mJSRRrqMo8CwFKRC7 \n";
+#ss+="\n";
+#ss+="Additional information also can be found at: \n";
+#ss+="http://atzberger.org \n";
+#
+#print(ss);
+
+
