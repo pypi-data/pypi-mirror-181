bb861c9b0b05b36d899f062f4ed895fa30f0034b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:45:59 +0000

    Add package rdotherout/1.0.6/rdotherout-1.0.6-py3-none-any.whl

---
+++ packages/rdotherout/rdotherout-1.0.6-py3-none-any.whl/rdotherout/mainModel.py
@@ -0,0 +1,33 @@
+from k3cloud_webapi_sdk.main import K3CloudApiSdk
+import rdotherout.operation as db
+import rdotherout.utility as ut
+import rdotherout.metadata as mt
+from pyrda.dbms.rds import RdClient
+
+def otherOut():
+    app2 = RdClient(token='4D181CAB-4CE3-47A3-8F2B-8AB11BB6A227')
+    app3 = RdClient(token='9B6F803F-9D37-41A2-BDA0-70A7179AF0F3')
+
+    data = db.getCode(app3)
+
+    if data:
+
+        res = ut.classification_process(app3, data)
+
+        # 新账套
+
+        option1 = {
+            "acct_id": '62777efb5510ce',
+            "user_name": 'DMS',
+            "app_id": '235685_4e6vScvJUlAf4eyGRd3P078v7h0ZQCPH',
+            # "app_sec": 'd019b038bc3c4b02b962e1756f49e179',
+            "app_sec": 'b105890b343b40ba908ed51453940935',
+            "server_url": 'http://cellprobio.gnway.cc/k3cloud',
+        }
+
+        api_sdk = K3CloudApiSdk()
+
+        msg=mt.erp_save(app2, api_sdk, option1, res, app3)
+
+        return msg
+
---
+++ packages/rdotherout/rdotherout-1.0.6-py3-none-any.whl/rdotherout/operation.py
@@ -0,0 +1,84 @@
+def getClassfyData(app3,code):
+    '''
+    获得分类数据
+    :param app2:
+    :param code:
+    :return:
+    '''
+
+    sql=f"select * from RDS_ECS_ODS_sal_delivery where FDELIVERYNO='{code}'"
+
+    res=app3.select(sql)
+
+    return res
+
+def getCode(app3):
+    '''
+    查询出表中的编码
+    :param app2:
+    :return:
+    '''
+
+    sql="select FDELIVERYNO from RDS_ECS_ODS_sal_delivery where FIsdo=0 and FIsFree=1"
+
+    res=app3.select(sql)
+
+    return res
+
+def code_conversion(app2,tableName,param,param2):
+    '''
+    通过ECS物料编码来查询系统内的编码
+    :param app2: 数据库操作对象
+    :param tableName: 表名
+    :param param:  参数1
+    :param param2: 参数2
+    :return:
+    '''
+
+    sql=f"select FNumber from {tableName} where {param}='{param2}'"
+
+    res=app2.select(sql)
+
+    if res==[]:
+
+        return ""
+
+    else:
+
+        return res[0]['FNumber']
+
+def code_conversion_org(app2,tableName,param,param2,param3,param4):
+    '''
+    通过ECS物料编码来查询系统内的编码
+    :param app2: 数据库操作对象
+    :param tableName: 表名
+    :param param:  参数1
+    :param param2: 参数2
+    :return:
+    '''
+
+    sql=f"select {param4} from {tableName} where {param}='{param2}' and FOrgNumber='{param3}'"
+
+    res=app2.select(sql)
+
+    if res==[]:
+
+        return ""
+
+    else:
+
+        return res[0][param4]
+
+
+def changeStatus(app3,fnumber,status):
+    '''
+    将没有写入的数据状态改为2
+    :param app2: 执行sql语句对象
+    :param fnumber: 订单编码
+    :param status: 数据状态
+    :return:
+    '''
+
+    sql=f"update a set a.Fisdo={status} from RDS_ECS_ODS_sal_delivery a where FDELIVERYNO='{fnumber}'"
+
+    app3.update(sql)
