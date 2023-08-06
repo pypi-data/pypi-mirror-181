51ee523630da860315401fbdf35ca4fbad89d7a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:43:22 +0000

    Add package epalgorithmwrapper/2022.12.13.1327/epalgorithmwrapper-2022.12.13.1327-py3-none-any.whl

---
+++ packages/epalgorithmwrapper/epalgorithmwrapper-2022.12.13.1327-py3-none-any.whl/epalgorithmwrapper/api/report.py
@@ -0,0 +1,29 @@
+from ..utils.filesystem_utils import get_file_contents
+from ..utils.configuration import Configuration
+import asset
+from logging import getLogger
+
+class Report:
+    class Status:
+        FAILURE = "FAILURE"
+        SUCCESS = "SUCCESS"
+
+    REPORT_LOCATION = Configuration.get_configuration()[Configuration.Key.OUTPUT_REPORT_LOCATION]
+    TEMPLATE_LOCATION = Configuration.get_configuration()[Configuration.Key.REPORT_TEMPLATE_LOCATION]
+
+    @staticmethod
+    def __get_report_template() -> str:
+        return asset.load(Report.TEMPLATE_LOCATION).read().decode('utf-8')
+
+    @staticmethod
+    def __save_report(report, path):
+        getLogger(__name__).info(f"Saving report: '{report}'")
+        with open(path, 'w') as f:
+            f.write(report)
+
+    @staticmethod
+    def write_report(status: Status):
+        logger = getLogger(__name__)
+        template = Report.__get_report_template()
+        report = template.format(status=status)
+        Report.__save_report(report, Report.REPORT_LOCATION)
