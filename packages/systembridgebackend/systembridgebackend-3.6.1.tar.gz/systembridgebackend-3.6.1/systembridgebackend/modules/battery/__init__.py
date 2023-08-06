ac0bf672fab661baaaf9686bf9ddbe15d15d895e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:29 +0000

    Add package systembridgebackend/3.6.1/systembridgebackend-3.6.1.tar.gz

---
+++ packages/systembridgebackend/systembridgebackend-3.6.1.tar.gz/systembridgebackend-3.6.1/systembridgebackend/modules/battery/__init__.py
@@ -0,0 +1,25 @@
+"""System Bridge: Battery"""
+from __future__ import annotations
+
+from typing import Optional
+
+from plyer import battery
+import psutil
+from systembridgeshared.base import Base
+
+
+class Battery(Base):
+    """Battery"""
+
+    def sensors(self) -> Optional[psutil._common.sfan]:
+        """Get battery sensors"""
+        if not hasattr(psutil, "sensors_battery"):
+            return None
+        return psutil.sensors_battery()  # type: ignore
+
+    def status(self) -> Optional[dict]:
+        """Get battery status"""
+        try:
+            return battery.status
+        except ValueError:
+            return None
