36c21d35bf7ca52e6af0c0fc9d67d5b7cb55d4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:22:02 +0000

    Add package makejinja/0.4.1/makejinja-0.4.1.tar.gz

---
+++ packages/makejinja/makejinja-0.4.1.tar.gz/makejinja-0.4.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['makejinja']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['jinja2>=3.1.2,<4.0.0',
+ 'python-simpleconf[all]>=0.5.7,<0.6.0',
+ 'rich>=12.6.0,<13.0.0',
+ 'typer>=0.7.0,<0.8.0']
+
+entry_points = \
+{'console_scripts': ['makejinja = makejinja.cli:app']}
+
+setup_kwargs = {
+    'name': 'makejinja',
+    'version': '0.4.1',
+    'description': 'Automatically generate files based on Jinja templates. Use it to easily generate complex Home Assistant dashboards!',
+    'long_description': '# MakeJinja\n',
+    'author': 'Mirko Lenz',
+    'author_email': 'mirko@mirkolenz.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/mirkolenz/makejinja',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
