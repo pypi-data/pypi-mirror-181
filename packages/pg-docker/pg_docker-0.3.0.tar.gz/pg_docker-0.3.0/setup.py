073d5b7dd28b347f02bf310fbbb427bf09e6e5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:17 +0000

    Add package pg-docker/0.3.0/pg_docker-0.3.0.tar.gz

---
+++ packages/pg-docker/pg_docker-0.3.0.tar.gz/pg_docker-0.3.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pg_docker']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['psycopg2>=2.9.5,<3.0.0']
+
+entry_points = \
+{'pytest11': ['pg_docker = pg_docker._plugin']}
+
+setup_kwargs = {
+    'name': 'pg-docker',
+    'version': '0.3.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
