e2528c270310a8ead49696b006a261089634ac8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:49 +0000

    Add package hcai-nova-server-nightly/0.1.0.dev202212131504/hcai-nova-server-nightly-0.1.0.dev202212131504.tar.gz

---
+++ packages/hcai-nova-server-nightly/hcai-nova-server-nightly-0.1.0.dev202212131504.tar.gz/hcai-nova-server-nightly-0.1.0.dev202212131504/nova_server/model_interface.py
@@ -0,0 +1,40 @@
+import logging
+
+from hcai_datasets.hcai_nova_dynamic.hcai_nova_dynamic_iterable import HcaiNovaDynamicIterable
+
+
+# TODO Check parameter types and correct them if necessary
+class ModelInterface:
+    """Includes all the necessary files to run this script"""
+    DEPENDENCIES = []
+    OPTIONS = {}
+
+    def set_options(self, options: dict):
+        """Overwrite/Append Options. No Return value"""
+        for key, value in options.items():
+            self.OPTIONS[key] = value
+
+    def preprocess(self, ds_iter:  HcaiNovaDynamicIterable, logger: logging) -> list:
+        """Possible pre-processing of the data. Returns a list with the pre-processed data."""
+        pass
+
+    def train(self, data_list: list, logger: logging) -> object:
+        """Trains a model with the given data. Returns this model."""
+        pass
+
+    def predict(self, model, data_list: list, logger: logging) -> list:
+        """Predicts the given data with the given model. Returns a list with the predicted values."""
+        pass
+
+    def postprocess(self, ds_iter: HcaiNovaDynamicIterable, logger: logging) -> list:
+        """Possible pre-processing of the data. Returns a list with the pre-processed data."""
+        pass
+
+    def save(self, model, path: str, logger: logging) -> str:
+        """Stores the weights of the given model at the given path. Returns the path of the weights."""
+        pass
+
+    def load(self, path: str, logger: logging) -> object:
+        """Loads a model with the given path. Returns this model."""
+        pass
+
