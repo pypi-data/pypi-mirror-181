1e1eb399ba2d0b1b24dcadb04cb899e259992aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:09 +0000

    Add package vitessce/1.0.11/vitessce-1.0.11-py2.py3-none-any.whl

---
+++ packages/vitessce/vitessce-1.0.11-py2.py3-none-any.whl/vitessce/nbextension/8.index.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{1335:function(e,n,r){"use strict";r.r(n),r.d(n,"default",(function(){return a}));var t=r(1330),c=r(706);class a extends c.a{decodeBlock(e){return Object(t.a)(new Uint8Array(e)).buffer}}}}]);
+//# sourceMappingURL=8.index.js.map
