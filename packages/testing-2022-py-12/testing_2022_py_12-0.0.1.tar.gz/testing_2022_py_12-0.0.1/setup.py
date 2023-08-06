cab81f5259e752af2e73abde096f8c742b1abcf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:07 +0000

    Add package testing-2022-py-12/0.0.1/testing_2022_py_12-0.0.1.tar.gz

---
+++ packages/testing-2022-py-12/testing_2022_py_12-0.0.1.tar.gz/testing_2022_py_12-0.0.1/setup.py
@@ -0,0 +1,27 @@
+﻿# from __future__ import annotations
+from setuptools import Extension
+from setuptools import setup
+__version__ = '0.0.1'
+
+
+
+
+
+printf = Extension(
+    'printf',
+    sources=["src/printf.cpp"],
+    # include_dirs=[pybind11.get_include()],
+)
+
+if __name__ == "__main__":
+    setup(
+        name = 'testing_2022_py_12',
+        version=__version__,
+        author="testing_2022",
+        author_email="",
+        url="",
+        description="testing 2022_",
+        long_description="",
+        ext_modules=[printf],
+        python_requires=">=3.0"
+    )
