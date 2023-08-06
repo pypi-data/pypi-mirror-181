9b8058db64e00ec4399662f4d4a3e58dfae29afa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:58 +0000

    Add package spotify-internal/0.1.6/spotify_internal-0.1.6.tar.gz

---
+++ packages/spotify-internal/spotify_internal-0.1.6.tar.gz/spotify_internal-0.1.6/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['spotify_internal']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['librespot>=0.0.7,<0.0.8',
+ 'melodine>=0.2.0,<0.3.0',
+ 'requests>=2.28.1,<3.0.0',
+ 'spotipy>=2.21.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'spotify-internal',
+    'version': '0.1.6',
+    'description': 'A Python wrapper around the Spotify Internal API which can let users login with their login creds (and not OAuth)',
+    'long_description': 'A Python wrapper around the Spotify Internal API which can let users login with their login creds (and not OAuth)\n',
+    'author': 'addyett',
+    'author_email': 'g.aditya2048@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
