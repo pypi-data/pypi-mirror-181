2043dc447914841588df37d67e335263bc420594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:57 +0000

    Add package phishing-hunter/0.0.1/phishing-hunter-0.0.1.tar.gz

---
+++ packages/phishing-hunter/phishing-hunter-0.0.1.tar.gz/phishing-hunter-0.0.1/phishing_hunter/paths/scanner_remove/__init__.py
@@ -0,0 +1,7 @@
+# do not import all endpoints into this module because that uses a lot of memory and stack frames
+# if you need the ability to import all endpoints from this module, import them with
+# from phishing_hunter.paths.scanner_remove import Api
+
+from phishing_hunter.paths import PathValues
+
+path = PathValues.SCANNER_REMOVE
