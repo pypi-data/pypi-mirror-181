13f01fdecd5090c42904cdbe673da597bdaba807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:14 +0000

    Add package newsworthycharts/1.38.1/newsworthycharts-1.38.1-py3-none-any.whl

---
+++ packages/newsworthycharts/newsworthycharts-1.38.1-py3-none-any.whl/newsworthycharts/lib/mimetypes.py
@@ -0,0 +1,10 @@
+""" Mime type data
+"""
+
+MIME_TYPES = {
+    'webp': "image/webp",
+    'png': "image/png",
+    'svg': "image/svg+xml",
+    'eps': "application/postscript",
+    'dw': "text/plain",
+}
