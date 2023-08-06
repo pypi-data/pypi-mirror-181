9839649713e83c8f52f6263784635ea913fb8b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:45 +0000

    Add package SigProfilerAssignment/0.0.22/SigProfilerAssignment-0.0.22-py3-none-any.whl

---
+++ packages/SigProfilerAssignment/SigProfilerAssignment-0.0.22-py3-none-any.whl/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.0.22'
+version = '0.0.22'
+Update = 'Resolve issue with ID decomposition plots, JOBMETADATA upgrade, add DBS tests.'
+
+    
+    
