341751cc71e2f709245e0b59404506ffb106dc43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:22 +0000

    Add package a-pandas-ex-obj-into-cell/0.12/a_pandas_ex_obj_into_cell-0.12.tar.gz

---
+++ packages/a-pandas-ex-obj-into-cell/a_pandas_ex_obj_into_cell-0.12.tar.gz/a_pandas_ex_obj_into_cell-0.12/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md'), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.12'
+DESCRIPTION = "Puts objects into cells / columns of a Pandas Dataframe"
+
+# Setting up
+setup(
+    name="a_pandas_ex_obj_into_cell",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/a_pandas_ex_obj_into_cell',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=['flatten_everything', 'numpy', 'pandas'],
+    keywords=['pandas', 'DataFrame', 'dict', 'list'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Programming Language :: Python :: 3.9', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Text Editors :: Text Processing', 'Topic :: Text Processing :: General', 'Topic :: Text Processing :: Indexing', 'Topic :: Text Processing :: Filters', 'Topic :: Utilities'],
+    install_requires=['flatten_everything', 'numpy', 'pandas'],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
