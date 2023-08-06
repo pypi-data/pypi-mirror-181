847584671864693939c1cd94161ece6b32a3c771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:49 +0000

    Add package piccolo/0.102.0/piccolo-0.102.0-py3-none-any.whl

---
+++ packages/piccolo/piccolo-0.102.0-py3-none-any.whl/piccolo/query/__init__.py
@@ -0,0 +1,43 @@
+from .base import Query
+from .methods import (
+    Alter,
+    Avg,
+    Count,
+    Create,
+    CreateIndex,
+    Delete,
+    DropIndex,
+    Exists,
+    Insert,
+    Max,
+    Min,
+    Objects,
+    Raw,
+    Select,
+    Sum,
+    TableExists,
+    Update,
+)
+from .mixins import OrderByRaw
+
+__all__ = [
+    "Alter",
+    "Avg",
+    "Count",
+    "Create",
+    "CreateIndex",
+    "Delete",
+    "DropIndex",
+    "Exists",
+    "Insert",
+    "Max",
+    "Min",
+    "Objects",
+    "OrderByRaw",
+    "Query",
+    "Raw",
+    "Select",
+    "Sum",
+    "TableExists",
+    "Update",
+]
