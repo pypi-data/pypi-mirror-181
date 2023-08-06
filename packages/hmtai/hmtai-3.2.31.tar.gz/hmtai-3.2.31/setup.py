cbb0715e71208705b1c6d03cd913fdf1778ad006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:40 +0000

    Add package hmtai/3.2.31/hmtai-3.2.31.tar.gz

---
+++ packages/hmtai/hmtai-3.2.31.tar.gz/hmtai-3.2.31/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+# read the contents of your README file
+
+setup(
+    name='hmtai',
+    version='3.2.31',
+    license='WTFPL',
+    author="NickSaltFoxu",
+    author_email='thnyawec@gmail.com',
+    packages=['hmtai'],
+    url='https://github.com/BlueBerrySans365/HMtai-PY',
+    keywords= ["anime", "hentai", "nsfw", "sfw", "images", "gifs", "wallpaper", "discord", "ahegao", "ass", "neko", "yuri", "panties", "thighs", "ero", "kawaii", "cute", "waifu", "hmtai", "zettaiRyouiki", "18+", "REST", "API", "Mikun"],
+    long_description=open('README.md', encoding='UTF8').read(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+          'requests',
+      ],
+)
