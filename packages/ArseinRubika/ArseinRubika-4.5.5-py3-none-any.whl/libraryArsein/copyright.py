a2e89bd6831b3834d71d4f18d1390757304507d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:20:58 +0000

    Add package ArseinRubika/4.5.5/ArseinRubika-4.5.5-py3-none-any.whl

---
+++ packages/ArseinRubika/ArseinRubika-4.5.5-py3-none-any.whl/libraryArsein/copyright.py
@@ -0,0 +1,12 @@
+# Copyright (C) 2022 ArianBOT
+
+version = "4.5.5"
+copyright = " Arsein Copyright (C) 2022 arianabasi Team ArianBOT \n\n"
+Arian = "ArianBOT"
+
+
+class copyright_Arsein:
+    arsein = print(
+        f"Arsein library version {version}\n{copyright}\n☞ library Arsein: https://github.com/Arseinlibrary/Arsein__library.git\n\nدر حال فعال شدن کمی صبور باشید...\n")
+    print(" ")
+    print(". . . . . . . . . . . ")
