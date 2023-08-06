565182d8a495f8fa8fc3f003bb0c9b5ff2e40dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:00 +0000

    Add package vkmini/0.7.0a2/vkmini-0.7.0a2.tar.gz

---
+++ packages/vkmini/vkmini-0.7.0a2.tar.gz/vkmini-0.7.0a2/vkmini/group_longpoll.py
@@ -0,0 +1,131 @@
+from typing import AsyncGenerator, Union, Optional
+
+from aiohttp.client import ClientSession
+
+from vkmini import VkApi, GroupVkApi
+from vkmini.utils import AbstractLogger
+from vkmini.request import longpoll_get, _check_longpoll_session
+
+
+class GroupLP:
+    """
+    Реализует получение событий через Bots Long Poll API
+    (https://dev.vk.com/api/bots-long-poll/getting-started)
+    """
+    wait: int
+
+    server: str
+    key: str
+    ts: Union[int, str]
+
+    _vk: GroupVkApi
+    _session: Optional[ClientSession]
+    _group_id: Optional[int]
+
+    __session_owner: bool = False
+
+    def __init__(
+            self,
+            vk: GroupVkApi,
+            group_id: Optional[int] = None,
+            wait: int = 25,
+            logger: Optional[AbstractLogger] = None,
+            session: Optional[ClientSession] = None
+    ):
+        """
+        `wait` -- максимальное время ожидания события, в секундах
+        (https://dev.vk.com/api/bots-long-poll/getting-started#Подключение)
+
+        `logger` -- любой объект, имеющий атрибуты info, debug и warning,
+        по умолчанию None, то есть логирование не ведется
+
+        `session` -- экземпляр aiohttp.ClientSession, который будет
+        использоваться при выполнении запросов к LongPoll серверу
+
+        Приоритет объектов сессий:
+        1. Сессия, переданная в параметре session
+        2. Сессия переданного экземпляра VkApi
+        3. Общая сессия (см vkmini.set_session)
+        """
+        if not isinstance(vk, VkApi):
+            raise TypeError('Аргумент vk должен быть экземпляром VkApi, '
+                            'передан %s' % repr(vk))
+
+        if isinstance(vk, GroupVkApi):
+            self._vk = vk
+        else:
+            # защита от дурака (с пользовательским API получение ID не сработает)
+            self._vk = GroupVkApi(
+                vk.access_token,
+                vk.excepts,
+                vk.version,
+                vk.retries,
+                vk.logger,
+                vk._session,
+            )
+
+        if wait > 90:
+            raise ValueError('Параметр wait не может превышать 90 секунд')
+        self.wait = wait
+        self.logger = logger or vk.logger
+        self._session = session or vk._session  # type: ignore
+        self._group_id = group_id
+
+    async def check(self):
+        """
+        Выполняет однократный запрос к API и возвращает
+        список событий из поля 'updates'
+        (https://dev.vk.com/api/bots-long-poll/getting-started#Формат%20данных)
+        """
+        _check_longpoll_session(self._session)
+        if self._group_id is None:
+            self._group_id = await self._vk.get_vk_id()
+        await self._set_longpoll_data(True)
+        self.check = self._check
+        return await self.check()
+
+    async def _check(self):
+        data = await longpoll_get(
+            f"{self.server}?act=a_check&key={self.key}" +
+            f"&ts={self.ts}&wait={self.wait}",
+            self._session
+        )
+
+        if 'failed' in data:
+            if data['failed'] == 1:
+                self.ts = data['ts']
+            elif data['failed'] == 2:
+                await self._set_longpoll_data(False)
+            else:
+                await self._set_longpoll_data(True)
+            return []
+        else:
+            self.ts = data['ts']
+            return data['updates']
+
+    async def _set_longpoll_data(self, new_ts: bool) -> None:
+        data = await self._vk.groups.getLongPollServer(
+            group_id=self._group_id  # type: ignore
+        )
+        self.server = data['server']
+        self.key = data['key']
+        if new_ts:
+            self.ts = data['ts']
+
+    async def __aenter__(self) -> 'GroupLP':
+        if self._session is None:
+            self._session = ClientSession()
+            self.__session_owner = True
+        return self
+
+    async def __aexit__(self, *_) -> None:
+        if self.__session_owner:
+            await self._session.close()  # type: ignore
+
+    async def listen(self) -> AsyncGenerator[dict, None]:
+        """
+        Асинхронный генератор результатов метода check()
+        """
+        while True:
+            for update in await self.check():
+                yield update
---
+++ packages/vkmini/vkmini-0.7.0a2.tar.gz/vkmini-0.7.0a2/vkmini/request.py
@@ -0,0 +1,104 @@
+import json
+
+from typing import Optional, Tuple, Union
+from aiohttp import ClientSession
+from warnings import warn
+
+from vkmini.exceptions import NetworkError
+
+
+default_session: Union[ClientSession, None] = None
+json_decoder = json.loads
+
+
+def set_decoder(loads) -> None:
+    """
+    Устанавливает функцию, которая будет использоваться при
+    десериализации ответа на запросы
+    """
+    global json_decoder
+    json_decoder = loads
+
+
+def set_session(session: ClientSession) -> None:
+    """
+    Устанавливает сессию, которая будет использоваться по умолчанию при
+    любых запросах к API, в т.ч. LongPoll (исключение - обращение к API c
+    помощью обектов, при создании которых была явно передана сессия)
+
+    Разумеется, стоит позаботиться о закрытии этой сессии перед завершением
+    работы приложения
+
+    Если при запросе не указана сессия (не установлена стандартная или не
+    передана в конструктор при создании класса), на каждый новый запрос
+    будет создаваться новая сессия (это плохое решение, лучше создать и
+    указать сессию)
+    """
+    global default_session
+    default_session = session
+
+
+async def _get_session(session: Optional[ClientSession]) -> Tuple[ClientSession, bool]:
+    if not (default_session or session):
+        return ClientSession(), True
+    return (session or default_session), False  # type: ignore
+
+
+def _check_longpoll_session(session: Optional[ClientSession]):
+    if not (session or default_session):
+        warn(ResourceWarning(
+            'При создании экземпляра LP не была указана сессия, при этом '
+            'сессия default_session также не задана. Это приведёт к '
+            'созданию новой сессии на каждый запрос.'
+        ))
+
+
+async def post(
+        url: str,
+        data: dict,
+        client_session: Optional[ClientSession] = None
+) -> dict:
+    session, should_close = await _get_session(client_session)
+    async with session.post(url, data=data) as resp:
+        if resp.status == 200:
+            data = await resp.json(loads=json_decoder)
+        else:
+            data = {}
+    if should_close:
+        await session.close()
+    if data:
+        return data
+    raise NetworkError(resp.status)
+
+
+async def longpoll_get(
+        url: str,
+        client_session: Optional[ClientSession] = None
+) -> dict:
+    session, should_close = await _get_session(client_session)
+    async with session.get(url) as resp:
+        if resp.status == 200:
+            data = await resp.json(loads=json_decoder)
+        else:
+            data = {}
+    if should_close:
+        await session.close()
+    if data:
+        return data
+    raise NetworkError(resp.status)
+
+
+async def get(
+        url: str,
+        client_session: Optional[ClientSession] = None
+) -> bytes:
+    session, should_close = await _get_session(client_session)
+    try:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                return await resp.content.read()
+            else:
+                raise NetworkError(resp.status)
+    finally:
+        if should_close:
+            await session.close()
