d987f6697e13938c71a57639a5b30c840f9c92a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:16:02 +0000

    Add package cmdz/2/cmdz-2.tar.gz

---
+++ packages/cmdz/cmdz-2.tar.gz/cmdz-2/cmdz/__init__.py
@@ -0,0 +1,17 @@
+# This file is placed in the Public Domain.
+# pylint: disable=W0622
+
+
+"write your own commands"
+
+
+import cmdz.handler as handler
+import cmdz.object as object
+import cmdz.run as run
+import cmdz.thread as thread
+
+
+from cmdz.handler import *
+from cmdz.object import *
+from cmdz.run import *
+from cmdz.thread import *
