2a70a36bafbb4f9f0402f63b5d84371764ee2f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:32:05 +0000

    Add package rmlst-api/0.1.2/rmlst_api-0.1.2.tar.gz

---
+++ packages/rmlst-api/rmlst_api-0.1.2.tar.gz/rmlst_api-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='rMLST-API',
+    version='0.1.2',
+    py_modules=['rmlst_api.*'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'rmlst-api = rmlst_api.cli:run_all',
+        ],
+    },
+)
